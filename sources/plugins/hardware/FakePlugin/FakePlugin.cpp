e8b2e46c3e27080020435d3033c3cb4ef47edf02
Jean-Michel DECORET <jano42@users.noreply.github.com>
2013-11-18 13:58:10 +0000

    * add CFakePlugin (forgotten)

---
+++ sources/plugins/hardware/FakePlugin/FakePlugin.cpp
@@ -0,0 +1,35 @@
+#include "FakePlugin.h"
+
+#include <boost\thread.hpp>
+
+namespace plugins
+{
+	  CFakePlugin::CFakePlugin()
+      {
+      }
+
+      CFakePlugin::~CFakePlugin()
+      {
+      }
+
+      // IHardwarePlugin implementation
+      
+      void CFakePlugin::doWork()
+      {
+         while(1)
+         {
+            // Give a chance to exit plugin thread
+            boost::this_thread::sleep(boost::posix_time::seconds(1000)); 
+
+            std::cout << "CFakePlugin is running..." << std::endl;
+         };
+      }
+
+	  // [END] IHardwarePlugin implementation
+
+
+      // TODO : WhatTheFuck ? ? ? C'est quoi ces adresses ?
+      void CFakePlugin::init(const std::string & pluginAddress, const std::string & serverAddress)
+      {
+      }
+}
---
+++ sources/plugins/hardware/FakePlugin/FakePlugin.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "../IHardwarePlugin.h"
+#include <string>
+
+namespace plugins
+{
+   class CFakePlugin : public IHardwarePlugin
+   {  
+   public:
+      CFakePlugin();
+
+      virtual ~CFakePlugin();
+
+      // IHardwarePlugin implementation
+      virtual void doWork();
+	  // [END] IHardwarePlugin implementation
+
+      // TODO : WhatTheFuck ? ? ? C'est quoi ces adresses ?
+      void init(const std::string & pluginAddress, const std::string & serverAddress);
+   };
+}
+
+IMPLEMENT_HARDWARE_PLUGIN(CFakePlugin)
