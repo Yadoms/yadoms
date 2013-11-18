830c5c28790586cadaad51faa92fd30c0d6e9a35
Sébastien Gallou <sgallou@users.noreply.github.com>
2013-11-18 21:36:08 +0000

    COptions update, do not try to build it

--- sources/server/Options.cpp
+++ sources/server/Options.cpp
@@ -6,13 +6,13 @@
 #include <Options.h>
 #include <boost/program_options.hpp>
 
+static const std::string ConfigFile("yadoms.cfg");
+
+namespace po = boost::program_options;
 
 COptions::m_instance = NULL;
+COptions* Options = COptions::getInstance();
 
-COptions::COptions()
-{
-   loadConfiguration();
-}
 
 COptions* COptions::instance()
 {
@@ -21,16 +21,50 @@ COptions* COptions::instance()
    return m_instance;
 }
 
-void COptions::loadConfiguration()
+std::istream& COptions::operator>>(std::istream& in, boost::log::trivial::severity_level& logLevel) const//TODO voir si réellement nécessaire (si c'est pas déjà implémenté dans le type severity_level (m'étonnerai, ça doit être un enum tout bête m'enfin faut vérifier. Et là je ferme toutes les parenthèses. Nico, si tu me lis, sache que tu perds ton temps à relire tout mon code, il compilera et marchera parfaitement ! ... un jour. Tu vois, je te l'avais dit, tu perds ton temps... Cette fois je les ferme les parenthèses. ) )
 {
-   boost::program_options::options_description desc("Allowed options");
-   desc.add_options()
-      ("help", "produce help message")
-      ("logLevel", boost::program_options::value<int>(), "set compression level")
-      ;
+    std::string token;
+    in >> token;
+    if      (token == "trace"  ) logLevel = boost::log::trivial::trace;
+    else if (token == "debug"  ) logLevel = boost::log::trivial::debug;
+    else if (token == "info"   ) logLevel = boost::log::trivial::info;
+    else if (token == "warning") logLevel = boost::log::trivial::warning;
+    else if (token == "error"  ) logLevel = boost::log::trivial::error;
+    else if (token == "fatal"  ) logLevel = boost::log::trivial::fatal;
+    else throw po::validation_error("Invalid log Level");
+    return in;
 }
 
-boost::log::trivial::severity_level COptions::getLogLevel()
+bool COptions::load(int argc, char** argv)
 {
-   instance()->
+   try
+   {
+      // Configuration description
+      po::options_description desc("Allowed options");
+      desc.add_options()
+         ("help", "produce help message")
+         ("logLevel", po::value<boost::log::trivial::severity_level>(&m_logLevel)->default_value(boost::log::trivial::warning),
+            "set log level, accepted values are : trace, debug, info, warning, error, fatal")
+         ;
+
+      // Load configuration from command line and complete with config file (second data read don't overwrite first)
+      po::variables_map vm;
+      po::store(po::parse_command_line(argc, argv, options), vm);
+      po::store(parse_config_file(ConfigFile, desc), vm);
+      po::notify(vm);
+   }
+   catch(po::validation_error &exception)
+   {
+      BOOST_LOG_TRIVIAL(fatal) << "Configuration error : " << exception.what();
+      return false;
+   }
+
+   return true;
+}
+
+bool COptions::save()
+{
+   // TODO : C'est moche, il n'y a rien pour enregistrer le fichier de conf
+   // En même temps, ça serait merdique vu qu'aucune différence n'est faite entre les données venant de la ligne de commande
+   // et celle venant du fichier de config.
 }
