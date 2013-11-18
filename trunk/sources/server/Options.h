19f016667cd913c5935fc16a5dab1a5b7c3e7057
sgallou <sgallou@8f66d739-0145-4685-a3fe-f7fe687021a9>
2013-11-18 21:36:08 +0000

    COptions update, do not try to build it

--- trunk/sources/server/Options.h
+++ trunk/sources/server/Options.h
@@ -7,6 +7,20 @@
 
 #include <boost/log/trivial.hpp>
 
+//TODO : à mettre dans le main :
+main()
+{
+   if (COptions::getInstance()->load(argc, argv))
+      // Sortie en erreur
+
+   // ... Boucle principale ...
+   if (COptions::getInstance()->getLogLevel() == boost::log::trivial::severity_level::fatal)
+      // Fais qqc
+
+   if (!COption::getInstance()->save())
+      // Juste une trace d'erreur
+}
+
 
 class COptions
 {
@@ -15,15 +29,46 @@ private:
    /// \brief	    Constructor
    /// \note       Declared as private to implement singleton
    //--------------------------------------------------------------
-   COptions();
+   COptions() {}
+
+   //--------------------------------------------------------------
+   /// \brief	    Destructor
+   //--------------------------------------------------------------
+   virtual ~COptions() {}
 
 public:
+   //--------------------------------------------------------------
+   /// \brief	    Get (create if necessary) the singleton instance
+   /// \return     The COptions singleton
+   //--------------------------------------------------------------
+   static COptions* getInstance();
+
+   //--------------------------------------------------------------
+   /// \brief	            Load configuration (from config file and commande line)
+   /// \param[in]  argc    Main argc parameter (from command line)
+   /// \param[in]  argv    Main argv parameter (from command line)
+   /// \return             Return false if load fails
+   //--------------------------------------------------------------
+   bool load(int argc, char** argv);
+
+   //--------------------------------------------------------------
+   /// \brief	            Save configuration in config file
+   /// \return             Return false if save fails
+   //--------------------------------------------------------------
+   bool save();
+
    //--------------------------------------------------------------
    /// \brief	    Get the log level
    /// \return     Configured log level
    //--------------------------------------------------------------
-   static boost::log::trivial::severity_level getLogLevel();
+   boost::log::trivial::severity_level getLogLevel() { return m_Loglevel; }
 
+
+protected:
+   //--------------------------------------------------------------
+   /// \brief	    Deserialization operator
+   //--------------------------------------------------------------
+   std::istream& operator>>(std::istream& in, Length::Unit& unit) const;
 
 private:
    //--------------------------------------------------------------
@@ -33,11 +78,15 @@ private:
    //--------------------------------------------------------------
    static COptions* m_instance;
 
+
    //--------------------------------------------------------------
-   /// \brief	    Get (create if necessary) the singleton instance
-   /// \return     The COptions singleton
+   /// \brief	    Log level
    //--------------------------------------------------------------
-   static COptions* instance();
+   boost::log::trivial::severity_level m_Loglevel;
 };
+
+// TODO : les règles de codage me permettent-elles de faire ça ? Faut déjà que ça marche
+// L'idée étant d'appeler Options->load() au lieu de COptions::getInstance()->load()
+extern COptions* Options;
 
 
