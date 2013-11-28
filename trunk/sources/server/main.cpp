8be88d6caeae280059ef1a28c801ad36c78a3eb1
lgm42 <lgm42@8f66d739-0145-4685-a3fe-f7fe687021a9>
2013-11-28 22:31:01 +0000

    * integration du log multithread multi fichiers en cours. Non terminé

--- trunk/sources/server/main.cpp
+++ trunk/sources/server/main.cpp
@@ -7,32 +7,52 @@
 
 #include <iostream>
 
-#include <boost/log/trivial.hpp>
-#include <boost/log/utility/setup/file.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+#include <boost/log/common.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/attributes.hpp>
+#include <boost/log/sources/logger.hpp>
+#include <boost/log/sinks/sync_frontend.hpp>
+#include <boost/log/sinks/text_multifile_backend.hpp>
+#include <boost/shared_ptr.hpp>
 #include <boost/function.hpp>
 
 #include "Supervisor.h"
 #include "StartupOptionsLoader.h"
 #include "ApplicationStopHandler.h"
 
+// Global logger declaration
+BOOST_LOG_INLINE_GLOBAL_LOGGER_DEFAULT(my_logger, boost::log::sources::logger_mt)
 
-//
-// \brief Configure the logger
-//
-void configureLogger(const IStartupOptions& startupOptions)
+   //
+   // \brief Configure the logger
+   //
+   void configureLogger(const IStartupOptions& startupOptions)
 {
-   boost::log::add_file_log
+   // Create a text file sink
+   typedef boost::log::sinks::synchronous_sink< boost::log::sinks::text_multifile_backend > file_sink;
+   boost::shared_ptr< file_sink > sink(new file_sink);
+
+   // Set up how the file names will be generated
+   sink->locked_backend()->set_file_name_composer(boost::log::sinks::file::as_file_name_composer(
+      boost::log::expressions::stream << "logs/" << boost::log::expressions::attr< boost::thread::id >("ThreadID") << ".log"));
+
+   // Set the log record formatter
+   sink->set_formatter
       (
-      boost::log::keywords::file_name = "yadoms_%N.log",                                        
-      boost::log::keywords::rotation_size = 10 * 1024 * 1024,                                   
-      boost::log::keywords::time_based_rotation = boost::log::sinks::file::rotation_at_time_point(0, 0, 0), 
-      boost::log::keywords::format = "[%TimeStamp%]: %Message%"                                 
+      boost::log::expressions::format("%1%: [%2%] - %3%")
+      % boost::log::expressions::attr< unsigned int >("RecordID")
+      % boost::log::expressions::attr< boost::posix_time::ptime >("TimeStamp")
+      % boost::log::expressions::smessage
       );
 
-   boost::log::core::get()->set_filter
-      (
-      boost::log::trivial::severity >= startupOptions.getLogLevel()
-      );
+   // Add it to the core
+   boost::log::core::get()->add_sink(sink);
+
+   // Add some attributes too
+   boost::log::core::get()->add_global_attribute("TimeStamp", boost::log::attributes::local_clock());
+   boost::log::core::get()->add_global_attribute("RecordID", boost::log::attributes::counter< unsigned int >());
 }
 
 void stopHandler()
@@ -51,9 +71,11 @@ int main (int argc, char** argv)
    {
       CStartupOptionsLoader startupOptions(argc, argv);
 
-      //configureLogger(startupOptions);
+      configureLogger(startupOptions);
+      BOOST_LOG_SCOPED_THREAD_TAG("ThreadID", boost::this_thread::get_id());
+      BOOST_LOG(my_logger::get()) << "Yadoms is starting";
 
-      BOOST_LOG_TRIVIAL(info) << "Yadoms is starting";
+      //BOOST_LOG_TRIVIAL(info) << "Yadoms is starting";
 
       CSupervisor supervisor(startupOptions);
       supervisor.start();
@@ -64,8 +86,8 @@ int main (int argc, char** argv)
       }
 
       supervisor.stop();
-
-      BOOST_LOG_TRIVIAL(info) << "Yadoms is stopped.";
+      BOOST_LOG(my_logger::get()) << "Yadoms is stopped ";
+      //BOOST_LOG_TRIVIAL(info) << "Yadoms is stopped.";
    }
    catch(CStartupOptionsLoaderError& e)
    {
