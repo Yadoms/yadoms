2bc69a149a463993ce35c951cd26b624ceb45791
Nicolas HILAIRE <nicolas.hilaire@gmail.com>
2013-11-30 21:19:22 +0000

    * Log integration in progress

--- sources/server/main.cpp
+++ sources/server/main.cpp
@@ -5,34 +5,31 @@
 \brief Can use "brief" tag to explicitly generate comments for file documentation.
 
 */
+
+#include <iostream>
+
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+#include <boost/log/common.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/attributes.hpp>
+#include <boost/log/sources/logger.hpp>
+#include <boost/log/sinks/sync_frontend.hpp>
+#include <boost/log/sinks/text_multifile_backend.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/function.hpp>
+
 #include "Supervisor.h"
 #include "StartupOptionsLoader.h"
 #include "ApplicationStopHandler.h"
-
-
-//
-// \brief Configure the logger
-//
-void configureLogger(const IStartupOptions& startupOptions)
-{
-   boost::log::add_file_log
-      (
-      boost::log::keywords::file_name = "yadoms_%N.log",                                        
-      boost::log::keywords::rotation_size = 10 * 1024 * 1024,                                   
-      boost::log::keywords::time_based_rotation = boost::log::sinks::file::rotation_at_time_point(0, 0, 0), 
-      boost::log::keywords::format = "[%TimeStamp%]: %Message%"                                 
-      );
-
-   boost::log::core::get()->set_filter
-      (
-      boost::log::trivial::severity >= startupOptions.getLogLevel()
-      );
-}
+#include "Log.h"
 
 void stopHandler()
 {
    //TODO
 }
+
+
 
 /*
 \brief Main application entry point
@@ -45,9 +42,11 @@ int main (int argc, char** argv)
    {
       CStartupOptionsLoader startupOptions(argc, argv);
 
-      //configureLogger(startupOptions);
+      CLog::configure();
+      BOOST_LOG_SCOPED_THREAD_TAG("ThreadName", "Main");
 
-      BOOST_LOG_TRIVIAL(info) << "Yadoms is starting";
+      BOOST_LOG_SEV(my_logger::get(), boost::log::trivial::info) << "Yadoms is starting";
+      BOOST_LOG_SEV(my_logger::get(), boost::log::trivial::error) << "test";
 
       CSupervisor supervisor(startupOptions);
       supervisor.start();
@@ -58,14 +57,14 @@ int main (int argc, char** argv)
       }
 
       supervisor.stop();
-
-      BOOST_LOG_TRIVIAL(info) << "Yadoms is stopped.";
+      BOOST_LOG_SEV(my_logger::get(), boost::log::trivial::info) << "Yadoms is stopped ";
+      //BOOST_LOG_TRIVIAL(info) << "Yadoms is stopped.";
    }
    catch(CStartupOptionsLoaderError& e)
    {
       if (e.isError())
       {
-         BOOST_LOG_TRIVIAL(fatal) << e.what();
+         BOOST_LOG_SEV(my_logger::get(), boost::log::trivial::fatal) << e.what();
       }
       else
       {
@@ -76,7 +75,7 @@ int main (int argc, char** argv)
    catch(...)
    {
       //dual logging in case logger fails/throws
-      BOOST_LOG_TRIVIAL(fatal) << "An unhandled exception occurs. Yadoms is now stopped";
+      BOOST_LOG_SEV(my_logger::get(), boost::log::trivial::fatal) << "An unhandled exception occurs. Yadoms is now stopped";
       std::cout << "An unhandled exception occurs. Yadoms is now stopped" << std::endl;
    }
 
