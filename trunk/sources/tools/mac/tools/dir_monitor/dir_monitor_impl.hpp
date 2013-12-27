bb896ee3a9f5d707be5d5199f149506b9c473f9a
jano42 <jano42@8f66d739-0145-4685-a3fe-f7fe687021a9>
2013-12-27 06:04:08 +0000

    * mac (disable code)

--- trunk/sources/tools/mac/tools/dir_monitor/dir_monitor_impl.hpp
+++ trunk/sources/tools/mac/tools/dir_monitor/dir_monitor_impl.hpp
@@ -19,8 +19,6 @@
 #include <boost/system/system_error.hpp> 
 #include <string> 
 #include <deque> 
-#include <sys/inotify.h> 
-#include <errno.h> 
 
 namespace boost { 
 namespace asio { 
@@ -30,16 +28,18 @@ class dir_monitor_impl :
 { 
 public: 
     dir_monitor_impl() 
-        : fd_(init_fd()), 
+        : /*fd_(init_fd()), 
         stream_descriptor_(inotify_io_service_, fd_), 
         inotify_work_(new boost::asio::io_service::work(inotify_io_service_)), 
         inotify_work_thread_(boost::bind(&boost::asio::io_service::run, &inotify_io_service_)), 
-        run_(true) 
+        run_(true) */
     { 
     } 
 
     void add_directory(const std::string &dirname) 
     { 
+         //\TODO : make it works for mac
+         /*
         int wd = inotify_add_watch(fd_, dirname.c_str(), IN_CREATE | IN_DELETE | IN_MOVED_FROM | IN_MOVED_TO); 
         if (wd == -1) 
         { 
@@ -49,10 +49,14 @@ public:
 
         boost::unique_lock<boost::mutex> lock(watch_descriptors_mutex_); 
         watch_descriptors_.insert(watch_descriptors_t::value_type(wd, dirname)); 
+        */
     } 
 
     void remove_directory(const std::string &dirname) 
     { 
+         //\TODO : make it works for mac
+         /*
+    
         boost::unique_lock<boost::mutex> lock(watch_descriptors_mutex_); 
         watch_descriptors_t::right_map::iterator it = watch_descriptors_.right.find(dirname); 
         if (it != watch_descriptors_.right.end()) 
@@ -60,10 +64,14 @@ public:
             inotify_rm_watch(fd_, it->second); 
             watch_descriptors_.right.erase(it); 
         } 
+        */
     } 
 
     void destroy() 
     { 
+         //\TODO : make it works for mac
+         /*
+    
         inotify_work_.reset(); 
         inotify_io_service_.stop(); 
         inotify_work_thread_.join(); 
@@ -71,10 +79,14 @@ public:
         boost::unique_lock<boost::mutex> lock(events_mutex_); 
         run_ = false; 
         events_cond_.notify_all(); 
+        */
     } 
 
     dir_monitor_event popfront_event(boost::system::error_code &ec) 
     { 
+         //\TODO : make it works for mac
+         /*
+
         boost::unique_lock<boost::mutex> lock(events_mutex_); 
         while (run_ && events_.empty()) 
             events_cond_.wait(lock); 
@@ -88,21 +100,28 @@ public:
         else 
             ec = boost::asio::error::operation_aborted; 
         return ev; 
+        */
+        return ec = boost::asio::error::operation_aborted; 
     } 
 
     void pushback_event(dir_monitor_event ev) 
     { 
+         //\TODO : make it works for mac
+         /*    
         boost::unique_lock<boost::mutex> lock(events_mutex_); 
         if (run_) 
         { 
             events_.push_back(ev); 
             events_cond_.notify_all(); 
         } 
+        */
     } 
 
 private: 
     int init_fd() 
     { 
+         //\TODO : make it works for mac
+         /*    
         int fd = inotify_init(); 
         if (fd == -1) 
         { 
@@ -110,19 +129,24 @@ private:
             boost::throw_exception(e); 
         } 
         return fd; 
+        */
     } 
 
 public: 
     void begin_read() 
     { 
-        stream_descriptor_.async_read_some(boost::asio::buffer(read_buffer_), 
+         //\TODO : make it works for mac
+         /*        stream_descriptor_.async_read_some(boost::asio::buffer(read_buffer_), 
             boost::bind(&dir_monitor_impl::end_read, shared_from_this(), 
             boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred)); 
+            */
     } 
 
 private: 
     void end_read(const boost::system::error_code &ec, std::size_t bytes_transferred) 
     { 
+         //\TODO : make it works for mac
+         /*    
         if (!ec) 
         { 
             pending_read_buffer_ += std::string(read_buffer_.data(), bytes_transferred); 
@@ -148,15 +172,22 @@ private:
             boost::system::system_error e(ec); 
             boost::throw_exception(e); 
         } 
+        */
     } 
 
     std::string get_dirname(int wd) 
     { 
+         //\TODO : make it works for mac
+         /*    
         boost::unique_lock<boost::mutex> lock(watch_descriptors_mutex_); 
         watch_descriptors_t::left_map::iterator it = watch_descriptors_.left.find(wd); 
         return it != watch_descriptors_.left.end() ? it->second : ""; 
+        */
+        return "";
     } 
 
+             //\TODO : make it works for mac
+         /*
     int fd_; 
     boost::asio::io_service inotify_io_service_; 
     boost::asio::posix::stream_descriptor stream_descriptor_; 
@@ -171,6 +202,7 @@ private:
     boost::condition_variable events_cond_; 
     bool run_; 
     std::deque<dir_monitor_event> events_; 
+    */
 }; 
 
 } 
