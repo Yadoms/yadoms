5e2ca27281117c9f0930fb2b14be53554cfee9ac
jmb42 <jmb42@8f66d739-0145-4685-a3fe-f7fe687021a9>
2013-12-27 16:01:35 +0000

    Add more tests for StartOptionsLoader Class

--- trunk/tests/server/MainTestStartOptionsLoader.cpp
+++ trunk/tests/server/MainTestStartOptionsLoader.cpp
@@ -13,6 +13,10 @@
 #include "../../sources/server/ApplicationStopHandler.h"
 //#include "../../sources/tools/tools/Log.h"
 
+bool validate(CStartupOptionsLoaderError str)
+{
+ return str.isError();
+}
 
 /*
 \brief Main application entry point
@@ -31,81 +35,202 @@ BOOST_AUTO_TEST_CASE(Initialisation_Test)
   BOOST_CHECK_EQUAL(StartupOptions.getDatabaseFile(), "yadoms.db3");
   BOOST_CHECK_EQUAL(StartupOptions.getHarwarePluginsPath(),"plugins/hardware");
   BOOST_CHECK_EQUAL(StartupOptions.getDevicePluginsPath(),"plugins/device");
-  BOOST_CHECK_EQUAL(StartupOptions.getStartXplHubFlag(),false);
+  BOOST_CHECK_EQUAL(StartupOptions.getStartXplHubFlag(),true);
 }
 
-BOOST_AUTO_TEST_CASE(Initialisation_Error1)
+
+BOOST_AUTO_TEST_CASE(Different_Port_p_Initialisation)
+{ 
+  char *argv[] = {"./MainTestStartOptionsLoader","-p","2000"};
+
+  CStartupOptionsLoader StartupOptions (3, argv);
+
+  BOOST_CHECK_EQUAL(StartupOptions.getLogLevel(), boost::log::trivial::info);
+  BOOST_CHECK_EQUAL(StartupOptions.getPortNumber(), 2000);
+  BOOST_CHECK_EQUAL(StartupOptions.getDatabaseFile(), "yadoms.db3");
+  BOOST_CHECK_EQUAL(StartupOptions.getHarwarePluginsPath(),"plugins/hardware");
+  BOOST_CHECK_EQUAL(StartupOptions.getDevicePluginsPath(),"plugins/device");
+  BOOST_CHECK_EQUAL(StartupOptions.getStartXplHubFlag(),true);
+}
+
+BOOST_AUTO_TEST_CASE(Different_Port_port_Initialisation)
 {
-  CStartupOptionsLoader StartupOptions (5, NULL);
+  char *argv[] = {"./MainTestStartOptionsLoader","--port","2000"};
+
+  CStartupOptionsLoader StartupOptions (3, argv);
+
+  BOOST_CHECK_EQUAL(StartupOptions.getLogLevel(), boost::log::trivial::info);
+  BOOST_CHECK_EQUAL(StartupOptions.getPortNumber(), 2000);
+  BOOST_CHECK_EQUAL(StartupOptions.getDatabaseFile(), "yadoms.db3");
+  BOOST_CHECK_EQUAL(StartupOptions.getHarwarePluginsPath(),"plugins/hardware");
+  BOOST_CHECK_EQUAL(StartupOptions.getDevicePluginsPath(),"plugins/device");
+  BOOST_CHECK_EQUAL(StartupOptions.getStartXplHubFlag(),true);
+}
+
+BOOST_AUTO_TEST_CASE(Port_Initialisation_Error1)
+{
+  char *argv[] = {"./MainTestStartOptionsLoader","--por","2000"};
+
+  CStartupOptionsLoader StartupOptions (3, argv);
 
   BOOST_CHECK_EQUAL(StartupOptions.getLogLevel(), boost::log::trivial::info);
   BOOST_CHECK_EQUAL(StartupOptions.getPortNumber(), 8080);
   BOOST_CHECK_EQUAL(StartupOptions.getDatabaseFile(), "yadoms.db3");
   BOOST_CHECK_EQUAL(StartupOptions.getHarwarePluginsPath(),"plugins/hardware");
   BOOST_CHECK_EQUAL(StartupOptions.getDevicePluginsPath(),"plugins/device");
-  BOOST_CHECK_EQUAL(StartupOptions.getStartXplHubFlag(),false);
+  BOOST_CHECK_EQUAL(StartupOptions.getStartXplHubFlag(),true);
+}
+
+BOOST_AUTO_TEST_CASE(Port_Initialisation_Error2)
+{
+  char *argv[] = {"./MainTestStartOptionsLoader","-port","2000"};
+  
+  //Test the exception, and if this one is the correct one !
+  BOOST_CHECK_EXCEPTION( CStartupOptionsLoader StartupOptions (3, argv), CStartupOptionsLoaderError, validate );
+
+  //BOOST_REQUIRE_THROW(CStartupOptionsLoader StartupOptions (3, argv), CStartupOptionsLoaderError);
+}
+
+BOOST_AUTO_TEST_CASE(Different_Database_databaseFile_Initialisation)
+{
+  char *argv[] = {"./MainTestStartOptionsLoader","--databaseFile","toto.db3"};
+
+  CStartupOptionsLoader StartupOptions (3, argv);
+
+  BOOST_CHECK_EQUAL(StartupOptions.getLogLevel(), boost::log::trivial::info);
+  BOOST_CHECK_EQUAL(StartupOptions.getPortNumber(), 8080);
+  BOOST_CHECK_EQUAL(StartupOptions.getDatabaseFile(), "toto.db3");
+  BOOST_CHECK_EQUAL(StartupOptions.getHarwarePluginsPath(),"plugins/hardware");
+  BOOST_CHECK_EQUAL(StartupOptions.getDevicePluginsPath(),"plugins/device");
+  BOOST_CHECK_EQUAL(StartupOptions.getStartXplHubFlag(),true);
+}
+
+BOOST_AUTO_TEST_CASE(Different_Database_databaseFile_Error1)
+{
+  char *argv[] = {"./MainTestStartOptionsLoader","--database","toto.db3"};
+
+  CStartupOptionsLoader StartupOptions (3, argv);
+
+  BOOST_CHECK_EQUAL(StartupOptions.getLogLevel(), boost::log::trivial::info);
+  BOOST_CHECK_EQUAL(StartupOptions.getPortNumber(), 8080);
+  BOOST_CHECK_EQUAL(StartupOptions.getDatabaseFile(), "yadoms.db3");
+  BOOST_CHECK_EQUAL(StartupOptions.getHarwarePluginsPath(),"plugins/hardware");
+  BOOST_CHECK_EQUAL(StartupOptions.getDevicePluginsPath(),"plugins/device");
+  BOOST_CHECK_EQUAL(StartupOptions.getStartXplHubFlag(),true);
+}
+
+BOOST_AUTO_TEST_CASE(Different_Database_d_Initialisation)
+{
+  char *argv[] = {"./MainTestStartOptionsLoader","-d","toto.db3"};
+
+  CStartupOptionsLoader StartupOptions (3, argv);
+
+  BOOST_CHECK_EQUAL(StartupOptions.getLogLevel(), boost::log::trivial::info);
+  BOOST_CHECK_EQUAL(StartupOptions.getPortNumber(), 8080);
+  BOOST_CHECK_EQUAL(StartupOptions.getDatabaseFile(), "toto.db3");
+  BOOST_CHECK_EQUAL(StartupOptions.getHarwarePluginsPath(),"plugins/hardware");
+  BOOST_CHECK_EQUAL(StartupOptions.getDevicePluginsPath(),"plugins/device");
+  BOOST_CHECK_EQUAL(StartupOptions.getStartXplHubFlag(),true);
+}
+
+BOOST_AUTO_TEST_CASE(Different_Log_l_trace_Initialisation)
+{
+  char *argv[] = {"./MainTestStartOptionsLoader","-l","trace"};
+
+  CStartupOptionsLoader StartupOptions (3, argv);
+
+  BOOST_CHECK_EQUAL(StartupOptions.getLogLevel(), boost::log::trivial::trace);
+  BOOST_CHECK_EQUAL(StartupOptions.getPortNumber(), 8080);
+  BOOST_CHECK_EQUAL(StartupOptions.getDatabaseFile(), "yadoms.db3");
+  BOOST_CHECK_EQUAL(StartupOptions.getHarwarePluginsPath(),"plugins/hardware");
+  BOOST_CHECK_EQUAL(StartupOptions.getDevicePluginsPath(),"plugins/device");
+  BOOST_CHECK_EQUAL(StartupOptions.getStartXplHubFlag(),true);
+}
+
+BOOST_AUTO_TEST_CASE(Different_Log_l_debug_Initialisation)
+{
+  char *argv[] = {"./MainTestStartOptionsLoader","-l","debug"};
+
+  CStartupOptionsLoader StartupOptions (3, argv);
+
+  BOOST_CHECK_EQUAL(StartupOptions.getLogLevel(), boost::log::trivial::debug);
+  BOOST_CHECK_EQUAL(StartupOptions.getPortNumber(), 8080);
+  BOOST_CHECK_EQUAL(StartupOptions.getDatabaseFile(), "yadoms.db3");
+  BOOST_CHECK_EQUAL(StartupOptions.getHarwarePluginsPath(),"plugins/hardware");
+  BOOST_CHECK_EQUAL(StartupOptions.getDevicePluginsPath(),"plugins/device");
+  BOOST_CHECK_EQUAL(StartupOptions.getStartXplHubFlag(),true);
+}
+
+BOOST_AUTO_TEST_CASE(Different_Log_l_info_Initialisation)
+{
+  char *argv[] = {"./MainTestStartOptionsLoader","-l","info"};
+
+  CStartupOptionsLoader StartupOptions (3, argv);
+
+  BOOST_CHECK_EQUAL(StartupOptions.getLogLevel(), boost::log::trivial::info);
+  BOOST_CHECK_EQUAL(StartupOptions.getPortNumber(), 8080);
+  BOOST_CHECK_EQUAL(StartupOptions.getDatabaseFile(), "yadoms.db3");
+  BOOST_CHECK_EQUAL(StartupOptions.getHarwarePluginsPath(),"plugins/hardware");
+  BOOST_CHECK_EQUAL(StartupOptions.getDevicePluginsPath(),"plugins/device");
+  BOOST_CHECK_EQUAL(StartupOptions.getStartXplHubFlag(),true);
+}
+
+BOOST_AUTO_TEST_CASE(Different_Log_l_warning_Initialisation)
+{
+  char *argv[] = {"./MainTestStartOptionsLoader","-l","warning"};
+
+  CStartupOptionsLoader StartupOptions (3, argv);
+
+  BOOST_CHECK_EQUAL(StartupOptions.getLogLevel(), boost::log::trivial::warning);
+  BOOST_CHECK_EQUAL(StartupOptions.getPortNumber(), 8080);
+  BOOST_CHECK_EQUAL(StartupOptions.getDatabaseFile(), "yadoms.db3");
+  BOOST_CHECK_EQUAL(StartupOptions.getHarwarePluginsPath(),"plugins/hardware");
+  BOOST_CHECK_EQUAL(StartupOptions.getDevicePluginsPath(),"plugins/device");
+  BOOST_CHECK_EQUAL(StartupOptions.getStartXplHubFlag(),true);
+}
+
+BOOST_AUTO_TEST_CASE(Different_Log_l_error_Initialisation)
+{
+  char *argv[] = {"./MainTestStartOptionsLoader","-l","error"};
+
+  CStartupOptionsLoader StartupOptions (3, argv);
+
+  BOOST_CHECK_EQUAL(StartupOptions.getLogLevel(), boost::log::trivial::error);
+  BOOST_CHECK_EQUAL(StartupOptions.getPortNumber(), 8080);
+  BOOST_CHECK_EQUAL(StartupOptions.getDatabaseFile(), "yadoms.db3");
+  BOOST_CHECK_EQUAL(StartupOptions.getHarwarePluginsPath(),"plugins/hardware");
+  BOOST_CHECK_EQUAL(StartupOptions.getDevicePluginsPath(),"plugins/device");
+  BOOST_CHECK_EQUAL(StartupOptions.getStartXplHubFlag(),true);
+}
+
+BOOST_AUTO_TEST_CASE(Different_Log_l_fatal_Initialisation)
+{
+  char *argv[] = {"./MainTestStartOptionsLoader","-l","fatal"};
+
+  CStartupOptionsLoader StartupOptions (3, argv);
+
+  BOOST_CHECK_EQUAL(StartupOptions.getLogLevel(), boost::log::trivial::fatal);
+  BOOST_CHECK_EQUAL(StartupOptions.getPortNumber(), 8080);
+  BOOST_CHECK_EQUAL(StartupOptions.getDatabaseFile(), "yadoms.db3");
+  BOOST_CHECK_EQUAL(StartupOptions.getHarwarePluginsPath(),"plugins/hardware");
+  BOOST_CHECK_EQUAL(StartupOptions.getDevicePluginsPath(),"plugins/device");
+  BOOST_CHECK_EQUAL(StartupOptions.getStartXplHubFlag(),true);
+}
+
+BOOST_AUTO_TEST_CASE(Unknow_Log_l_Error1)
+{
+  char *argv[] = {"./MainTestStartOptionsLoader","-l","toto"};
+
+  BOOST_CHECK_EXCEPTION( CStartupOptionsLoader StartupOptions (3, argv), CStartupOptionsLoaderError, validate );
+}
+
+BOOST_AUTO_TEST_CASE(Unknow_option_Error2)
+{
+  char *argv[] = {"./MainTestStartOptionsLoader","-a","toto"};
+
+  BOOST_CHECK_EXCEPTION( CStartupOptionsLoader StartupOptions (3, argv), CStartupOptionsLoaderError, validate );
 }
 
 BOOST_AUTO_TEST_SUITE_END()
 
-/*
-int main (int argc, char** argv)
-{
 
-   CApplicationStopHandler::configure();
-
-   try
-   {
-      CStartupOptionsLoader startupOptions(argc, argv);
-
-      CLog::configure(startupOptions.getLogLevel());
-      YADOMS_LOG_CONFIGURE("Main");
-      
-      YADOMS_LOG(info) << "********************************************************************";
-      YADOMS_LOG(info) << "Yadoms is starting";
-      YADOMS_LOG(info) << "********************************************************************";
-      YADOMS_LOG(info) << "Startup options :";
-      YADOMS_LOG(info) << "\tlog level = " << startupOptions.getLogLevel();
-      YADOMS_LOG(info) << "\tport number = " << startupOptions.getPortNumber();
-      YADOMS_LOG(info) << "\tdb path = " << startupOptions.getDatabaseFile();
-      YADOMS_LOG(info) << "\thardware plugins = " << startupOptions.getHarwarePluginsPath();
-      YADOMS_LOG(info) << "\tdevice plugins = " << startupOptions.getDevicePluginsPath();
-      YADOMS_LOG(info) << "\tstart Xpl Hub = " << startupOptions.getStartXplHubFlag();
-      YADOMS_LOG(info) << "********************************************************************";
-
-
-      CSupervisor supervisor(startupOptions);
-      supervisor.start();
-
-      while(!CApplicationStopHandler::stopRequested())
-      {
-         boost::this_thread::sleep(boost::posix_time::milliseconds(100));
-      }
-
-      supervisor.stop();
-      
-      YADOMS_LOG(info) << "Yadoms is stopped ";
-   }
-   catch(CStartupOptionsLoaderError& e)
-   {
-      if (e.isError())
-      {
-         YADOMS_LOG(fatal) << e.what();
-      }
-      else
-      {
-         // Help was invoked, just print to console
-         YADOMS_LOG(info) << e.what();
-      }
-   }
-   catch(...)
-   {
-      //dual logging in case logger fails/throws
-      std::cout << "An unhandled exception occurs. Yadoms is now stopped" << std::endl;
-      YADOMS_LOG(fatal) << "An unhandled exception occurs. Yadoms is now stopped";
-   }
-
-
-   return 0;
-}
-*/
