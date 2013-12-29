b2be51aab626c6cb450ac1d420151a23d1d68b3f
lgm42 <lgm42@8f66d739-0145-4685-a3fe-f7fe687021a9>
2013-12-29 16:45:42 +0000

    * Add first version of Xpl management (not used in the project for the moment) * Modified stdafx.h to add all new needed boost includes * Add optional value for second parameter of CStringExtension::tryParse() to set decimal base as a default value * Modified CMakeLists.txt to add xpl files

---
+++ trunk/sources/tools/tools/Xpl/XplException.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "stdafx.h"
+
+//--------------------------------------------------------------
+/// \class Exception for xpl exception
+//--------------------------------------------------------------
+class CXplException : public std::exception
+{
+public:
+   //--------------------------------------------------------------
+   /// \brief	                        Constructor
+   /// \param [in] message : Message to display
+   //--------------------------------------------------------------
+   CXplException(const std::string & message) : m_message(message)
+   {
+   }
+
+   //--------------------------------------------------------------
+   /// \brief      Destructor
+   //--------------------------------------------------------------
+   virtual ~CXplException() throw()
+   {
+   }
+
+   //--------------------------------------------------------------
+   /// \brief	    Build full message explaining exception reason
+   /// \return     message explaining exception reason
+   //--------------------------------------------------------------
+   virtual const char* what() const throw() { return m_message.c_str(); }
+
+private:
+   std::string m_message;
+};
---
+++ trunk/sources/tools/tools/Xpl/XplService.h
@@ -0,0 +1,111 @@
+#pragma once
+#include "stdafx.h"
+
+#include "XplActor.h"
+#include "XplMessage.h"
+
+class CXplService
+{
+public:
+   typedef boost::signals2::signal<void (CXplMessage&)> SigMessageReceived;
+   typedef SigMessageReceived::slot_type SigMessageReceivedDelegate;
+   
+   //--------------------------------------------------------------
+   /// \brief			Ctor using default network interface
+   /// \param [in]   vendorId : The vendor Id delivered by Xpl Project
+   /// \param [in]   deviceId : The device Id delivered by Xpl Project
+   /// \param [in]   instanceId : The instance Id
+   //--------------------------------------------------------------
+   CXplService(const std::string & vendorId, const std::string & deviceId, const std::string & instanceId);
+   
+   //--------------------------------------------------------------
+   /// \brief			Ctor
+   /// \param [in]   vendorId : The vendor Id delivered by Xpl Project
+   /// \param [in]   deviceId : The device Id delivered by Xpl Project
+   /// \param [in]   instanceId : The instance Id
+   /// \param [in] : localIPOfTheInterfaceToUse : Local @ ip used to select network interface to send
+   //--------------------------------------------------------------
+   CXplService(const std::string & vendorId, const std::string & deviceId, const std::string & instanceId, const std::string & localIPOfTheInterfaceToUse);   
+   
+   //--------------------------------------------------------------
+   /// \brief			Dtor
+   //--------------------------------------------------------------
+   virtual ~CXplService();
+
+   //--------------------------------------------------------------
+   /// \brief			Send the message to the remoteEndpoint
+   /// \param [in]   message : The message to send
+   //--------------------------------------------------------------
+   void sendMessage(const CXplMessage & message);
+
+   //--------------------------------------------------------------
+   /// \brief			Record the given method to the event Message received
+   /// \param [in]   dlg : the method to call when a message is received (ex : boost::bind(&CClass::onMessageReceived, this, _1, _2)
+   //--------------------------------------------------------------
+   void messageReceived(const SigMessageReceivedDelegate &dlg);
+
+    
+   //--------------------------------------------------------------
+   /// \brief			Configure the filter to apply to the received messages
+   /// \param [in]   filter to apply. It must be formed like : [msgtype].[vendor].[device].[instance].[class].[type] (* is allowed for each param)
+   //--------------------------------------------------------------
+   void setFilter(const std::string & filter);
+
+   //--------------------------------------------------------------
+   /// \brief			Configure the filter to apply to the received messages
+   /// \param [in]   filter to apply. It must be formed like : [msgtype].[vendor].[device].[instance].[class].[type] (* is allowed for each param)
+   //--------------------------------------------------------------
+   void setFilter(const std::string & msgtype, const std::string & vendor, const std::string & device, const std::string & instance, 
+                  const std::string & classId, const std::string & typeId);
+
+private:
+
+   typedef struct SFilter
+   {
+      boost::shared_ptr<std::string> msgtype;
+      boost::shared_ptr<std::string> vendor;
+      boost::shared_ptr<std::string> device;
+      boost::shared_ptr<std::string> instance;
+      boost::shared_ptr<std::string> classId;
+      boost::shared_ptr<std::string> typeId;
+   };
+
+   void initializeConnector();
+   void heartbeatSequence();
+   void runHeartbeatSequenceIn(const int seconds);
+   void handleReceive(const boost::system::error_code& error, std::size_t bytes_transferred);
+
+   //--------------------------------------------------------------
+   /// \brief			Fire the event MessageReceived
+   /// \param [in]   message : The message which just has been received
+   //--------------------------------------------------------------
+   void fireMessageReceivedEvent(CXplMessage & msg);
+
+   boost::asio::io_service m_ioService;
+   boost::asio::deadline_timer m_timer;
+
+   boost::asio::ip::udp::endpoint m_localEndPoint;
+   boost::asio::ip::udp::endpoint m_remoteEndPoint;
+   
+   boost::asio::ip::udp::socket m_socket;
+
+   bool m_hubHasBeenFound;
+   boost::posix_time::ptime m_startDate;
+   CXplActor m_source;
+
+   boost::array<char, 1024> m_receiveBuffer;
+
+   SigMessageReceived m_sigMessageReceived;
+
+   SFilter m_filter;
+
+   static const int HeartbeatInterval = 1;//5;
+
+   static const int HeartbeatFrequencyDuringInitialDiscoveryPhase = 5;
+   static const int HeartbeatFrequencyDuringSecondDiscoveryPhase = 30;
+   static const int HeartbeatFrequencyDuringNormalRun = HeartbeatInterval * 60;
+
+   static const int HubDiscoveryTimeOut = 2 * 60;
+
+};
+
