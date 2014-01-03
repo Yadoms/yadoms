b80a0747ebccc5cfbf40bea51eb20d918c6b5f01
jano42 <jano42@8f66d739-0145-4685-a3fe-f7fe687021a9>
2014-01-03 10:56:11 +0000

    * Tools : add a version class to handle versions from string and compare them * Database : add system tables * Database : upgrade srcipt for initial version (in progress)

--- trunk/sources/server/database/sqlite/versioning/SQLiteVersion1.cpp
+++ trunk/sources/server/database/sqlite/versioning/SQLiteVersion1.cpp
@@ -1,6 +1,12 @@
 #include "stdafx.h"
 #include "SQLiteVersion1.h"
 #include "database/sqlite/Query.h"
+#include "SQLiteSystemTables.h"
+#include "database/sqlite/SQLiteDatabaseTables.h"
+#include "database/sqlite/adapters/SingleValueAdapter.hpp"
+#include "tools/Version.h"
+
+
 
 CSQLiteVersion1::CSQLiteVersion1()
 {
@@ -17,13 +23,75 @@ void CSQLiteVersion1::checkForUpgrade(const boost::shared_ptr<CSQLiteRequester> 
    CQuery sCheckForConfigurationTableExists;
 
    sCheckForConfigurationTableExists.  SelectCount().
-                                       From("sqlite_master").
-                                       Where("type", CQUERY_OP_EQUAL, "table").
-                                       And("name", CQUERY_OP_EQUAL, "configuration");
+                                       From(CSqliteMasterTable::getTableName()).
+                                       Where(CSqliteMasterTable::getTypeColumnName(), CQUERY_OP_EQUAL, SQLITEMASTER_TABLE).
+                                       And(CSqliteMasterTable::getNameColumnName(), CQUERY_OP_EQUAL, CConfigurationTable::getTableName());
    int count = pRequester->queryCount(sCheckForConfigurationTableExists);
 
-   
+   if(count != 1)
+   {
+      //configuration table is missing,
+      //create the full database structure
+      YADOMS_LOG(info) << "-> need to create database";
+      CreateDatabase(pRequester);
+   }
+   else
+   {
+      bool bNeedToUpgrade = false;
+      CQuery qVersion;
+      qVersion.Select(CConfigurationTable::getValueColumnName()).
+         From(CConfigurationTable::getTableName()).
+         Where(CConfigurationTable::getSectionColumnName(), CQUERY_OP_EQUAL, "Database").
+         And(CConfigurationTable::getNameColumnName(), CQUERY_OP_EQUAL, "Version");
 
+      CSingleValueAdapter<std::string> adapter;
+      pRequester->queryEntities<std::string>(&adapter, qVersion);
+      std::vector<std::string> results = adapter.getResults();
+      if(results.size() >= 1)
+      {
+         //une version est déjà configurée
+         CVersion versionFromdatabase(results[0]);
+         YADOMS_LOG(info) << "database version  : " << results[0];
+         if(versionFromdatabase >= CVersion(1,0,0,0))
+         {
+            //not for me, version is correct
+            bNeedToUpgrade = false;
+         }
+         else
+         {
+            //version is lower to 1.0.0.0, then upgrade database
+            bNeedToUpgrade = true;
+         }
+      }
+      else
+      {
+         //la version n'a jamais été configurée
+         YADOMS_LOG(info) << "version has never been configured";
+         bNeedToUpgrade = true;
+      }
 
+      if(bNeedToUpgrade)
+      {
+            YADOMS_LOG(info) << "-> need to upgrade database";
+            UpgradeDatabase(pRequester);
+      }
+   }
 }
 // [END] ISQLiteVersionUpgrade implementation
+
+
+//-----------------------------------
+/// \brief     Create the database (when tables are not found)
+///\param [in] pRequester : database requester object
+//-----------------------------------
+void CSQLiteVersion1::CreateDatabase(const boost::shared_ptr<CSQLiteRequester> & pRequester)
+{
+}
+
+//-----------------------------------
+/// \brief     Upgrade the database (missing or bad database version)
+///\param [in] pRequester : database requester object
+//-----------------------------------
+void CSQLiteVersion1::UpgradeDatabase(const boost::shared_ptr<CSQLiteRequester> & pRequester)
+{
+}
