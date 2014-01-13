260c07ca74c2e3b03208e869137e06e2ff111f14
Jean-Michel DECORET <jano42@users.noreply.github.com>
2014-01-13 16:04:21 +0000

    * Database : update/creation system : add version in configuration table * Database : structure : add version in configuration table * tools/CVersion : bug fix with CVersion comparisons (comparison method build from scratch)

--- sources/server/database/sqlite/versioning/SQLiteVersion1.cpp
+++ sources/server/database/sqlite/versioning/SQLiteVersion1.cpp
@@ -19,55 +19,59 @@ CSQLiteVersion1::~CSQLiteVersion1()
 // ISQLiteVersionUpgrade implementation
 void CSQLiteVersion1::checkForUpgrade(const boost::shared_ptr<CSQLiteRequester> & pRequester)
 {
-    bool bNeedToCreateOrUpgrade = false;
+   bool bNeedToCreateOrUpgrade = true;
 
-   //check that table all tables exists
-   if(!pRequester->checkTableExists(CAcquisitionTable::getTableName()) ||
-      !pRequester->checkTableExists(CConfigurationTable::getTableName()) ||
-      !pRequester->checkTableExists(CDeviceTable::getTableName()) ||
-      !pRequester->checkTableExists(CHardwareTable::getTableName()) ||
-      !pRequester->checkTableExists(CKeywordTable::getTableName()))
-   {
-      //configuration table is missing,
-      //create the full database structure
-      YADOMS_LOG(info) << "-> need to create database";
-      bNeedToCreateOrUpgrade = true;
-   }
-   else
-   {
-     
-      CQuery qVersion;
-      qVersion.Select(CConfigurationTable::getValueColumnName()).
-         From(CConfigurationTable::getTableName()).
-         Where(CConfigurationTable::getSectionColumnName(), CQUERY_OP_EQUAL, "Database").
-         And(CConfigurationTable::getNameColumnName(), CQUERY_OP_EQUAL, "Version");
+   //get the database version
+   CQuery qVersion;
+   qVersion.Select(CConfigurationTable::getValueColumnName()).
+      From(CConfigurationTable::getTableName()).
+      Where(CConfigurationTable::getSectionColumnName(), CQUERY_OP_EQUAL, "Database").
+      And(CConfigurationTable::getNameColumnName(), CQUERY_OP_EQUAL, "Version");
 
-      CSingleValueAdapter<std::string> adapter;
-      pRequester->queryEntities<std::string>(&adapter, qVersion);
-      std::vector<std::string> results = adapter.getResults();
-      if(results.size() >= 1)
+   CSingleValueAdapter<std::string> adapter;
+   pRequester->queryEntities<std::string>(&adapter, qVersion);
+   std::vector<std::string> results = adapter.getResults();
+   if(results.size() >= 1)
+   {
+      //une version est déjà configurée
+      CVersion versionFromdatabase(results[0]);
+      YADOMS_LOG(info) << "database version  : " << results[0];
+      if(versionFromdatabase >= CVersion(1,0,0,0))
       {
-         //une version est déjà configurée
-         CVersion versionFromdatabase(results[0]);
-         YADOMS_LOG(info) << "database version  : " << results[0];
-         if(versionFromdatabase >= CVersion(1,0,0,0))
-         {
-            //not for me, version is correct
-            bNeedToCreateOrUpgrade = false;
-         }
-         else
-         {
-            //version is lower to 1.0.0.0, then create database
-            bNeedToCreateOrUpgrade = true;
-         }
+         //not for me, version is correct
+         bNeedToCreateOrUpgrade = false;
       }
       else
       {
-         //la version n'a jamais été configurée
-         YADOMS_LOG(info) << "version has never been configured";
+         //version is lower to 1.0.0.0, then create database
          bNeedToCreateOrUpgrade = true;
       }
    }
+   else
+   {
+      //la version n'a jamais été configurée
+      YADOMS_LOG(info) << "version has never been configured";
+      bNeedToCreateOrUpgrade = true;
+   }
+
+   /*
+   //check that table all tables exists
+   if(!pRequester->checkTableExists(CAcquisitionTable::getTableName()) ||
+   !pRequester->checkTableExists(CConfigurationTable::getTableName()) ||
+   !pRequester->checkTableExists(CDeviceTable::getTableName()) ||
+   !pRequester->checkTableExists(CHardwareTable::getTableName()) ||
+   !pRequester->checkTableExists(CKeywordTable::getTableName()))
+   {
+   //configuration table is missing,
+   //create the full database structure
+   YADOMS_LOG(info) << "-> need to create database";
+   bNeedToCreateOrUpgrade = true;
+   }
+   else
+   {
+
+
+   }*/
 
    if(bNeedToCreateOrUpgrade)
    {
@@ -113,6 +117,12 @@ void CSQLiteVersion1::CreateDatabase(const boost::shared_ptr<CSQLiteRequester> &
          throw CSQLiteVersionException("Failed to create Hardware table");
       if(!pRequester->createTableIfNotExists(CKeywordTable::getTableName(), CKeywordTable::getTableCreationScript()))
          throw CSQLiteVersionException("Failed to create Keyword table");
+
+      //set the database version
+      CQuery qInsert;
+      qInsert.InsertInto(CConfigurationTable::getTableName(), CConfigurationTable::getSectionColumnName(), CConfigurationTable::getNameColumnName(), CConfigurationTable::getValueColumnName(), CConfigurationTable::getDescriptionColumnName()).
+         Values( "Database", "Version", 1.0, "Database version");
+      pRequester->query(qInsert);
 
       //commit transaction
       pRequester->transactionCommit();
