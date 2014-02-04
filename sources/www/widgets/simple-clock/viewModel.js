7d64205c933117a71bc5abc36152f74d6c17159b
Nicolas HILAIRE <nicolas.hilaire@gmail.com>
2014-02-04 16:02:07 +0000

    simple-clock widget are now independant and can be instantiate twice on the same page without problem

--- sources/www/widgets/simple-clock/viewModel.js
+++ sources/www/widgets/simple-clock/viewModel.js
@@ -2,11 +2,11 @@ function SimpleClockViewModel() {
     this.data;
     this.resized = function($widget) {
         if ($widget.width() <= 150)
-            $(".simpleClockWidget").css("font-size", "1.0em");
+            $widget.find(".simpleClockWidget").css("font-size", "1.0em");
         else if ($widget.width() <= 300)
-            $(".simpleClockWidget").css("font-size", "2.0em");
+            $widget.find(".simpleClockWidget").css("font-size", "2.0em");
         else
-            $(".simpleClockWidget").css("font-size", "4.0em");
+            $widget.find(".simpleClockWidget").css("font-size", "4.0em");
     }
 }
 
