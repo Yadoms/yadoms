6c3032a55a096cb9dcaf25ede4da275a75c0c131
sgallou <sgallou@8f66d739-0145-4685-a3fe-f7fe687021a9>
2014-02-07 15:25:10 +0000

    - plugin managment : qualifier, add cache and plugin safety getter

--- trunk/sources/server/pluginSystem/HardwarePluginQualifier.h
+++ trunk/sources/server/pluginSystem/HardwarePluginQualifier.h
@@ -12,6 +12,61 @@
 //--------------------------------------------------------------
 class CHardwarePluginQualifier : public IHardwarePluginQualifier
 {
+private:
+   //--------------------------------------------------------------
+   /// \brief	Internal plugin identity
+   //--------------------------------------------------------------
+   class CPluginIdentityCompare;
+   class CPluginIdentity   // TODO à déplacer dans un autre fichier ?
+   {
+   public:
+      //--------------------------------------------------------------
+      /// \brief	Constructor
+      //--------------------------------------------------------------
+      CPluginIdentity(const boost::shared_ptr<const IHardwarePluginInformation> pluginInformation)
+         :m_name(pluginInformation->getName()), m_version(pluginInformation->getVersion()), m_releaseType(pluginInformation->getReleaseType())
+      {
+      }
+
+      //--------------------------------------------------------------
+      /// \brief	Constructor
+      //--------------------------------------------------------------
+      CPluginIdentity(const std::string& name, const std::string& version, IHardwarePluginInformation::EReleaseType releaseType)
+         :m_name(name), m_version(version), m_releaseType(releaseType)
+      {
+      }
+
+      //--------------------------------------------------------------
+      /// \brief	Destructor
+      //--------------------------------------------------------------
+      virtual ~CPluginIdentity()
+      {
+      }
+
+   private:
+      const std::string m_name;
+      const std::string m_version;
+      const IHardwarePluginInformation::EReleaseType m_releaseType;
+
+      friend CPluginIdentityCompare;
+   };
+
+   //--------------------------------------------------------------
+   /// \brief	Compare operator (used in map)
+   //--------------------------------------------------------------
+   class CPluginIdentityCompare// TODO à déplacer dans un autre fichier ?
+   {
+   public:
+      bool operator() (const CPluginIdentity& lhs, const CPluginIdentity& rhs)
+      {
+         if (lhs.m_name != rhs.m_name)
+            return lhs.m_name < rhs.m_name;
+         if (lhs.m_version != rhs.m_version)
+            return lhs.m_version < rhs.m_version;
+         return lhs.m_releaseType < rhs.m_releaseType;
+      }
+   };
+
 public:
    //--------------------------------------------------------------
    /// \brief	Constructor
@@ -24,10 +79,40 @@ public:
    virtual ~CHardwarePluginQualifier();
 
    // IHardwarePluginQualifier implementation
-   virtual void signalLoad(const boost::shared_ptr<const CHardwarePluginFactory> plugin);
-   virtual void signalUnload(const boost::shared_ptr<const CHardwarePluginFactory> plugin);
-   virtual void signalCrash(const boost::shared_ptr<const CHardwarePluginFactory> plugin, const std::string& exceptionName);
-   virtual bool isSafe(const boost::shared_ptr<const CHardwarePluginFactory> plugin);
-   virtual int getQualityLevel(const boost::shared_ptr<const CHardwarePluginFactory> plugin);
+   virtual void signalLoad(const boost::shared_ptr<const IHardwarePluginInformation> pluginInformation);
+   virtual void signalUnload(const boost::shared_ptr<const IHardwarePluginInformation> pluginInformation);
+   virtual void signalCrash(const boost::shared_ptr<const IHardwarePluginInformation> pluginInformation, const std::string& exceptionName);
+   virtual bool isSafe(const boost::shared_ptr<const IHardwarePluginInformation> pluginInformation);
+   virtual int getQualityLevel(const boost::shared_ptr<const IHardwarePluginInformation> pluginInformation);
    // [END] IHardwarePluginQualifier implementation
+
+private:
+   //--------------------------------------------------------------
+   /// \brief	               Make corresponding quality indicator obsolete in cache
+   /// \param[in] identity    Plugin identity
+   //--------------------------------------------------------------
+   void obsoleteQualityIndicatorCache(const CPluginIdentity& identity);
+
+   //--------------------------------------------------------------
+   /// \brief	               compute quality indicator of a plugin
+   /// \param[in] identity    Plugin identity
+   /// \return                Computed quality, from 0(lower) to 100(best)
+   //--------------------------------------------------------------
+   int computeQuality(const CPluginIdentity& identity) const;
+
+private:
+   //--------------------------------------------------------------
+   /// \brief	Safety threshold : a plugin with a quality upper or equal to
+   ///         this threshold, is considered as safe.
+   //--------------------------------------------------------------
+   static const int m_SafetyThreshold;
+
+   //--------------------------------------------------------------
+   /// \brief	Cache of quality indicators
+   ///         Key is plugin identity
+   ///         Value is quality indicator
+   ///         Need specific comparator
+   //--------------------------------------------------------------
+   typedef std::map<CPluginIdentity, int, CPluginIdentityCompare> QualityIndicatorsCache;
+   QualityIndicatorsCache m_qualityIndicatorsCache;//TODO : protect by mutex
 };
--- trunk/sources/server/pluginSystem/IHardwarePluginQualifier.h
+++ trunk/sources/server/pluginSystem/IHardwarePluginQualifier.h
@@ -21,38 +21,34 @@ public:
 
    //--------------------------------------------------------------
    /// \brief			         Signal that plugin was loaded
-   /// \param[in]  pluginName       Plugin name
-   /// \param[in]  pluginVersion    Plugin version
-   /// \param[in]  pluginReleaseType Plugin release type (beta, release...)
+   /// \param[in]  pluginInformation     Plugin information (name, version...)
    //--------------------------------------------------------------
-   virtual void signalLoad(const boost::shared_ptr<const CHardwarePluginFactory> plugin) = 0;
+   virtual void signalLoad(const boost::shared_ptr<const IHardwarePluginInformation> pluginInformation) = 0;
 
    //--------------------------------------------------------------
    /// \brief			         Signal that plugin was unloaded
-   /// \param[in]  pluginName       Plugin name
-   /// \param[in]  pluginVersion    Plugin version
-   /// \param[in]  pluginReleaseType Plugin release type (beta, release...)
+   /// \param[in]  pluginInformation     Plugin information (name, version...)
    //--------------------------------------------------------------
-   virtual void signalUnload(const boost::shared_ptr<const CHardwarePluginFactory> plugin) = 0;
+   virtual void signalUnload(const boost::shared_ptr<const IHardwarePluginInformation> pluginInformation) = 0;
 
    //--------------------------------------------------------------
    /// \brief			         Signal that plugin has crashed
-   /// \param[in]  plugin     Plugin
+   /// \param[in]  pluginInformation     Plugin information (name, version...)
    /// \param[in]  reason     Crash cause (exception...)
    //--------------------------------------------------------------
-   virtual void signalCrash(const boost::shared_ptr<const CHardwarePluginFactory> plugin, const std::string& exceptionName) = 0;
+   virtual void signalCrash(const boost::shared_ptr<const IHardwarePluginInformation> pluginInformation, const std::string& exceptionName) = 0;
 
    //--------------------------------------------------------------
    /// \brief			         Check if plugin is safe
-   /// \param[in]  plugin     Plugin
+   /// \param[in]  pluginInformation     Plugin information (name, version...)
    /// \return                true if safe (don't crash too much)
    //--------------------------------------------------------------
-   virtual bool isSafe(const boost::shared_ptr<const CHardwarePluginFactory> plugin) = 0;//TODO pas encore appelé
+   virtual bool isSafe(const boost::shared_ptr<const IHardwarePluginInformation> pluginInformation) = 0;//TODO pas encore appelé
 
    //--------------------------------------------------------------
    /// \brief			         Get The quality level
-   /// \param[in]  plugin     Plugin
+   /// \param[in]  pluginInformation     Plugin information (name, version...)
    /// \return                Plugin quality level, value from 0 (very bad) to 100 (perfect, never crashed)
    //--------------------------------------------------------------
-   virtual int getQualityLevel(const boost::shared_ptr<const CHardwarePluginFactory> plugin) = 0;//TODO pas encore appelé
+   virtual int getQualityLevel(const boost::shared_ptr<const IHardwarePluginInformation> pluginInformation) = 0;//TODO pas encore appelé
 };
