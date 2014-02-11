77950910fdd72d2f94910fa5227b18069c13b591
Nicolas HILAIRE <nicolas.hilaire@gmail.com>
2014-02-11 11:52:33 +0000

    Database: change default values of widget columns add positionX and Y to the field when making an insert into widget table

--- sources/server/database/sqlite/SQLiteWidgetRequester.h
+++ sources/server/database/sqlite/SQLiteWidgetRequester.h
@@ -28,6 +28,7 @@ public:
    virtual std::vector<boost::shared_ptr<CWidget> > getWidgetsForPage(int pageId);
    virtual void updateWidgetConfiguration(int widgetId, const std::string& newConfiguration);
    virtual void updateWidgetSize(int widgetId, int sizeX, int sizeY);
+   virtual void updateWidgetPosition(int widgetId, int positionX, int positionY);
    virtual void removeWidget(int widgetId);
    virtual void removeWidgetsInPage(int pageId);
    virtual void removeAllWidgets();
