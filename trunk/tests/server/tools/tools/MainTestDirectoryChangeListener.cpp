c564caba92616ba7db33abd0448021a7619c2da5
sgallou <sgallou@8f66d739-0145-4685-a3fe-f7fe687021a9>
2014-02-21 14:34:29 +0000

    - Tests : complete CDirectoryChangeListener tests

--- trunk/tests/server/tools/tools/MainTestDirectoryChangeListener.cpp
+++ trunk/tests/server/tools/tools/MainTestDirectoryChangeListener.cpp
@@ -32,10 +32,24 @@ void CreateFile(std::string dir, std::string file)
    BOOST_TEST_REQUIRE(outfile.is_open());
 }
 
-void RemoveFile(std::string name)
+void RemoveFile(std::string dir, std::string file)
 {
-   if(boost::filesystem::exists(name.c_str()))
-      boost::filesystem::remove(name.c_str());
+   boost::filesystem::path fullPath = boost::filesystem::path(dir) / file;
+   BOOST_TEST_REQUIRE(boost::filesystem::remove(fullPath));
+}
+
+void WriteFile(std::string dir, std::string file)
+{
+   boost::filesystem::path fullPath = boost::filesystem::path(dir) / file;
+   std::ofstream outfile(fullPath.string());
+   outfile << "some text...";
+}
+
+void RenameFile(std::string dir, std::string oldFile, std::string newFile)
+{
+   boost::filesystem::path oldFullPath = boost::filesystem::path(dir) / oldFile;
+   boost::filesystem::path newFullPath = boost::filesystem::path(dir) / newFile;
+   boost::filesystem::rename(oldFullPath, newFullPath);
 }
 
 class CTestContext
@@ -58,6 +72,7 @@ private:
 };
 
 static const std::string TestDirectory("test_directory");
+static const std::string TestFile("test_file");
 std::vector<const boost::asio::dir_monitor_event> Events;
 boost::mutex EventsMutex;
 
@@ -78,19 +93,96 @@ void onEventOnDirectory(const boost::asio::dir_monitor_event& evt)
 BOOST_AUTO_TEST_CASE(NewFile_Test)
 {
    CTestContext context(TestDirectory);
+   Events.clear();
    CDirectoryChangeListener listener (TestDirectory, onEventOnDirectory);
    boost::this_thread::sleep_for(boost::chrono::milliseconds(200));
 
-   CreateFile(TestDirectory, "testFile");
+   CreateFile(TestDirectory, TestFile);
    boost::this_thread::sleep_for(boost::chrono::milliseconds(200));
 
    // Now, check results
    boost::mutex::scoped_lock lock(EventsMutex);
    BOOST_REQUIRE_EQUAL(Events.size(), (size_t)1);
    BOOST_CHECK_EQUAL(Events[0].dirname, TestDirectory);
-   BOOST_CHECK_EQUAL(Events[0].filename, "testFile");
+   BOOST_CHECK_EQUAL(Events[0].filename, TestFile);
    BOOST_CHECK_EQUAL(Events[0].type, boost::asio::dir_monitor_event::added);
 }
 
+//--------------------------------------------------------------
+/// \brief	    Test of file deletion
+/// \result         No Error
+//--------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(DeleteFile_Test)
+{
+   CTestContext context(TestDirectory);
+   Events.clear();
+   CreateFile(TestDirectory, TestFile);
 
-//TODO : need more tests
+   CDirectoryChangeListener listener (TestDirectory, onEventOnDirectory);
+   boost::this_thread::sleep_for(boost::chrono::milliseconds(200));
+   RemoveFile(TestDirectory, TestFile);
+   boost::this_thread::sleep_for(boost::chrono::milliseconds(200));
+
+   // Now, check results
+   boost::mutex::scoped_lock lock(EventsMutex);
+   BOOST_REQUIRE_EQUAL(Events.size(), (size_t)1);
+   BOOST_CHECK_EQUAL(Events[0].dirname, TestDirectory);
+   BOOST_CHECK_EQUAL(Events[0].filename, TestFile);
+   BOOST_CHECK_EQUAL(Events[0].type, boost::asio::dir_monitor_event::removed);
+}
+
+//--------------------------------------------------------------
+/// \brief	    Test of file modification
+/// \result         No Error
+//--------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(ModifyFile_Test)
+{
+   CTestContext context(TestDirectory);
+   Events.clear();
+   CreateFile(TestDirectory, TestFile);
+
+   CDirectoryChangeListener listener (TestDirectory, onEventOnDirectory);
+   boost::this_thread::sleep_for(boost::chrono::milliseconds(200));
+   WriteFile(TestDirectory, TestFile);
+   boost::this_thread::sleep_for(boost::chrono::milliseconds(200));
+
+   // Now, check results (2 events : at file open, at file close)
+   boost::mutex::scoped_lock lock(EventsMutex);
+   BOOST_REQUIRE_EQUAL(Events.size(), (size_t)2);
+   BOOST_CHECK_EQUAL(Events[0].dirname, TestDirectory);
+   BOOST_CHECK_EQUAL(Events[0].filename, TestFile);
+   BOOST_CHECK_EQUAL(Events[0].type, boost::asio::dir_monitor_event::modified);
+   BOOST_CHECK_EQUAL(Events[1].dirname, TestDirectory);
+   BOOST_CHECK_EQUAL(Events[1].filename, TestFile);
+   BOOST_CHECK_EQUAL(Events[1].type, boost::asio::dir_monitor_event::modified);
+}
+
+//--------------------------------------------------------------
+/// \brief	    Test of rename file
+/// \result         No Error
+//--------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(RenameFile_Test)
+{
+   CTestContext context(TestDirectory);
+   Events.clear();
+   CreateFile(TestDirectory, TestFile);
+
+   CDirectoryChangeListener listener (TestDirectory, onEventOnDirectory);
+   boost::this_thread::sleep_for(boost::chrono::milliseconds(200));
+   const std::string newFileName (TestFile + "2");
+   RenameFile(TestDirectory, TestFile, newFileName);
+   boost::this_thread::sleep_for(boost::chrono::milliseconds(200));
+
+   // Now, check results
+   boost::mutex::scoped_lock lock(EventsMutex);
+   BOOST_REQUIRE_EQUAL(Events.size(), (size_t)3);
+   BOOST_CHECK_EQUAL(Events[0].dirname, TestDirectory);
+   BOOST_CHECK_EQUAL(Events[0].filename, TestFile);
+   BOOST_CHECK_EQUAL(Events[0].type, boost::asio::dir_monitor_event::renamed_old_name);
+   BOOST_CHECK_EQUAL(Events[1].dirname, TestDirectory);
+   BOOST_CHECK_EQUAL(Events[1].filename, newFileName);
+   BOOST_CHECK_EQUAL(Events[1].type, boost::asio::dir_monitor_event::renamed_new_name);
+   BOOST_CHECK_EQUAL(Events[2].dirname, TestDirectory);
+   BOOST_CHECK_EQUAL(Events[2].filename, newFileName);
+   BOOST_CHECK_EQUAL(Events[2].type, boost::asio::dir_monitor_event::modified);
+}
