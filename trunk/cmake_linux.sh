5db486854515af843c82fc2af6b5b1c0ab6b4e1c
jano42 <jano42@8f66d739-0145-4685-a3fe-f7fe687021a9>
2014-02-26 13:44:24 +0000

    * CMake scripts : merge scripts by platforms. In each, it asks user which generation tool to use (or use shell script parameter)

--- trunk/cmake_linux.sh
+++ trunk/cmake_linux.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
 
-
+#move to project directory
 BASEDIR=$(dirname $0);
 set -e
-
 cd $BASEDIR/projects
-cmake ../sources
 
+#treat script parameter (if there is one, then directly use it for cases)
+if [ -z "$1" ]
+then
+	echo "Which choice would you like?"
+	echo " -> Generate a linux makefile (m)"
+	echo " -> Generate a Codeblock project (c)"
+	read choice
+else
+	choice=$1
+fi
 
+#run cmake depending on user choice (or script parameter)
+case "$choice" in
+
+    m)
+	# cmake for makefile
+	cmake ../sources
+	;;
+    c)
+	# cmake for codeblock
+	cmake  -G "CodeBlocks - Unix Makefiles" ../sources
+	;;
+    *)
+	# generate an error and return 1
+	echo "$choice is not a valid choice"
+	exit 1
+	;;
+esac
 
