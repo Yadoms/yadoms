f4ddb6c6b6ea4e3fd954c81909636c8af643be15
sgallou <sgallou@8f66d739-0145-4685-a3fe-f7fe687021a9>
2014-03-02 17:25:37 +0000

    - FileSystemExtension : replace macro getting library path by function

--- trunk/sources/shared/windows/shared/FileSystemExtension.h
+++ trunk/sources/shared/windows/shared/FileSystemExtension.h
@@ -27,46 +27,45 @@ namespace shared
       virtual ~CFileSystemExtension();
 
       //--------------------------------------------------------------
-      /// \brief			Macro implementing the getting of the current module path.
-      ///               This must be a macro because this code must be executed
-      ///               in the module which the path is required.
+      /// \brief			Get the current module path.
+      ///               This code must be executed in the module which the path is required,
+      ///               so let it in the header file.
       //--------------------------------------------------------------
-      #define IMPLEMENT_GET_MODULE_PATH                                                                                 \
-      static const boost::filesystem::path getModulePath()                                                              \
-      {                                                                                                                 \
-         std::vector<char> modulePath(MAX_PATH);                                                                        \
-                                                                                                                        \
-         HMODULE hModule = NULL;                                                                                        \
-         GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS, (LPCTSTR)getModulePath, &hModule);                   \
-         if (hModule == NULL)                                                                                           \
-         {                                                                                                              \
-            DWORD error = GetLastError();                                                                               \
-            throw shared::exception::CException(std::string("CFileSystemExtension::getModulePath fails with error ")    \
-               + boost::lexical_cast<std::string>(error));                                                              \
-         }                                                                                                              \
-                                                                                                                        \
-         /* Try to get the executable path with a buffer of MAX_PATH characters. */                                     \
-         DWORD result = GetModuleFileNameA(hModule, &modulePath[0], static_cast<DWORD>(modulePath.size()));             \
-                                                                                                                        \
-         /* As long the function returns the buffer size, it is indicating that the buffer */                           \
-         /* was too small. Keep enlarging the buffer by a factor of 2 until it fits. */                                 \
-         while(result == modulePath.size())                                                                             \
-         {                                                                                                              \
-            modulePath.resize(modulePath.size() * 2);                                                                   \
-            result = GetModuleFileNameA(hModule, &modulePath[0], static_cast<DWORD>(modulePath.size()));                \
-         }                                                                                                              \
-                                                                                                                        \
-         /* If the function returned 0, something went wrong */                                                         \
-         if(result == 0)                                                                                                \
-         {                                                                                                              \
-            DWORD error = GetLastError();                                                                               \
-            throw shared::exception::CException(std::string("CFileSystemExtension::getModulePath fails with error ")    \
-               + boost::lexical_cast<std::string>(error));                                                              \
-         }                                                                                                              \
-                                                                                                                        \
-         /* We've got the path, construct a path from it */                                                             \
-         boost::filesystem::path fullPath(std::string(modulePath.begin(), modulePath.begin() + result));                \
-         return fullPath.parent_path();                                                                                 \
+      static const boost::filesystem::path getModulePath()
+      {
+         std::vector<char> modulePath(MAX_PATH);
+
+         HMODULE hModule = NULL;
+         GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS, (LPCTSTR)getModulePath, &hModule);
+         if (hModule == NULL)
+         {
+            DWORD error = GetLastError();                                                                               
+            throw shared::exception::CException(std::string("CFileSystemExtension::getModulePath fails with error ")    
+               + boost::lexical_cast<std::string>(error));                                                              
+         }                                                                                                              
+
+         /* Try to get the executable path with a buffer of MAX_PATH characters. */                                     
+         DWORD result = GetModuleFileNameA(hModule, &modulePath[0], static_cast<DWORD>(modulePath.size()));             
+
+         /* As long the function returns the buffer size, it is indicating that the buffer */                           
+         /* was too small. Keep enlarging the buffer by a factor of 2 until it fits. */                                 
+         while(result == modulePath.size())                                                                             
+         {                                                                                                              
+            modulePath.resize(modulePath.size() * 2);                                                                   
+            result = GetModuleFileNameA(hModule, &modulePath[0], static_cast<DWORD>(modulePath.size()));                
+         }                                                                                                              
+
+         /* If the function returned 0, something went wrong */                                                         
+         if(result == 0)                                                                                                
+         {                                                                                                              
+            DWORD error = GetLastError();                                                                               
+            throw shared::exception::CException(std::string("CFileSystemExtension::getModulePath fails with error ")    
+               + boost::lexical_cast<std::string>(error));                                                              
+         }                                                                                                              
+
+         /* We've got the path, construct a path from it */                                                             
+         boost::filesystem::path fullPath(std::string(modulePath.begin(), modulePath.begin() + result));                
+         return fullPath.parent_path();                                                                                 
       }
    };
 
