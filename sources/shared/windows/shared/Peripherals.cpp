fef19816668f0df2332b5fa1d6e69f98dea0bcf6
Sébastien Gallou <sgallou@users.noreply.github.com>
2014-03-03 11:06:48 +0000

    - Peripherals/Windows : remove getUsedSerialPorts (doesn't work well, unuseful and plateform-specific)

--- sources/shared/windows/shared/Peripherals.cpp
+++ sources/shared/windows/shared/Peripherals.cpp
@@ -9,88 +9,6 @@
 namespace shared
 {
 
-// Defines From MsPorts.h original file
-DECLARE_HANDLE(HCOMDB);
-typedef HCOMDB *PHCOMDB;
-
-/* ReportType flags for ComDBGetCurrentPortUsage */
-#define CDB_REPORT_BITS  0x0
-#define CDB_REPORT_BYTES 0x1
-// [END] Defines From MsPorts.h original file
-
-
-class CMsPortsLibrary : protected shared::CDynamicLibrary
-{
-protected:
-   typedef LONG (CALLBACK* ComDBOpenFunctionType)(PHCOMDB);
-   ComDBOpenFunctionType m_ComDBOpen;
-
-   typedef LONG (CALLBACK* ComDBCloseFunctionType)(HCOMDB);
-   ComDBCloseFunctionType m_ComDBClose;
-
-   typedef LONG (CALLBACK* ComDBGetCurrentPortUsageFunctionType)(HCOMDB, PBYTE, DWORD, DWORD, LPDWORD);
-   ComDBGetCurrentPortUsageFunctionType m_ComDBGetCurrentPortUsage;
-
-   typedef LONG (CALLBACK* ComDBClaimNextFreePortFunctionType)(HCOMDB, LPDWORD);
-   ComDBClaimNextFreePortFunctionType m_ComDBClaimNextFreePort;
-
-public:
-   CMsPortsLibrary()
-   {
-      if (!load("MsPorts.dll"))
-         throw exception::CException("MsPorts.dll not found");
-
-      m_ComDBOpen = (ComDBOpenFunctionType) GetFunctionPointer("ComDBOpen");
-      m_ComDBClose = (ComDBCloseFunctionType) GetFunctionPointer("ComDBClose");
-      m_ComDBGetCurrentPortUsage = (ComDBGetCurrentPortUsageFunctionType) GetFunctionPointer("ComDBGetCurrentPortUsage");
-      m_ComDBClaimNextFreePort = (ComDBClaimNextFreePortFunctionType) GetFunctionPointer("ComDBClaimNextFreePort");
-
-      if(m_ComDBOpen == NULL || m_ComDBClose == NULL || m_ComDBGetCurrentPortUsage == NULL || m_ComDBClaimNextFreePort == NULL)
-      {
-         unload();
-         throw exception::CException("MsPorts.dll is invalid");
-      }
-   }
-
-   virtual ~CMsPortsLibrary()
-   {
-      unload();
-   }
-
-   LONG ComDBOpen(HCOMDB* phComDB)
-   {
-      BOOST_ASSERT(m_ComDBOpen);
-      if(m_ComDBOpen != NULL)
-         return m_ComDBOpen(phComDB);
-      return false;
-   }
-
-   LONG ComDBClose(HCOMDB phComDB)
-   {
-      BOOST_ASSERT(m_ComDBClose);
-      if(m_ComDBClose != NULL)
-         return m_ComDBClose(phComDB);
-      return false;
-   }
-
-   LONG ComDBGetCurrentPortUsage(HCOMDB hComDB, PBYTE Buffer, DWORD BufferSize, DWORD ReportType, LPDWORD MaxPortsReported)
-   {
-      BOOST_ASSERT(m_ComDBGetCurrentPortUsage);
-      if(m_ComDBGetCurrentPortUsage != NULL)
-         return m_ComDBGetCurrentPortUsage(hComDB, Buffer, BufferSize, ReportType, MaxPortsReported);
-      return false;
-   }
-
-   LONG ComDBClaimNextFreePort(HCOMDB hComDB, LPDWORD ComNumber)
-   {
-      BOOST_ASSERT(m_ComDBClaimNextFreePort);
-      if(m_ComDBClaimNextFreePort != NULL)
-         return m_ComDBClaimNextFreePort(hComDB, ComNumber);
-      return false;
-   }
-};
-
-
 CPeripherals::CPeripherals()
 {
 }
@@ -99,7 +17,7 @@ CPeripherals::~CPeripherals()
 {
 }
 
-const boost::shared_ptr<CPeripherals::SerialPortsMap> CPeripherals::getSerialPorts()
+const boost::shared_ptr<const CPeripherals::SerialPortsMap> CPeripherals::getSerialPorts()
 {
    boost::shared_ptr<SerialPortsMap> serialPorts(new SerialPortsMap);
 
@@ -109,8 +27,6 @@ const boost::shared_ptr<CPeripherals::SerialPortsMap> CPeripherals::getSerialPor
       // Unable to access registry ==> HARDWARE\\DEVICEMAP\\SERIALCOMM key does not exist ==> no serial port on this machine
       return serialPorts;
    }
-
-   boost::shared_ptr<std::set<std::string> > usedSerialPorts = getUsedSerialPorts();
 
    DWORD valueIndex = 0;
    static const DWORD serialPortNameMaxLength = 1000;    // Should be enough
@@ -125,64 +41,17 @@ const boost::shared_ptr<CPeripherals::SerialPortsMap> CPeripherals::getSerialPor
       if (dataType == REG_SZ)
       {
          std::string portName((char*)mountPoint);
-         (*serialPorts)[portName] = portName;
-         if (usedSerialPorts->find(portName) != usedSerialPorts->end())
-         {
-            // Port is in use
-            (*serialPorts)[portName] += " (in use)";
-         }
+         (*serialPorts)[portName] = std::string(serialPortName);
       }
 
+      // Increment index of key, and reset string length for next iteration
       valueIndex++;
+      serialPortNameLength = serialPortNameMaxLength;
+      mountPointLength = mountPointMaxLength;
    }
 
    RegCloseKey(serialcommKey);
    return serialPorts;
-}
-
-const boost::shared_ptr<std::set<std::string> > CPeripherals::getUsedSerialPorts()
-{
-   boost::shared_ptr<std::set<std::string> > usedSerialPorts(new std::set<std::string>);
-
-   try
-   {
-      CMsPortsLibrary msPortLibrary;
-
-      HCOMDB hComDB;
-      if (msPortLibrary.ComDBOpen(&hComDB) != ERROR_SUCCESS)
-         throw exception::CException("Unable to open COM database");
-
-      // First get the ports number
-      DWORD maxPortsReported = 0;
-      if (msPortLibrary.ComDBGetCurrentPortUsage(hComDB, NULL, 0, 0, &maxPortsReported) != ERROR_SUCCESS)
-         throw exception::CException("Unable to get serial ports number");
-
-      // Now get the serial port usage
-      unsigned char* portsInUse = new unsigned char[maxPortsReported]();
-      if (msPortLibrary.ComDBGetCurrentPortUsage(hComDB, portsInUse, maxPortsReported, CDB_REPORT_BYTES, NULL) != ERROR_SUCCESS)
-         throw exception::CException("Unable to get serial ports usage");
-
-      for (DWORD idxPort = 0 ; idxPort < maxPortsReported ; idxPort ++)
-      {
-         if (portsInUse[idxPort])
-         {
-            std::ostringstream os;
-            os << "COM" << (idxPort + 1);
-            usedSerialPorts->insert(os.str());
-         }
-      }
-
-      delete[] portsInUse;
-      msPortLibrary.ComDBClose(hComDB);
-
-      return usedSerialPorts;
-   }
-   catch (exception::CException& e)
-   {
-      YADOMS_LOG(error) << "unable to load MsPorts.dll : " << e.what();
-   }
-
-   return usedSerialPorts;
 }
 
 } // namespace shared
