c3d1091e6e54c4d37cb8eec9078e2a82605111de
jano42 <jano42@8f66d739-0145-4685-a3fe-f7fe687021a9>
2014-04-02 10:26:46 +0000

    * server : communication : using interface between DeviceRestService and XplGateway

--- trunk/sources/server/web/rest/service/DeviceRestService.h
+++ trunk/sources/server/web/rest/service/DeviceRestService.h
@@ -3,13 +3,14 @@
 #include "IRestService.h"
 #include "database/IDataProvider.h"
 #include <shared/event/EventHandler.hpp>
+#include "communication/ISendMessageEventHandler.h"
 
 namespace web { namespace rest { namespace service {
 
    class CDeviceRestService : public IRestService
    {
    public:
-      CDeviceRestService(boost::shared_ptr<database::IDataProvider> dataProvider, shared::event::CEventHandler & eventHandler, int sendMessageEventIdentifier);
+      CDeviceRestService(boost::shared_ptr<database::IDataProvider> dataProvider, communication::ISendMessageAsync & messageSender);
       virtual ~CDeviceRestService();
 
    public:
@@ -18,18 +19,49 @@ namespace web { namespace rest { namespace service {
       // [END] IRestService implementation
 
       static const std::string & getRestKeyword();
+
+   private:
+      //-----------------------------------------
+      ///\brief   get a device
+      //-----------------------------------------
       web::rest::json::CJson getOneDevice(const std::vector<std::string> & parameters, const web::rest::json::CJson & requestContent);   
+
+      //-----------------------------------------
+      ///\brief   get all devices
+      //-----------------------------------------
       web::rest::json::CJson getAllDevices(const std::vector<std::string> & parameters, const web::rest::json::CJson & requestContent);  
+
+      //-----------------------------------------
+      ///\brief   get all devices which support a given keyword
+      //-----------------------------------------
       web::rest::json::CJson getDeviceWithKeyword(const std::vector<std::string> & parameters, const web::rest::json::CJson & requestContent);   
+
+      //-----------------------------------------
+      ///\brief   get the last data for each keyword supported by a device
+      //-----------------------------------------
       web::rest::json::CJson getLastDeviceData(const std::vector<std::string> & parameters, const web::rest::json::CJson & requestContent);   
+
+      //-----------------------------------------
+      ///\brief   send a command to a device
+      //-----------------------------------------
       web::rest::json::CJson sendDeviceCommand(const std::vector<std::string> & parameters, const web::rest::json::CJson & requestContent);   
 
    private:
+      //-----------------------------------------
+      ///\brief   Data provider
+      //-----------------------------------------
       boost::shared_ptr<database::IDataProvider> m_dataProvider;
+
+      //-----------------------------------------
+      ///\brief   The rest keyword which identifies this rule
+      //-----------------------------------------
       static std::string m_restKeyword;
 
-      shared::event::CEventHandler & m_eventHandler;
-      int m_sendMessageEventIdentifier;
+      //-----------------------------------------
+      ///\brief   The send message interface
+      //-----------------------------------------
+      communication::ISendMessageAsync & m_messageSender;
+
    };
 
 
