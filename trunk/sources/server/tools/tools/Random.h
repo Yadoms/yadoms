ec9280db6f27f2e18daa0bb75250811d50fe609e
sgallou <sgallou@8f66d739-0145-4685-a3fe-f7fe687021a9>
2014-04-14 16:29:45 +0000

    - Random : fix MSVC10 build

--- trunk/sources/server/tools/tools/Random.h
+++ trunk/sources/server/tools/tools/Random.h
@@ -11,10 +11,10 @@ namespace tools
    public:
 
       //---------------------------------------------
-      ///\brief Generate a random number using a nu:ber of random bits (from 1 to 32)
+      ///\brief Generate a random number using a number of random bits (from 1 to 32)
       ///\param [in] zeroAllowed If true the value 0 may be returns, else never
       ///\return a random integer ni the range [0; (2^NbBits) -1]
-      ///\template NbBits the number of independant bits in random generator
+      ///\template NbBits the number of independent bits in random generator
       //---------------------------------------------
       template<size_t NbBits>
       static unsigned int generateRandomNumber(bool zeroAllowed)
@@ -23,14 +23,14 @@ namespace tools
          BOOST_ASSERT(NbBits <= 32);
 
          boost::random::independent_bits_engine<boost::random::mt19937, NbBits, boost::uint32_t> genbits((unsigned int)time(0));
-         boost::random::uniform_int_distribution<> m_dist(zeroAllowed?0:1, ((int)pow(2,NbBits))-1);
+         boost::random::uniform_int_distribution<> m_dist(zeroAllowed?0:1, ((int)pow(2.0,(int)NbBits))-1);//TODO : ça ne serait pas mieux un décalage ?
          return m_dist(genbits); 
       }      
 
 
    private:
       //---------------------------------------------
-      ///\brief Contructor
+      ///\brief Constructor
       //---------------------------------------------
       CRandom()
       {
