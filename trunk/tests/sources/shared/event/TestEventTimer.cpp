947ffd3d7811c547c45b09d78b389997bfb00744
sgallou <sgallou@8f66d739-0145-4685-a3fe-f7fe687021a9>
2014-04-28 12:56:13 +0000

    - shared/event/timePoint : add method to cancel running time point - shared/event/timer : add method to stop timer - Tests/shared/event : update timer/timePoint tests

--- trunk/tests/sources/shared/event/TestEventTimer.cpp
+++ trunk/tests/sources/shared/event/TestEventTimer.cpp
@@ -17,6 +17,20 @@ shared::event::CEventHandler EvtHandler;
 BOOST_AUTO_TEST_SUITE(TestEventTimer)
 
 
+// Class used to gain access to protected members of CEventTimer
+class CEventTimerAccessProtectedMembers : public shared::event::CEventTimer
+{
+public:
+   CEventTimerAccessProtectedMembers(int eventId, EPeriodicity periodicity = kOneShot,
+      const boost::posix_time::time_duration& period = boost::date_time::not_a_date_time)
+      :CEventTimer(eventId, periodicity, period) {}
+   virtual ~CEventTimerAccessProtectedMembers() {}
+   boost::posix_time::ptime getNextStopPoint() const { return CEventTimer::getNextStopPoint(); }
+   void reset() { CEventTimer::reset(); }
+   bool canBeRemoved() const { return CEventTimer::canBeRemoved(); }
+   int getId() const { return CEventTimer::getId(); }
+};
+
 // Function checking if provided times are close enough
 bool isTimeClose(const boost::posix_time::ptime& left, const boost::posix_time::ptime& right, const boost::posix_time::time_duration& tolerance)
 {
@@ -28,35 +42,51 @@ bool isTimeClose(const boost::posix_time::ptime& left, const boost::posix_time::
 //--------------------------------------------------------------
 /// \brief	    Nominal case 1
 //--------------------------------------------------------------
-
 BOOST_AUTO_TEST_CASE(Nominal1)
 {
-   const boost::posix_time::time_duration period(0,0,5);
+   const boost::posix_time::time_duration period = boost::posix_time::seconds(5);
    const int evtId = 123456;
 
-   shared::event::CEventTimer event(evtId, shared::event::CEventTimer::kOneShot, period);
+   CEventTimerAccessProtectedMembers timer(evtId, shared::event::CEventTimer::kOneShot, period);
    const boost::posix_time::ptime nextTimePoint(shared::event::now() + period);
 
-   BOOST_CHECK_EQUAL(event.getId(), evtId);
-   BOOST_CHECK(isTimeClose(event.getNextStopPoint(), nextTimePoint, boost::posix_time::millisec(1)));
-   BOOST_CHECK_EQUAL(event.canBeRemoved(), false);
+   BOOST_CHECK_EQUAL(timer.getId(), evtId);
+   BOOST_CHECK(isTimeClose(timer.getNextStopPoint(), nextTimePoint, boost::posix_time::millisec(1)));
+   BOOST_CHECK_EQUAL(timer.canBeRemoved(), false);
 
-   event.reset();
+   timer.reset();
 
-   BOOST_CHECK_EQUAL(event.getId(), evtId);
-   BOOST_CHECK_EQUAL(event.getNextStopPoint(), boost::date_time::not_a_date_time);
-   BOOST_CHECK_EQUAL(event.canBeRemoved(), true);
+   BOOST_CHECK_EQUAL(timer.getId(), evtId);
+   BOOST_CHECK_EQUAL(timer.getNextStopPoint(), boost::date_time::not_a_date_time);
+   BOOST_CHECK_EQUAL(timer.canBeRemoved(), true);
 }
 
 //--------------------------------------------------------------
-/// \brief	    Try to create CEventTimePoint with time point in the past
+/// \brief	    Create, stop and restart timer
 //--------------------------------------------------------------
-/*
-BOOST_AUTO_TEST_CASE(timePointInThePast)
+BOOST_AUTO_TEST_CASE(StopAndRestartTimer)
 {
-   const boost::posix_time::ptime timePoint(shared::event::now() - boost::posix_time::seconds(3));
+   const boost::posix_time::time_duration period = boost::posix_time::seconds(5);
    const int evtId = 123456;
-   BOOST_REQUIRE_THROW(shared::event::CEventTimePoint event(evtId, timePoint), shared::exception::CInvalidParameter);
+
+   CEventTimerAccessProtectedMembers timer(evtId, shared::event::CEventTimer::kOneShot, period);
+   boost::posix_time::ptime nextTimePoint(shared::event::now() + period);
+
+   BOOST_CHECK_EQUAL(timer.getId(), evtId);
+   BOOST_CHECK(isTimeClose(timer.getNextStopPoint(), nextTimePoint, boost::posix_time::millisec(1)));
+   BOOST_CHECK_EQUAL(timer.canBeRemoved(), false);
+
+   timer.stop();
+
+   BOOST_CHECK_EQUAL(timer.getNextStopPoint(), boost::date_time::not_a_date_time);
+   BOOST_CHECK_EQUAL(timer.canBeRemoved(), true);
+
+   nextTimePoint = shared::event::now() + period;
+   timer.start();
+
+   BOOST_CHECK_EQUAL(timer.getId(), evtId);
+   BOOST_CHECK(isTimeClose(timer.getNextStopPoint(), nextTimePoint, boost::posix_time::millisec(1)));
+   BOOST_CHECK_EQUAL(timer.canBeRemoved(), false);
 }
-*/
+
 BOOST_AUTO_TEST_SUITE_END()
