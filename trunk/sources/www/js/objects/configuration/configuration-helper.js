22ec68a66012345f3f8be396cd635c0134741e1f
lgm42 <lgm42@8f66d739-0145-4685-a3fe-f7fe687021a9>
2014-04-30 13:39:12 +0000

    GUI : moment.js lib has been tomanage time display and UTC DateTimeFormatter class has been created to permit parse of ISO UTC date (from DB) and display date time using custom format string and text timezone (like "Europe/Paris") removed commented code

--- trunk/sources/www/js/objects/configuration/configuration-helper.js
+++ trunk/sources/www/js/objects/configuration/configuration-helper.js
@@ -2,34 +2,7 @@
  * Created by nicolasHILAIRE on 06/03/14.
  */
 function ConfigurationHelper(){}
-/*
-ConfigurationHelper.createControlGroup = function(parameterHandler, controlToInsert) {
 
-   assert(parameterHandler !== undefined, "parameterHandler must be defined");
-   assert(controlToInsert !== undefined, "controlToInsert must be defined");
-
-   var s =
-      "<div class=\"control-group\">" +
-         "<label for=\"" + parameterHandler.paramName + "\" data-i18n=\"" + parameterHandler.i18nContext + parameterHandler.paramName + ".name\" class=\"control-label col-sm-5\">" + parameterHandler.name + "</label>" +
-         "<button id=\"" + parameterHandler.uuid + "-help\" class=\"col-sm-1\" type=\"button\" class=\"btn btn-default\" data-container=\"body\">" +
-            "<i class=\"fa fa-question\"></i>" +
-         "</button>" +
-         "<div class=\"controls col-sm-6\">" +
-            controlToInsert +
-         "</div>" +
-      "</div>" +
-      "<script>" +
-         "$(\"#" + parameterHandler.uuid + "\").popover({\"placement\" : \"right\", \"trigger\": \"manual\"});\n" +
-         "$(\"button#" + parameterHandler.uuid + "-help\").click(function () {\n" +
-            "$(\"#" + parameterHandler.uuid + "\").popover(\"toggle\");\n" +
-            "setTimeout(function () {\n" +
-               "$(\"#" + parameterHandler.uuid + "\").popover(\"hide\"); \n" +
-            "}, 5000);\n" +
-         "});\n" +
-      "</script>";
-
-   return s;
-}*/
 ConfigurationHelper.createControlGroup = function (parameterHandler, controlToInsert, placeInsideLabel, classOfControlGroup) {
 
    assert(parameterHandler !== undefined, "parameterHandler must be defined");
