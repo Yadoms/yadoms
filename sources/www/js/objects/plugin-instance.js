5cb3eb372fd436977a5abd60b135d971d5a65213
Nicolas HILAIRE <nicolas.hilaire@gmail.com>
2014-05-19 12:21:17 +0000

    GUI : plugin dashboard in progress

--- sources/www/js/objects/plugin-instance.js
+++ sources/www/js/objects/plugin-instance.js
@@ -4,16 +4,83 @@
  * Creates an instance of Page
  * @constructor
  */
-function PluginInstance(id, address, protocol, hardwareIdentifier, name) {
+function PluginInstance(id, name, pluginName, configuration, enabled) {
    assert(id !== undefined, "id of a pluginInstance must be defined");
-   assert(address !== undefined, "address of a pluginInstance must be defined");
-   assert(protocol !== undefined, "protocol of a pluginInstance must be defined");
-   assert(hardwareIdentifier !== undefined, "hardwareIdentifier of a pluginInstance must be defined");
    assert(name !== undefined, "name of a pluginInstance must be defined");
+   assert(pluginName !== undefined, "pluginName of a pluginInstance must be defined");
+   assert(configuration !== undefined, "configuration of a pluginInstance must be defined");
+   assert(enabled !== undefined, "enabled of a pluginInstance must be defined");
 
    this.id = id;
-   this.address = address;
-   this.protocol = protocol;
-   this.hardwareIdentifier = hardwareIdentifier;
    this.name = name;
+   this.pluginName = pluginName;
+   this.configuration = configuration;
+   this.enabled = enabled;
+}
+
+PluginInstance.prototype.getStatus = function(callback) {
+   //we ask for the status of current pluginInstance
+   var self = this;
+   $.getJSON("/rest/plugin/" + this.id + "/status/")
+      .done(function(data) {
+         //we parse the json answer
+         //the default answer is false
+         var result = false;
+         if (data.result != "true")
+         {
+            notifyError($.t("objects.pluginInstance.errorGettingStatus", {pluginName : self.name}), JSON.stringify(data));
+            return;
+         }
+         if (parseBool(data.data.running))
+         {
+            result = true;
+         }
+         if (!isNullOrUndefined(callback))
+            callback(result);
+         return result;
+      })
+      .fail(function() { notifyError($.t("objects.pluginInstance.errorGettingStatus", {pluginName : self.name}), JSON.stringify(data)); });
+};
+
+PluginInstance.prototype.start = function(callback) {
+   var self = this;
+   $.ajax({
+      type: "PUT",
+      url: "/rest/plugin/" + this.id + "/start",
+      contentType: "application/json; charset=utf-8",
+      dataType: "json"
+   })
+      .done(function(data) {
+         //we parse the json answer
+         if (data.result != "true")
+         {
+            notifyError($.t("objects.pluginInstance.errorStarting", {pluginName : self.name}), JSON.stringify(data));
+            return;
+         }
+         if (!isNullOrUndefined(callback))
+            callback();
+      })
+      .fail(function() {notifyError($.t("objects.pluginInstance.errorStarting", {pluginName : self.name}), JSON.stringify(data));});
+}
+
+PluginInstance.prototype.start = function() {
+   var self = this;
+   $.ajax({
+      type: "PUT",
+      url: "/rest/plugin/" + this.id + "/stop",
+      contentType: "application/json; charset=utf-8",
+      dataType: "json"
+   })
+      .done(function(data) {
+         //we parse the json answer
+         if (data.result != "true")
+         {
+            notifyError($.t("objects.pluginInstance.errorStopping", {pluginName : self.name}), JSON.stringify(data));
+            return;
+         }
+
+         if (!isNullOrUndefined(callback))
+            callback();
+      })
+      .fail(function() {notifyError($.t("objects.pluginInstance.errorStopping", {pluginName : self.name}), JSON.stringify(data));});
 }
