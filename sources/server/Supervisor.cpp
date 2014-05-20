7ce0711cf5bcb0fac7a8ce5b6b54e0d9bf0b66f6
Nicolas HILAIRE <nicolas.hilaire@gmail.com>
2014-05-20 12:53:04 +0000

    GUI : plugin dashboard in progress. Add / remove / start / stop plugin partially work. remains pb in server side, jqbootstrap validation in form and other error case in GUI added a generic confirmation form to use in all confirmations

--- sources/server/Supervisor.cpp
+++ sources/server/Supervisor.cpp
@@ -77,6 +77,7 @@ void CSupervisor::doWork()
 
       boost::shared_ptr<web::IWebServer> webServer(new web::webem::CWebServer(webServerIp, webServerPort, webServerPath, "/rest/"));
       webServer->configureAlias("widget", webServerWidgetPath);
+      webServer->configureAlias("plugin", m_startupOptions.getPluginsPath());
       boost::shared_ptr<web::IRestHandler> restHanlder = webServer->getRestHandler();
       if(restHanlder.get() != NULL)
       {
