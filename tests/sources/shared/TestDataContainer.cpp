82289e104c0e7e1eb0c96f91d8b58a9685afbcb6
Jean-Michel DECORET <jano42@users.noreply.github.com>
2014-06-26 14:57:49 +0000

    * test : remove plugin/configuration tests * test : shared::CDataContainer tests

---
+++ tests/sources/shared/TestDataContainer.cpp
@@ -0,0 +1,127 @@
+#include "stdafx.h"
+#include <boost/test/unit_test.hpp>
+
+// Includes needed to compile tested classes
+#include "../../../../sources/shared/shared/DataContainer.h"
+
+
+BOOST_AUTO_TEST_SUITE(TestDataContainer)
+
+
+/*
+enum EEnumType
+{
+   kEnumValue1 = 7,
+   kEnumValue2 = 12,
+   kEnumValue3
+};
+
+static const shared::plugin::configuration::CConfiguration::EnumValuesNames EEnumTypeNames = boost::assign::map_list_of
+   ("EnumValue1", kEnumValue1)
+   ("EnumValue2", kEnumValue2)
+   ("EnumValue3", kEnumValue3);
+*/
+
+BOOST_AUTO_TEST_CASE(SimpleContainer)
+{
+   shared::CDataContainer dc;
+
+   //ensure en new container is empty
+   BOOST_CHECK_EQUAL(dc.empty(), true);
+   
+   dc.set<bool>("BoolParameter", true);
+   dc.set<double>("DecimalParameter", 18.4);
+   //dc.set<std::string>("EnumParameter", kEnumValue1);
+   dc.set<int>("IntParameter", 42);
+   dc.set<std::string>("Serial port", "tty0");
+   dc.set<std::string>("StringParameter", "Yadoms is so powerful !");
+   dc.set<int>("MySection.SubIntParameter", 123);
+   dc.set<std::string>("MySection.SubStringParameter", "Just a string parameter in the sub-section");
+
+   BOOST_CHECK_EQUAL(dc.get<bool>("BoolParameter"), true);
+   BOOST_CHECK_EQUAL(dc.get<double>("DecimalParameter"), 18.4);
+   //BOOST_CHECK_EQUAL(dc.getEnumValue<EEnumType>("EnumParameter", EEnumTypeNames), kEnumValue1);
+   BOOST_CHECK_EQUAL(dc.get<int>("IntParameter"), 42);
+   BOOST_CHECK_EQUAL(dc.get<std::string>("Serial port"), "tty0");
+   BOOST_CHECK_EQUAL(dc.get<std::string>("StringParameter"), "Yadoms is so powerful !");
+   BOOST_CHECK_EQUAL(dc.get<int>("MySection.SubIntParameter"), 123);
+   BOOST_CHECK_EQUAL(dc.get<std::string>("MySection.SubStringParameter"), "Just a string parameter in the sub-section");
+
+
+   shared::CDataContainer test;
+   test.set<int>("int", 5);
+   test.set<double>("double", 4.0);
+   test.set<std::string>("string", "plouf");
+
+   BOOST_CHECK_EQUAL(test.get<int>("int"), 5);
+   BOOST_CHECK_EQUAL(test.get<double>("double"),4.0);
+   BOOST_CHECK_EQUAL(test.get<std::string>("string"), "plouf");
+   
+
+   shared::CDataContainer subContainer;
+   subContainer.set<int>("int4", 6);
+   subContainer.set<double>("double1", 8.0);
+   subContainer.set<std::string>("string2", "plouf2");
+
+   test.set("config1", subContainer);
+
+   BOOST_CHECK_EQUAL(test.get<double>("config1.double1"), 8.0);
+}
+
+BOOST_AUTO_TEST_CASE(CollectionContainer)
+{
+   shared::CDataContainer test;
+
+   std::vector<int> vi;
+   for (int i = 0; i<10; ++i)
+      vi.push_back(i);
+   test.set < std::vector<int> >("vectorint", vi);
+   std::vector<int> vi2 = test.get<std::vector<int>>("vectorint");
+   BOOST_CHECK_EQUAL_COLLECTIONS(vi.begin(), vi.end(), vi2.begin(), vi2.end());
+
+
+   std::vector<double> vd;
+   for (int i = 0; i<10; ++i)
+      vd.push_back(i * 3.0);
+   test.set< std::vector<double> >("vectordouble", vd);
+   std::vector<double> vd2 = test.get< std::vector<double> >("vectordouble");
+   BOOST_CHECK_EQUAL_COLLECTIONS(vd.begin(), vd.end(), vd2.begin(), vd2.end());
+}
+
+BOOST_AUTO_TEST_CASE(Serialization)
+{
+   const std::string defaultConf("{"
+      "\"BoolParameter\": \"true\","
+      "\"DecimalParameter\": \"18.4\","
+      "\"EnumParameter\": \"EnumValue1\","
+      "\"IntParameter\": \"42\","
+      "\"Serial port\": \"tty0\","
+      "\"StringParameter\": \"Yadoms is so powerful !\","
+      "\"MySection\": {"
+      "\"SubIntParameter\": \"123\","
+      "\"SubStringParameter\": \"Just a string parameter in the sub-section\""
+      "}"
+      "}");
+
+   shared::CDataContainer cfg(defaultConf);
+
+   BOOST_CHECK_EQUAL(cfg.get<bool>("BoolParameter"), true);
+   BOOST_CHECK_EQUAL(cfg.get<double>("DecimalParameter"), 18.4);
+   BOOST_CHECK_EQUAL(cfg.get<int>("IntParameter"), 42);
+   BOOST_CHECK_EQUAL(cfg.get<std::string>("Serial port"), "tty0");
+   BOOST_CHECK_EQUAL(cfg.get<std::string>("StringParameter"), "Yadoms is so powerful !");
+   BOOST_CHECK_EQUAL(cfg.get<int>("MySection.SubIntParameter"), 123);
+   BOOST_CHECK_EQUAL(cfg.get<std::string>("MySection.SubStringParameter"), "Just a string parameter in the sub-section");
+
+   //check that serialization match original values
+   //just remove space, \n, \t and \r from strings
+   std::string str = cfg.serialize();
+   std::string str2 = defaultConf;
+
+   str.erase(std::remove_if(str.begin(), str.end(), boost::is_any_of(" \t\n\r")), str.end());
+   str2.erase(std::remove_if(str2.begin(), str2.end(), boost::is_any_of(" \t\n\r")), str2.end());
+
+   BOOST_CHECK_EQUAL(str, str2);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
