eb1431634307e67a24f4fea09fc8748ee000909e
jmb42 <jean-michel@berhault.org>
2014-06-30 18:08:28 +0000

    - Wrong capacity name corrected

--- sources/plugins/WindowsSystemInformation/WindowsSystemCPULoad.cpp
+++ sources/plugins/WindowsSystemInformation/WindowsSystemCPULoad.cpp
@@ -76,7 +76,7 @@ void CWindowsSystemCPULoad::historizeData(boost::shared_ptr<yApi::IYadomsApi> co
 {
    BOOST_ASSERT_MSG(context, "context must be defined");
    
-   context->historizeData(m_deviceId, "LoadWindowsCPU"  , m_CPULoad);
+   context->historizeData(m_deviceId, "WindowsCPULoad"  , m_CPULoad);
 }
 
 double CWindowsSystemCPULoad::getValue() /*const*/
