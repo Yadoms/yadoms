a5349232a99a68cbe74d664aa5503c1686de368e
jmb42 <jmb42@8f66d739-0145-4685-a3fe-f7fe687021a9>
2014-07-19 18:16:54 +0000

    ApplicationLoader : - Compilation for Linux and RaspBerry Pi.

--- trunk/sources/server/tools/linux/tools/ApplicationLoader.cpp
+++ trunk/sources/server/tools/linux/tools/ApplicationLoader.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.h"
 #include "ApplicationLoader.h"
 
+#include "../../tools/IApplication.h"
+
 namespace tools {
 
    int CApplicationLoader::run(const IApplication & application, int argc, char ** argv)
