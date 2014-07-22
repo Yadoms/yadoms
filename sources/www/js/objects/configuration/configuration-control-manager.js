f66fd690828d0d6328c5c4bf865f6ef0094a36c0
Nicolas HILAIRE <nicolas.hilaire@gmail.com>
2014-07-22 12:26:27 +0000

    GUI : system with no configuration are now autoconfigured using navigator language and navigator timezone system configuration in dashboard permit now to change language add jsTimezoneDetect lib add object to manage configuration easily

---
+++ sources/www/js/objects/configuration/configuration-control-manager.js
@@ -0,0 +1,63 @@
+/**
+ * Created by Nicolas on 28/02/14.
+ */
+
+/**
+ * Creates an instance of LazyLoaderManager
+ * @param objectToConfigure
+ * @param $domContainer
+ * @constructor
+ */
+function ConfigurationControlManager(objectToConfigure, i18nNamespace, $domContainer) {
+   assert(objectToConfigure !== undefined, "objectToConfigure must contain widget or plugin object");
+   assert(i18nNamespace !== undefined, "i18nNamespace must contain widget or plugin object");
+   assert($domContainer !== undefined, "$domContainer must be defined");
+
+   this.objectToConfigure = objectToConfigure;
+   this.configurationSchema = objectToConfigure.package.configurationSchema;
+   this.configurationValues = objectToConfigure.configuration;
+   this.configurationHandlers = [];
+
+   var self = this;
+
+   //we build the i18n context
+   var i18nContext = i18nNamespace + ":configurationSchema.";
+
+   //for each key in package
+   $.each(self.configurationSchema, function (key, value) {
+      var currentValue = self.configurationValues[key];
+      var handler = ConfigurationHelper.createParameterHandler(self.objectToConfigure, i18nContext, key, value, currentValue);
+      self.configurationHandlers.push(handler);
+   });
+
+   $domContainer.append(this.getDOMObject());
+}
+
+/**
+ * Get the DOM Object to insert
+ * @returns {string}
+ */
+ConfigurationControlManager.prototype.getDOMObject = function () {
+   var result = "";
+   $.each(this.configurationHandlers, function (key, value) {
+      result += value.getDOMObject();
+      result += "\n";
+   });
+
+   result += "";
+   return result;
+};
+
+/**
+ * Get the current configuration in the form
+ * @returns {object}
+ */
+ConfigurationControlManager.prototype.getCurrentConfiguration = function () {
+   //we update configurationValues with content of DOM
+   this.configurationValues = {};
+   var self = this;
+   $.each(this.configurationHandlers, function (key, value) {
+      self.configurationValues[value.getParamName()] = value.getCurrentConfiguration();
+   });
+   return this.configurationValues;
+};
