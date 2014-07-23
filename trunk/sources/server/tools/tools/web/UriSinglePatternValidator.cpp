d70b1ba538f5b5ccb36b63a2acc1125a4cd66acf
jano42 <jano42@8f66d739-0145-4685-a3fe-f7fe687021a9>
2014-07-23 06:52:34 +0000

    * fix UriSinglePatternValidator.cpp regex * update tests

--- trunk/sources/server/tools/tools/web/UriSinglePatternValidator.cpp
+++ trunk/sources/server/tools/tools/web/UriSinglePatternValidator.cpp
@@ -6,7 +6,8 @@ namespace tools { namespace web {
 
       bool CUriSinglePatternValidator::isValid(const std::string & uriPart)
       {
-         boost::regex validator("[a-zA-Z0-9-_@\.!\*#]{1,}");
+         //\_\@\.\!\*\#
+         boost::regex validator("^[a-zA-Z0-9_.@#-]{1,}$");
          boost::match_results<std::string::const_iterator> results;
          return boost::regex_match(uriPart, results, validator);
       }
