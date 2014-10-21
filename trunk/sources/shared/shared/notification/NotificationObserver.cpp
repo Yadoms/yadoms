561614c13873887876747a337f1251a5ee85cbdc
jano42 <jano42@8f66d739-0145-4685-a3fe-f7fe687021a9>
2014-10-21 13:04:44 +0000

    * notification center : add some checks before waiting for events (in case an eventhadler is already waiting)

--- trunk/sources/shared/shared/notification/NotificationObserver.cpp
+++ trunk/sources/shared/shared/notification/NotificationObserver.cpp
@@ -4,7 +4,7 @@
 namespace shared { namespace notification{
 
    CNotificationObserver::CNotificationObserver()
-      :m_EventHandler(new shared::event::CEventHandler())
+      :m_EventHandler(new shared::event::CEventHandler()), m_isWaiting(false)
    {
    }
 
@@ -15,8 +15,23 @@ namespace shared { namespace notification{
 
    int CNotificationObserver::waitForNotifications(const boost::posix_time::time_duration& timeout)
    {
-      return m_EventHandler->waitForEvents(timeout);
+      m_isWaiting = true;
+      try
+      {
+         int result = m_EventHandler->waitForEvents(timeout);
+         m_isWaiting = false;
+         return result;
+      }
+      catch (...)
+      {
+         m_isWaiting = false;
+         throw;
+      }
    }
 
+   bool CNotificationObserver::isWaiting() const
+   {
+      return m_isWaiting;
+   }
 } //namespace notification
 } //namespace shared
