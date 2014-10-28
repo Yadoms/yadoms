a170d529141162909f2c736169322e1374a0ee6a
jano42 <jano42@8f66d739-0145-4685-a3fe-f7fe687021a9>
2014-10-28 09:49:35 +0000

    * www : widget engine : on acquisition notification : adapt code to new data format from server * www : acquisition : remove the acquisition id which do not exist anymore

--- trunk/sources/www/js/objects/acquisition-manager.js
+++ trunk/sources/www/js/objects/acquisition-manager.js
@@ -10,10 +10,9 @@ function AcquisitionManager(){}
 
 AcquisitionManager.factory = function(json) {
    assert(!isNullOrUndefined(json), "json must be defined");
-   assert(!isNullOrUndefined(json.id), "json.id must be defined");
    assert(!isNullOrUndefined(json.date), "json.date must be defined");
    assert(!isNullOrUndefined(json.keywordId), "json.keywordId must be defined");
    assert(!isNullOrUndefined(json.value), "json.value must be defined");
 
-   return new Acquisition(json.id, json.date, json.keywordId, json.value);
+   return new Acquisition(json.date, json.keywordId, json.value);
 };
