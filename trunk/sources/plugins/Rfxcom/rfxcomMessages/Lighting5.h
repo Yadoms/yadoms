61cef149e66ac57d6368b0cdfc5468ab7f0b1d4d
sgallou <sgallou@8f66d739-0145-4685-a3fe-f7fe687021a9>
2014-10-31 15:37:20 +0000

    - plugin/rfxcom : implement Lighting5 protocol

---
+++ trunk/sources/plugins/Rfxcom/rfxcomMessages/Lighting5.h
@@ -0,0 +1,127 @@
+#pragma once
+
+#include "IRfxcomMessage.h"
+#include "RFXtrxHelpers.h"
+#include <shared/plugin/yadomsApi/IYadomsApi.h>
+#include <shared/DataContainer.h>
+
+namespace yApi = shared::plugin::yadomsApi;
+
+namespace rfxcomMessages
+{
+   //--------------------------------------------------------------
+   /// \brief	The Lightning5 protocol support
+   //--------------------------------------------------------------
+   class CLighting5 : public IRfxcomMessage
+   {
+   public:
+      //--------------------------------------------------------------
+      /// \brief	                        Constructor
+      /// \param[in] context              Yadoms APi context
+      /// \param[in] command              The command
+      /// \param[in] deviceParameters     The device parameters
+      /// \throw                          shared::exception::CInvalidParameter if fail to interpret command
+      /// \note                           Use this constructor for command (to build RFXCom message)
+      //--------------------------------------------------------------
+      CLighting5(boost::shared_ptr<yApi::IYadomsApi> context, const shared::CDataContainer& command, const shared::CDataContainer& deviceParameters);
+
+      //--------------------------------------------------------------
+      /// \brief	                        Constructor
+      /// \param[in] context              Yadoms APi context
+      /// \param[in] subType              Device subType
+      /// \param[in] manuallyDeviceCreationConfiguration The device concfiguration
+      /// \throw                          shared::exception::CInvalidParameter or shared::exception::COutOfRange if fail to interpret configuration
+      /// \note                           Use this constructor for manually device creation
+      //--------------------------------------------------------------
+      CLighting5(boost::shared_ptr<yApi::IYadomsApi> context, unsigned char subType, const shared::CDataContainer& manuallyDeviceCreationConfiguration);
+
+      //--------------------------------------------------------------
+      /// \brief	                        Constructor
+      /// \param[in] context              Yadoms APi context
+      /// \param[in] rbuf                 The received buffer
+      /// \param[in] seqNumberProvider    The sequence number provider
+      /// \note                           Use this constructor for received messages (to historize received data to Yadoms)
+      /// \throw                          shared::exception::CInvalidParameter
+      //--------------------------------------------------------------
+      CLighting5(boost::shared_ptr<yApi::IYadomsApi> context, const RBUF& rbuf, boost::shared_ptr<const ISequenceNumberProvider> seqNumberProvider);
+
+      //--------------------------------------------------------------
+      /// \brief	Destructor
+      //--------------------------------------------------------------
+      virtual ~CLighting5();
+
+      // IRfxcomMessage implementation
+      virtual const shared::communication::CByteBuffer encode(boost::shared_ptr<ISequenceNumberProvider> seqNumberProvider) const;
+      virtual void historizeData(boost::shared_ptr<yApi::IYadomsApi> context) const;
+      // [END] IRfxcomMessage implementation
+
+   protected:
+      //--------------------------------------------------------------
+      /// \brief	Global initialization method
+      /// \param[in] context              Yadoms APi context
+      //--------------------------------------------------------------
+      void Init(boost::shared_ptr<yApi::IYadomsApi> context);
+
+      //--------------------------------------------------------------
+      /// \brief	                        Build the device name
+      //--------------------------------------------------------------
+      void buildDeviceName();
+
+      //--------------------------------------------------------------
+      /// \brief	                        Build the sensor model
+      //--------------------------------------------------------------
+      void buildDeviceModel();
+
+      //--------------------------------------------------------------
+      /// \brief	                        Convert Yadoms state command to protocol value
+      /// \param[out] protocolCmnd        The command known by the protocol
+      /// \param[out] protocolLevel       The level known by the protocol
+      //--------------------------------------------------------------
+      void toProtocolState(unsigned char& protocolCmnd, unsigned char& protocolLevel) const;
+
+      //--------------------------------------------------------------
+      /// \brief	                        Convert protocol value to Yadoms state
+      /// \param[in] protocolCmnd         The command known by the protocol
+      /// \param[in] protocolLevel        The level known by the protocol
+      /// \return                         The Yadoms compliant value
+      /// \throw                          shared::exception::CInvalidParameter if fails to interpret command
+      //--------------------------------------------------------------
+      int fromProtocolState(unsigned char protocolCmnd, unsigned char protocolLevel) const;
+
+   private:
+      //--------------------------------------------------------------
+      /// \brief	The device sub-type
+      //--------------------------------------------------------------
+      unsigned char m_subType;
+
+      //--------------------------------------------------------------
+      /// \brief	The device id
+      //--------------------------------------------------------------
+      unsigned int m_id;
+
+      //--------------------------------------------------------------
+      /// \brief	The device unit code
+      //--------------------------------------------------------------
+      unsigned char m_unitCode;
+
+      //--------------------------------------------------------------
+      /// \brief	The device name
+      //--------------------------------------------------------------
+      std::string m_deviceName;
+
+      //--------------------------------------------------------------
+      /// \brief	The device model
+      //--------------------------------------------------------------
+      std::string m_deviceModel;
+
+      //--------------------------------------------------------------
+      /// \brief	The keyword associated with state
+      //--------------------------------------------------------------
+      yApi::historization::CSwitch m_state;
+
+      //--------------------------------------------------------------
+      /// \brief	The keyword associated with rssi
+      //--------------------------------------------------------------
+      yApi::historization::CRssi m_rssi;
+   };
+} // namespace rfxcomMessages
