0435c05b1e997e6020aca5eaadc772d944633ed9
Sébastien Gallou <sgallou@users.noreply.github.com>
2014-11-05 13:44:50 +0000

    - plugin/rfxcom : add new protocols support : Thermostat1 (Digimax, Digimax-short), Thermostat2 (HE105, RTS10, RFS10, TLX1206)

---
+++ sources/plugins/Rfxcom/rfxcomMessages/Thermostat1.cpp
@@ -0,0 +1,113 @@
+#include "stdafx.h"
+#include "Thermostat1.h"
+#include <shared/plugin/yadomsApi/StandardCapacities.h>
+#include <shared/exception/InvalidParameter.hpp>
+#include "Thermostat1Digimax.h"
+#include "Thermostat1DigimaxShort.h"
+// Shortcut to yadomsApi namespace
+namespace yApi = shared::plugin::yadomsApi;
+
+namespace rfxcomMessages
+{
+
+CThermostat1::CThermostat1(boost::shared_ptr<yApi::IYadomsApi> context, const shared::CDataContainer& command, const shared::CDataContainer& deviceDetails)
+   :m_rssi("rssi")
+{
+   m_rssi.set(0);
+
+   m_subType = deviceDetails.get<unsigned char>("subType");
+   m_id = deviceDetails.get<unsigned int>("id");
+
+   Init(context);
+   m_subTypeManager->set(command);
+}
+
+CThermostat1::CThermostat1(boost::shared_ptr<yApi::IYadomsApi> context, unsigned char subType, const shared::CDataContainer& manuallyDeviceCreationConfiguration)
+   :m_rssi("rssi")
+{
+   m_rssi.set(0);
+
+   m_subType = subType;
+
+   m_id = manuallyDeviceCreationConfiguration.get<unsigned int>("id");
+
+   Init(context);
+   m_subTypeManager->default();
+}
+
+CThermostat1::CThermostat1(boost::shared_ptr<yApi::IYadomsApi> context, const RBUF& rbuf, boost::shared_ptr<const ISequenceNumberProvider> seqNumberProvider)
+   :m_rssi("rssi")
+{
+   CheckReceivedMessage(rbuf, pTypeThermostat1, GET_RBUF_STRUCT_SIZE(THERMOSTAT1), DONT_CHECK_SEQUENCE_NUMBER);
+
+   m_subType = rbuf.THERMOSTAT1.subtype;
+   m_id = rbuf.THERMOSTAT1.id1 << 8 | rbuf.THERMOSTAT1.id2;
+   m_subTypeManager->setFromProtocolState(rbuf);
+   m_rssi.set(NormalizeRssiLevel(rbuf.THERMOSTAT1.rssi));
+
+   Init(context);
+}
+
+CThermostat1::~CThermostat1()
+{
+}
+
+void CThermostat1::Init(boost::shared_ptr<yApi::IYadomsApi> context)
+{
+   switch(m_subType)
+   {
+   case sTypeAC : m_subTypeManager.reset(new CThermostat1Digimax()); break;
+   case sTypeHEU : m_subTypeManager.reset(new CThermostat1DigimaxShort()); break;
+   default:
+      throw shared::exception::COutOfRange("Manually device creation : subType is not supported");
+   }
+
+   // Build device description
+   buildDeviceName();
+
+   // Create device and keywords if needed
+   if (!context->deviceExists(m_deviceName))
+   {
+      shared::CDataContainer details;
+      details.set("type", pTypeThermostat1);
+      details.set("subType", m_subType);
+      details.set("id", m_id);
+      context->declareDevice(m_deviceName, m_subTypeManager->getModel(), details.serialize());
+
+      m_subTypeManager->declare(context, m_deviceName);
+      context->declareKeyword(m_deviceName, m_rssi);
+   }
+}
+
+boost::shared_ptr<std::queue<const shared::communication::CByteBuffer> > CThermostat1::encode(boost::shared_ptr<ISequenceNumberProvider> seqNumberProvider) const
+{
+   RBUF rbuf;
+   MEMCLEAR(rbuf.THERMOSTAT1);
+
+   rbuf.THERMOSTAT1.packetlength = ENCODE_PACKET_LENGTH(THERMOSTAT1);
+   rbuf.THERMOSTAT1.packettype = pTypeThermostat1;
+   rbuf.THERMOSTAT1.subtype = m_subType;
+   rbuf.THERMOSTAT1.seqnbr = seqNumberProvider->next();
+   rbuf.THERMOSTAT1.id1 = (unsigned char) (0xFF & (m_id >> 8));
+   rbuf.THERMOSTAT1.id2 = (unsigned char) (0xFF & m_id);
+   m_subTypeManager->toProtocolState(rbuf);
+   rbuf.THERMOSTAT1.rssi = 0;
+   rbuf.THERMOSTAT1.filler = 0;
+
+   return toBufferQueue(rbuf, GET_RBUF_STRUCT_SIZE(THERMOSTAT1));
+}
+
+void CThermostat1::historizeData(boost::shared_ptr<yApi::IYadomsApi> context) const
+{
+   m_subTypeManager->historize(context, m_deviceName);
+   context->historizeData(m_deviceName, m_rssi);
+}
+
+void CThermostat1::buildDeviceName()
+{
+   std::ostringstream ssdeviceName;
+   ssdeviceName << (unsigned int)m_subType << "." << (unsigned int)m_id;
+   m_deviceName = ssdeviceName.str();
+}
+
+} // namespace rfxcomMessages
---
+++ sources/plugins/Rfxcom/rfxcomMessages/Thermostat2.h
@@ -0,0 +1,120 @@
+#pragma once
+
+#include "IRfxcomMessage.h"
+#include "RFXtrxHelpers.h"
+#include <shared/plugin/yadomsApi/IYadomsApi.h>
+
+namespace yApi = shared::plugin::yadomsApi;
+
+namespace rfxcomMessages
+{
+   //--------------------------------------------------------------
+   /// \brief	The Thermostat2 protocol support
+   //--------------------------------------------------------------
+   class CThermostat2 : public IRfxcomMessage
+   {
+   public:
+      //--------------------------------------------------------------
+      /// \brief	                        Constructor
+      /// \param[in] context              Yadoms APi context
+      /// \param[in] command              The command
+      /// \param[in] deviceDetails        The device parameters
+      /// \throw                          shared::exception::CInvalidParameter if fail to interpret command
+      /// \note                           Use this constructor for command (to build RFXCom message)
+      //--------------------------------------------------------------
+      CThermostat2(boost::shared_ptr<yApi::IYadomsApi> context, const shared::CDataContainer& command, const shared::CDataContainer& deviceDetails);
+
+      //--------------------------------------------------------------
+      /// \brief	                        Constructor
+      /// \param[in] context              Yadoms APi context
+      /// \param[in] subType              Device subType
+      /// \param[in] manuallyDeviceCreationConfiguration The device concfiguration
+      /// \throw                          shared::exception::CInvalidParameter or shared::exception::COutOfRange if fail to interpret configuration
+      /// \note                           Use this constructor for manually device creation
+      //--------------------------------------------------------------
+      CThermostat2(boost::shared_ptr<yApi::IYadomsApi> context, unsigned char subType, const shared::CDataContainer& manuallyDeviceCreationConfiguration);
+
+      //--------------------------------------------------------------
+      /// \brief	                        Constructor
+      /// \param[in] context              Yadoms APi context
+      /// \param[in] rbuf                 The received buffer
+      /// \param[in] seqNumberProvider    The sequence number provider
+      /// \note                           Use this constructor for received messages (to historize received data to Yadoms)
+      /// \throw                          shared::exception::CInvalidParameter
+      //--------------------------------------------------------------
+      CThermostat2(boost::shared_ptr<yApi::IYadomsApi> context, const RBUF& rbuf, boost::shared_ptr<const ISequenceNumberProvider> seqNumberProvider);
+
+      //--------------------------------------------------------------
+      /// \brief	Destructor
+      //--------------------------------------------------------------
+      virtual ~CThermostat2();
+
+      // IRfxcomMessage implementation
+      virtual boost::shared_ptr<std::queue<const shared::communication::CByteBuffer> > encode(boost::shared_ptr<ISequenceNumberProvider> seqNumberProvider) const;
+      virtual void historizeData(boost::shared_ptr<yApi::IYadomsApi> context) const;
+      // [END] IRfxcomMessage implementation
+      
+   protected:
+      //--------------------------------------------------------------
+      /// \brief	Global initialization method
+      /// \param[in] context              Yadoms APi context
+      //--------------------------------------------------------------
+      void Init(boost::shared_ptr<yApi::IYadomsApi> context);
+
+      //--------------------------------------------------------------
+      /// \brief	                        Build the device name
+      //--------------------------------------------------------------
+      void buildDeviceName();
+
+      //--------------------------------------------------------------
+      /// \brief	                        Build the sensor model
+      //--------------------------------------------------------------
+      void buildDeviceModel();
+
+      //--------------------------------------------------------------
+      /// \brief	                        Convert Yadoms state command to protocol value
+      /// \param[in] switchState          The state from Yadoms
+      /// \return                         The value known by the protocol
+      //--------------------------------------------------------------
+      static unsigned char toProtocolState(const yApi::historization::CSwitch& switchState);
+      
+      //--------------------------------------------------------------
+      /// \brief	                        Convert protocol value to Yadoms state
+      /// \param[in] protocolState        The value known by the protocol
+      /// \return                         The Yadoms compliant value
+      /// \throw                          shared::exception::CInvalidParameter if fails to interpret command
+      //--------------------------------------------------------------
+      static bool fromProtocolState(unsigned char protocolState);
+
+   private:
+      //--------------------------------------------------------------
+      /// \brief	The device sub-type
+      //--------------------------------------------------------------
+      unsigned char m_subType;
+
+      //--------------------------------------------------------------
+      /// \brief	The device unit code
+      //--------------------------------------------------------------
+      unsigned int m_unitCode;
+
+      //--------------------------------------------------------------
+      /// \brief	The device name
+      //--------------------------------------------------------------
+      std::string m_deviceName;
+
+      //--------------------------------------------------------------
+      /// \brief	The device model
+      //--------------------------------------------------------------
+      std::string m_deviceModel;
+
+      //--------------------------------------------------------------
+      /// \brief	The keyword associated with state
+      //--------------------------------------------------------------
+      yApi::historization::CSwitch m_state;
+
+      //--------------------------------------------------------------
+      /// \brief	The keyword associated with rssi
+      //--------------------------------------------------------------
+      yApi::historization::CRssi m_rssi;
+   };
+} // namespace rfxcomMessages
