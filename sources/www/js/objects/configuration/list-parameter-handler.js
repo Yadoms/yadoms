04331cb134a88e146cc88bd3fc2893f33d167574
Nicolas HILAIRE <nicolas.hilaire@gmail.com>
2014-12-08 13:18:13 +0000

    GUI : List configuration Item almost finished (functionnal on chart to select a list of color (unused for the moment))

--- sources/www/js/objects/configuration/list-parameter-handler.js
+++ sources/www/js/objects/configuration/list-parameter-handler.js
@@ -30,7 +30,7 @@ function ListParameterHandler(i18nContext, paramName, content, currentValue) {
    this.selectorUuid = createUUID();
 
    var self = this;
-debugger;
+
    //for each key in currentValue we re-create items previously saved
    if (!isNullOrUndefined(self.configurationValues)) {
       $.each(self.configurationValues, function (key, value) {
@@ -47,20 +47,21 @@ debugger;
  * @returns {string}
  */
 ListParameterHandler.prototype.getDOMObject = function () {
-   debugger;
+
+   var self = this;
    var input = "<div class=\"control-group configuration-section well\" >" +
                   "<div class=\"configuration-header\" >";
 
    input +=                "<span data-i18n=\"" + this.i18nContext + this.paramName + ".name\" >" +
                               this.name +
                            "</span>";
-   input +=                "<button class=\"pull-right btn btn-primary\" id=\"" + this.addBtnUuid + "\" type=\"button\"><span><i class=\"glyphicon glyphicon-plus\"></i></span></button>";
+   input +=                "<button class=\"pull-right btn btn-primary\" id=\"" + this.addBtnUuid + "\" type=\"button\"><span><i class=\"fa fa-file-o\"></i></span></button>";
 
    input +=       "</div>" +
                   "<div class=\"configuration-description\" data-i18n=\"" + this.i18nContext + this.paramName + ".description\" >" +
                      this.description +
                   "</div>" +
-                  "<div id=\"" + this.uuid + "\" >";
+                  "<div id=\"" + this.uuid + "\" class=\"well control-group list-item-container\">";
 
    //we append each param in the section
    $.each(this.items, function (key, value) {
@@ -76,14 +77,17 @@ ListParameterHandler.prototype.getDOMObject = function () {
 
 ListParameterHandler.prototype.createItemLine = function(item) {
    //div container of one item
-   //TODO : add control boutons remove / duplicate
-   var itemLine = "<div class=\"well\" >" +
-                     "<div class=\"col-md-11\" >" +
-                        item.getDOMObject() +
+   var itemLine = "<div class=\"list-item-line\">" +
+                      "<div class=\"col-md-10\" >" +
+                           item.getDOMObject() +
                       "</div>" +
-                      "<button class=\"col-md1 btn btn-primary btn-remove\" type=\"button\"><span><i class=\"glyphicon glyphicon-minus\"></i></span></button>" +
-                  "</div>";
-
+                      "<div class=\"col-md-2\" >" +
+                         "<div class=\"btn-group\" role=\"group\">" +
+                            "<button class=\"btn btn-primary btn-duplicate\" type=\"button\"><span><i class=\"fa fa-files-o\"></i></span></button>" +
+                            "<button class=\"btn btn-danger btn-remove\" type=\"button\"><span><i class=\"fa fa-trash-o\"></i></span></button>" +
+                         "</div>" +
+                      "</div>" +
+                   "</div>";
    return itemLine;
 }
 
@@ -92,7 +96,7 @@ ListParameterHandler.prototype.createItemLine = function(item) {
  * @returns {}
  */
 ListParameterHandler.prototype.applyScript = function () {
-debugger;
+
    var self = this;
 
    $("button#" + self.addBtnUuid).unbind("click").bind("click", function() {
@@ -101,16 +105,93 @@ debugger;
       self.items.push(item);
       var itemLine = self.createItemLine(item);
       $("div#" + self.uuid).append(itemLine);
+      $("div#" + self.uuid).i18n();
+
+      //we manage event binding
+      $("div#" + self.uuid).find("button.btn-duplicate").unbind('click').bind('click', self.duplicateLine());
+      $("div#" + self.uuid).find("button.btn-remove").unbind('click').bind('click', self.deleteLine());
+
       if ($.isFunction(item.applyScript))
          item.applyScript();
    });
+
+   $("div#" + self.uuid).find("button.btn-duplicate").unbind('click').bind('click', self.duplicateLine());
+   $("div#" + self.uuid).find("button.btn-remove").unbind('click').bind('click', self.deleteLine());
 
    //we apply script in each children
    $.each(self.items, function (key, value) {
       if ($.isFunction(value.applyScript))
          value.applyScript();
    });
+
+   $("div#" + self.uuid).i18n();
 }
+
+ListParameterHandler.prototype.getPosFromSiblings = function(item) {
+   var i = -1;
+   var c = item;
+   while (c.length > 0) {
+      i++;
+      c = c.prev();
+   }
+   return i;
+}
+
+ListParameterHandler.prototype.duplicateLine = function() {
+   var self = this;
+   return function(handler) {
+      var $container = $(handler.currentTarget).parents("div.list-item-line");
+      assert($container.length >= 1, "Unable to locate list item container");
+      //we take the nearest
+
+      //we look for the position of the current element
+      var i = self.getPosFromSiblings($container);
+
+      //i contains the position into the items array
+      assert(i >= 0, "Unable to locate item position");
+
+      var confToDuplicate = self.items[i].getCurrentConfiguration();
+
+      var newI18nContext = self.i18nContext + self.paramName + ".item.";
+      var item = ConfigurationHelper.createParameterHandler(newI18nContext, self.paramName, self.content.item, confToDuplicate);
+
+      //we insert item in the right place
+      self.items.insert(i+1, item);
+
+      var itemLine = self.createItemLine(item);
+
+      //and in the right place for the DOM
+
+      $(itemLine).insertAfter($container);
+      $("div#" + self.uuid).i18n();
+
+      //we manage event binding
+      $("div#" + self.uuid).find("button.btn-duplicate").unbind('click').bind('click', self.duplicateLine());
+      $("div#" + self.uuid).find("button.btn-remove").unbind('click').bind('click', self.deleteLine());
+
+      if ($.isFunction(item.applyScript))
+         item.applyScript();
+
+      //TODO : duplicate $container[0]
+   };
+};
+
+ListParameterHandler.prototype.deleteLine = function() {
+   var self = this;
+   return function(handler) {
+      var $container = $(handler.currentTarget).parents("div.list-item-line");
+      assert($container.length >= 1, "Unable to locate main item container");
+      //we look for the position of the current element
+      var i = self.getPosFromSiblings($container);
+
+      //i contains the position into the items array
+      assert(i >= 0, "Unable to locate item position");
+      //we delete it from list
+      self.items.splice(i, 1);
+      //we delete it from the DOM
+      $container[0].remove();
+   };
+};
 
 /**
  * Get the param name
--- sources/www/js/utility.js
+++ sources/www/js/utility.js
@@ -180,3 +180,9 @@ if (typeof String.prototype.endsWith != 'function') {
       return this.slice(-str.length) == str;
    };
 }
+
+Array.prototype.insert = function(index) {
+   this.splice.apply(this, [index, 0].concat(
+      Array.prototype.slice.call(arguments, 1)));
+   return this;
+};
