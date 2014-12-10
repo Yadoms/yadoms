14ed91416c9afe9e8a7e58ed33d284a875164f0d
jano42 <jano42@8f66d739-0145-4685-a3fe-f7fe687021a9>
2014-12-10 12:55:43 +0000

    Tests =================
    * add **poco** link and include for test project
    * shared : CDataContainer : add tests for **vector of CDataContainer**
    * shared : Communication : **fix build** (changes of shared::communication::Buffer)

--- trunk/tests/sources/shared/communication/TestBuffer.cpp
+++ trunk/tests/sources/shared/communication/TestBuffer.cpp
@@ -10,23 +10,26 @@ BOOST_AUTO_TEST_CASE(ByteBufferNominal)
 {
    unsigned char tab[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g'};
    size_t tabSize = sizeof(tab);
-   shared::communication::CByteBuffer buffer(tab, tabSize);
+   shared::communication::CByteBuffer buffer(tabSize);
+   memcpy(buffer.begin(), tab, tabSize);
 
    // Check buffer content
-   BOOST_CHECK_NE(buffer.content(), tab);                         // Ensure that data were copied
+   BOOST_CHECK_NE(buffer.begin(), tab);                         // Ensure that data were copied
    BOOST_CHECK_EQUAL(buffer.size(), tabSize);                     // Check size
    for (size_t i = 0 ; i < tabSize ; ++i)
    {
-      BOOST_CHECK_EQUAL(buffer.content()[i], tab[i]);             // Check content
+      BOOST_CHECK_EQUAL(buffer.begin()[i], tab[i]);             // Check content
    }
-   BOOST_CHECK_EQUAL(memcmp(buffer.content(), tab, tabSize), 0);  // Other check content method
+   BOOST_CHECK_EQUAL(memcmp(buffer.begin(), tab, tabSize), 0);  // Other check content method
 }
 
 shared::communication::CByteBuffer getBuffer()
 {
    unsigned char tab[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g'};
    size_t tabSize = sizeof(tab);
-   return shared::communication::CByteBuffer (tab, tabSize);
+   shared::communication::CByteBuffer buffer(tabSize);
+   memcpy(buffer.begin(), tab, tabSize);
+   return buffer;
 }
 
 void passBufferByRef(shared::communication::CByteBuffer& buffer)
@@ -34,7 +37,7 @@ void passBufferByRef(shared::communication::CByteBuffer& buffer)
    // Check buffer content
    unsigned char sameTab[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g'};
    BOOST_CHECK_EQUAL(buffer.size(), sizeof(sameTab));                         // Check size
-   BOOST_CHECK_EQUAL(memcmp(buffer.content(), sameTab, sizeof(sameTab)), 0);  // Check content
+   BOOST_CHECK_EQUAL(memcmp(buffer.begin(), sameTab, sizeof(sameTab)), 0);  // Check content
 }
 
 BOOST_AUTO_TEST_CASE(ByteBufferCopy)
@@ -44,21 +47,21 @@ BOOST_AUTO_TEST_CASE(ByteBufferCopy)
    // Check buffer content
    unsigned char sameTab[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g'};
    BOOST_CHECK_EQUAL(buffer.size(), sizeof(sameTab));                         // Check size
-   BOOST_CHECK_EQUAL(memcmp(buffer.content(), sameTab, sizeof(sameTab)), 0);  // Check content
+   BOOST_CHECK_EQUAL(memcmp(buffer.begin(), sameTab, sizeof(sameTab)), 0);  // Check content
 
    // Pass buffer by reference
    passBufferByRef(buffer);
 
    {
       shared::communication::CByteBuffer buffer2 = buffer;
-      BOOST_CHECK_EQUAL(buffer2.content(), buffer.content());                     // Ensure that data were not-copied (just ref-count inceremented)
+      BOOST_CHECK_EQUAL(buffer2.begin(), buffer.begin());                     // Ensure that data were not-copied (just ref-count inceremented)
       BOOST_CHECK_EQUAL(buffer2.size(), sizeof(sameTab));                         // Check size
-      BOOST_CHECK_EQUAL(memcmp(buffer2.content(), sameTab, sizeof(sameTab)), 0);  // Check content
+      BOOST_CHECK_EQUAL(memcmp(buffer2.begin(), sameTab, sizeof(sameTab)), 0);  // Check content
    }
 
    // buffer2 is now deleted, ensure that data were not lost
    BOOST_CHECK_EQUAL(buffer.size(), sizeof(sameTab));                         // Check size
-   BOOST_CHECK_EQUAL(memcmp(buffer.content(), sameTab, sizeof(sameTab)), 0);  // Check content
+   BOOST_CHECK_EQUAL(memcmp(buffer.begin(), sameTab, sizeof(sameTab)), 0);  // Check content
 }
 
 BOOST_AUTO_TEST_CASE(ByteBufferCopy2)
@@ -68,12 +71,12 @@ BOOST_AUTO_TEST_CASE(ByteBufferCopy2)
    // Check buffer content
    unsigned char sameTab[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g'};
    BOOST_CHECK_EQUAL(buffer->size(), sizeof(sameTab));                         // Check size
-   BOOST_CHECK_EQUAL(memcmp(buffer->content(), sameTab, sizeof(sameTab)), 0);  // Check content
+   BOOST_CHECK_EQUAL(memcmp(buffer->begin(), sameTab, sizeof(sameTab)), 0);  // Check content
 
    shared::communication::CByteBuffer buffer2 = *buffer;
    delete buffer;
    BOOST_CHECK_EQUAL(buffer2.size(), sizeof(sameTab));                         // Check size
-   BOOST_CHECK_EQUAL(memcmp(buffer2.content(), sameTab, sizeof(sameTab)), 0);  // Check content
+   BOOST_CHECK_EQUAL(memcmp(buffer2.begin(), sameTab, sizeof(sameTab)), 0);  // Check content
 }
 
 BOOST_AUTO_TEST_SUITE_END()
