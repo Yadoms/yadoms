7dd192bdd2ce4469a0c573f73613ce0397b5fc0d
jmb42 <jean-michel@berhault.org>
2014-12-29 12:51:07 +0000

    Plugin Weather Underground ========================== - Simplify the configuration. Now Only two parameters for this frame.

--- sources/plugins/WeatherUnderGround/WeatherConditions.cpp
+++ sources/plugins/WeatherUnderGround/WeatherConditions.cpp
@@ -61,99 +61,67 @@ void CWeatherConditions::Parse( boost::shared_ptr<yApi::IYPluginApi> context, co
 		}
 		else
 		{
-			if (WUConfiguration.IsTemperatureEnabled())
+			//TODO : Ne laisser que 2 conditions sur l'ensemble. le vent et si la condition "WeatherConditions" sont cochés
+
+			if (WUConfiguration.IsStandardInformationEnabled())
 			{
 				CTemp m_Temp      ( context, PluginName, Prefix + "temperature" );
 
 				m_Temp.SetValue           ( m_data, "current_observation.temp_c" );
 				m_Temp.historizeData      ( context );
-			}
 
-			if (WUConfiguration.IsPressureEnabled())
-			{
 				CBarometric m_Pressure  ( context, PluginName, Prefix + "pressure" );
 
-			   m_Pressure.SetValue       ( m_data, "current_observation.pressure_mb" );
-			   m_Pressure.historizeData  ( context );
-			}
+                m_Pressure.SetValue       ( m_data, "current_observation.pressure_mb" );
+			    m_Pressure.historizeData  ( context );
 
-			//TODO : Séparer les conditions ici, et les mettres dans un autre champs que les autres
-			if (WUConfiguration.IsWindEnabled())
-			{
-				CWind m_Wind             ( context, PluginName, Prefix );
-
-			   m_Wind.SetValue           ( m_data, "current_observation.wind_degrees", "current_observation.wind_kph", "current_observation.wind_gust_kph");
-			   m_Wind.historizeData      ( context );
-			}
-			if (WUConfiguration.IsHumidityEnabled())
-			{
 				CHumidity m_Humidity     ( context, PluginName, Prefix + "Humidity" );
 
-			   m_Humidity.SetValue       ( m_data, "current_observation.relative_humidity" );
-			   m_Humidity.historizeData  ( context );
-			}
+			    m_Humidity.SetValue       ( m_data, "current_observation.relative_humidity" );
+			    m_Humidity.historizeData  ( context );
 
-			if (WUConfiguration.IsVisibilityEnabled())
-			{
 				CVisibility m_Visibility( context, PluginName, Prefix + "Visibility" );
 
-			   m_Visibility.SetValue     ( m_data, "current_observation.visibility_km" );
-			   m_Visibility.historizeData( context );
-			}
+			    m_Visibility.SetValue     ( m_data, "current_observation.visibility_km" );
+			    m_Visibility.historizeData( context );
 
-			if (WUConfiguration.IsUVEnabled())
-			{
 				CUV m_UV        ( context, PluginName, Prefix + "UV" );
 
-			   m_UV.SetValue             ( m_data, "current_observation.UV" );
-			   m_UV.historizeData        ( context );
-			}
+			    m_UV.SetValue             ( m_data, "current_observation.UV" );
+			    m_UV.historizeData        ( context );
 
-			if (WUConfiguration.IsDewPointEnabled())
-			{
 				CTemp m_DewPoint  ( context, PluginName, Prefix + "DewPoint");
 
-			   m_DewPoint.SetValue       ( m_data, "current_observation.dewpoint_c" );
-			   m_DewPoint.historizeData  ( context );
-			}
+			    m_DewPoint.SetValue       ( m_data, "current_observation.dewpoint_c" );
+			    m_DewPoint.historizeData  ( context );
 
-			if (WUConfiguration.IsRain1hrEnabled())
-			{
 				CRain m_Rain_1hr  ( context, PluginName , Prefix + "Rain_1hr");
 
-			   m_Rain_1hr.SetValue       ( m_data, "current_observation.precip_today_metric" );
-			   m_Rain_1hr.historizeData  ( context );
-			}
+			    m_Rain_1hr.SetValue       ( m_data, "current_observation.precip_today_metric" );
+			    m_Rain_1hr.historizeData  ( context );
 
-			if (WUConfiguration.IsRainDayEnabled())
-			{
-				CRain m_Rain_today( context, PluginName , Prefix + "Rain_today");
-
-			   m_Rain_today.SetValue     ( m_data, "current_observation.precip_1hr_metric" );
-			   m_Rain_today.historizeData( context );
-			}
-
-			if (WUConfiguration.IsFeelsLikeEnabled())
-			{
-				CTemp m_FeelsLike ( context, PluginName, Prefix + "FeelsLike" );
-
-			   m_FeelsLike.SetValue      ( m_data, "current_observation.feelslike_c" );
-			   m_FeelsLike.historizeData ( context );
-			}
-
-			if (WUConfiguration.IsWinChillEnabled())
-			{
-				CTemp m_Windchill ( context, PluginName, Prefix + "Windchill" );
-
-			   m_Windchill.SetValue      ( m_data, "current_observation.windchill_c" );
-			   m_Windchill.historizeData ( context );
-			}
-			if (WUConfiguration.IsWeatherConditionIconEnabled())
-			{
 				CWeatherIcon m_WeatherConditionUrl ( context, PluginName, Prefix + "WeatherCondition" );
 
 				m_WeatherConditionUrl.SetValue ( m_data, "current_observation.icon");
 				m_WeatherConditionUrl.historizeData ( context );
+			}
+
+			if (WUConfiguration.IsWindEnabled())
+			{
+				CWind m_Wind             ( context, PluginName, Prefix );
+
+			    m_Wind.SetValue           ( m_data, "current_observation.wind_degrees", "current_observation.wind_kph", "current_observation.wind_gust_kph");
+			    m_Wind.historizeData      ( context );
+
+				CTemp m_FeelsLike ( context, PluginName, Prefix + "FeelsLike" );
+
+			    m_FeelsLike.SetValue      ( m_data, "current_observation.feelslike_c" );
+			    m_FeelsLike.historizeData ( context );
+
+				CTemp m_Windchill ( context, PluginName, Prefix + "Windchill" );
+
+			    m_Windchill.SetValue      ( m_data, "current_observation.windchill_c" );
+			    m_Windchill.historizeData ( context );
 			}
 		}
 	}
