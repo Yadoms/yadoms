3402fe4c7d53771b61e37c912e16f85225aac930
jmb42 <jmb42@8f66d739-0145-4685-a3fe-f7fe687021a9>
2014-12-29 12:51:07 +0000

    Plugin Weather Underground ========================== - Simplify the configuration. Now Only two parameters for this frame.

--- trunk/sources/plugins/WeatherUnderGround/IWUConfiguration.h
+++ trunk/sources/plugins/WeatherUnderGround/IWUConfiguration.h
@@ -31,21 +31,10 @@ public:
    virtual std::string getLocalisation() const = 0;
 
    //--------------------------------------------------------------
-   /// \brief	    Get the Localisation from the configuration
-   /// \return     The Localisation String
+   /// \brief	    Get options enabled from the configuration
    //--------------------------------------------------------------
 
-   virtual bool IsTemperatureEnabled (void) const = 0;
-   virtual bool IsDewPointEnabled (void) const = 0;
-   virtual bool IsPressureEnabled (void) const = 0;
-   virtual bool IsHumidityEnabled (void) const = 0;
-   virtual bool IsVisibilityEnabled (void) const = 0;
-   virtual bool IsUVEnabled (void) const = 0;
-   virtual bool IsRain1hrEnabled (void) const = 0;
-   virtual bool IsRainDayEnabled (void) const = 0;
    virtual bool IsWindEnabled (void) const = 0;
-   virtual bool IsFeelsLikeEnabled (void) const = 0;
-   virtual bool IsWinChillEnabled (void) const = 0;
-   virtual bool IsWeatherConditionIconEnabled (void ) const = 0;
+   virtual bool IsStandardInformationEnabled (void ) const = 0;
 };
 
--- trunk/sources/plugins/WeatherUnderGround/WUConfiguration.cpp
+++ trunk/sources/plugins/WeatherUnderGround/WUConfiguration.cpp
@@ -5,18 +5,8 @@
 
 CWUConfiguration::~CWUConfiguration()
 {
-   m_isTemperatureEnabled = false;
-   m_isDewPointEnabled = false;
-   m_isPressureEnabled = false;
-   m_isHumidityEnabled = false;
-   m_isVisibilityEnabled = false;
-   m_isUVEnabled = false;
-   m_isRain1hrEnabled = false;
-   m_isRainDayEnabled = false;
    m_isWindEnabled = false;
-   m_isFeelsLikeEnabled = false;
-   m_isWinChillEnabled = false;
-   m_isWeatherConditionIconEnabled = false;
+   m_isStandardInformationEnabled = false;
 }
 
 void CWUConfiguration::initializeWith(const shared::CDataContainer &data)
@@ -25,18 +15,8 @@ void CWUConfiguration::initializeWith(const shared::CDataContainer &data)
 
    try
    {
-	   m_isTemperatureEnabled = m_data.get<bool>("Information.content.Temperature");
-	   m_isDewPointEnabled    = m_data.get<bool>("Information.content.DewPoint");
-	   m_isPressureEnabled    = m_data.get<bool>("Information.content.Pressure");
-	   m_isHumidityEnabled    = m_data.get<bool>("Information.content.Humidity");
-	   m_isVisibilityEnabled  = m_data.get<bool>("Information.content.Visibility");
-	   m_isUVEnabled          = m_data.get<bool>("Information.content.UV");
-	   m_isRain1hrEnabled     = m_data.get<bool>("Information.content.Rain1h");
-	   m_isRainDayEnabled     = m_data.get<bool>("Information.content.RainDay");
-	   m_isWindEnabled        = m_data.get<bool>("Information.content.WindInformation.content.Wind");
-	   m_isWeatherConditionIconEnabled = m_data.get<bool>("Information.content.WeatherConditionIcon");
-	   m_isFeelsLikeEnabled   = m_data.get<bool>("Information.content.WindInformation.content.Feelslike");
-	   m_isWinChillEnabled    = m_data.get<bool>("Information.content.WindInformation.content.WindChill");
+	   m_isWindEnabled        = m_data.get<bool>("Information.content.Wind");
+	   m_isStandardInformationEnabled = m_data.get<bool>("Information.content.Standard");
    }
 	catch (boost::thread_interrupted&)
 	{
@@ -54,15 +34,5 @@ std::string CWUConfiguration::getLocalisation() const
    return m_data.get<std::string>("Localisation");
 }
 
-bool CWUConfiguration::IsTemperatureEnabled (void) const {return m_isTemperatureEnabled;}
-bool CWUConfiguration::IsDewPointEnabled (void) const    {return m_isDewPointEnabled;}
-bool CWUConfiguration::IsPressureEnabled (void) const    {return m_isPressureEnabled;}
-bool CWUConfiguration::IsHumidityEnabled (void) const    {return m_isHumidityEnabled;}
-bool CWUConfiguration::IsVisibilityEnabled (void) const  {return m_isVisibilityEnabled;}
-bool CWUConfiguration::IsUVEnabled (void) const          {return m_isUVEnabled;}
-bool CWUConfiguration::IsRain1hrEnabled (void) const     {return m_isRain1hrEnabled;}
-bool CWUConfiguration::IsRainDayEnabled (void) const     {return m_isRainDayEnabled;}
-bool CWUConfiguration::IsWindEnabled (void) const        {return m_isWindEnabled;}
-bool CWUConfiguration::IsFeelsLikeEnabled (void) const   {return m_isFeelsLikeEnabled;}
-bool CWUConfiguration::IsWinChillEnabled (void) const    {return m_isWinChillEnabled;}
-bool CWUConfiguration::IsWeatherConditionIconEnabled (void ) const {return m_isWeatherConditionIconEnabled;}
+bool CWUConfiguration::IsWindEnabled (void) const                 {return m_isWindEnabled;}
+bool CWUConfiguration::IsStandardInformationEnabled (void ) const {return m_isStandardInformationEnabled;}
