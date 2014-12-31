9d54a28fdcce2536c5a6cc6946507928b8d7478f
jmb42 <jmb42@8f66d739-0145-4685-a3fe-f7fe687021a9>
2014-12-31 13:09:48 +0000

    plugin LinuxSystemInformation ========================= - Change the read method to avoid the use of scanf function for cppcheck.

--- trunk/sources/plugins/LinuxSystemInformation/LinuxSystemCPULoad.cpp
+++ trunk/sources/plugins/LinuxSystemInformation/LinuxSystemCPULoad.cpp
@@ -4,14 +4,14 @@
 #include <shared/exception/Exception.hpp>
 #include <shared/plugin/yPluginApi/StandardCapacities.h>
 #include <shared/plugin/yPluginApi/StandardUnits.h>
+#include <fstream>
+#include <boost/regex.hpp> 
+#include <boost/lexical_cast.hpp>
 
 CLinuxSystemCPULoad::CLinuxSystemCPULoad(const std::string & device)
    :m_device(device), m_keyword("LinuxCPULoad")
 {
-   FILE* file = fopen("/proc/stat", "r");
-   fscanf(file, "cpu %Lu %Lu %Lu %Lu", &lastTotalUser, &lastTotalUserLow,
-      &lastTotalSys, &lastTotalIdle);
-   fclose(file);
+   ReadFromFile ( &lastTotalUser, &lastTotalUserLow, &lastTotalSys, &lastTotalIdle);
 }
 
 CLinuxSystemCPULoad::~CLinuxSystemCPULoad()
@@ -31,39 +31,58 @@ void CLinuxSystemCPULoad::historizeData(boost::shared_ptr<yApi::IYPluginApi> con
    context->historizeData(m_device, m_keyword);
 }
 
+void CLinuxSystemCPULoad::ReadFromFile(unsigned long long *dtotalUser,
+                                       unsigned long long *dtotalUserLow,
+                                       unsigned long long *dtotalSys,
+                                       unsigned long long *dtotalIdle)
+{
+   std::ifstream procFile("/proc/stat");
+
+   //This might broke if there are not 4 columns minimum in /proc/stat
+   boost::regex reg("cpu  (\\d+) (\\d+) (\\d+) (\\d+)");
+
+   //each counters (64 bits) is incremented each time of the number of ticks corresponding. counters should be equal or greater at each reads. if not, it's the overflow of the counter.
+
+   std::string line;
+   while ( std::getline(procFile, line) ) 
+   {
+      boost::smatch match;
+      if ( boost::regex_search( line, match, reg ) ) 
+      {
+          *dtotalUser    = boost::lexical_cast<long long>(match[1]);
+          *dtotalUserLow = boost::lexical_cast<long long>(match[2]);
+          *dtotalSys     = boost::lexical_cast<long long>(match[3]);
+          *dtotalIdle    = boost::lexical_cast<long long>(match[4]);
+      }
+   }
+
+   procFile.close();
+}
+
 void CLinuxSystemCPULoad::read()
 {
-   //TODO : Keep the last value, if an overflow occured
    double percent;
    unsigned long long totalUser, totalUserLow, totalSys, totalIdle;
-   FILE* file;
 
-   file = fopen("/proc/stat", "r");
-   fscanf(file, "cpu %Lu %Lu %Lu %Lu", &totalUser, &totalUserLow,
-      &totalSys, &totalIdle);
-   fclose(file);
+   ReadFromFile ( &totalUser, &totalUserLow, &totalSys, &totalIdle );
 
-   if (totalUser < lastTotalUser || totalUserLow < lastTotalUserLow ||
-      totalSys < lastTotalSys || totalIdle < lastTotalIdle){
-         //Overflow detection. Just skip this value.
-         percent = -1.0;
+   if (totalUser < lastTotalUser || totalUserLow < lastTotalUserLow || totalSys < lastTotalSys || totalIdle < lastTotalIdle)
+   {
+     //Overflow detection. Just skip this value.
    }
-   else{
-      unsigned long long total = (totalUser - lastTotalUser) + (totalUserLow - lastTotalUserLow) +
-         (totalSys - lastTotalSys);
+   else
+   { 
+      unsigned long long total = (totalUser - lastTotalUser) + (totalUserLow - lastTotalUserLow) + (totalSys - lastTotalSys);
       percent = total;
       total += (totalIdle - lastTotalIdle);
       percent /= total;
       percent *= 100;
-   }
 
-   lastTotalUser = totalUser;
-   lastTotalUserLow = totalUserLow;
-   lastTotalSys = totalSys;
-   lastTotalIdle = totalIdle;
-
-   m_keyword.set (percent);
-   YADOMS_LOG(debug) << "WindowsSystemInformation plugin :  CPU Load : " << m_keyword.formatValue();
+      m_keyword.set (percent);
+      YADOMS_LOG(debug) << "CPU Load : " << m_keyword.formatValue();
+    }
+    lastTotalUser = totalUser;
+    lastTotalUserLow = totalUserLow;
+    lastTotalSys = totalSys;
+    lastTotalIdle = totalIdle;
 }
-
-
