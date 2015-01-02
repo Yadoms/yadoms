183fb355a543398d4d12983c8cc1565052cc8d26
jmb42 <jmb42@8f66d739-0145-4685-a3fe-f7fe687021a9>
2015-01-02 12:52:30 +0000

    Plugin RaspberryPI System Information =============================== - correct some compilation errors due to yadoms interface changes. - correct cppcheck warning by changing scanf to regex function. - delete some TODO.

--- trunk/sources/plugins/RaspberryPISystemInformation/RaspBerryPiSystemCPULoad.cpp
+++ trunk/sources/plugins/RaspberryPISystemInformation/RaspBerryPiSystemCPULoad.cpp
@@ -4,14 +4,14 @@
 #include <shared/exception/Exception.hpp>
 #include <shared/plugin/yPluginApi/StandardCapacities.h>
 #include <shared/plugin/yPluginApi/StandardUnits.h>
+#include <fstream>
+#include <boost/regex.hpp> 
+#include <boost/lexical_cast.hpp>
 
 CRaspBerryPiSystemCPULoad::CRaspBerryPiSystemCPULoad(const std::string & device)
    :m_device(device), m_keyword("RaspBerryPiCPULoad")
 {
-   FILE* file = fopen("/proc/stat", "r");
-   fscanf(file, "cpu %Lu %Lu %Lu %Lu", &lastTotalUser, &lastTotalUserLow,
-      &lastTotalSys, &lastTotalIdle);
-   fclose(file);
+   ReadFromFile ( &m_lastTotalUser, &m_lastTotalUserLow, &m_lastTotalSys, &m_lastTotalIdle);
 }
 
 CRaspBerryPiSystemCPULoad::~CRaspBerryPiSystemCPULoad()
@@ -31,39 +31,60 @@ void CRaspBerryPiSystemCPULoad::historizeData(boost::shared_ptr<yApi::IYPluginAp
    context->historizeData(m_device, m_keyword);
 }
 
+void CRaspBerryPiSystemCPULoad::ReadFromFile(unsigned long long *dtotalUser,
+                                       unsigned long long *dtotalUserLow,
+                                       unsigned long long *dtotalSys,
+                                       unsigned long long *dtotalIdle)
+{
+   std::ifstream procFile("/proc/stat");
+
+   //This might broke if there are not 4 columns minimum in /proc/stat
+   boost::regex reg("cpu  (\\d+) (\\d+) (\\d+) (\\d+)");
+
+   //each counters (64 bits) is incremented each time of the number of ticks corresponding. counters should be equal or greater at each reads. if not, it's the overflow of the counter.
+
+   std::string line;
+   while ( std::getline(procFile, line) ) 
+   {
+      boost::smatch match;
+      if ( boost::regex_search( line, match, reg ) ) 
+      {
+          *dtotalUser    = boost::lexical_cast<long long>(match[1]);
+          *dtotalUserLow = boost::lexical_cast<long long>(match[2]);
+          *dtotalSys     = boost::lexical_cast<long long>(match[3]);
+          *dtotalIdle    = boost::lexical_cast<long long>(match[4]);
+      }
+   }
+
+   procFile.close();
+}
+
 void CRaspBerryPiSystemCPULoad::read()
 {
-   //TODO : Keep the last value, if an overflow occured
-   float percent;
-   unsigned long long totalUser, totalUserLow, totalSys, totalIdle;
-   FILE* file;
+unsigned long long totalUser, totalUserLow, totalSys, totalIdle;
 
-   file = fopen("/proc/stat", "r");
-   fscanf(file, "cpu %Lu %Lu %Lu %Lu", &totalUser, &totalUserLow,
-      &totalSys, &totalIdle);
-   fclose(file);
+   ReadFromFile ( &totalUser, &totalUserLow, &totalSys, &totalIdle );
 
-   if (totalUser < lastTotalUser || totalUserLow < lastTotalUserLow ||
-      totalSys < lastTotalSys || totalIdle < lastTotalIdle){
-         //Overflow detection. Just skip this value.
-         percent = -1.0;
+   if (totalUser < m_lastTotalUser || totalUserLow < m_lastTotalUserLow || totalSys < m_lastTotalSys || totalIdle < m_lastTotalIdle)
+   {
+     //Overflow detection. Just skip this value.
    }
-   else{
-      unsigned long long total = (totalUser - lastTotalUser) + (totalUserLow - lastTotalUserLow) +
-         (totalSys - lastTotalSys);
+   else
+   {
+      double percent; 
+      unsigned long long total = (totalUser - m_lastTotalUser) + (totalUserLow - m_lastTotalUserLow) + (totalSys - m_lastTotalSys);
       percent = total;
-      total += (totalIdle - lastTotalIdle);
+      total += (totalIdle - m_lastTotalIdle);
       percent /= total;
       percent *= 100;
-   }
 
-   lastTotalUser = totalUser;
-   lastTotalUserLow = totalUserLow;
-   lastTotalSys = totalSys;
-   lastTotalIdle = totalIdle;
-
-   m_keyword.set (percent);
-   YADOMS_LOG(debug) << "RaspBerryPiSystemInformation plugin :  CPU Load : " << m_keyword.formatValue();
+      m_keyword.set (percent);
+      YADOMS_LOG(debug) << "CPU Load : " << m_keyword.formatValue();
+    }
+    m_lastTotalUser = totalUser;
+    m_lastTotalUserLow = totalUserLow;
+    m_lastTotalSys = totalSys;
+    m_lastTotalIdle = totalIdle;
 }
 
 
