a252d8d6cf51d9bb8b2e87a4ef5aaf0e81ac5dcf
sgallou <sgallou@8f66d739-0145-4685-a3fe-f7fe687021a9>
2015-01-12 16:32:49 +0000

    Automation center, script action ================== * Python script interpreter in progress

--- trunk/sources/scriptInterpreters/Python/Python.cpp
+++ trunk/sources/scriptInterpreters/Python/Python.cpp
@@ -3,6 +3,7 @@
 #include "Initializer.h"
 #include <shared/script/ImplementationHelper.h>
 #include "PythonLibInclude.h"
+#include <shared/Log.h>
 
 // Declare the script interpreter
 IMPLEMENT_SCRIPT_INTERPRETER(CPython)
@@ -35,6 +36,24 @@ bool CPython::isAvailable() const
 
 bool CPython::canInterpret(const std::string& scriptPath) const
 {
+   //TODO : structure en répertoire + fichier (pour permettre d'avoir des ressources par script) ?
+   if (!boost::filesystem::exists(scriptPath))
+   {
+      YADOMS_LOG(error) << scriptPath << " : can not interpret, file doesn't exist";
+      return false;
+   }
+
+   // TODO faire un CPythonObject
+   PyObject* pyScriptPath = PyString_FromString(scriptPath.c_str());
+   PyObject* pyModule = PyImport_Import(pyScriptPath);
+   Py_DECREF(pyScriptPath);
+
+   if (pyModule == NULL)
+   {
+      YADOMS_LOG(error) << scriptPath << " : can not interpret, unable to create Python module";
+      return false;
+   }
+
    //TODO : charger le fichier et vérifier que la fonction "yMain" existe
    return false;
 }
