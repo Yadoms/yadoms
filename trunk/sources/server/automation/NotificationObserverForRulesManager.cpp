5cbd4c25dc546c31d9d5e97237a9c55fd43028ac
sgallou <sgallou@8f66d739-0145-4685-a3fe-f7fe687021a9>
2015-01-23 21:10:31 +0000

    Automation center ============== * Light refactoring to prepare managment of dateTime/duration operators

--- trunk/sources/server/automation/NotificationObserverForRulesManager.cpp
+++ trunk/sources/server/automation/NotificationObserverForRulesManager.cpp
@@ -39,31 +39,40 @@ void CNotificationObserverForRulesManager::doWork()
 void CNotificationObserverForRulesManager::notifyListeners(boost::shared_ptr<notifications::CNewAcquisitionNotification> newAcquisition)
 {
    // For this keyword ID, notify each listeners, and each root condition (but just one time)
-
    boost::recursive_mutex::scoped_lock lock(m_listenersMutex);
 
-   std::queue<boost::shared_ptr<condition::IConditionRootUpdater> > m_rootConditionsQueue;
-   std::set<boost::shared_ptr<condition::IConditionRootUpdater> > m_rootConditionsSet; // Used to disable duplicates
+   std::queue<boost::shared_ptr<condition::IConditionRootUpdater> > rootConditionsQueue;
+   notifyKeywordListeners(newAcquisition, rootConditionsQueue);
+   notifyRootconditions(rootConditionsQueue);
+}
 
-   std::pair<KeywordUpdaterList::iterator, KeywordUpdaterList::iterator> range = m_listeners.equal_range(newAcquisition->getAcquisition()->KeywordId);
-   for (KeywordUpdaterList::iterator listener = range.first ; listener != range.second; ++listener)
+void CNotificationObserverForRulesManager::notifyKeywordListeners(
+   boost::shared_ptr<notifications::CNewAcquisitionNotification> newAcquisition,
+   std::queue<boost::shared_ptr<condition::IConditionRootUpdater> >& rootConditionsQueue)
+{
+   std::set<boost::shared_ptr<condition::IConditionRootUpdater> > rootConditionsSet; // Used to prevent duplicates
+
+   std::pair<KeywordUpdaterList::iterator, KeywordUpdaterList::iterator> range = m_KeywordListeners.equal_range(newAcquisition->getAcquisition()->KeywordId);
+   for (KeywordUpdaterList::iterator listener = range.first; listener != range.second; ++listener)
    {
       KeywordUpdater& keywordUpdater = listener->second;
 
       // Notify keyword
       keywordUpdater.first->onKeywordStateChange(newAcquisition->getAcquisition()->Value);
-      if (m_rootConditionsSet.find(keywordUpdater.second) == m_rootConditionsSet.end())
+      if (rootConditionsSet.find(keywordUpdater.second) == rootConditionsSet.end())
       {
-         m_rootConditionsQueue.push(keywordUpdater.second);
-         m_rootConditionsSet.insert(keywordUpdater.second);
+         rootConditionsQueue.push(keywordUpdater.second);
+         rootConditionsSet.insert(keywordUpdater.second);
       }
    }
+}
 
-   // Now notify root conditions
-   while (!m_rootConditionsQueue.empty())
+void CNotificationObserverForRulesManager::notifyRootconditions(std::queue<boost::shared_ptr<condition::IConditionRootUpdater> >& rootConditionsQueue)
+{
+   while (!rootConditionsQueue.empty())
    {
-      m_rootConditionsQueue.front()->onKeywordStateChange();
-      m_rootConditionsQueue.pop();
+      rootConditionsQueue.front()->onKeywordStateChange();
+      rootConditionsQueue.pop();
    }
 }
 
@@ -71,7 +80,7 @@ void CNotificationObserverForRulesManager::registerKeywordUpdater(boost::shared_
 {
    boost::recursive_mutex::scoped_lock lock(m_listenersMutex);
 
-   m_listeners.insert(std::pair<int, KeywordUpdater>(keywordNotifier->getKeywordId(), KeywordUpdater(keywordNotifier, conditionRootNotifier)));
+   m_KeywordListeners.insert(std::pair<int, KeywordUpdater>(keywordNotifier->getKeywordId(), KeywordUpdater(keywordNotifier, conditionRootNotifier)));
 }
 
 void CNotificationObserverForRulesManager::unregisterKeywordUpdater(boost::shared_ptr<condition::IKeywordUpdater> keywordNotifier)
@@ -79,11 +88,11 @@ void CNotificationObserverForRulesManager::unregisterKeywordUpdater(boost::share
    boost::recursive_mutex::scoped_lock lock(m_listenersMutex);
 
    // Unregister the specified keyword updater, ands its associated root condition
-   for (KeywordUpdaterList::iterator listener = m_listeners.begin(); listener != m_listeners.end(); ++listener)
+   for (KeywordUpdaterList::iterator listener = m_KeywordListeners.begin(); listener != m_KeywordListeners.end(); ++listener)
    {
       if (listener->second.first == keywordNotifier)
       {
-         m_listeners.erase(listener);
+         m_KeywordListeners.erase(listener);
          break;
       }
    }
