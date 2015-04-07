f4b40ecf8dd6db80ad20f23ff59e2539f4f4ae4f
Nicolas HILAIRE <nicolas.hilaire@gmail.com>
2015-04-07 12:32:49 +0000

    GUI ========================= Script editor is working in "code" mode Some bugs remains bug corrected in regex of recipient field

--- sources/www/js/objects/automation/automation-rule.js
+++ sources/www/js/objects/automation/automation-rule.js
@@ -4,12 +4,12 @@
  * Creates an instance of Device
  * @constructor
  */
-function AutomationRule(id, name, description, interpreter, editor, model, content, configuration, enabled, state, errorMessage, startDate, stopDate) {
+function AutomationRule(id, name, description, interpreter, editorName, model, content, configuration, enabled, state, errorMessage, startDate, stopDate) {
    assert(!isNullOrUndefined(id), "id of a AutomationRule must be defined");
    assert(!isNullOrUndefined(name), "name of a AutomationRule must be defined");
    assert(!isNullOrUndefined(description), "description of a AutomationRule must be defined");
    assert(!isNullOrUndefined(interpreter), "interpreter of a AutomationRule must be defined");
-   assert(!isNullOrUndefined(editor), "editor of a AutomationRule must be defined");
+   assert(!isNullOrUndefined(editorName), "editor of a AutomationRule must be defined");
    assert(!isNullOrUndefined(model), "actions of a AutomationRule must be defined");
    assert(!isNullOrUndefined(content), "content of a AutomationRule must be defined");
    assert(!isNullOrUndefined(configuration), "content of a AutomationRule must be defined");
@@ -19,7 +19,8 @@ function AutomationRule(id, name, description, interpreter, editor, model, conte
    this.name = name;
    this.description = description;
    this.interpreter = interpreter;
-   this.editor = editor;
+   this.editorName = editorName;
+   this.editor = null;
    this.model = model;
    this.content = content;
    this.configuration = configuration;
@@ -30,6 +31,7 @@ function AutomationRule(id, name, description, interpreter, editor, model, conte
    this.stopDate = stopDate;
    this.code = "";
    this.codeHasBeenDownloaded = false;
+   this.editorHasBeenDownloaded = false;
 }
 
 AutomationRule.prototype.toJSON = function () {
@@ -38,11 +40,31 @@ AutomationRule.prototype.toJSON = function () {
       name: encodeURIComponent(this.name),
       description: encodeURIComponent(this.description),
       interpreter: this.interpreter,
-      editor: this.editor,
+      editor: this.editorName,   //we provide only the name of the editor and not the full object
       model: this.model,
       content: this.content,
       configuration: this.configuration,
       enabled: this.enabled
       //we don't take fields that are filled by the server
    };
+};
+
+/**
+ * Download the editor attached to the rule
+ */
+AutomationRule.prototype.downloadEditor = function (callback) {
+   assert($.isFunction(callback), "callback must be a function");
+   var self = this;
+   if (!self.editorHasBeenDownloaded) {
+      AutomationEditorManager.getByName(self.editorName, function (editor) {
+         //if there is an error getting editor
+         if (!editor) {
+            notifyError($.t("objects.generic.errorEditing", {objectName: "automation rule"}));
+            return;
+         }
+         self.editor = editor;
+         self.editorHasBeenDownloaded = true;
+         callback();
+      });
+   }
 };
