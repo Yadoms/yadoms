67d98d5a3c9d5d617bf0713fda5d9f35edc92a7a
jmb42 <jmb42@8f66d739-0145-4685-a3fe-f7fe687021a9>
2015-04-08 19:36:13 +0000

    Widget Forecast =============== - [fix] correct icons centering. - some cleaning.
    Widget Moon-Phases
    ==================
    - some cleaning too.

--- trunk/sources/www/widgets/moon-phases/viewModel.js
+++ trunk/sources/www/widgets/moon-phases/viewModel.js
@@ -1,37 +1,3 @@
-ko.extenders.numeric = function(target, precision) {
-   //create a writeable computed observable to intercept writes to our observable
-   var result = ko.computed({
-      read: target,  //always return the original observables value
-      write: function(newValue) {
-         var current = target(),
-            roundingMultiplier = Math.pow(10, precision),
-            newValueAsNum = isNaN(newValue) ? 0 : parseFloat(+newValue),
-            valueToWrite = Math.round(newValueAsNum * roundingMultiplier) / roundingMultiplier;
-
-         //only write if it changed
-         if (valueToWrite !== current) {
-            target(valueToWrite);
-         } else {
-            //if the rounded value is the same, but a different value was written, force a notification for the current field
-            if (newValue !== current) {
-               target.notifySubscribers(valueToWrite);
-            }
-         }
-      }
-   }).extend({ notify: 'always' });
-
-   //initialize with current value to make sure it is rounded appropriately
-   result(target());
-
-   //return the new computed observable
-   return result;
-};
-
-function twoDigit(number) {
-  var twodigit = number >= 10 ? number : "0"+number.toString();
-  return twodigit;
-}
-
 widgetViewModelCtor =
 
 /**
@@ -41,7 +7,6 @@ widgetViewModelCtor =
 function MoonPhasesViewModel() {
 
    //observable data
-   //this.data = ko.observable(0).extend({ numeric: 1 });
    this.data = ko.observable("");
    this.WidgetHeight = ko.observable ("100px");
    this.WidgetWidth  = ko.observable ("100px");
