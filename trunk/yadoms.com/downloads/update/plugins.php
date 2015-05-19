dbf8fa012ebb7ac9a15b5b0586ea3d237dbb4b2c
lgm42 <lgm42@8f66d739-0145-4685-a3fe-f7fe687021a9>
2015-05-19 10:52:16 +0000

    Web site ===============
    update plugin.php to manage tolerance of os and arch names (windows, win, windows NT, etc..)

--- trunk/yadoms.com/downloads/update/plugins.php
+++ trunk/yadoms.com/downloads/update/plugins.php
@@ -28,18 +28,45 @@ function remove_utf8_bom($text)
     $text = preg_replace("/^$bom/", '', $text);
     return $text;
 }
+
+//Make a insensitive and begin with item search
+function matchInArray($element, $array, $elementIfMatch)
+{
+	foreach ($array as $key => $value)
+	{
+		if (strchr(strtolower($element), $value))
+			return $elementIfMatch;
+	}
+	return $element;
+}
 ?>
 <?
+//variables initialization
+$osWin = array( "windows", "win");
+$osLinux = array("linux", "unix");
+$osMac = array("darwin", "mac", "osx");
+
+$archX86 = array("x86", "ia32", "i686");
+$archX64 = array("x64", "ia64");
+
 //getting parameters
 $os = $_GET["os"];
 $arch = $_GET["arch"];
 $name = $_GET["name"];
 $lang = $_GET["lang"];
 
-$answer = array( 'os' => $os, 'arch' => $arch, 'name' => $name );
+$answer = array( 'inputParameters' => array('os' => $os, 'arch' => $arch, 'name' => $name));
 
 if (($os == null) || ($arch == null))
 	sendAnswerAndExit(false, "os and arch GET parameters must be defined", $answer);
+
+//we manage param tolerance
+$os = matchInArray($os, $osWin, "windows");
+$os = matchInArray($os, $osLinux, "linux");
+$os = matchInArray($os, $osMac, "darwin");
+
+$arch = matchInArray($arch, $archX86, "x86");
+$arch = matchInArray($arch, $archX64, "x64");
 
 //if the name is defined we get information only on this item
 if ($name != null)
