f52076133b9bc31b64e84aa4a130b3ebd20f23a3
Jean-Michel DECORET <jano42@users.noreply.github.com>
2015-05-28 10:49:29 +0000

    * add missing files

---
+++ sources/server/task/update/WidgetRemove.cpp
@@ -0,0 +1,40 @@
+#include "stdafx.h"
+#include "WidgetRemove.h"
+#include <shared/Log.h>
+
+#include "update/worker/Widget.h"
+
+namespace task { namespace update {
+
+   std::string CWidgetRemove::m_taskName = "widget.remove";
+
+   CWidgetRemove::CWidgetRemove(const std::string & widgetName)
+      :m_widgetName(widgetName)
+   {
+   }
+
+   CWidgetRemove::~CWidgetRemove()
+   {
+   }
+
+   const std::string & CWidgetRemove::getName()
+   {
+      return m_taskName;
+   }
+
+   bool CWidgetRemove::doWork(TaskProgressFunc pFunctor)
+   {
+      YADOMS_LOG(information) << "Start removing widget...";
+
+      //adapt the progress callback (signature are same; but could change in future, so an adaptation is better)
+      ::update::worker::CWidget::WorkerProgressFunc adapter = boost::bind(pFunctor, _1, _2, _3);
+
+      ::update::worker::CWidget worker(adapter);
+      worker.remove(m_widgetName);
+
+      YADOMS_LOG(information) << "End of removing widget";
+      return true;
+   }
+
+} //namespace update
+} //namespace task
---
+++ sources/server/task/update/WidgetUpdate.cpp
@@ -0,0 +1,39 @@
+#include "stdafx.h"
+#include "WidgetUpdate.h"
+#include <shared/Log.h>
+#include "update/worker/Widget.h"
+
+namespace task { namespace update {
+
+   std::string CWidgetUpdate::m_taskName = "widget.update";
+
+   CWidgetUpdate::CWidgetUpdate(const std::string & widgetName, const std::string & downloadUrl)
+      :m_widgetName(widgetName), m_downloadUrl(downloadUrl)
+   {
+   }
+
+   CWidgetUpdate::~CWidgetUpdate()
+   {
+   }
+
+   const std::string & CWidgetUpdate::getName()
+   {
+      return m_taskName;
+   }
+
+   bool CWidgetUpdate::doWork(TaskProgressFunc pFunctor)
+   {
+      YADOMS_LOG(information) << "Start updating widget...";
+
+      //adapt the progress callback (signature are same; but could change in future, so an adaptation is better)
+      ::update::worker::CWidget::WorkerProgressFunc adapter = boost::bind(pFunctor, _1, _2, _3);
+
+      ::update::worker::CWidget worker(adapter);
+      worker.update(m_widgetName, m_downloadUrl);
+
+      YADOMS_LOG(information) << "End of updating widget";
+      return true;
+   }
+
+} //namespace update
+} //namespace task
