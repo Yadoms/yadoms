c67e7d9d6ffbff83b81c9a7d27e57bdaa8263b2e
sgallou <sgallou@8f66d739-0145-4685-a3fe-f7fe687021a9>
2015-06-09 10:06:00 +0000

    Server/Task ================== * Fix GCC warning

--- trunk/sources/server/task/FinishedInstance.cpp
+++ trunk/sources/server/task/FinishedInstance.cpp
@@ -3,10 +3,10 @@
 
 #include "FinishedInstance.h"
 
-namespace task { 
+namespace task {
 
    CFinishedInstance::CFinishedInstance(const std::string & guid, const std::string & name, ETaskStatus status, boost::optional< float > progression, const std::string & message, const shared::CDataContainer & data, const boost::posix_time::ptime & creationDate)
-      : m_name(name), m_guid(guid), m_message(message), m_progression(progression), m_status(status), m_data(data),  m_creationDate(creationDate)
+      : m_name(name), m_guid(guid), m_progression(progression), m_status(status), m_message(message), m_data(data), m_creationDate(creationDate)
    {
    }
 
