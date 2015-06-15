df2037259e81f7fd135866cc3997abed9a62e6dc
Nicolas HILAIRE <nicolas.hilaire@gmail.com>
2015-06-15 11:46:38 +0000

    GUI ======================= update plugin in progress all knock out extenders have been merged into a unique file knock-out-extenders.js remove knock out extenders from all widgets

--- sources/www/widgets/gauge/viewModel.js
+++ sources/www/widgets/gauge/viewModel.js
@@ -1,34 +1,3 @@
-//Disable the chart context menu
-
-ko.extenders.numeric = function(target, precision) {
-   //create a writeable computed observable to intercept writes to our observable
-   var result = ko.computed({
-      read: target,  //always return the original observables value
-      write: function(newValue) {
-         var current = target(),
-            roundingMultiplier = Math.pow(10, precision),
-            newValueAsNum = isNaN(newValue) ? 0 : parseFloat(+newValue),
-            valueToWrite = Math.round(newValueAsNum * roundingMultiplier) / roundingMultiplier;
-
-         //only write if it changed
-         if (valueToWrite !== current) {
-            target(valueToWrite);
-         } else {
-            //if the rounded value is the same, but a different value was written, force a notification for the current field
-            if (newValue !== current) {
-               target.notifySubscribers(valueToWrite);
-            }
-         }
-      }
-   }).extend({ notify: 'always' });
-
-   //initialize with current value to make sure it is rounded appropriately
-   result(target());
-
-   //return the new computed observable
-   return result;
-};
-
 widgetViewModelCtor =
 
 /**
--- sources/www/widgets/numeric-display/viewModel.js
+++ sources/www/widgets/numeric-display/viewModel.js
@@ -1,32 +1,3 @@
-ko.extenders.numeric = function(target, precision) {
-   //create a writeable computed observable to intercept writes to our observable
-   var result = ko.computed({
-      read: target,  //always return the original observables value
-      write: function(newValue) {
-         var current = target(),
-            roundingMultiplier = Math.pow(10, precision),
-            newValueAsNum = isNaN(newValue) ? 0 : parseFloat(+newValue),
-            valueToWrite = Math.round(newValueAsNum * roundingMultiplier) / roundingMultiplier;
-
-         //only write if it changed
-         if (valueToWrite !== current) {
-            target(valueToWrite);
-         } else {
-            //if the rounded value is the same, but a different value was written, force a notification for the current field
-            if (newValue !== current) {
-               target.notifySubscribers(valueToWrite);
-            }
-         }
-      }
-   }).extend({ notify: 'always' });
-
-   //initialize with current value to make sure it is rounded appropriately
-   result(target());
-
-   //return the new computed observable
-   return result;
-};
-
 widgetViewModelCtor =
 
 /**
@@ -57,7 +28,7 @@ function NumericDisplayViewModel() {
 
       //we get the unit of the keyword
       KeywordManager.get(self.widget.configuration.device.keywordId, function(keyword) {
-         self.unit($.t(keyword.units));
+         self.unit(keyword.units);
       });
    }
    
