1fc9154a616bbd05184546af871a0a0a7fba5733
jano42 <jano42@8f66d739-0145-4685-a3fe-f7fe687021a9>
2015-07-06 11:43:55 +0000

    Blockly =======
    * remove "debugger"

--- trunk/sources/www/js/objects/automation/automation-blockly-custom-blocks.js
+++ trunk/sources/www/js/objects/automation/automation-blockly-custom-blocks.js
@@ -1556,9 +1556,7 @@ Blockly.Blocks['yadoms_enumeration'] = {
                         //all is OK, this is a new enum, ask for translation
                         var translatedEnum = [];
                         $.each(typeInfo.values, function (index, value) {
-                            debugger;
                             var trad = $.t(pluginData.type + ":enumerations." + typeInfo.name + "." + value, {defaultValue : pluginData.package.enumerations[typeInfo.name][value]  });
-
                             translatedEnum.push([trad, value]);
                         });
                         //apply new dropdown list
