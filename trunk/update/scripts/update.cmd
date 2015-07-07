35f47a7b624c942e75c6a82b46e6a48df79c564d
jano42 <jano42@8f66d739-0145-4685-a3fe-f7fe687021a9>
2015-07-07 12:41:17 +0000

    Update ===========
    * add update scripts to repo : an update script is included in each yadoms update software package and allow to
      -> wait for yadoms to ends
      -> update file (overwrite files)
      -> restart yadoms

---
+++ trunk/update/scripts/update.cmd
@@ -0,0 +1,99 @@
+@ECHO OFF
+ 
+::/ NAME
+::/  update.cmd
+::/ 
+::/ SYNOPSIS
+::/  Wait for exit of Yadoms, update the software and restart Yadoms
+::/  
+::/ 
+::/ SYNTAX
+::/  update YadomsPath timeout (default 60)
+::/ 
+::/ DETAILED DESCRIPTION
+::/  The update.cmd wait for the end of execution of yadoms, 
+::/  update the yadoms application and ask for a restart.
+::/  There is a global timeout which avoid an infinity wait
+ 
+SETLOCAL ENABLEDELAYEDEXPANSION
+ 
+SET YadomsExecutable="yadoms.exe"
+ 
+set CurrentScriptDir=%~dp0
+ 
+::Parse  command line arguments
+IF "%1"=="/?" (
+    TYPE "%~f0" | findstr.exe /R "^::/"
+    GOTO :END
+)
+ 
+SET YadomsPath=%~1
+IF NOT DEFINED YadomsPath (
+    ECHO %~n0 : Cannot bind argument to parameter 'YadomsPath' because it is empty.
+    GOTO :END
+)
+
+SET SecTimeout=%~2
+IF NOT DEFINED SecTimeout (
+    SET SecTimeout=60
+)
+
+::Check if program is running
+SET programPid=""
+FOR /F "tokens=2 skip=3" %%i IN (
+    'tasklist.exe /FI "IMAGENAME eq %YadomsExecutable%"'
+) DO (
+SET programPid=%%~i
+)
+
+::If not running then continue script
+if %programPid%=="" (
+	echo %YadomsExecutable% is not running
+	goto :PROCESSIFENDED
+)
+
+::Wait for process to exit
+set secWait=1
+:while1
+if not %programPid%=="" (
+	echo Wait for %YadomsExecutable% to exit...
+	timeout /t 1 /nobreak > nul
+	SET programPid=""
+	FOR /F "tokens=2 skip=3" %%i IN (
+		'tasklist.exe /FI "IMAGENAME eq %YadomsExecutable%"'
+	) DO (
+	SET programPid=%%~i
+	)
+
+	if %secWait% GEQ %SecTimeout% (
+		echo %YadomsExecutable% is still running after %secWait% seconds
+		echo Could not apply script
+		goto :END
+	)
+	
+	set /A secWait+=1 
+
+    goto :while1
+)
+echo %YadomsExecutable% ends
+
+:PROCESSIFENDED
+
+@ECHO ON
+
+echo Updating Yadoms ...
+
+::INSERT UPDATE COMMAND HERE
+copy "%CurrentScriptDir%package\*.*" "%YadomsPath%"
+
+echo Yadoms updated successfully
+
+echo Restarting Yadoms ...
+
+start %YadomsPath%/%YadomsExecutable%
+
+goto :END
+
+
+:END
+pause
