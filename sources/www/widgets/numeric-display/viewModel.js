f1a861416a19c48ed79a8ef0371e24bdda1b3fe5
jmb42 <jean-michel@berhault.org>
2015-08-28 19:00:46 +0000

    Widget Chart ============ - Cleaning some logs.
    Widget Gauge 
    ============
    [fix] display the unit.
    
    Widget Numeric-Display 
    ======================
    - Possibility to extend to 2 cases in long.
    - center the title and the data.
    [fix] Size the font of the data depending of the number of digits.

--- sources/www/widgets/numeric-display/viewModel.js
+++ sources/www/widgets/numeric-display/viewModel.js
@@ -9,6 +9,7 @@ function NumericDisplayViewModel() {
    this.text = ko.observable("");
    this.data = ko.observable(0).extend({ numeric: 1 });
    this.unit = ko.observable("");
+   this.fontSize = ko.observable (25);
 
    //widget identifier
    this.widget = null;
@@ -21,6 +22,14 @@ function NumericDisplayViewModel() {
       this.widget = widget;
    };
 
+   /**
+    * FontSize Method for the size of the Data to display
+    */   
+   
+   this.fontStyleCSS = ko.computed(function() {
+	   return {"font-size": this.fontSize() + "px"};
+   }, this );
+   
    this.configurationChanged = function() {
       this.text(this.widget.configuration.text);
 
@@ -28,7 +37,7 @@ function NumericDisplayViewModel() {
 
       //we get the unit of the keyword
       KeywordManager.get(self.widget.configuration.device.keywordId, function(keyword) {
-         self.unit(keyword.units);
+         self.unit($.t(keyword.units));
       });
    }
    
@@ -40,17 +49,67 @@ function NumericDisplayViewModel() {
    this.onNewAcquisition = function(device, data) {
       var self = this;
 
-      if (device == self.widget.configuration.device) {
+      if (device == self.widget.configuration.device) 
+	  {
          //it is the good device
          self.data(data.value);
+		 
+		 self.resizefont();
       }
    };
 
+   this.resizefont = function()
+   {
+	   self = this;
+	   
+	 switch ( self.data().toString().length + self.unit().toString().length )
+	 {
+		 case 1:
+		 case 2:
+		 case 3:
+		 case 4:
+		 case 5:
+		    self.fontSize (30);
+			break;
+		 case 6:
+		 case 7:		 
+			if (self.widget.width() < 120)
+			   self.fontSize ( 25 );
+			else
+				self.fontSize ( 30 );
+			break;
+			break;
+		 case 8:
+		 case 9:		 
+			if (self.widget.width() < 120)
+			   self.fontSize ( 22 );
+			else
+				self.fontSize ( 30 );
+			break;
+         case 10:
+		 case 11:
+			if (self.widget.width() < 120)
+			   self.fontSize ( 18 );
+			else
+				self.fontSize ( 30 );
+			break;
+		 default:
+			self.fontSize ( 25 );
+			break;
+	 }
+   }
+   
+   this.resized = function() 
+   {
+       var self = this;
+   
+	   self.resizefont();
+   };	   
+   
    this.getDevicesForAcquisitions = function() {
       var result = [];
       result.push(this.widget.configuration.device);
 
       return result;
    }
-
 };
