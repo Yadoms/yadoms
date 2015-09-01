8ff203fe6f60e36e90c3bea11f56012d786c9765
jmb42 <jmb42@8f66d739-0145-4685-a3fe-f7fe687021a9>
2015-09-01 17:06:48 +0000

    Widget counter-display ====================== - Add first release.

---
+++ trunk/sources/www/widgets/counter-display/viewModel.js
@@ -0,0 +1,118 @@
+widgetViewModelCtor =
+
+/**
+ * Create a Counter Display ViewModel
+ * @constructor
+ */
+function CounterDisplayViewModel() {
+   //observable data
+   this.text = ko.observable("");
+   this.data = ko.observable(0).extend({ numeric: 1 });
+   this.unit = ko.observable("");
+   this.fontSize = ko.observable (20);
+   
+   this.minimumIntegerDigit = 9;
+   this.odometer = null;
+
+   //widget identifier
+   this.widget = null;
+
+   /**
+    * Initialization method
+    * @param widget widget class object
+    */
+   this.initialize = function(widget) {
+      this.widget = widget;
+	  
+	odometerOptions = { auto: false }; // Disables auto-initialization
+	
+	  var elementID = "widget-counter-" + this.widget.id; // Unique ID
+  
+  // Initialisation of a unique div associated to this widget
+	var elem = $('<div />').attr({
+	id: elementID,
+	"class":"odometer"
+	}).appendTo( "#widget-" + this.widget.id );
+	
+	// For each odometer, initialize with the theme passed in:
+	this.odometer = new Odometer({ el: $("#" + elementID + ".odometer")[0], format: '(.ddd)', value: 0, theme: 'car', duration: 1000, selector: '.my-numbers', minimumIntegerDigit: this.minimumIntegerDigit });
+   };
+
+   /**
+    * FontSize Method for the size of the Data to display
+    */   
+   
+   this.fontStyleCSS = ko.computed(function() {
+	   return {"font-size": this.fontSize() + "px"};
+   }, this );
+   
+   this.configurationChanged = function() {
+      this.text(this.widget.configuration.text);
+
+      var self = this;
+	  
+      //we get the unit of the keyword
+      KeywordManager.get(self.widget.configuration.device.keywordId, function(keyword) {
+		  
+		  // Read the unit
+         self.unit($.t(keyword.units));
+		 
+		 //Set the Unit for odometer
+		 self.odometer.SetUnit( self.unit() );
+		 
+		 self.resizefont();
+/*		 
+		 //Compute the font-size needed.
+		 self.fontSize ( ((self.widget.width() - 9 - ( (self.minimumIntegerDigit / 3) - 1)*8.33) / (self.minimumIntegerDigit + self.unit().length) ) *1.11 );
+		 
+		 //Change the font-size value
+		 var elementID = "widget-counter-" + self.widget.id; // Unique ID
+		 $("div#" + elementID + ".odometer").css({"font-size": self.fontSize() + "px", "margin":"auto", "display": "table"});
+*/
+      });
+   }
+   
+   /**
+    * New acquisition handler
+    * @param searchedDevice Device on which new acquisition was received
+    * @param data Acquisition data
+    */
+   this.onNewAcquisition = function(device, data) {
+      var self = this;	  
+	  
+	  var elementID = "widget-counter-" + self.widget.id; // Unique ID
+	  
+      if (device == self.widget.configuration.device) 
+	  {
+         //it is the good device
+		 $("#" + elementID + ".odometer").html( data.value );
+      }
+   };
+
+   this.resizefont = function()
+   {
+	   self = this;
+	   
+	   var elementID = "widget-counter-" + self.widget.id; // Unique ID
+	   
+       //Compute the font-size needed.
+	   self.fontSize ( ((this.widget.width() - 9 - ( (this.minimumIntegerDigit / 3) - 1)*8.33) / (this.minimumIntegerDigit + self.unit().length) ) *1.11 );
+	   
+	   //Change the font-size value
+	   $("div#" + elementID + ".odometer").css({"font-size": self.fontSize() + "px", "margin":"auto", "display": "table"});
+   }
+   
+   this.resized = function() 
+   {
+       var self = this;
+   
+	   self.resizefont();
+   };	   
+   
+   this.getDevicesForAcquisitions = function() {
+      var result = [];
+      result.push(this.widget.configuration.device);
+
+      return result;
+   }
+};
