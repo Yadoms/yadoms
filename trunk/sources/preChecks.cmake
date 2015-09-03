eddd1200f52ee27be6cb47edf8857cc74cc701a1
sgallou <sgallou@8f66d739-0145-4685-a3fe-f7fe687021a9>
2015-09-03 07:55:51 +0000

    * Fix too quick commit ! * Fix cmake warning in preChecks.cmake

--- trunk/sources/preChecks.cmake
+++ trunk/sources/preChecks.cmake
@@ -5,12 +5,12 @@
 MESSAGE(STATUS "Compiler = ${CMAKE_CXX_COMPILER_ID}")
 MESSAGE(STATUS "Compiler Version = ${CMAKE_CXX_COMPILER_VERSION}")
 
-if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+if(${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU")
     # require at least gcc 4.5 (for boost::log)
     if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 4.5)
         message(FATAL_ERROR "GCC version must be at least 4.5!")
     endif()
-elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+elseif (${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
     # require at least clang 3.2
     if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 3.2)
         message(FATAL_ERROR "Clang version must be at least 3.2!")
