6179a2282cb2fd471d087aaef2cf12e48124efb4
Sébastien Gallou <sgallou@users.noreply.github.com>
2015-09-07 13:24:30 +0000

    Plugin MegatecUps ================== * Simplify Power managment, removing functionnalities that are not responsability of the plugin

--- sources/plugins/MegatecUps/IMegatecUpsConfiguration.h
+++ sources/plugins/MegatecUps/IMegatecUpsConfiguration.h
@@ -8,17 +8,6 @@ class IMegatecUpsConfiguration
 {
 public:
    //--------------------------------------------------------------
-   /// \brief	    Power failure management choice
-   //--------------------------------------------------------------
-   enum EPowerFailureManagement
-   {
-      kNotifyImmediately = 0,    ///< Notify immediately Yadoms when AC power failure is detected
-      kFilter,                   ///< Notify Yadoms when AC power failure is detected for a certain time
-      kLowBattery,               ///< Notify Yadoms when AC power failure is detected and battery becomes low
-      kRemainingBattery          ///< Notify Yadoms when AC power failure is detected and battery comes under a threshold
-   };
-
-   //--------------------------------------------------------------
    /// \brief	    Destructor
    //--------------------------------------------------------------
    virtual ~IMegatecUpsConfiguration() {}
@@ -40,24 +29,6 @@ public:
    /// \return     true if UPS beep should be enable
    //--------------------------------------------------------------
    virtual bool upsBeepEnable() const = 0;
-
-   //--------------------------------------------------------------
-   /// \brief	    Get the power failure management choice
-   /// \return     Power failure management choice
-   //--------------------------------------------------------------
-   virtual EPowerFailureManagement powerFailureManagement() const = 0;
-
-   //--------------------------------------------------------------
-   /// \brief	    Get the power failure filter delay
-   /// \return     Delay (seconds), 0 means no filter
-   //--------------------------------------------------------------
-   virtual unsigned int powerFailureFilterDelay() const = 0;
-
-   //--------------------------------------------------------------
-   /// \brief	    Get the power failure remaining battery threshold
-   /// \return     remaining battery voltage threshold (V)
-   //--------------------------------------------------------------
-   virtual double powerFailureRemainingBatteryThreshold() const = 0;
 
    //--------------------------------------------------------------
    /// \brief	    Get the output shutdown delay
