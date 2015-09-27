78746b84a13ff493503def41825566e067f309e4
sgallou <sgallou@8f66d739-0145-4685-a3fe-f7fe687021a9>
2015-09-27 13:41:21 +0000

    dashboard/devices ============== * Fix devices state display when unknown (no acquisition)

--- trunk/sources/www/js/objects/acquisition.js
+++ trunk/sources/www/js/objects/acquisition.js
@@ -21,3 +21,21 @@ Acquisition.prototype.toJSON = function () {
       value: this.value
    };
 };
+
+/**
+ * Creates an empty Acquisition
+ * @constructor
+ */
+function EmptyAcquisition() {
+   this.date = "";
+   this.keywordId = "";
+   this.value = "";
+}
+
+EmptyAcquisition.prototype.toJSON = function () {
+   return {
+      date: this.date,
+      keywordId: this.keywordId,
+      value: this.value
+   };
+};
