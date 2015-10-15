5bfd9f5c76b4a0c5699a27d8948d214682799d68
lgm42 <lgm42@8f66d739-0145-4685-a3fe-f7fe687021a9>
2015-10-15 12:21:39 +0000

    GUI : Made some tidies in css files Chart YadomsPromise object have been generelized in promises.js and now named PromiseCounter Install update dashboard page now display python script Interpreter

--- trunk/sources/www/js/objects/automation/automation-interpreter.js
+++ trunk/sources/www/js/objects/automation/automation-interpreter.js
@@ -8,4 +8,19 @@ function AutomationInterpreter(name) {
    assert(!isNullOrUndefined(name), "name of a AutomationInterpreter must be defined");
 
    this.name = name;
+   this.package = null;
+   this.type = name.toLowerCase();
+}
+
+/**
+ * Fill the detailed provided by its package.json file
+ */
+AutomationInterpreter.prototype.fillDetails = function(json) {
+   assert(!isNullOrUndefined(json), "json must be defined");
+   assert(!isNullOrUndefined(json.type), "json.type must be defined");
+   assert(json.type == this.type, "json.type incompatible with type previously defined");
+   assert(!isNullOrUndefined(json.releaseType), "json.releaseType of a pluginInstance must be defined");
+   assert(!isNullOrUndefined(json.version), "json.version of a pluginInstance must be defined");
+
+   this.package = json;
 }
