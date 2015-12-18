94a3a633cabcfa9baa07f806f884398cc27415ff
Nicolas HILAIRE <nicolas.hilaire@gmail.com>
2015-12-18 14:40:05 +0000

    GUI : translation problem fixed. Recipient bug in progress

--- sources/www/js/objects/recipient.js
+++ sources/www/js/objects/recipient.js
@@ -10,13 +10,13 @@
  * @constructor
  */
 function Recipient(id, firstName, lastName, fields) {
-   assert(firstName !== undefined, "firstName of a recipient must be defined");
-   assert(lastName !== undefined, "lastName of a recipient must be defined");
+    assert(firstName !== undefined, "firstName of a recipient must be defined");
+    assert(lastName !== undefined, "lastName of a recipient must be defined");
 
-   this.id = id;
-   this.firstName = firstName;
-   this.lastName = lastName;
-   this.fields = fields;
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.fields = fields;
 }
 
 /**
@@ -32,12 +32,12 @@ Recipient.prototype.toString = function() {
  * @return {{id: *, firstName: string, lastName: string, fields: *}}
  */
 Recipient.prototype.toJSON = function () {
-   return {
-      id : this.id,
-      firstName: this.firstName,
-      lastName: this.lastName,
-      fields : this.fields
-   };
+    return {
+        id : this.id,
+        firstName: this.firstName,
+        lastName: this.lastName,
+        fields : this.fields
+    };
 };
 
 
@@ -48,8 +48,8 @@ Recipient.prototype.toJSON = function () {
  * There is no regex for mobile because too many phone number format.
  */
 Recipient.systemFields = {
-   "email": "^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$",
-   "mobile": ""
+    "email": "^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$",
+    "mobile": ""
 };
 
 
@@ -57,66 +57,78 @@ Recipient.systemFields = {
  * Merge current recipient data with plugin fields
  * @param allPlugins
  */
-Recipient.prototype.mergeFields = function () {
-   var self = this;
+Recipient.prototype.mergeFields = function() {
+    var self = this;
 
-   var definedFields = [];
-   $.each(this.fields, function(index, f){
-      definedFields.push(f);
-   });
+    var definedFields = [];
+    $.each(this.fields, function(index, f) {
+        definedFields.push(f);
+    });
 
-   this.fields = [];
+    this.fields = [];
+    debugger;
+    //add system fields
+    for (var systemField in Recipient.systemFields) {
+        var currentField = {
+            fieldName: systemField,
+            pluginType: "system",
+            pluginDefaultDisplayName: "system",
+            name: $.t("objects.recipient.fields." + systemField + ".name"),
+            description: $.t("objects.recipient.fields." + systemField + ".description"),
+            regexErrorMessage: $.t("objects.recipient.fields." + systemField + ".regexErrorMessage"),
+            regex: Recipient.systemFields[systemField],
+            value: ""
+        };
 
-   //add system fields
-   for(var systemField in Recipient.systemFields) {
-       var currentField = {
-           fieldName: systemField,
-           pluginType: "system",
-           pluginDefaultDisplayName : "system",
-           name: $.t("objects.recipient.fields." + systemField + ".name"),
-           description: $.t("objects.recipient.fields." + systemField + ".description"),
-           regexErrorMessage: $.t("objects.recipient.fields." + systemField + ".regexErrorMessage"),
-           regex: Recipient.systemFields[systemField],
-           value: ""
-       };
-
-      //search the field value in definedFields (search if the recipient is already configured for this field)
-      $.each(definedFields, function(index, field) {
-         if(field.pluginType.toLowerCase() == "system" && field.fieldName.toUpperCase() == systemField.toUpperCase()) {
-            //the field match an already saved one, just reuse the value
-            currentField.value = field.value;
-         }
-      });
-
-      self.fields.push(currentField);
-   }
-
-   //add plugin fields from the cache array
-   Object.keys(PluginManager.pluginTypes).forEach(function(key, index) {
-      //for each recipient fields in the plugin
-      var plugin = PluginManager.pluginTypes[key];
-      var recipientFieldsFromPlugin = plugin.getRecipientFields();
-      for(var recipientField in recipientFieldsFromPlugin) {
-         //make a copy of the object
-         var currentField = $.extend(true, {}, recipientFieldsFromPlugin[recipientField]);
-
-         //force the field value to empty string
-         currentField.value = currentField.defaultValue;
-
-         //search the field value in definedFields (search if the recipient is already configured for this field)
-         $.each(definedFields, function(index, field) {
-            if(field.pluginType.toUpperCase() == plugin.type.toUpperCase() && field.fieldName.toUpperCase() == recipientField.toUpperCase()) {
-               //the field match an already saved one, just reuse the value
-               currentField.value = field.value;
+        //search the field value in definedFields (search if the recipient is already configured for this field)
+        $.each(definedFields, function(index, field) {
+            if (field.pluginType.toLowerCase() == "system" && field.fieldName.toUpperCase() == systemField.toUpperCase()) {
+                //the field match an already saved one, just reuse the value
+                currentField.value = field.value;
             }
-         });
+        });
 
-         currentField.fieldName = recipientField;
-         currentField.pluginType = plugin.type;
-         currentField.pluginDefaultDisplayName = plugin.package.name;
+        self.fields.push(currentField);
+    }
 
-         self.fields.push(currentField);
-      }
-   });
-};
+    //add plugin fields from the cache array
+    //we take only plugin that have at least one instance
+    //we dynamically ask for plugins (async)
+    PluginInstanceManager.getAll(function(allInstances) {
+        var instanciatedTypes = [];
+        debugger;
+        $.each(allInstances, function(index, value) {
+            if (!PluginInstanceManager.isSystemCategory(value))
+                instanciatedTypes.push(value.type);
+        });
 
+        instanciatedTypes = duplicateRemoval(instanciatedTypes);
+
+        $.each(instanciatedTypes, function(key, value) {
+            //for each recipient fields in the plugin
+            var plugin = PluginManager.pluginTypes[value];
+            var recipientFieldsFromPlugin = plugin.getRecipientFields();
+            for (var recipientField in recipientFieldsFromPlugin) {
+                //make a copy of the object
+                var currentField = $.extend(true, {}, recipientFieldsFromPlugin[recipientField]);
+
+                //force the field value to empty string
+                currentField.value = currentField.defaultValue;
+
+                //search the field value in definedFields (search if the recipient is already configured for this field)
+                $.each(definedFields, function(index, field) {
+                    if (field.pluginType.toUpperCase() == plugin.type.toUpperCase() && field.fieldName.toUpperCase() == recipientField.toUpperCase()) {
+                        //the field match an already saved one, just reuse the value
+                        currentField.value = field.value;
+                    }
+                });
+
+                currentField.fieldName = recipientField;
+                currentField.pluginType = plugin.type;
+                currentField.pluginDefaultDisplayName = plugin.package.name;
+
+                self.fields.push(currentField);
+            }
+        });
+    });
+}
