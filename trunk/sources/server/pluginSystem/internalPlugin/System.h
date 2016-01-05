615b72d0d52f540a8bbd4aabce543744aca66b03
sgallou <sgallou@8f66d739-0145-4685-a3fe-f7fe687021a9>
2016-01-05 12:58:13 +0000

    Server - internal plugin ================== * Remove usefulness daylight keyword

--- trunk/sources/server/pluginSystem/internalPlugin/System.h
+++ trunk/sources/server/pluginSystem/internalPlugin/System.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <shared/plugin/IPlugin.h>
-#include <shared/plugin/information/IInformation.h>
 
 
 // Shortcut to yPluginApi namespace
@@ -27,25 +26,6 @@ namespace pluginSystem { namespace internalPlugin {
          // IPlugin implementation
          virtual void doWork(boost::shared_ptr<yApi::IYPluginApi> context);
          // [END] IPlugin implementation
-
-      protected:
-         //--------------------------------------------------------------
-         /// \brief	Create the next daylight events timers
-         /// \param[in] eventHandler Event handler used to create daylight timers
-         /// \return the actual daylight state (true is day, false if night)
-         //--------------------------------------------------------------
-         bool createDayLightTimers(shared::event::CEventHandler& eventHandler) const;
-
-         //--------------------------------------------------------------
-         /// \brief	Create the next daylight event timer
-         /// \param[in] eventHandler Event handler used to create timer
-         /// \param[in] daylightEventTimerId Event Id to use
-         /// \param[in] now Reference time for now
-         /// \param[in] daylightEventTime Time of the daylight event
-         //--------------------------------------------------------------
-         static void createSunEventTimer(shared::event::CEventHandler& eventHandler, int daylightEventTimerId,
-            const boost::posix_time::ptime& now, const boost::posix_time::ptime& daylightEventTime);
-
       };
 
 
