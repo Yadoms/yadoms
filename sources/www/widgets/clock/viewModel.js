4b7900af855033bbd1ee679434811e159a8cf2e7
lgm42 <nicolas.hilaire@gmail.com>
2016-02-03 09:08:42 +0100

    GUI : updated clock widget to set it compatible with new widget display

--- sources/www/widgets/clock/viewModel.js
+++ sources/www/widgets/clock/viewModel.js
@@ -5,82 +5,77 @@ widgetViewModelCtor =
  * @constructor
  */
 function ClockViewModel() {
-   /**
-    * Observable data
+    /**
+     * Observable data
+     */
+    var self = this;
+    this.time = ko.observable("00:00");
+
+    this.timeFontSize = ko.observable("1.5em");
+    this.timeFontSizeCSS = ko.computed(function () {
+        return { "font-size": self.timeFontSize() };
+    }, this);
+
+    /**
+     * Widget identifier
+     */
+    this.widget = null;
+
+    /**
+     * Initialization method
+     * @param widget widget class object
+     */
+    this.initialize = function (widget) {
+        this.widget = widget;
+
+        var target = this;
+
+        //we set the date into toolbar
+        ToolbarApi.appendCustomIcon(this.widget, "<span class=\"widget-clock-date-field\"></span>");
+
+        this.$dateField = this.widget.$gridWidget.find(".widget-clock-date-field");
+
+        setInterval(function () {
+            ClockViewModel_updateTime(target);
+        }, 1000);
+        ClockViewModel_updateTime(target);
+    };
+
+    /**
+    * Callback for a resized event
     */
-   this.time = ko.observable("00:00");
-   this.date = ko.observable("");
+    this.resized = function () {
+        if (this.widget.width() <= 200) {
+            this.timeFontSize("2.7em");
+        }
+        else if (this.widget.width() <= 300) {
+            this.timeFontSize("3.3em");
+        }
+        else {
+            this.timeFontSize("6.0em");
+        }
+    };
 
-   this.timeFontSize = ko.observable("1.5em");
-   this.timeFontSizeCSS = ko.computed(function() {
-      return {"font-size": this.timeFontSize()};
-   }, this);
-
-   this.dateFontSize = ko.observable("1.0em");
-   this.dateFontSizeCSS = ko.computed(function() {
-      return {"font-size": this.dateFontSize()};
-   }, this);
-
-   /**
-    * Widget identifier
-    */
-   this.widget = null;
-
-   /**
-    * Initialization method
-    * @param widget widget class object
-    */
-   this.initialize = function(widget) {
-      this.widget = widget;
-
-      var target = this;
-      setInterval(function() {
-         ClockViewModel_updateTime(target)
-      }, 1000);
-      ClockViewModel_updateTime(target);
-   };
-
-   /**
-   * Callback for a resized event
-   */
-   this.resized = function() {
-      if (this.widget.width() <= 200) {
-         this.dateFontSize("1.0em");
-         this.timeFontSize("2.7em");
-      }
-      else if (this.widget.width() <= 300) {
-         this.dateFontSize("1.0em");
-         this.timeFontSize("3.3em");
-      }
-      else {
-         this.dateFontSize("1.5em");
-         this.timeFontSize("6.0em");
-      }
-   };
-
-   this.configurationChanged = function() {
-   };
-
-   };
+};
 
 /**
  * Update the time onto the widget
  * @param target
  */
 function ClockViewModel_updateTime(target) {
-   var currentTime = new Date ( );
-   var currentHours = currentTime.getHours ( );
-   var currentMinutes = currentTime.getMinutes ( );
-   //var currentSeconds = currentTime.getSeconds ( );
+    var currentTime = new Date();
+    var currentHours = currentTime.getHours();
+    var currentMinutes = currentTime.getMinutes();
+    //var currentSeconds = currentTime.getSeconds ( );
 
-   // Pad the minutes and seconds with leading zeros, if required
-   currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
-   //currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+    // Pad the minutes and seconds with leading zeros, if required
+    currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
+    //currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
 
-   // Compose the string for display
-   var currentTimeString = currentHours + ":" + currentMinutes;// + ":" + currentSeconds ;
+    // Compose the string for display
+    var currentTimeString = currentHours + ":" + currentMinutes;// + ":" + currentSeconds ;
 
-   // Update the date and time display
-   target.time(currentTimeString);
-   target.date(moment().format("LL"));
+    // Update the date and time display
+    target.time(currentTimeString);
+    target.$dateField.text(moment().format("LL"));
 }
