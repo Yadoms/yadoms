e9c1b5e8bbe579841678e12ce5d4ea15c10fc5a1
Sébastien GALLOU <sebastien.gallou@gmail.com>
2016-02-05 09:34:09 +0100

    User tests : add common modals methods

---
+++ tests/user/modals.py
@@ -0,0 +1,19 @@
+﻿
+
+
+class RemoveObjectConfirmationModal():
+   """ Operations on delete object (plugind, rule...) confirmation modal """
+   
+   def __init__(self, removeObjectConfirmationModalWebElement):
+       self.__removeObjectConfirmationModalWebElement = removeObjectConfirmationModalWebElement
+   
+   def getConfirmationModalText(self):
+      return self.__removeObjectConfirmationModalWebElement.find_element_by_class_name("modal-body").text
+
+   def getConfirmationModalOkButton(self):
+      footer = self.__removeObjectConfirmationModalWebElement.find_element_by_class_name("modal-footer")
+      return footer.find_element_by_class_name("btn-confirm")
+      
+   def getConfirmationModalCancelButton(self):
+      footer = self.__removeObjectConfirmationModalWebElement.find_element_by_class_name("modal-footer")
+      return footer.find_element_by_class_name("btn-cancel")
