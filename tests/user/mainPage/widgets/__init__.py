af04ded8779555570ba96a9d932102c0b6faaa95
sgallou <sebastien.gallou@gmail.com>
2016-03-11 14:06:55 +0100

    Add widget creation user test

---
+++ tests/user/mainPage/widgets/__init__.py
@@ -0,0 +1,145 @@
+﻿from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as Condition
+from selenium.webdriver.common.keys import Keys
+from configurationPanel import ConfigurationPanel
+import tools
+import re
+
+   
+def getCustomizeButton(browser):
+   return browser.find_element_by_id("customizeButton")
+
+
+def isCustomizing(browser):
+   return getAddWidgetButton(browser).is_displayed()
+
+
+def enterCustomizingMode(browser):
+   getCustomizeButton(browser).click()
+   assert tools.waitUntil(lambda: isCustomizing(browser))
+
+
+def exitCustomizingMode(browser):
+   getCustomizeButton(browser).click()
+   assert tools.waitUntil(lambda: not isCustomizing(browser))
+
+
+def getAddWidgetButton(browser):
+   return browser.find_element_by_id("btn-add-widget")
+   
+   
+def waitNewWidgetModal(browser):
+   WebDriverWait(browser, 10).until(Condition.visibility_of_element_located((By.ID, "new-widget-modal")))
+   return NewWidgetModal(browser.find_element_by_id("new-widget-modal"))
+   
+   
+def waitConfigureWidgetModal(browser):
+   WebDriverWait(browser, 10).until(Condition.visibility_of_element_located((By.ID, "configure-widget-modal")))
+   return ConfigureWidgetModal(browser.find_element_by_id("configure-widget-modal"))
+   
+   
+def getCurrentPage(browser):
+   pagesContainer = browser.find_element_by_id("tabContainer")
+   return pagesContainer.find_element_by_css_selector('div.widgetPage.active')
+   
+   
+def getWidgetGrid(browser, page):
+   return page.find_element_by_class_name("grid")
+
+   
+def listWidgetsOnCurrentPage(browser):
+   widgetContainer = getWidgetGrid(getCurrentPage(browser), getCurrentPage(browser)).find_elements_by_class_name("widget")
+   widgets = []
+   for widget in widgetContainer:
+      widgets.append(Widget(widget))
+   return widgets
+
+   
+def findWidgetOnCurrentPage(browser, widgetType, widgetTitle):
+   widgets = listWidgetsOnCurrentPage(browser)
+   for widget in widgets:
+      if (widget.getType() == widgetType and widget.getTitle() == widgetTitle):
+         return widget
+
+   # Not found
+   return None
+
+   
+   
+class NewWidgetModal():
+   """ Operations on new widget modal (widget selection) """
+   
+   def __init__(self, newWidgetModalWebElement):
+       self.__newWidgetModalWebElement = newWidgetModalWebElement
+
+   def selectWidget(self, expectedWidgetName):
+      widgetList = WebDriverWait(self.__newWidgetModalWebElement, 10).until(Condition.visibility_of_element_located((By.ID, "ul-add-widget")))
+      for item in widgetList.find_elements_by_tag_name('button'):
+         if item.get_attribute('widget-type') == expectedWidgetName:
+            return item
+      print "selectWidget : Nothing to select, ", expectedWidgetName, " not found"
+      assert False
+
+   def getConfirmButton(self):
+      WebDriverWait(self.__newWidgetModalWebElement, 10).until(Condition.visibility_of_element_located((By.ID, "btn-confirm-add-widget")))
+      return self.__newWidgetModalWebElement.find_element_by_id("btn-confirm-add-widget")
+         
+   def getCloseButton(self):
+      return self.__newWidgetModalWebElement.find_element_by_id("btn-close-details")
+
+
+
+class ConfigureWidgetModal():
+   """ Operations on widget configuration modal """
+   
+   def __init__(self, configureWidgetModalWebElement):
+      self.__configureWidgetModalWebElement = configureWidgetModalWebElement
+      self.__configurationPanel = ConfigurationPanel(self.__configureWidgetModalWebElement)
+      
+   def getConfigurationPanel(self):
+      return self.__configurationPanel    
+
+   def getWidgetTitle(self):
+      return self.__configurationPanel.getItemByName('modals.configure-widget.titleName.name')
+      
+   def setWidgetTitle(self, newTitle):
+      titleField = self.getWidgetTitle()
+      titleField.send_keys(Keys.CONTROL + "a")
+      titleField.send_keys(Keys.DELETE)
+      titleField.send_keys(newTitle)
+         
+   def getConfirmButton(self):
+      return self.__configureWidgetModalWebElement.find_element_by_id("btn-confirm-configure-widget")
+      
+   def waitForClosed(self):
+      # TODO à garder ?
+      assert tools.waitUntil(lambda: 'display: none;' in self.__configureWidgetModalWebElement.get_attribute('style'))
+      print "self.__configureWidgetModalWebElement.get_attribute('style')", self.__configureWidgetModalWebElement.get_attribute('style')
+
+   def ok(self):
+      self.getConfirmButton().click()
+      self.waitForClosed()
+      
+      
+class Widget():
+   """ Class representing a displayed widget """
+   
+   def __init__(self, widgetWebElement):
+      self.__widgetWebElement = widgetWebElement 
+
+   def getType(self):
+      widgetBody = self.__widgetWebElement.find_element_by_class_name('panel-widget-body')
+      linkElement = widgetBody.find_element_by_tag_name('link')
+      hrefValue = linkElement.get_attribute('href')
+      return re.search('widgets/(.*)/widget.css', hrefValue).group(1)
+      
+   def getTitle(self):
+      return self.__widgetWebElement.find_element_by_class_name('panel-widget-title').text
+      
+   def getPosition(self):
+      return [int(self.__widgetWebElement.get_attribute('data-gs-x')), int(self.__widgetWebElement.get_attribute('data-gs-y'))]
+
+   def getSize(self):
+      return [int(self.__widgetWebElement.get_attribute('data-gs-width')), int(self.__widgetWebElement.get_attribute('data-gs-height'))]
+      
---
+++ tests/user/mainPage/widgets/testCreateWidget.py
@@ -0,0 +1,78 @@
+﻿import unittest
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+import database
+import scripts
+import yadomsServer
+import mainPage.widgets
+import i18n
+import tools
+import os.path
+
+class CreateWidget(unittest.TestCase):
+   """Create widget test"""
+   
+   def setUp(self):
+      database.deploy('OneFakePlugin')
+      scripts.deleteAll()
+      self.serverProcess = yadomsServer.start()
+      self.browser = webdriver.Firefox()
+      self.browser.implicitly_wait(10)
+      yadomsServer.openClient(self.browser)
+            
+            
+   def test_createWidget(self):
+      """Nominal test of widget creation"""
+      self.doTest(
+         "switch",
+         "My switch widget",
+         lambda editWidgetModal, widgetTitle: self.configureSwitchWidget(editWidgetModal, widgetTitle, 'fakeOnOffReadWriteSwitch', 'Switch'),
+         lambda widgetType, widgetTitle: self.checkCreateWidget(widgetType, widgetTitle, [0, 0], [1, 1]))
+      
+         
+   def configureSwitchWidget(self, editWidgetModal, widgetTitle, device, keyword):
+      editWidgetModal.setWidgetTitle(widgetTitle)
+      editWidgetModal.getConfigurationPanel().getAddObjectButton('widgets/switch:configurationSchema.devices.name').click()
+      selects = editWidgetModal.getConfigurationPanel().getItemsByName('widgets/switch:configurationSchema.devices.item.content.source.name')
+      Select(selects[0]).select_by_visible_text(device)
+      Select(selects[1]).select_by_visible_text(keyword)
+      editWidgetModal.ok()
+
+
+   def checkCreateWidget(self, widgetType, widgetTitle, widgetExpectedPosition, widgetExpectedSize):
+      widget = mainPage.widgets.findWidgetOnCurrentPage(self.browser, widgetType, widgetTitle)
+      self.assertIsNotNone(widget)
+      self.assertEqual(widget.getPosition(), widgetExpectedPosition)
+      self.assertEqual(widget.getSize(), widgetExpectedSize)
+
+         
+      
+   def doTest(self, widgetType, widgetTitle, configureWidgetFct, checkFct):
+      # Enter customizing mode
+      mainPage.widgets.enterCustomizingMode(self.browser)
+
+      # Open add widget modal
+      mainPage.widgets.getAddWidgetButton(self.browser).click()
+      newWidgetModal = mainPage.widgets.waitNewWidgetModal(self.browser)
+      
+      # Select widget
+      newWidgetModal.selectWidget(widgetType).click()
+      newWidgetModal.getConfirmButton().click()
+
+      # - Widget configuration
+      configureWidgetFct(mainPage.widgets.waitConfigureWidgetModal(self.browser), widgetTitle)
+      
+      # Exit customizing mode
+      mainPage.widgets.exitCustomizingMode(self.browser)
+      
+      
+      # Check created widget
+      checkFct(widgetType, widgetTitle)
+      
+      
+   def tearDown(self):
+      self.browser.close()
+      yadomsServer.stop(self.serverProcess)
+
+if __name__ == "__main__":
+   unittest.main()
