ddb8d7da7d86e09285cd6bf5311bb3b711cd42cc
sgallou <sebastien.gallou@gmail.com>
2016-03-16 11:19:34 +0100

    Add gauge widget creation test (actually fails because of a gauge widget bug)

--- tests/user/mainPage/widgets/testCreateWidget.py
+++ tests/user/mainPage/widgets/testCreateWidget.py
@@ -8,6 +8,7 @@ import mainPage.widgets
 import i18n
 import tools
 import os.path
+import notification
 
 class CreateWidget(unittest.TestCase):
    """Create widget test"""
@@ -21,52 +22,78 @@ class CreateWidget(unittest.TestCase):
       yadomsServer.openClient(self.browser)
             
             
-   def test_createWidget(self):
-      """Nominal test of widget creation"""
+   def test_createSwitchWidget(self):
+      print '=== Nominal test of switch widget creation ==='
       self.doTest(
          "switch",
          "My switch widget",
          lambda editWidgetModal, widgetTitle: self.configureSwitchWidget(editWidgetModal, widgetTitle, 'fakeOnOffReadWriteSwitch', 'Switch'),
          lambda widgetType, widgetTitle: self.checkCreateWidget(widgetType, widgetTitle, [0, 0], [1, 1]))
       
-         
    def configureSwitchWidget(self, editWidgetModal, widgetTitle, device, keyword):
+      print '  Change widget title'
       editWidgetModal.setWidgetTitle(widgetTitle)
+      print '  Select device'
       editWidgetModal.getConfigurationPanel().getAddObjectButton('widgets/switch:configurationSchema.devices.name').click()
       selects = editWidgetModal.getConfigurationPanel().getItemsByName('widgets/switch:configurationSchema.devices.item.content.source.name')
       Select(selects[0]).select_by_visible_text(device)
       Select(selects[1]).select_by_visible_text(keyword)
+      print '  Confirm'
       editWidgetModal.ok()
 
 
    def checkCreateWidget(self, widgetType, widgetTitle, widgetExpectedPosition, widgetExpectedSize):
+      print '  Check no notification occurs'
+      self.assertTrue(notification.noNotification(self.browser))
+      print '  Check widget present on main page'
       widget = mainPage.widgets.findWidgetOnCurrentPage(self.browser, widgetType, widgetTitle)
       self.assertIsNotNone(widget)
+      print '  Check widget position'
       self.assertEqual(widget.getPosition(), widgetExpectedPosition)
+      print '  Check widget size'
       self.assertEqual(widget.getSize(), widgetExpectedSize)
 
          
+   def test_createGaugeWidget(self):
+      print '=== Nominal test of gauge widget creation ==='
+      self.doTest(
+         "gauge",
+         "My gauge widget",
+         lambda editWidgetModal, widgetTitle: self.configureGaugeWidget(editWidgetModal, widgetTitle, 'fakeSensor2', 'temp1'),
+         lambda widgetType, widgetTitle: self.checkCreateWidget(widgetType, widgetTitle, [0, 0], [2, 2]))
+      
+   def configureGaugeWidget(self, editWidgetModal, widgetTitle, device, keyword):
+      print '  Change widget title'
+      editWidgetModal.setWidgetTitle(widgetTitle)
+      print '  Select device'
+      selects = editWidgetModal.getConfigurationPanel().getItemsByName('widgets/gauge:configurationSchema.device.name')
+      Select(selects[0]).select_by_visible_text(device)
+      Select(selects[1]).select_by_visible_text(keyword)
+      print '  Confirm'
+      editWidgetModal.ok()
+
+
       
    def doTest(self, widgetType, widgetTitle, configureWidgetFct, checkFct):
-      # Enter customizing mode
+      print 'Enter customizing mode'
       mainPage.widgets.enterCustomizingMode(self.browser)
 
-      # Open add widget modal
+      print 'Open add widget modal'
       mainPage.widgets.getAddWidgetButton(self.browser).click()
       newWidgetModal = mainPage.widgets.waitNewWidgetModal(self.browser)
       
-      # Select widget
+      print 'Select widget'
       newWidgetModal.selectWidget(widgetType).click()
       newWidgetModal.getConfirmButton().click()
 
-      # - Widget configuration
+      print 'Configure widget'
       configureWidgetFct(mainPage.widgets.waitConfigureWidgetModal(self.browser), widgetTitle)
       
-      # Exit customizing mode
+      print 'Exit customizing mode'
       mainPage.widgets.exitCustomizingMode(self.browser)
       
       
-      # Check created widget
+      print 'Check created widget'
       checkFct(widgetType, widgetTitle)
       
       
