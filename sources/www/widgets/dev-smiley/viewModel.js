a0ad17775aa89ce8b77c0cd4b67bbbbb27a92e79
lgm42 <nicolas.hilaire@gmail.com>
2016-03-25 12:33:19 +0100

    updated widget smiley

--- sources/www/widgets/dev-smiley/viewModel.js
+++ sources/www/widgets/dev-smiley/viewModel.js
@@ -2,24 +2,17 @@
  * Create a Smiley ViewModel
  * @constructor
  */
- 
-widgetViewModelCtor = function() {
-   /**
-    * Initialization method
-    * @param widget widget class object
-    */
-   this.initialize = function () {
-      var self = this;
-      self.widgetApi.loadCss([
-         "widgets/dev-smiley/widget.css"
-      ]).done(function() {
-         self.widgetApi.find(".widget-api-textfit").fitText();
-      });
-   };
 
-   this.configurationChanged = function() {
-      
-   }
+widgetViewModelCtor = function () {
+    /**
+     * Initialization method
+     * @param widget widget class object
+     */
+    this.initialize = function () {
+        var self = this;
 
-   
+        self.widgetApi.toolbar({
+            activated: false
+        });
+    };
 };
