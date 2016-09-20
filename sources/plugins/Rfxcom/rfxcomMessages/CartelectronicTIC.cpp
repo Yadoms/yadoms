45890d5a9c5df580f9e43dcf000757edfa7b184e
jmberhault <jean-michel@berhault.org>
2016-09-20 22:07:20 +0200

    Plugin RFXCOM
    =========
    - fix a bug with CartelectronicTIC protocol.
    - Adding a period due to evolution of the CartelectronicTIC protocol.

--- sources/plugins/Rfxcom/rfxcomMessages/CartelectronicTIC.cpp
+++ sources/plugins/Rfxcom/rfxcomMessages/CartelectronicTIC.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "CartelectronicTIC.h"
+#include "boost/lexical_cast.hpp"
 
 // Shortcut to yPluginApi namespace
 namespace yApi = shared::plugin::yPluginApi;
@@ -60,17 +61,18 @@ namespace rfxcomMessages
 
       m_counter1 = boost::make_shared<yApi::historization::CEnergy>(NameCounter1);
       m_keywords.push_back(m_counter1);
+
+      m_counter1->set((rbuf.TIC.counter1_0 << 24) + (rbuf.TIC.counter1_1 << 16) + (rbuf.TIC.counter1_2 << 8) + (rbuf.TIC.counter1_3));
+
       if (!NameCounter2.empty())
       {
          m_counter2 = boost::make_shared<yApi::historization::CEnergy>(NameCounter2);
          m_keywords.push_back(m_counter2);
+
+         m_counter2->set((rbuf.TIC.counter2_0 << 24) + (rbuf.TIC.counter2_1 << 16) + (rbuf.TIC.counter2_2 << 8) + (rbuf.TIC.counter2_3));
       }
 
-      m_counter1->set((rbuf.TIC.counter1_0 << 24) + (rbuf.TIC.counter1_1 << 16) + (rbuf.TIC.counter1_2 << 8) + (rbuf.TIC.counter1_3));
-      m_counter2->set((rbuf.TIC.counter2_0 << 24) + (rbuf.TIC.counter2_1 << 16) + (rbuf.TIC.counter2_2 << 8) + (rbuf.TIC.counter2_3));
-
-
-      m_apparentePower->set((rbuf.TIC.power_H << 8) + rbuf.TIC.power_L);
+      m_apparentePower->set(boost::lexical_cast<double>((rbuf.TIC.power_H << 8) + rbuf.TIC.power_L));
       m_period->set(teleInfo::specificHistorizers::EPeriod(rbuf.TIC.contract_type & 0x0F));
    }
 
