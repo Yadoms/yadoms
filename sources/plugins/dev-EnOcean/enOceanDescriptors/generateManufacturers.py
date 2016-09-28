d63a2660a2b8819d2727249dcfe60fad184c50b2
sgallou <sebastien.gallou@gmail.com>
2016-09-28 09:04:40 +0200

    WIP

--- sources/plugins/dev-EnOcean/enOceanDescriptors/generateManufacturers.py
+++ sources/plugins/dev-EnOcean/enOceanDescriptors/generateManufacturers.py
@@ -47,8 +47,48 @@ def delcareEnum(cppOutFile, cppEnumName, xmlInputFile, xmlElementPath, xmlNameKe
 
 
 #-------------------------------------------------------------------------------
-def implementMap(cppOutFile, cppEnumName, xmlInputFile, xmlElementPath, xmlNameKey, xmlValueKey = None):
-   pass
+def declareMap(cppOutFile, cppMapName, cppKeyType, cppValueType, isStatic = False, isConst = False):
+   if isStatic:
+      cppOutFile.write("static ");
+   if isConst:
+      cppOutFile.write("const ");
+   cppOutFile.write("std::map<" + cppKeyType + ", " + cppValueType + "> " + cppMapName + ";\n")
+
+
+#-------------------------------------------------------------------------------
+def implementMap(cppOutFile, cppMapName, cppKeyType, cppValueType, xmlInputFile, xmlElementPath, xmlNameKey):
+   cppOutFile.write("const std::map<" + cppKeyType + ", " + cppValueType + "> " + cppMapName + " = boost::assign::map_list_of\n")
+   for child in getXmlEnumNodes(xmlInputFile, xmlElementPath):
+      cppOutFile.write("   (" + toEnumValueName(child.find(xmlNameKey).text))
+      cppOutFile.write(", \"" + child.find(xmlNameKey).text.strip() + "\")\n")
+   cppOutFile.write(";\n")
+
+
+#-------------------------------------------------------------------------------
+def declareFunction(cppOutFile, cppReturnType, cppFunctionName, cppArgs, isVirtual = False, isStatic = False, isConst = False, isPureVirtual = False):
+   if isStatic:
+      cppOutFile.write("static ");
+   if isVirtual | isPureVirtual:
+      cppOutFile.write("virtual ");
+   cppOutFile.write(cppReturnType + " " + cppFunctionName + "(" + cppArgs + ")")
+   if isConst:
+      cppOutFile.write(" const");
+   if isPureVirtual:
+      cppOutFile.write(" = 0");
+   cppOutFile.write(";\n");
+
+
+#-------------------------------------------------------------------------------
+def implementFunction(cppOutFile, cppReturnType, cppNamespaceName, cppFunctionName, cppArgs, isStatic = False, isConst = False):
+   if isStatic:
+      cppOutFile.write("static ");
+   cppOutFile.write(cppReturnType + " ");
+   if cppNamespaceName:
+      cppOutFile.write(cppNamespaceName + "::");
+   cppOutFile.write(cppFunctionName + "(" + cppArgs + ")");
+   if isConst:
+      cppOutFile.write(" const");
+   cppOutFile.write(" {\n");
 
 
 #-------------------------------------------------------------------------------
@@ -64,7 +104,9 @@ def generateHeader() :
       f.write("CManufacturers();\n")
       f.write("virtual ~CManufacturers();\n")
       f.write("\n")
-      f.write("static const std::string& name(unsigned int id);\n")
+      declareFunction(f, "const std::string&", "name", "unsigned int id", False, True, False);
+      f.write("private:\n")
+      declareMap(f, "ManufacturersMap", "int", "std::string", True, True);
       f.write("};\n")
 
 #-------------------------------------------------------------------------------
@@ -75,18 +117,14 @@ def generateSource() :
       f.write("#include \"{}\"\n".format(os.path.basename(headerPath)))
       f.write("\n")
 
-      f.write("const std::map<int, std::string> ManufacturersMap = boost::assign::map_list_of\n")
-      for child in getXmlEnumNodes(xmlInputFilePath, "manufacturers.manufacturer"):
-         f.write("   (CManufacturers::" + toEnumValueName(child.find("name").text))
-         f.write(", \"" + child.find("name").text.strip() + "\")\n")
-      f.write(";\n")
+      implementMap(f, "CManufacturers::ManufacturersMap", "int", "std::string", xmlInputFilePath, "manufacturers.manufacturer", "name")
 
       f.write("\n")
       f.write("CManufacturers::CManufacturers(){}\n")
       f.write("\n")
       f.write("CManufacturers::~CManufacturers(){}\n")
       f.write("\n")
-      f.write("const std::string& CManufacturers::name(unsigned int id) {\n")
+      implementFunction(f, "const std::string&", "CManufacturers", "name", "unsigned int id");
       f.write("   return ManufacturersMap.at(id);\n")
       f.write("}\n")
 
