2fb407ad4e71fabafc746ab1590955b1c11c9330
jano42 <decoretjm@gmail.com>
2016-09-30 10:51:17 +0200

    * CMake : manage Windows XP compatibility from cmake script * NSIS : install vcredict 2013 (needed, due to postgresql lib dependency) only if not already installed * Add vcredict installer to distribution

--- cmake_windows.cmd
+++ cmake_windows.cmd
@@ -7,4 +7,22 @@ setlocal & pushd .
 
 ::Move to destination folder
 cd /D %~dp0/projects
-cmake %~dp0/sources
+
+
+@echo Yadoms build for Windows
+@echo ========================
+@echo.
+@echo To enable Windows XP compatiblity, please enter the platform toolset
+@echo.
+@echo v120_xp : platform toolset "Visual Studio 2013 - WindowsXP"
+@echo v140_xp : platform toolset "Visual Studio 2015 - WindowsXP"
+@echo Leave empty to disable Windows XP compatibility
+
+set /p input=""
+
+
+IF [%input%] == [] (
+	cmake %~dp0/sources
+) else (
+	cmake %~dp0/sources -T %input%
+)
