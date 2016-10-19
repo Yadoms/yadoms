67d39531d40f32774b7b51c89beeac24699b1820
sgallou <sebastien.gallou@gmail.com>
2016-10-19 16:04:26 +0200

    Cleanup

--- sources/server/web/rest/service/Configuration.h
+++ sources/server/web/rest/service/Configuration.h
@@ -3,38 +3,49 @@
 #include "IRestService.h"
 #include "dataAccessLayer/IConfigurationManager.h"
 
-namespace web { namespace rest { namespace service {
-
-   class CConfiguration : public IRestService
+namespace web
+{
+   namespace rest
    {
-   public:
-      explicit CConfiguration(boost::shared_ptr<dataAccessLayer::IConfigurationManager> configurationManager);
-      virtual ~CConfiguration();
+      namespace service
+      {
+         class CConfiguration : public IRestService
+         {
+         public:
+            explicit CConfiguration(boost::shared_ptr<dataAccessLayer::IConfigurationManager> configurationManager);
+            virtual ~CConfiguration();
 
-   public:
-      // IRestService implementation
-      virtual void configureDispatcher(CRestDispatcher & dispatcher);
-      // [END] IRestService implementation
-
-
-      static const std::string & getRestKeyword();
-
-   public:
-      shared::CDataContainer getConfiguration(const std::vector<std::string> & parameters, const std::string & requestContent);
-      shared::CDataContainer getSectionConfigurations(const std::vector<std::string> & parameters, const std::string & requestContent);
-      shared::CDataContainer getAllConfigurations(const std::vector<std::string> & parameters, const std::string & requestContent);
-
-      shared::CDataContainer createOneConfiguration(const std::vector<std::string> & parameters, const std::string & requestContent);
-      shared::CDataContainer updateOneConfiguration(const std::vector<std::string> & parameters, const std::string & requestContent);
-      shared::CDataContainer updateAllConfigurations(const std::vector<std::string> & parameters, const std::string & requestContent);
-      shared::CDataContainer deleteOneConfiguration(const std::vector<std::string> & parameters, const std::string & requestContent);
-
-   private:
-      boost::shared_ptr<dataAccessLayer::IConfigurationManager> m_configurationManager;
-      static std::string m_restKeyword;
-   };
+         public:
+            // IRestService implementation
+            void configureDispatcher(CRestDispatcher& dispatcher) override;
+            // [END] IRestService implementation
 
 
-} //namespace service
-} //namespace rest
+            static const std::string& getRestKeyword();
+
+         public:
+            shared::CDataContainer getConfiguration(const std::vector<std::string>& parameters,
+                                                    const std::string& requestContent) const;
+            shared::CDataContainer getSectionConfigurations(const std::vector<std::string>& parameters,
+                                                            const std::string& requestContent) const;
+            shared::CDataContainer getAllConfigurations(const std::vector<std::string>& parameters,
+                                                        const std::string& requestContent) const;
+
+            shared::CDataContainer createOneConfiguration(const std::vector<std::string>& parameters
+                                                          , const std::string& requestContent) const;
+            shared::CDataContainer updateOneConfiguration(const std::vector<std::string>& parameters,
+                                                          const std::string& requestContent) const;
+            shared::CDataContainer updateAllConfigurations(const std::vector<std::string>& parameters,
+                                                           const std::string& requestContent) const;
+            shared::CDataContainer deleteOneConfiguration(const std::vector<std::string>& parameters,
+                                                          const std::string& requestContent) const;
+
+         private:
+            boost::shared_ptr<dataAccessLayer::IConfigurationManager> m_configurationManager;
+            static std::string m_restKeyword;
+         };
+      } //namespace service
+   } //namespace rest
 } //namespace web 
+
+
