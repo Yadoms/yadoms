46b5682b5de91a5ae960e13437bce0dfef6e482b
sgallou <sebastien.gallou@gmail.com>
2016-10-30 17:43:27 +0100

    Fix log

--- sources/server/pluginSystem/Information.cpp
+++ sources/server/pluginSystem/Information.cpp
@@ -61,7 +61,7 @@ namespace pluginSystem
       {
          // Set plugin as not supported
          m_isSupportedOnThisPlatform = false;
-         throw shared::exception::CInvalidParameter(pluginPath.stem().string() + std::string("Error reading package.json : data not found : ") + e.what());
+         throw shared::exception::CInvalidParameter(pluginPath.stem().string() + std::string(" : Error reading package.json : data not found : ") + e.what());
       }
 
       auto pluginFolder = m_path.filename().string();
