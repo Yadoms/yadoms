938f84c95c9ea5dfdc915953a85eeb8e5125ccb2
lgm42 <nicolas.hilaire@gmail.com>
2016-11-04 13:59:07 +0100

    updated update script to correct bugs on pid detection and behavior

--- update/scripts/update.sh
+++ update/scripts/update.sh
@@ -3,7 +3,7 @@
 #Manage script parameters
 yadomsPath=$1
 timeoutYadoms=${2:-60}
-appNameToWait=gnome-calculator
+appNameToWait=yadoms
 
 echo "Starting...."
 echo "    Param : Path : $yadomsPath"
@@ -21,39 +21,40 @@ pid=$(ps -ef | awk '$8=="'"$appNameToWait"'" {print $2}')
 
 
 #wait for the pid to end (manage a timeout in seconds)
-if [ $? -eq 0 ]
+if [ ! -z $pid ]
 then
-    echo "Waiting for $appNameToWait to exit..."
+   echo "Waiting for $appNameToWait to exit..."
 
-    waitedTime=0
-    while ps -p $pid > /dev/null; do 
-	sleep 1; 
-	waitedTime=$((waitedTime+1))
-	if [ $waitedTime -ge $timeoutYadoms ] 
-	then
-	    echo "$appNameToWait is still running after $timeoutYadoms seconds" >&2
+   waitedTime=0
+   while ps -p $pid > /dev/null; do 
+      sleep 1; 
+      waitedTime=$((waitedTime+1))
+      if [ $waitedTime -ge $timeoutYadoms ] 
+      then
+         echo "$appNameToWait is still running after $timeoutYadoms seconds" >&2
             echo "Could not apply script"
-	    exit 1
-	fi
-    done;
+         exit 1
+      fi
+   done;
 
-    echo "Process $appNameToWait is now stopped"
-    echo "Updating Yadoms ..."
-
-    BASEDIR=$(dirname $0)
-
-    cp -R "$BASEDIR/package/*.*" "$yadomsPath"
-
-    echo "Yadoms updated successfully"
-
-    echo "Restarting Yadoms ..."
-
-    $yadomsPath/$appNameToWait &
-
+   echo "Process $appNameToWait is now stopped"
 else
     echo "$me failed to find process with name $appNameToWait" >&2
-    exit 1
 fi
+
+echo "Updating Yadoms ..."
+
+BASEDIR=$(dirname $0)
+
+cp -R "$BASEDIR/package/*" "$yadomsPath"
+
+echo "Yadoms updated successfully"
+
+echo "Restarting Yadoms ..."
+
+$yadomsPath/$appNameToWait &
+
+
 
 
 
