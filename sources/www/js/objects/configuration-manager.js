45bb0a28daffa447efcb5121c0106e0c5ad3619b
jmberhault <jean-michel@berhault.org>
2016-11-26 13:53:04 +0100

    WIP

--- sources/www/js/objects/configuration-manager.js
+++ sources/www/js/objects/configuration-manager.js
@@ -39,6 +39,9 @@ ConfigurationManager.items.system.basicAuthenticationUser = "basicAuthentication
 ConfigurationManager.items.system.basicAuthenticationPassword = "basicAuthenticationPassword";
 ConfigurationManager.items.system.basicAuthenticationPassword2 = "basicAuthenticationPassword2";
 
+ConfigurationManager.items.system.city = "city";
+ConfigurationManager.items.system.cityName = "cityName";
+
 ConfigurationManager.items.system.advancedParameters = "advancedParameters";
 ConfigurationManager.items.system.dateFormatString = "dateFormatString";
 
