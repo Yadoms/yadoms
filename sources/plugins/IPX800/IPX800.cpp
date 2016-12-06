f3ec1c8adaa07764a08b00305fd0138dc4cadfa8
jmberhault <jean-michel@berhault.org>
2016-12-06 20:45:21 +0100

    - clean up.

--- sources/plugins/IPX800/IPX800.cpp
+++ sources/plugins/IPX800/IPX800.cpp
@@ -98,8 +98,7 @@ void CIPX800::doWork(boost::shared_ptr<yApi::IYPluginApi> api)
 
                // Retrieve event information, if any !
                try { forceRefresh = api->getEventHandler().getEventData<bool>(); }
-               catch (shared::exception::CBadConversion&) { std::cout << "Bad dynamic cast" << std::endl; } // TODO : Voir tests CDataContainer
-               catch (shared::exception::CNullReference&) { std::cout << "Null reference" << std::endl; }
+               catch (shared::exception::CBadConversion&) { }
 
                m_ioManager->readAllIOFromDevice(api, forceRefresh);
             }
