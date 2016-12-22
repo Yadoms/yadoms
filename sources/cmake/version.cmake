3e20d5fdc17711a3ed84bc9167e55554749a7d4c
jano42 <decoretjm@gmail.com>
2016-12-22 14:06:27 +0100

    * CMake : fix yadoms version parsing from "Version.h" to make it compatible with SEMVER syntax (partially compatible because cmake is not 100% compliant with classic regex)

--- sources/cmake/version.cmake
+++ sources/cmake/version.cmake
@@ -1,3 +1,3 @@
 #This macro extract the Yadoms version from "Version.h"
-file (STRINGS "${CMAKE_SOURCE_DIR}/server/Version.h" YADOMS_VERSION REGEX "[0-9]\.[0-9]\.[0-9]\.[0-9]")
-STRING(REGEX MATCH "[0-9]\.[0-9]\.[0-9]\.[0-9]" YADOMS_VERSION ${YADOMS_VERSION})
+file (STRINGS "${CMAKE_SOURCE_DIR}/server/Version.h" YADOMS_VERSION REGEX "[0-9]\.[0-9]\.[0-9]")
+STRING(REGEX MATCH "[0-9]\.[0-9]\.[0-9]" YADOMS_VERSION ${YADOMS_VERSION})
