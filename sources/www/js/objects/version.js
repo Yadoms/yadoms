767b509dbb7137b988cf1d44d5c56a65dfd44c1d
lgm42 <nicolas.hilaire@gmail.com>
2016-12-22 12:50:25 +0100

    WIP managing version following SEMVER

---
+++ sources/www/js/objects/version.js
@@ -0,0 +1,40 @@
+/** @module Version class 
+ * Following Semver 2.0.0
+*/
+
+/**
+ * Creates an instance of Version
+ * @constructor
+ */
+function Version(versionAsText) {
+   assert(!isNullOrUndefined(versionAsText), "versionAsText must be defined");
+
+   //TODO : check if valid
+
+   this.versionAsText_ = versionAsText;
+}
+
+//Regex for Semver
+//Taken from http://rgxdb.com/
+Version.regex = "/(?<=^[Vv]|^)(?:(?<major>(?:0|[1-9](?:(?:0|[1-9])+)*))[.](?<minor>(?:0|[1-9](?:(?:0|[1-9])+)*))[.](?<patch>(?:0|[1-9](?:(?:0|[1-9])+)*))(?:-(?<prerelease>(?:(?:(?:[A-Za-z]|-)(?:(?:(?:0|[1-9])|(?:[A-Za-z]|-))+)?|(?:(?:(?:0|[1-9])|(?:[A-Za-z]|-))+)(?:[A-Za-z]|-)(?:(?:(?:0|[1-9])|(?:[A-Za-z]|-))+)?)|(?:0|[1-9](?:(?:0|[1-9])+)*))(?:[.](?:(?:(?:[A-Za-z]|-)(?:(?:(?:0|[1-9])|(?:[A-Za-z]|-))+)?|(?:(?:(?:0|[1-9])|(?:[A-Za-z]|-))+)(?:[A-Za-z]|-)(?:(?:(?:0|[1-9])|(?:[A-Za-z]|-))+)?)|(?:0|[1-9](?:(?:0|[1-9])+)*)))*))?(?:[+](?<build>(?:(?:(?:[A-Za-z]|-)(?:(?:(?:0|[1-9])|(?:[A-Za-z]|-))+)?|(?:(?:(?:0|[1-9])|(?:[A-Za-z]|-))+)(?:[A-Za-z]|-)(?:(?:(?:0|[1-9])|(?:[A-Za-z]|-))+)?)|(?:(?:0|[1-9])+))(?:[.](?:(?:(?:[A-Za-z]|-)(?:(?:(?:0|[1-9])|(?:[A-Za-z]|-))+)?|(?:(?:(?:0|[1-9])|(?:[A-Za-z]|-))+)(?:[A-Za-z]|-)(?:(?:(?:0|[1-9])|(?:[A-Za-z]|-))+)?)|(?:(?:0|[1-9])+)))*))?)$/";
+
+Version.prototype.toJSON = function () {
+   return {
+      version: this.versionAsText_
+   };
+};
+
+/**
+ * Get the version as string format 
+*/
+Version.prototype.toString = function() {
+  return this.versionAsText_;
+};
+
+/**
+ * Get the version as string format 
+*/
+Version.prototype.major() = function() {
+  return this.versionAsText_;
+};
+
