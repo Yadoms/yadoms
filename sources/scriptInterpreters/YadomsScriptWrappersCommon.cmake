2b1eb15d8aca39c357ade0a360a2ee9808f91058
sgallou <sebastien.gallou@gmail.com>
2017-01-05 23:46:54 +0100

    Fix wrapper output directory

--- sources/scriptInterpreters/YadomsScriptWrappersCommon.cmake
+++ sources/scriptInterpreters/YadomsScriptWrappersCommon.cmake
@@ -4,8 +4,14 @@
 # CMake macro for SWIG generates a target called _yScriptApiWrapper
 
 INCLUDE(${SWIG_USE_FILE})
-   
 
+MACRO(SET_PARENT_INTERPRETER interpreter)
+   foreach(OUTPUTCONFIG ${CMAKE_CONFIGURATION_TYPES})
+       string(TOUPPER ${OUTPUTCONFIG} OUTPUTCONFIG)
+       set(CMAKE_LIBRARY_OUTPUT_DIRECTORY_${OUTPUTCONFIG} ${YADOMS_OUTPUT_DIR}/${OUTPUTCONFIG}/scriptInterpreters/${interpreter})
+   endforeach(OUTPUTCONFIG CMAKE_CONFIGURATION_TYPES)
+ENDMACRO()
+   
 MACRO(SCRIPT_API_WRAPPER_SOURCES targetLanguage)
    include_directories(${CMAKE_CURRENT_SOURCE_DIR} ${SHARED_INCL_DIR})
    SET(SWIG_MODULE_yScriptApiWrapper_EXTRA_DEPS ${SHARED_COMMON_INCL_DIR}/shared/script/yScriptApi/IYScriptApi.h)
