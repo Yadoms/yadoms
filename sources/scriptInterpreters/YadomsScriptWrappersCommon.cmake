caab805c0b2526e81d3fe1aaaca2c50600e16805
sgallou <sebastien.gallou@gmail.com>
2017-01-20 15:09:40 +0100

    Fix folder location of the script wrapper project in MSVC solution

--- sources/scriptInterpreters/YadomsScriptWrappersCommon.cmake
+++ sources/scriptInterpreters/YadomsScriptWrappersCommon.cmake
@@ -5,10 +5,10 @@
 
 INCLUDE(${SWIG_USE_FILE})
 
-MACRO(SET_PARENT_INTERPRETER interpreter)
+MACRO(SCRIPT_API_WRAPPER_SET_PARENT_INTERPRETER parentInterpreter)
    foreach(OUTPUTCONFIG ${CMAKE_CONFIGURATION_TYPES})
        string(TOUPPER ${OUTPUTCONFIG} OUTPUTCONFIG)
-       set(CMAKE_LIBRARY_OUTPUT_DIRECTORY_${OUTPUTCONFIG} ${YADOMS_OUTPUT_DIR}/${OUTPUTCONFIG}/scriptInterpreters/${interpreter})
+       set(CMAKE_LIBRARY_OUTPUT_DIRECTORY_${OUTPUTCONFIG} ${YADOMS_OUTPUT_DIR}/${OUTPUTCONFIG}/scriptInterpreters/${parentInterpreter})
    endforeach(OUTPUTCONFIG CMAKE_CONFIGURATION_TYPES)
 ENDMACRO()
    
@@ -16,10 +16,6 @@ MACRO(SCRIPT_API_WRAPPER_SOURCES targetLanguage)
    include_directories(${CMAKE_CURRENT_SOURCE_DIR} ${SHARED_INCL_DIR})
    SET(SWIG_MODULE_yScriptApiWrapper_EXTRA_DEPS ${SHARED_COMMON_INCL_DIR}/shared/script/yScriptApi/IYScriptApi.h)
    SWIG_ADD_MODULE(yScriptApiWrapper ${targetLanguage} ${ARGN})
-
-   IF(MSVC OR XCODE)
-      SET_PROPERTY(TARGET ${SWIG_MODULE_yScriptApiWrapper_REAL_NAME} PROPERTY FOLDER "scriptInterpreters/${targetLanguage}/yScriptApiWrapper")
-   ENDIF()
 ENDMACRO()
 
 MACRO(SCRIPT_API_WRAPPER_INCLDIR)
@@ -28,4 +24,10 @@ ENDMACRO()
 
 MACRO(SCRIPT_API_WRAPPER_LINK)
    SWIG_LINK_LIBRARIES(yScriptApiWrapper yadoms-shared ${ARGN})
+ENDMACRO()
+
+MACRO(SCRIPT_API_SOURCE_GROUP parentInterpreter)
+   IF(MSVC OR XCODE)
+      SET_PROPERTY(TARGET ${SWIG_MODULE_yScriptApiWrapper_REAL_NAME} PROPERTY FOLDER "scriptInterpreters/${parentInterpreter}/yScriptApiWrapper")
+   ENDIF()
 ENDMACRO()
