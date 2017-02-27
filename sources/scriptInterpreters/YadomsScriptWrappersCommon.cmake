c0dfacb8200d4888b2d982968985844f44b80ff5
Jean-Michel DECORET <jmdecoret@nse-groupe.com>
2017-02-27 15:40:55 +0100

    CMake : _ScriptApiwrapper : manage unity mode, and post build copies

--- sources/scriptInterpreters/YadomsScriptWrappersCommon.cmake
+++ sources/scriptInterpreters/YadomsScriptWrappersCommon.cmake
@@ -21,7 +21,7 @@ MACRO(SCRIPT_API_WRAPPER_SOURCES targetLanguage)
 ENDMACRO()
 
 MACRO(SCRIPT_API_WRAPPER_INCLDIR)
-   include_directories(${CMAKE_CURRENT_SOURCE_DIR} ${SHARED_INCL_DIR} ${ARGN})
+   set_property( TARGET _yScriptApiWrapper PROPERTY INCLUDE_DIRECTORIES ${CMAKE_CURRENT_SOURCE_DIR} ${SHARED_INCL_DIR} ${ARGN})
 ENDMACRO()
 
 MACRO(SCRIPT_API_WRAPPER_LINK)
@@ -37,3 +37,29 @@ MACRO(SCRIPT_API_SOURCE_GROUP parentInterpreter)
       SET_PROPERTY(TARGET ${SWIG_MODULE_yScriptApiWrapper_REAL_NAME} PROPERTY FOLDER "scriptInterpreters/${parentInterpreter}")
    ENDIF()
 ENDMACRO()
+
+
+# brief Copy a file to the target output dir
+# param [in] _resource The resource (absolute path) to copy to the target output dir
+# param [in] parentInterpreter The parent interpreter
+MACRO(SCRIPT_API_WRAPPER_POST_BUILD_COPY_FILE _resource parentInterpreter)
+
+   get_filename_component(_resourcePath ${_resource}  DIRECTORY)
+   get_filename_component(_resourceFile ${_resource}  NAME)
+
+   string(REPLACE "-" "_" ComponentCompatibleName ${parentInterpreter})
+   
+   install(FILES ${_resource} 
+			DESTINATION ${INSTALL_BINDIR}/scriptInterpreters/${parentInterpreter}/${_resourcePath}
+			COMPONENT  ${ComponentCompatibleName})
+			
+   add_custom_command(TARGET _yScriptApiWrapper POST_BUILD
+      COMMAND ${CMAKE_COMMAND} -E copy_if_different ${_resource} $<TARGET_FILE_DIR:_yScriptApiWrapper>/${_resourceFile})
+   if(COTIRE_USE)
+      if(COTIRE_USE_UNITY)
+         add_custom_command(TARGET _yScriptApiWrapper_unity POST_BUILD
+            COMMAND ${CMAKE_COMMAND} -E copy_if_different ${_resource} $<TARGET_FILE_DIR:_yScriptApiWrapper_unity>/${_resourceFile})
+	  endif()	
+   endif()	
+ENDMACRO()
+
