ffe280c7fbaf21164fd12f2536e1a3fcfe43dc56
sgallou <sebastien.gallou@gmail.com>
2017-04-10 16:18:47 +0200

    Fix #215

--- sources/shared/shared/plugin/yPluginApi/historization/Curtain.h
+++ sources/shared/shared/plugin/yPluginApi/historization/Curtain.h
@@ -3,37 +3,42 @@
 #include <shared/enumeration/EnumHelpers.hpp>
 #include "SingleHistorizableData.hpp"
 
-namespace shared { namespace plugin { namespace yPluginApi { namespace historization
+namespace shared
 {
-   //-----------------------------------------------------
-   ///\brief               The curtain command values
-   //-----------------------------------------------------   
-   DECLARE_ENUM_HEADER_SHARED(ECurtainCommand, YADOMS_SHARED_EXPORT,
-      ((Stop))
-      ((Open))
-      ((Close))
-   );
-
-
-   //-----------------------------------------------------
-   ///\brief A curtain historizable object
-   //-----------------------------------------------------
-   class YADOMS_SHARED_EXPORT CCurtain : public CSingleHistorizableData<ECurtainCommand>
+   namespace plugin
    {
-   public:
-      //-----------------------------------------------------
-      ///\brief                     Constructor
-      ///\param[in] keywordName     Yadoms keyword name
-      //-----------------------------------------------------
-      explicit CCurtain(const std::string& keywordName);
+      namespace yPluginApi
+      {
+         namespace historization
+         {
+            //-----------------------------------------------------
+            ///\brief               The curtain command values
+            //-----------------------------------------------------   
+            DECLARE_ENUM_HEADER_SHARED(ECurtainCommand, YADOMS_SHARED_EXPORT,
+               ((Stop))
+               ((Open))
+               ((Close))
+            );
 
-      //-----------------------------------------------------
-      ///\brief                     Destructor
-      //-----------------------------------------------------
-      virtual ~CCurtain();
-   };
 
+            //-----------------------------------------------------
+            ///\brief A curtain historizable object
+            //-----------------------------------------------------
+            class YADOMS_SHARED_EXPORT CCurtain : public CSingleHistorizableData<ECurtainCommand>
+            {
+            public:
+               //-----------------------------------------------------
+               ///\brief                     Constructor
+               ///\param[in] keywordName     Yadoms keyword name
+               //-----------------------------------------------------
+               explicit CCurtain(const std::string& keywordName);
 
-
-} } } } // namespace shared::plugin::yPluginApi::historization
-
+               //-----------------------------------------------------
+               ///\brief                     Destructor
+               //-----------------------------------------------------
+               virtual ~CCurtain();
+            };
+         }
+      }
+   }
+} // namespace shared::plugin::yPluginApi::historization
