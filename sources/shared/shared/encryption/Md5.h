010ad3b25675fae7acc0414bb6733e55043f5a3b
Jean-Michel DECORET <jmdecoret@nse-groupe.com>
2017-04-11 15:22:40 +0200

    Update MD5 hash computation (using poco) Update usage Fix #219

---
+++ sources/shared/shared/encryption/Md5.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <shared/Export.h>
+
+namespace shared { namespace encryption { 
+
+   //--------------------------------------------------------------
+   /// \brief	Md5 hash generator
+   //--------------------------------------------------------------
+   class CMd5
+   {
+      public:
+         //--------------------------------------------------------------
+         /// \brief	compute a md5 hash on a file
+         /// \param [in] file  The file to compute hash
+         /// \return	The md5 hash
+         //--------------------------------------------------------------
+         static std::string digestFile(const std::string & file);
+   };
+
+
+} // namespace encryption
+} // namespace shared
