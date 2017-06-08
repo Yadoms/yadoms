e3314220b37ebe9714681a9a4ebd4db6266ffca6
sgallou <sebastien.gallou@gmail.com>
2017-06-08 15:31:27 +0200

    Create virtual device (server-side)

--- sources/server/pluginSystem/internalPlugin/Instance.cpp
+++ sources/server/pluginSystem/internalPlugin/Instance.cpp
@@ -1,5 +1,7 @@
 #include "stdafx.h"
 #include "Instance.h"
+#include <shared/Log.h>
+#include <tools/OperatingSystem.h>
 
 namespace pluginSystem
 {
@@ -11,8 +13,8 @@ namespace pluginSystem
                            boost::shared_ptr<CInstanceStateHandler> instanceStateHandler)
          : m_instanceInformation(instanceInformation),
            m_pluginInformation(pluginInformation),
-           m_eventHandler(boost::make_shared<shared::event::CEventHandler>()),
-           m_thread(&CInstance::Thread, api, m_eventHandler, instanceStateHandler)
+           m_eventHandler(boost::make_shared<shared::event::CEventHandler>()), //TODO utile ? Ne peut-on pas utiliser api->GetEventHandler() ?
+           m_thread(&CInstance::doWorkThread, this, api, m_eventHandler, instanceStateHandler)
       {
       }
 
@@ -29,18 +31,6 @@ namespace pluginSystem
       {
          m_thread.interrupt();
          m_thread.join();
-      }
-
-      void CInstance::Thread(boost::shared_ptr<shared::plugin::yPluginApi::IYPluginApi> api,
-                             boost::shared_ptr<shared::event::CEventHandler> eventHandler,
-                             boost::shared_ptr<CInstanceStateHandler> instanceStateHandler)
-      {
-         instanceStateHandler->onStart();
-
-         CSystem system;
-         system.doWork(api, *eventHandler);
-
-         instanceStateHandler->onFinish(0, std::string());
       }
 
       void CInstance::postDeviceCommand(boost::shared_ptr<const shared::plugin::yPluginApi::IDeviceCommand> deviceCommand)
@@ -73,7 +63,7 @@ namespace pluginSystem
                                    request);
       }
 
-      void CInstance::postExtraQuery(boost::shared_ptr<shared::plugin::yPluginApi::IExtraQuery> extraQuery, const std::string & taskId)
+      void CInstance::postExtraQuery(boost::shared_ptr<shared::plugin::yPluginApi::IExtraQuery> extraQuery, const std::string& taskId)
       {
          m_eventHandler->postEvent(yApi::IYPluginApi::kEventExtraQuery,
                                    extraQuery);
@@ -99,6 +89,119 @@ namespace pluginSystem
       {
          m_eventHandler->postEvent(yApi::IYPluginApi::kEventDeviceRemoved,
                                    event);
+      }
+
+      void CInstance::doWorkThread(boost::shared_ptr<shared::plugin::yPluginApi::IYPluginApi> api,
+                                   boost::shared_ptr<shared::event::CEventHandler> eventHandler,
+                                   boost::shared_ptr<CInstanceStateHandler> instanceStateHandler) const
+      {
+         instanceStateHandler->onStart();
+         doWork(api, *eventHandler);
+         instanceStateHandler->onFinish(0, std::string());
+      }
+
+      void CInstance::doWork(boost::shared_ptr<yApi::IYPluginApi> api,
+                             shared::event::CEventHandler& eventHandler) const
+      {
+         try
+         {
+            YADOMS_LOG_CONFIGURE("InternalPlugin");
+
+            // the main loop
+            YADOMS_LOG(debug) << "InternalPlugin is running...";
+
+            // Declare all device/keywords
+            static const std::string& systemDevice("system");
+            auto keywordShutdown(boost::make_shared<yApi::historization::CEvent>("shutdown"));
+            auto keywordRestart(boost::make_shared<yApi::historization::CEvent>("restart"));
+            std::vector<boost::shared_ptr<const shared::plugin::yPluginApi::historization::IHistorizable>> keywords({keywordShutdown, keywordRestart});
+
+            // Device creation if needed
+            if (!api->deviceExists(systemDevice))
+               api->declareDevice(systemDevice,
+                                  "system",
+                                  "yadoms system",
+                                  keywords);
+
+            while (1)
+            {
+               switch (eventHandler.waitForEvents())
+               {
+               case yApi::IYPluginApi::kEventDeviceCommand:
+                  {
+                     auto command = eventHandler.getEventData<boost::shared_ptr<const yApi::IDeviceCommand>>();
+
+                     if (boost::iequals(command->getKeyword(), keywordShutdown->getKeyword()))
+                     {
+                        YADOMS_LOG(information) << "Shutdown the system";
+                        if (!tools::COperatingSystem::shutdown())
+                        YADOMS_LOG(error) << "Fail to shutdown";
+                     }
+                     else if (boost::iequals(command->getKeyword(), keywordRestart->getKeyword()))
+                     {
+                        YADOMS_LOG(information) << "Reboot the system";
+                        if (!tools::COperatingSystem::shutdown(true))
+                        YADOMS_LOG(error) << "Fail to reboot";
+                     }
+                     else
+                     {
+                        YADOMS_LOG(warning) << "Received command for unknown keyword from Yadoms : " << yApi::IDeviceCommand::toString(command);
+                     }
+                     break;
+                  }
+
+               case yApi::IYPluginApi::kEventManuallyDeviceCreation:
+                  {
+                     auto request = eventHandler.getEventData<boost::shared_ptr<yApi::IManuallyDeviceCreationRequest>>();
+                     YADOMS_LOG(information) << "Manually device creation request received for device :" << request->getData().getDeviceName();
+                     try
+                     {
+                        request->sendSuccess(createVirtualDevice(api,
+                                                                 request->getData()));
+                     }
+                     catch (std::exception& e)
+                     {
+                        YADOMS_LOG(error) << "Unable to create virtual device " << request->getData().getDeviceName() << ", " << e.what();
+                        request->sendError("Unable to create virtual device");
+                     }
+
+                     break;
+                  }
+
+               default:
+                  break;
+               }
+            }
+         }
+         catch (boost::thread_interrupted&)
+         {
+            YADOMS_LOG(information) << "Thread is stopping...";
+         }
+         catch (shared::exception::CException& ex)
+         {
+            YADOMS_LOG(fatal) << "The Yadoms Sytem plugin fails. Unknown expcetion : " << ex.what();
+         }
+      }
+
+      std::string CInstance::createVirtualDevice(boost::shared_ptr<yApi::IYPluginApi> api,
+                                                 const yApi::IManuallyDeviceCreationData& data) const
+      {
+         if (api->deviceExists(data.getDeviceName()))
+            throw std::invalid_argument("device already exists");
+
+         boost::shared_ptr<const yApi::historization::IHistorizable> keyword;
+         if (data.getDeviceType() == "switch") //TODO ne pas laisser en dur
+            keyword = boost::make_shared<yApi::historization::CSwitch>("state");
+
+         if (!keyword)
+            throw std::invalid_argument("Unsupported device type");
+
+         api->declareDevice(data.getDeviceName(),
+                            data.getDeviceType(),
+                            data.getDeviceType(),
+                            keyword);
+
+         return data.getDeviceName();
       }
    }
 } // namespace pluginSystem::internalPlugin
--- sources/shared/shared/plugin/yPluginApi/IManuallyDeviceCreationData.h
+++ sources/shared/shared/plugin/yPluginApi/IManuallyDeviceCreationData.h
@@ -2,38 +2,45 @@
 
 #include <shared/DataContainer.h>
 
-namespace shared { namespace plugin { namespace yPluginApi
+namespace shared
 {
-   //-----------------------------------------------------
-   ///\brief The API used for manually created devices
-   //-----------------------------------------------------
-   class IManuallyDeviceCreationData
+   namespace plugin
    {
-   public:
-      //-----------------------------------------------------
-      ///\brief               Destructor
-      //-----------------------------------------------------
-      virtual ~IManuallyDeviceCreationData() {}
+      namespace yPluginApi
+      {
+         //-----------------------------------------------------
+         ///\brief The API used for manually created devices
+         //-----------------------------------------------------
+         class IManuallyDeviceCreationData
+         {
+         public:
+            //-----------------------------------------------------
+            ///\brief               Destructor
+            //-----------------------------------------------------
+            virtual ~IManuallyDeviceCreationData()
+            {
+            }
 
-      //-----------------------------------------------------
-      ///\brief               Get the device
-      ///\return              The device to create
-      //-----------------------------------------------------
-      virtual const std::string& getDeviceName() const = 0;
+            //-----------------------------------------------------
+            ///\brief               Get the device
+            ///\return              The device to create
+            //-----------------------------------------------------
+            virtual const std::string& getDeviceName() const = 0;
 
-      //-----------------------------------------------------
-      ///\brief               Get the device
-      ///\return              The device to create
-      //-----------------------------------------------------
-      virtual const std::string& getDeviceType() const = 0;
+            //-----------------------------------------------------
+            ///\brief               Get the device
+            ///\return              The device to create
+            //-----------------------------------------------------
+            virtual const std::string& getDeviceType() const = 0;
 
-      //-----------------------------------------------------
-      ///\brief               Get the device configuration
-      ///\return              The device parameters (in data container)
-      //-----------------------------------------------------
-      virtual const CDataContainer& getConfiguration() const = 0;
-};
-	
-} } } // namespace shared::plugin::yPluginApi	
-	
-	
+            //-----------------------------------------------------
+            ///\brief               Get the device configuration
+            ///\return              The device parameters (in data container)
+            //-----------------------------------------------------
+            virtual const CDataContainer& getConfiguration() const = 0;
+         };
+      }
+   }
+} // namespace shared::plugin::yPluginApi	
+
+
