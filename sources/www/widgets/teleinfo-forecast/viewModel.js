4e13c9a9a098ab3ad97d10a4b169f323c1345de0
jmberhault <jean-michel@berhault.org>
2017-06-17 22:24:50 +0200

    widget forecast-teleinfo
    ==============
    - WIP

--- sources/www/widgets/teleinfo-forecast/viewModel.js
+++ sources/www/widgets/teleinfo-forecast/viewModel.js
@@ -9,6 +9,10 @@ widgetViewModelCtor =
           //observable data
           this.bg_color = ko.observable("");
 
+          // default size
+          this.WidgetHeight = 95;
+          this.WidgetWidth  = 95;          
+          
           /**
            * Initialization method
            * @param widget widget class object
@@ -42,6 +46,43 @@ widgetViewModelCtor =
               }
           };
 
+this.refresh = function()
+   {
+	   var self = this;
+
+      var element = self.widgetApi.find(".teleinfo-forecast-canvas");
+
+		//get a reference to the canvas
+      var ctx = element.get(0).getContext("2d");
+		
+		// Refresh the canvas, clear all existing information
+		ctx.clearRect(0, 0, element.width, element.height);
+		
+		//Attributes of canvas could only be changed trough theses variables. In an other way the canvas is stretched.
+		element.attr('width', self.WidgetWidth);
+		element.attr('height', self.WidgetHeight);
+		element.css('width', self.WidgetWidth);
+		element.css('height', self.WidgetHeight);
+		  
+        // Draw the thermometer
+		//draw a circle
+		ctx.beginPath();
+		
+		//black color
+      ctx.fillStyle = "rgb(0,0,0)";
+		ctx.lineWidth = 3;		
+		
+		//bubble
+		         // position x        position y             diameter
+		ctx.arc(self.WidgetWidth / 2, self.WidgetHeight / 2, 12 * self.WidgetWidth / 2, Math.PI*1.3, Math.PI*1.7, true);
+		
+		//top
+		ctx.arc(self.WidgetWidth / 2, 10 * self.WidgetHeight / 2, 7 * self.WidgetWidth / 100, Math.PI*2, Math.PI*1, true);
+		
+		ctx.closePath();
+		ctx.stroke();
+   }          
+          
           /**
           * New acquisition handler
           * @param keywordId keywordId on which new acquisition was received
