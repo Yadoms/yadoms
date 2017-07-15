4f61797f7d616e03642854cfc38865dce8a4bb60
jmberhault <jean-michel@berhault.org>
2017-07-15 17:42:50 +0200

    widget rain-gauge
    ==========
    - fix last errors (date/time formatting).
    - clean the widget.
    - Add a message during the first acquisition : loading data.

--- sources/www/widgets/rain-gauge/viewModel.js
+++ sources/www/widgets/rain-gauge/viewModel.js
@@ -16,10 +16,10 @@ function rainGaugeDisplayViewModel() {
     
     //observable data - last receive information
     this.shouldBeVisible = ko.observable(false);
-    this.lastReceiveDate = ko.observable("");    
+    this.lastReceiveDate = ko.observable($.t("widgets/rain-gauge:loadingData"));
     this.serverTime = null;
     this.acquisitionData = [];
-
+    
     /**
      * Initialization method
      * @param widget widget class object
@@ -47,27 +47,16 @@ function rainGaugeDisplayViewModel() {
     this.getValues = function (keywordId) {
        self = this;
        
-       console.log ("self.acquisitionData", self.acquisitionData);
-       console.log ("self.acquisitionData[0] : ", self.acquisitionData[0]);
-       
-       while(self.acquisitionData.length>0){
-          if (self.serverTime - DateTimeFormatter.isoDateToDate(self.acquisitionData[0].date) > 86400000){
-             self.acquisitionData.shift();
-          }
-          else
-             break;
-       }
-       
        //clean old values at the begin of the tab
        if (self.acquisitionData.length != 0)
        {
-          // TODO : Treat the case with only 1, or 2 values
-       
           var index = self.acquisitionData.length-1;
        
           // retrieve all values
           var lastValue = self.acquisitionData[index];
           var last24h = self.acquisitionData[0];
+          
+          self.rate_24h( (parseFloat(lastValue.key) - parseFloat(last24h.key) ).toFixed(1).toString());
           
           // Retrieve 1h ago
           
@@ -78,27 +67,22 @@ function rainGaugeDisplayViewModel() {
           if (index != -1)
           {
              var last1h = self.acquisitionData[index];
-             
-             self.rate_1h = ( parseFloat(lastValue.key) - parseFloat(last1h.key) ).toFixed(1).toString();
-             self.rate_24h = ( parseFloat(lastValue.key) - parseFloat(last24h.key) ).toFixed(1).toString();
+             self.rate_1h( (parseFloat(lastValue.key) - parseFloat(last1h.key) ).toFixed(1).toString());
           }
           else
-          {
-             self.rate_1h = ("-");
-             self.rate_24h = ("-");
-          }
+             self.rate_1h("-");
        }
        else
        {
-          self.rate_1h = ("-");
-          self.rate_24h = ("-");
+          self.rate_1h("-");
+          self.rate_24h("-");
        }
        
        // treat the time  
        if (self.shouldBeVisible())
        {
          if (self.acquisitionData.length!=0)
-            self.lastReceiveDate(moment(lastValue.date).calendar().toString());
+            self.lastReceiveDate(DateTimeFormatter.isoDateToDate(lastValue.date).calendar().toString());
          else
             self.lastReceiveDate($.t("widgets/rain-gauge:NoAcquisition"));
        }
@@ -120,12 +104,13 @@ function rainGaugeDisplayViewModel() {
         //we fill the deviceId of the battery indicator
         self.widgetApi.configureBatteryIcon(self.widget.configuration.device.deviceId);
         
-       // the actual time
+       //the actual time
        var date = DateTimeFormatter.dateToIsoDate(moment(self.serverTime));
        
-       console.log ("self.serverTime", self.serverTime);
        self.shouldBeVisible(self.widget.configuration.dateDisplay);
        
+       //format the text before the following frame that could take more than 1 second.
+       self.widgetApi.fitText();
        
        // Retrieve last values
        date24h = DateTimeFormatter.dateToIsoDate(moment(self.serverTime).subtract(1, 'days').startOf('minute'));
@@ -133,32 +118,36 @@ function rainGaugeDisplayViewModel() {
        
        deffered.done(function (data) {
           
-          console.log ("configurationChanged-Data", data);
-          
           if (data.data !==""){
-             if (self.acquisitionData.length!=0)
-             {
-                self.acquisitionData.splice(0, self.acquisitionData.length);
-                Array.prototype.push.apply(self.acquisitionData, {date: moment(data.data.date), key: data.data.key});
-                //else if (self.acquisitionData[self.acquisitionData.length-1].date < data.data.date){
-                   // Add the last received data to the list
-                //   self.acquisitionData.push(data.data);
-                //}
-             }
-             else{
-                Array.prototype.push.apply(self.acquisitionData, {date: moment(data.data.date), key: data.data.key});
-             }
+             self.acquisitionData.splice(0, self.acquisitionData.length);
+             Array.prototype.push.apply(self.acquisitionData, data.data);
           }
           
           // treat the tab
           self.getValues(self.widget.configuration.device.keywordId);
        })
       .fail(function(error) {
-         var message = "Fail to get server lastvalue : " + error;
+         var message = "Fail to get server data : " + error;
          console.warn(message);
       });
+      
+      return deffered.promise();
     }
 
+    this.onTime = function (serverLocalTime) {
+         var self = this;
+         
+         self.serverTime = DateTimeFormatter.isoDateToDate (serverLocalTime);
+         
+         while(self.acquisitionData.length>0){
+          if (self.serverTime - DateTimeFormatter.isoDateToDate(self.acquisitionData[0].date) > 86400000){
+             self.acquisitionData.shift();
+          }
+          else
+             break;
+       }         
+     };         
+    
     /**
     * New acquisition handler
     * @param keywordId keywordId on which new acquisition was received
@@ -166,19 +155,20 @@ function rainGaugeDisplayViewModel() {
     */
     this.onNewAcquisition = function (keywordId, data) {
         var self = this;
-
+        
         if (keywordId === self.widget.configuration.device.keywordId) {
             //it is the right device
             if (data.value !==""){
-               console.log ("new data",data);
+
+               self.serverTime = data.date;
                
                if (self.acquisitionData.length!=0){
-                  if (data.date != self.acquisitionData[self.acquisitionData.length-1].date) {
-                     self.acquisitionData.push({key: data.value, date: data.date});
+                  if (data.date != DateTimeFormatter.isoDateToDate(self.acquisitionData[self.acquisitionData.length-1].date)) {
+                     self.acquisitionData.push({date: DateTimeFormatter.dateToIsoDate(data.date), key: data.value});
                   }
                }
                else{
-                  self.acquisitionData.push({key: data.value, date: data.date});
+                  self.acquisitionData.push({date: DateTimeFormatter.dateToIsoDate(data.date), key: data.value});
                }
             }
             
