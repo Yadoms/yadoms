4836712c68f4c234948e7aa8c1fcc734a92dda48
jmberhault <jean-michel@berhault.org>
2017-08-16 10:17:59 +0200

    - resizing the widget is now working.

--- sources/www/widgets/colorRGB/viewModel.js
+++ sources/www/widgets/colorRGB/viewModel.js
@@ -6,10 +6,15 @@ widgetViewModelCtor =
  */
 function colorRGBViewModel() {
     //observable data
+     
+     this.colorpicker = null;
+     this.WidgetWidth  = 140;
+     this.WidgetHeight = 140;
+     
     /**
      * Initialization method
      * @param widget widget class object
-     */
+     */     
     this.initialize = function () {
         self = this;
        
@@ -20,21 +25,26 @@ function colorRGBViewModel() {
             batteryItem: false
         });
         
+        // TODO : display colorSelector.
+        // TODO : Change the place of the top/bot to hide the border
+        
         // Display the color picker
-        console.log (this.widgetApi.find(".picker-canvas"));
-        this.widgetApi.find(".picker-canvas").colorpicker({
-            customClass: 'colorpicker-2x',
-            sliders: {
+        self.colorpicker = this.widgetApi.find(".picker-canvas").colorpicker({
+            customClass: 'cssClass', // TODO : Change the name
+            sliders: { // TODO : Check how to modify theses values after the initialization
                 saturation: {
-                    maxLeft: 200,
-                    maxTop: 200
+                    maxLeft: 140,
+                    maxTop: 140
                 },
                 hue: {
-                    maxTop: 200
+                    maxTop: 140
                 },
                 alpha: {
-                    maxTop: 200
+                    maxTop: 140
                 }
+            },
+            slidersHorz: {
+               // TODO : To be defined
             },
             color: '#000000',
             format: 'rgb',
@@ -54,7 +64,7 @@ function colorRGBViewModel() {
         });
         
         // capture the event changeColor
-        this.widgetApi.find(".picker-canvas").colorpicker().unbind('changeColor').bind('changeColor', self.changeColorButtonClick());
+        self.colorpicker.unbind('changeColor').bind('changeColor', self.changeColorButtonClick());
     };
 
     // function called when the color changed
@@ -70,9 +80,35 @@ function colorRGBViewModel() {
     this.configurationChanged = function () {
         var self = this;
 
-        // TODO
+        // Nothing to do
+        
+        //self.colorpicker.colorpicker('reposition');
+    };
+    
+    this.changeCss = function(width, height) {
+      $('.cssClass .colorpicker-saturation').css('height', height+'px');
+      $('.cssClass .colorpicker-saturation').css('width', width+'px');
+      $('.cssClass .colorpicker-saturation').css('background-size', width+'px '+height+'px');
+      $('.cssClass .colorpicker-hue').css('height', height+'px');
+      $('.cssClass .colorpicker-hue').css('background-size', '15px '+height+'px');
+      $('.cssClass .colorpicker-alpha').css('height', height+'px');
+      $('.cssClass .colorpicker-alpha').css('background-size', '15px '+height+'px');
+      $('.cssClass .colorpicker-color').css('background-size', '15px '+height+'px');
+      $('.cssClass .colorpicker-color div').css('background-size', '15px '+height+'px');
     };
 
+   this.resized = function() 
+   {
+      var self = this;
+      
+      self.WidgetWidth = this.widget.getWidth()-60;
+      self.WidgetHeight = this.widget.getHeight()-70;
+      
+      self.changeCss(self.WidgetWidth, self.WidgetHeight);
+      
+      //this.widgetApi.find(".colorpicker-selectors-visible").css('enable', 'true');
+   };	
+    
     /**
     * New acquisition handler
     * @param keywordId keywordId on which new acquisition was received
@@ -82,9 +118,8 @@ function colorRGBViewModel() {
         var self = this;
 
         if (keywordId === self.widget.configuration.device.keywordId) {
-
-          // TODO
-
+          console.log(data);
+          self.colorpicker.setColor(data.data);
         }
     };
 };
