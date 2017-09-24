28421f6a8682fbf40863f1fa3f81c8db9c5bc4fe
sgallou <sebastien.gallou@gmail.com>
2017-09-24 12:18:21 +0200

    Update iconpicker to 1.9.0

--- sources/www/libs/bootstrap-iconpicker-1.9.0/js/bootstrap-iconpicker.js
+++ sources/www/libs/bootstrap-iconpicker-1.9.0/js/bootstrap-iconpicker.js
@@ -1,21 +1,12 @@
 /*!========================================================================
- * Bootstrap: bootstrap-iconpicker.js v1.7.0 by @recktoner
- * https://victor-valencia.github.com/bootstrap-iconpicker
- * ========================================================================
- * Copyright 2013-2015 Victor Valencia Rico.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * ======================================================================== */
+* File: bootstrap-iconpicker.js v1.9.0 by @victor-valencia
+* https://victor-valencia.github.com/bootstrap-iconpicker
+* ========================================================================
+* Copyright 2013-2017 Victor Valencia Rico.
+* Licensed under MIT license.
+* https://github.com/victor-valencia/bootstrap-iconpicker/blob/master/LICENSE
+* ========================================================================
+*/
 
 ;(function($){ "use strict";
 
@@ -26,7 +17,11 @@
       this.options  = $.extend({}, Iconpicker.DEFAULTS, this.$element.data());
       this.options  = $.extend({}, this.options, options);
     };
-    
+
+    // ICONPICKER VERSION
+    // ==============================
+    Iconpicker.VERSION = '1.9.0';
+
     // ICONPICKER ICONSET_EMPTY
     // ==============================
     Iconpicker.ICONSET_EMPTY = {
@@ -40,9 +35,10 @@
     Iconpicker.ICONSET = {
         _custom: null,
         elusiveicon: $.iconset_elusiveicon || Iconpicker.ICONSET_EMPTY,
+        flagicon: $.iconset_flagicon || Iconpicker.ICONSET_EMPTY,
         fontawesome: $.iconset_fontawesome || Iconpicker.ICONSET_EMPTY,
+        glyphicon: $.iconset_glyphicon || Iconpicker.ICONSET_EMPTY,
         ionicon: $.iconset_ionicon || Iconpicker.ICONSET_EMPTY,
-        glyphicon: $.iconset_glyphicon || Iconpicker.ICONSET_EMPTY,        
         mapicon: $.iconset_mapicon || Iconpicker.ICONSET_EMPTY,
         materialdesign: $.iconset_materialdesign || Iconpicker.ICONSET_EMPTY,
         octicon: $.iconset_octicon || Iconpicker.ICONSET_EMPTY,
@@ -60,7 +56,8 @@
         cols: 4,
         icon: '',
         iconset: 'glyphicon',
-        header: true,        
+        iconsetVersion: 'lastest',
+        header: true,
         labelHeader: '{0} / {1}',
         footer: true,
         labelFooter: '{0} - {1} of {2}',
@@ -73,14 +70,16 @@
     };
 
     // ICONPICKER PRIVATE METHODS
-    // ==============================    
+    // ==============================
     Iconpicker.prototype.bindEvents = function () {
         var op = this.options;
         var el = this;
         op.table.find('.btn-previous, .btn-next').off('click').on('click', function(e) {
             e.preventDefault();
-            var inc = parseInt($(this).val(), 10);
-            el.changeList(op.page + inc);
+            if(!$(this).hasClass('disabled')){
+                var inc = parseInt($(this).val(), 10);
+                el.changeList(op.page + inc);
+            }
         });
         op.table.find('.btn-icon').off('click').on('click', function(e) {
             e.preventDefault();
@@ -96,42 +95,53 @@
             el.changeList(1);
         });
     };
-    
-    Iconpicker.prototype.changeList = function (page) {        
+
+    Iconpicker.prototype.changeList = function (page) {
         this.filterIcons();
         this.updateLabels(page);
         this.updateIcons(page);
         this.options.page = page;
         this.bindEvents();
     };
-    
+
     Iconpicker.prototype.filterIcons = function () {
         var op = this.options;
         var search = op.table.find('.search-control').val();
+        var icons = [];
+        if(op.iconsetVersion != 'lastest' && typeof Iconpicker.ICONSET[op.iconset].allVersions != 'undefined'){
+            $.each(Iconpicker.ICONSET[op.iconset].allVersions, function(i, v){
+                if(op.iconsetVersion == v.version){
+                    icons = v.icons;
+                }
+            });
+        }
+        else
+            icons = Iconpicker.ICONSET[op.iconset].icons;
+
         if (search === "") {
-            op.icons = Iconpicker.ICONSET[op.iconset].icons;
+            op.icons = icons;
         }
         else {
             var result = [];
-            $.each(Iconpicker.ICONSET[op.iconset].icons, function(i, v) {
-               if (v.indexOf(search) > -1) {
+            $.each(icons, function(i, v) {
+               if (v.toLowerCase().indexOf(search) > -1) {
                    result.push(v);
                }
             });
             op.icons = result;
         }
     };
-    
+
     Iconpicker.prototype.removeAddClass = function (target, remove, add) {
         this.options.table.find(target).removeClass(remove).addClass(add);
         return add;
     };
-    
+
     Iconpicker.prototype.reset = function () {
-        this.updatePicker();                
+        this.updatePicker();
         this.changeList(1);
     };
-    
+
     Iconpicker.prototype.select = function (icon) {
         var op = this.options;
         var el = this.$element;
@@ -150,7 +160,8 @@
                 el.trigger({ type: "change", icon: 'empty' });
             }
             else {
-                el.trigger({ type: "change", icon: icon }); 
+                el.trigger({ type: "change", icon: icon });
+                el.find('input').val(icon);
             }
             op.table.find('button.' + op.selectedClass).removeClass(op.selectedClass);
         }
@@ -159,27 +170,27 @@
     Iconpicker.prototype.switchPage = function (icon) {
         var op = this.options;
         op.selected = $.inArray(icon.replace(op.iconClassFix, ''), op.icons);
-        
+
         if(op.selected >= 0) {
             var page = Math.ceil((op.selected + 1) / this.totalIconsPerPage());
             this.changeList(page);
-        }        
+        }
         if(icon === ''){
             op.table.find('i.' + op.iconClassFix).parent().addClass(op.selectedClass);
         }
         else{
             op.table.find('i.' + icon).parent().addClass(op.selectedClass);
-        }        
+        }
     };
-    
+
     Iconpicker.prototype.totalPages = function () {
-        return Math.ceil(this.totalIcons() / this.totalIconsPerPage());        
+        return Math.ceil(this.totalIcons() / this.totalIconsPerPage());
     };
-    
+
     Iconpicker.prototype.totalIcons = function () {
         return this.options.icons.length;
     };
-    
+
     Iconpicker.prototype.totalIconsPerPage = function () {
         if(this.options.rows === 0){
             return this.options.icons.length;
@@ -188,28 +199,24 @@
             return this.options.cols * this.options.rows;
         }
     };
-    
+
     Iconpicker.prototype.updateArrows = function (page) {
         var op = this.options;
         var total_pages = this.totalPages();
-        if (page === 1) { 
+        if (page === 1) {
             op.table.find('.btn-previous').addClass('disabled');
-            op.table.find('.btn-previous').prop('disabled', true);
         }
         else {
             op.table.find('.btn-previous').removeClass('disabled');
-            op.table.find('.btn-previous').prop('disabled', false);
         }
-        if (page === total_pages || total_pages === 0) { 
+        if (page === total_pages || total_pages === 0) {
             op.table.find('.btn-next').addClass('disabled');
-            op.table.find('.btn-next').prop('disabled', true);
         }
         else {
             op.table.find('.btn-next').removeClass('disabled');
-            op.table.find('.btn-next').prop('disabled', false);
         }
     };
-    
+
     Iconpicker.prototype.updateIcons = function (page) {
         var op = this.options;
         var tbody = op.table.find('tbody').empty();
@@ -235,7 +242,7 @@
             tbody.append(tr);
         }
     };
-    
+
     Iconpicker.prototype.updateIconsCount = function () {
         var op = this.options;
         if(op.footer === true){
@@ -249,7 +256,7 @@
             op.table.find('tfoot').empty().append(icons_count.join(''));
         }
     };
-    
+
     Iconpicker.prototype.updateLabels = function (page) {
         var op = this.options;
         var total_icons = this.totalIcons();
@@ -257,12 +264,12 @@
         op.table.find('.page-count').html(op.labelHeader.replace('{0}', (total_pages === 0 ) ? 0 : page).replace('{1}', total_pages));
         var offset = (page - 1) * this.totalIconsPerPage();
         var total = page * this.totalIconsPerPage();
-        op.table.find('.icons-count').html(op.labelFooter.replace('{0}', offset + 1).replace('{1}', (total < total_icons) ? total: total_icons).replace('{2}', total_icons));
-        this.updateArrows(page);        
+        op.table.find('.icons-count').html(op.labelFooter.replace('{0}', total_icons ? offset + 1 : 0).replace('{1}', (total < total_icons) ? total: total_icons).replace('{2}', total_icons));
+        this.updateArrows(page);
     };
-    
+
     Iconpicker.prototype.updatePagesCount = function () {
-        var op = this.options;        
+        var op = this.options;
         if(op.header === true){
             var tr = $('<tr></tr>');
             for (var i = 0; i < op.cols; i++) {
@@ -280,11 +287,11 @@
                     td.attr('colspan', op.cols - 2).append('<span class="page-count"></span>');
                     tr.append(td);
                 }
-            }            
+            }
             op.table.find('thead').empty().append(tr);
         }
     };
-    
+
     Iconpicker.prototype.updatePicker = function () {
         var op = this.options;
         if (op.cols < 4) {
@@ -299,7 +306,7 @@
             this.updateIconsCount();
         }
     };
-    
+
     Iconpicker.prototype.updateSearch = function () {
         var op = this.options;
         var search = [
@@ -310,7 +317,7 @@
             '</tr>'
         ];
         search = $(search.join(''));
-        if (op.search === true) { 
+        if (op.search === true) {
             search.show();
         }
         else {
@@ -318,34 +325,34 @@
         }
         op.table.find('thead').append(search);
     };
-    
+
     // ICONPICKER PUBLIC METHODS
     // ==============================
     Iconpicker.prototype.setAlign = function (value) {
         this.$element.removeClass(this.options.align).addClass(value);
         this.options.align = value;
     };
-    
+
     Iconpicker.prototype.setArrowClass = function (value) {
         this.options.arrowClass = this.removeAddClass('.btn-arrow', this.options.arrowClass, value);
     };
-    
+
     Iconpicker.prototype.setArrowNextIconClass = function (value) {
         this.options.arrowNextIconClass = this.removeAddClass('.btn-next > span', this.options.arrowNextIconClass, value);
     };
-    
+
     Iconpicker.prototype.setArrowPrevIconClass = function (value) {
         this.options.arrowPrevIconClass = this.removeAddClass('.btn-previous > span', this.options.arrowPrevIconClass, value);
     };
-    
+
     Iconpicker.prototype.setCols = function (value) {
         this.options.cols = value;
         this.reset();
     };
-        
+
     Iconpicker.prototype.setFooter = function (value) {
         var footer = this.options.table.find('tfoot');
-        if (value === true) { 
+        if (value === true) {
             footer.show();
         }
         else {
@@ -353,10 +360,10 @@
         }
         this.options.footer = value;
     };
-    
+
     Iconpicker.prototype.setHeader = function (value) {
         var header = this.options.table.find('thead');
-        if (value === true) { 
+        if (value === true) {
             header.show();
         }
         else {
@@ -364,14 +371,14 @@
         }
         this.options.header = value;
     };
-    
+
     Iconpicker.prototype.setIcon = function (value) {
         this.select(value);
     };
-    
+
     Iconpicker.prototype.setIconset = function (value) {
-        var op = this.options;                
-        if ($.isPlainObject(value)) {                    
+        var op = this.options;
+        if ($.isPlainObject(value)) {
             Iconpicker.ICONSET._custom = $.extend(Iconpicker.ICONSET_EMPTY, value);
             op.iconset = '_custom';
         }
@@ -385,29 +392,29 @@
         this.reset();
         this.select(op.icon);
     };
-    
+
     Iconpicker.prototype.setLabelHeader = function (value) {
         this.options.labelHeader = value;
         this.updateLabels(this.options.page);
     };
-    
+
     Iconpicker.prototype.setLabelFooter = function (value) {
         this.options.labelFooter = value;
         this.updateLabels(this.options.page);
     };
-    
+
     Iconpicker.prototype.setPlacement = function (value) {
         this.options.placement = value;
     };
-    
+
     Iconpicker.prototype.setRows = function (value) {
         this.options.rows = value;
         this.reset();
     };
-    
+
     Iconpicker.prototype.setSearch = function (value) {
         var search = this.options.table.find('.search-control');
-        if (value === true) { 
+        if (value === true) {
             search.show();
         }
         else {
@@ -417,20 +424,20 @@
         this.changeList(1);
         this.options.search = value;
     };
-    
+
     Iconpicker.prototype.setSearchText = function (value) {
         this.options.table.find('.search-control').attr('placeholder', value);
         this.options.searchText = value;
     };
-    
+
     Iconpicker.prototype.setSelectedClass = function (value) {
         this.options.selectedClass = this.removeAddClass('.btn-icon-selected', this.options.selectedClass, value);
     };
-    
+
     Iconpicker.prototype.setUnselectedClass = function (value) {
         this.options.unselectedClass = this.removeAddClass('.btn-icon', this.options.unselectedClass, value);
     };
-    
+
     // ICONPICKER PLUGIN DEFINITION
     // ========================
     var old = $.fn.iconpicker;
@@ -448,10 +455,10 @@
                 }
                 else {
                     data[option](params);
-                }                
+                }
             }
             else{
-                var op = data.options;                
+                var op = data.options;
                 op = $.extend(op, {
                     inline: false,
                     page: 1,
@@ -459,13 +466,13 @@
                     table: $('<table class="table-icons"><thead></thead><tbody></tbody><tfoot></tfoot></table>')
                 });
                 var name = (typeof $this.attr('name') !== 'undefined') ? 'name="' + $this.attr('name') + '"' : '';
-                
+
                 if($this.prop('tagName') === 'BUTTON'){
                     $this.empty()
                         .append('<i></i>')
                         .append('<input type="hidden" ' + name + '></input>')
                         .append('<span class="caret"></span>')
-                        .addClass('iconpicker'); 
+                        .addClass('iconpicker');
                     data.setIconset(op.iconset);
                     $this.on('click', function(e) {
                         e.preventDefault();
@@ -482,7 +489,7 @@
                         });
                         $this.data('bs.popover').tip().addClass('iconpicker-popover');
                         $this.popover('show');
-                    }); 
+                    });
                 }
                 else{
                     op.inline = true;
@@ -491,11 +498,11 @@
                         .append('<input type="hidden" ' + name + '></input>')
                         .append(op.table)
                         .addClass('iconpicker')
-                        .addClass(op.align);                
+                        .addClass(op.align);
                     data.switchPage(op.icon);
                     data.bindEvents();
                 }
-                                
+
             }
         });
     };
