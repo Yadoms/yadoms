fa17fd022519de3394f01471a541b3b49c91a08d
sgallou <sebastien.gallou@gmail.com>
2017-10-20 00:29:18 +0200

    Try to fix Travis build

---
+++ sources/plugins/OrangeBusiness/OrangeBusinessConfiguration.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "OrangeBusinessConfiguration.h"
+
+COrangeBusinessConfiguration::COrangeBusinessConfiguration()
+{
+}
+
+COrangeBusinessConfiguration::~COrangeBusinessConfiguration()
+{
+}
+
+void COrangeBusinessConfiguration::initializeWith(const shared::CDataContainer& data)
+{
+   m_data.initializeWith(data);
+}
+
+std::string COrangeBusinessConfiguration::getAPIKey() const
+{
+   return m_data.get<std::string>("APIKey");
+}
