fd7ed7d62b4438e650bb86c5544f6a49211293e4
sgallou <sebastien.gallou@gmail.com>
2017-12-19 14:12:48 +0100

    Fix Travis build

--- sources/plugins/EnOcean/profiles/util.py
+++ sources/plugins/EnOcean/profiles/util.py
@@ -33,24 +33,20 @@ __nb_errors__ = 0
 
 def info(s):
    print __FILE__() + "(" + str(__LINE__()) + "):" + " information: " + s
-   sys.stdout.flush()
 
 def warning(s):
    print __FILE__() + "(" + str(__LINE__()) + "):" + " warning: " + s
-   sys.stdout.flush()
    global __nb_warnings__
    __nb_warnings__ += 1
 
 def error(s):
    print __FILE__() + "(" + str(__LINE__()) + "):" + " error: " + s
-   sys.stdout.flush()
    global __nb_errors__
    __nb_errors__ += 1
 
 def finish():
    resultText = "Conversion failed" if __nb_errors__ != 0 else "Successfully converted"
    print "   --- " + resultText + " (" +  str(__nb_errors__) + ") error(s), (" + str(__nb_warnings__) + ") warning(s) ---"
-   sys.stdout.flush()
    sys.exit(0 if __nb_errors__ == 0 else 1)
 
 
