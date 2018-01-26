fac520ea12791ba92b09458174abf1f47063321d
sgallou <sebastien.gallou@gmail.com>
2018-01-26 10:36:49 +0100

    User tests : add test on adding page

--- tests/user/mainPage/__init__.py
+++ tests/user/mainPage/__init__.py
@@ -1,2 +1,73 @@
-﻿
+﻿from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as Condition
+from selenium.webdriver.common.keys import Keys
+from configurationPanel import ConfigurationPanel
+import tools
+import i18n
+import modals
 
+
+
+
+def getPagesMenuBar(browser):
+   return PageMenuBar(browser.find_element_by_xpath(".//div[@id='pageMenu']"))
+   
+def waitAddPageModal(browser):
+   modal = WebDriverWait(browser, 10).until(Condition.visibility_of_element_located((By.XPATH, ".//div[@id='modify-page-modal']")))
+   if modal.find_element_by_xpath(".//h4[@class='modal-title']").text == i18n.get()["modals"]["modify-page"]["createNewPageTitle"]:
+      return AddPageModal(modal)
+   assert False
+
+
+class PageMenuBar():
+   """ Operations on the pages menu bar """
+   
+   def __init__(self, barWebElement):
+      self.__bar = barWebElement
+   
+   def getAddPageButton(self):
+      return self.__bar.find_element_by_xpath(".//i[@class='fa fa-plus']")
+
+   def getPagesCount(self):
+      return len(self.__bar.find_elements_by_xpath(".//li[contains(@class, 'tabPagePills')]"))
+
+   def getPage(self, index):
+      return PageMenuBarTab(self.__bar.find_element_by_xpath(".//li[contains(@page-id, '" + str(index + 1) + "')]"))
+
+
+class PageMenuBarTab():
+   """ Operations on a tab of the pages menu bar """
+   
+   def __init__(self, tabWebElement):
+      self.__tab = tabWebElement
+   
+   def getName(self):
+      return self.__tab.find_element_by_xpath(".//a[@data-toggle='tab']//span").text
+
+
+class AddPageModal():
+   """ Operations on add page modal """
+   
+   def __init__(self, modalWebElement):
+      self.__modal = modalWebElement
+
+   def enterPageName(self, pageName):
+      item = self.__modal.find_element_by_xpath(".//input[@id='modify-page-text']")
+      item.send_keys(Keys.CONTROL + 'a')
+      item.send_keys(Keys.DELETE)
+      item.send_keys(pageName)
+
+   def getConfirmButton(self):
+      return self.__modal.find_element_by_xpath(".//button[@id='btn-confirm-modify-page']")
+         
+   def getCancelButton(self):
+      return self.__modal.find_element_by_xpath(".//button[@data-i18n='common.cancel']")
+      
+   def ok(self):
+      self.getConfirmButton().click()
+      modals.waitForClosed(self.__modal)
+      
+   def cancel(self):
+      self.getCancelButton().click()
+      modals.waitForClosed(self.__modal)
---
+++ tests/user/mainPage/testAddPage.py
@@ -0,0 +1,75 @@
+﻿import sys
+sys.path.append('.')
+import unittest
+from selenium import webdriver
+import database
+import config
+import scripts
+import yadomsServer
+import mainPage.widgets
+import time
+
+class AddPage(unittest.TestCase):
+   """App page test"""
+   
+   def setUp(self):
+      yadomsServer.ensureStopped()
+      database.new()
+      config.deploy("nominal")
+      scripts.deleteAll()
+      self.serverProcess = yadomsServer.start()
+      self.browser = webdriver.Chrome()
+      self.browser.implicitly_wait(10)
+      yadomsServer.openClient(self.browser)
+            
+            
+   def test_cancelAddPage(self):
+      print '=== Entering/Exiting customization test ==='
+      
+      print 'Enter customizing mode'
+      mainPage.widgets.enterCustomizingMode(self.browser)
+         
+      print 'Open add page modal'
+      mainPage.getPagesMenuBar(self.browser).getAddPageButton().click()
+      addPageModal = mainPage.waitAddPageModal(self.browser)
+      print 'Cancel add page modal'
+      addPageModal.cancel()
+      
+      print 'Exit customizing mode'
+      mainPage.widgets.exitCustomizingMode(self.browser)
+            
+            
+   def test_addPage(self):
+      print '=== Add a page test ==='
+      newPageName = "New page"
+      
+      print 'Enter customizing mode'
+      mainPage.widgets.enterCustomizingMode(self.browser)
+         
+      print 'Open add page modal'
+      pagesMenuBar = mainPage.getPagesMenuBar(self.browser)
+      pagesMenuBar.getAddPageButton().click()
+      addPageModal = mainPage.waitAddPageModal(self.browser)
+
+      print 'Create the page'
+      addPageModal.enterPageName(newPageName)
+      addPageModal.ok()
+
+      print 'Check page was created (customizing mode)'
+      self.assertEqual(pagesMenuBar.getPagesCount(), 2)
+      self.assertEqual(pagesMenuBar.getPage(1).getName(), newPageName)      
+      
+      print 'Exit customizing mode'
+      mainPage.widgets.exitCustomizingMode(self.browser)
+      
+      print 'Check page was created (normal mode)'
+      pagesMenuBar = mainPage.getPagesMenuBar(self.browser)
+      self.assertEqual(pagesMenuBar.getPagesCount(), 2)
+      self.assertEqual(pagesMenuBar.getPage(1).getName(), newPageName)
+      
+   def tearDown(self):
+      self.browser.close()
+      yadomsServer.stop(self.serverProcess)
+
+if __name__ == "__main__":
+   unittest.main()
