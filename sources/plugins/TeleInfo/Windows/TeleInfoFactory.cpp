9a57f58c3e9919555143c06f35a0ab6167ac9de5
sgallou <sebastien.gallou@gmail.com>
2018-05-04 15:35:57 +0200

    Fix unit tests

--- sources/plugins/TeleInfo/Windows/TeleInfoFactory.cpp
+++ sources/plugins/TeleInfo/Windows/TeleInfoFactory.cpp
@@ -59,13 +59,10 @@ boost::shared_ptr<shared::communication::IAsyncPort> CTeleInfoFactory::construct
    {
       port = boost::make_shared<shared::communication::CAsyncSerialPort>(configuration.getSerialPort(),
                                                                          boost::asio::serial_port_base::baud_rate(1200),
-                                                                         boost::asio::serial_port_base::parity(
-                                                                            boost::asio::serial_port_base::parity::even),
+                                                                         boost::asio::serial_port_base::parity(boost::asio::serial_port_base::parity::even),
                                                                          boost::asio::serial_port_base::character_size(7),
-                                                                         boost::asio::serial_port_base::stop_bits(
-                                                                            boost::asio::serial_port_base::stop_bits::one),
-                                                                         boost::asio::serial_port_base::flow_control(
-                                                                            boost::asio::serial_port_base::flow_control::none));
+                                                                         boost::asio::serial_port_base::stop_bits(boost::asio::serial_port_base::stop_bits::one),
+                                                                         boost::asio::serial_port_base::flow_control(boost::asio::serial_port_base::flow_control::none));
    }
 
    port->subscribeForConnectionEvents(eventHandler,
