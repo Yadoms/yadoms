cdbb49c246991a1a90c3ac6a4b6ff416d0d34cf0
sgallou <sebastien.gallou@gmail.com>
2019-03-28 11:25:35 +0100

    Fix Linux build

--- sources/server/database/common/versioning/Version_4_3_0.h
+++ sources/server/database/common/versioning/Version_4_3_0.h
@@ -35,22 +35,6 @@ namespace database
 
             static void updateFrom4_2_0(const boost::shared_ptr<IDatabaseRequester>& requester);
 
-            static boost::optional<bool> loadFirstStart(const boost::shared_ptr<IDatabaseRequester>& requester);
-            static boost::optional<std::string> loadLocation(const boost::shared_ptr<IDatabaseRequester>& requester);
-            static boost::optional<std::string> loadLanguage(const boost::shared_ptr<IDatabaseRequester>& requester);
-            static boost::optional<bool> loadAdvancedParameters(const boost::shared_ptr<IDatabaseRequester>& requester);
-            static boost::optional<std::string> loadDateFormatString(const boost::shared_ptr<IDatabaseRequester>& requester);
-            static boost::optional<bool> loadRefreshPage(const boost::shared_ptr<IDatabaseRequester>& requester);
-            static boost::optional<std::string> loadBasicAuthentication(const boost::shared_ptr<IDatabaseRequester>& requester);
-
-            static boost::optional<shared::CDataContainer> convertLocation(const boost::optional<std::string>& oldLocation);
-            static boost::optional<shared::CDataContainer> convertBasicAuthentication(const boost::optional<std::string>& oldBasicAuthentication);
-
-            static void insertConfigurationValue(const boost::shared_ptr<IDatabaseRequester> requester,
-                                                 const std::string& section,
-                                                 const shared::CDataContainer& value,
-                                                 const boost::posix_time::ptime& insertDate);
-
             static std::string convertDuration(const std::string& duration);
          };
       } //namespace versioning
