62c15d54a6e690540d05b5120a8cb3eead2ffaac
jano42 <decoretjm@gmail.com>
2019-05-28 07:53:12 +0200

    Add new database version 4.4
    Fix #568
    At yadoms update from database 4.3 to 4.4, the summary acquisitions for months and years are computed.

---
+++ sources/server/database/common/versioning/Version_4_4_0.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "Version_4_3_0.h"
+#include "database/IDatabaseRequester.h"
+
+
+namespace database
+{
+   namespace common
+   {
+      namespace versioning
+      {
+         //
+         /// \brief Database version 4.3.0 update manager
+         //
+         class CVersion_4_4_0 : public CVersion_4_3_0
+         {
+         public:
+            //
+            /// \brief Constructor
+            //
+            CVersion_4_4_0();
+
+            //
+            /// \brief Destructor
+            //
+            virtual ~CVersion_4_4_0();
+
+            // ISQLiteVersionUpgrade implementation
+            void checkForUpgrade(const boost::shared_ptr<IDatabaseRequester>& requester,
+                                 const shared::versioning::CVersion& currentVersion) override;
+            // [END] ISQLiteVersionUpgrade implementation
+
+         private:
+            static const shared::versioning::CVersion Version;
+
+            static void updateFrom4_3_0(const boost::shared_ptr<IDatabaseRequester>& requester);
+         };
+      } //namespace versioning
+   } //namespace common
+} //namespace database 
