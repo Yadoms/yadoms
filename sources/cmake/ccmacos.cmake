369a66b9de8ce850f40f214c156e52856ae1d806
jano42 <decoretjm@gmail.com>
2019-08-14 11:38:57 +0200

    Fix cross compilation for macos
    execute osxcross-conf to generate all env vars
    fix find poco path (add lowercase darwin folder name)
    rewrite ccmacos.cmake from osxcross one and adjust it

--- sources/cmake/ccmacos.cmake
+++ sources/cmake/ccmacos.cmake
@@ -1,39 +1,53 @@
-# this one is important
-SET(CMAKE_SYSTEM_NAME Darwin)
+# OSXCross toolchain
 
-#this one not so much
-SET(CMAKE_SYSTEM_VERSION 1)
-SET(CMAKE_CROSSCOMPILING True)
+macro(osxcross_getconf VAR)
+  if(NOT ${VAR})
+    set(${VAR} "$ENV{${VAR}}")
+    if(${VAR})
+      set(${VAR} "${${VAR}}" CACHE STRING "${VAR}")
+      message(STATUS "Found ${VAR}: ${${VAR}}")
+    else()
+      message(FATAL_ERROR "Cannot determine \"${VAR}\"")
+    endif()
+  endif()
+endmacro()
 
-# User specific configuration
-include(CMakeListsUserConfig.txt OPTIONAL)
+osxcross_getconf(OSXCROSS_HOST)
+osxcross_getconf(OSXCROSS_TARGET_DIR)
+osxcross_getconf(OSXCROSS_TARGET)
+osxcross_getconf(OSXCROSS_SDK)
 
-# cross compiler tools
-set(CC_RPI_ROOT /opt/osxcross/target)
-set(CC_RPI_GCC ${CC_RPI_ROOT}/bin/x86_64-apple-darwin17-gcc)
-set(CC_RPI_GXX ${CC_RPI_ROOT}/bin/x86_64-apple-darwin17-g++)
-set(CC_RPI_LIBS /opt/osxcross/target/bin/../SDK/MacOSX10.13.sdk)
-set(CMAKE_SYSROOT ${CC_RPI_LIBS})
-
-#fix boost find libs by enableing GHSMULTI
-set(GHSMULTI ON)
+set(CMAKE_SYSTEM_NAME "Darwin")
+string(REGEX REPLACE "-.*" "" CMAKE_SYSTEM_PROCESSOR "${OSXCROSS_HOST}")
 
 # specify the cross compiler
-SET(CMAKE_C_COMPILER   ${CC_RPI_GCC})
-SET(CMAKE_CXX_COMPILER ${CC_RPI_GXX})
+#if(CMAKE_SYSTEM_PROCESSOR MATCHES "^i.86$")
+#  set(CMAKE_C_COMPILER "${OSXCROSS_TARGET_DIR}/bin/o32-gcc")
+#  set(CMAKE_CXX_COMPILER "${OSXCROSS_TARGET_DIR}/bin/o32-g++")
+#elseif(CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64")
+#  set(CMAKE_C_COMPILER "${OSXCROSS_TARGET_DIR}/bin/x86_64-apple-darwin17-gcc")
+#  set(CMAKE_CXX_COMPILER "${OSXCROSS_TARGET_DIR}/bin/x86_64-apple-darwin17-g++")
+#else()
+#  message(FATAL_ERROR "Unrecognized target architecture")
+#endif()
 
-message(STATUS "Cross building for RaspberryPI")
-message(STATUS "CC_RPI_ROOT : ${CC_RPI_ROOT}")
-message(STATUS "CC_RPI_GCC : ${CC_RPI_GCC}")
-message(STATUS "CC_RPI_GXX : ${CC_RPI_GXX}")
-message(STATUS "CC_RPI_LIBS : ${CC_RPI_LIBS}")
+# where is the target environment
+set(CMAKE_FIND_ROOT_PATH
+  "${OSXCROSS_SDK}"
+  "${OSXCROSS_TARGET_DIR}/macports/pkgs/opt/local")
 
 # search for programs in the build host directories
-SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # for libraries and headers in the target directories
-SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
-#define the systemname (for good package name)
-set(CMAKE_PACKAGE_PLATFORM_NAME "Darwin")
+set(CMAKE_AR "${OSXCROSS_TARGET_DIR}/bin/${OSXCROSS_HOST}-ar" CACHE FILEPATH "ar")
+set(CMAKE_RANLIB "${OSXCROSS_TARGET_DIR}/bin/${OSXCROSS_HOST}-ranlib" CACHE FILEPATH "ranlib")
+set(CMAKE_INSTALL_NAME_TOOL "${OSXCROSS_TARGET_DIR}/bin/${OSXCROSS_HOST}-install_name_tool" CACHE FILEPATH "install_name_tool")
+
+set(ENV{PKG_CONFIG_LIBDIR} "${OSXCROSS_TARGET_DIR}/macports/pkgs/opt/local/lib/pkgconfig")
+set(ENV{PKG_CONFIG_SYSROOT_DIR} "${OSXCROSS_TARGET_DIR}/macports/pkgs")
+
+set(CMAKE_FIND_FRAMEWORK_EXTRA_LOCATIONS ${OSXCROSS_SDK})
