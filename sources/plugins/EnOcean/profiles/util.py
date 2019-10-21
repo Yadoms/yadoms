3559ebed8f19da728f3fac74c732e600205b1e03
oben01 <oussama.dahmaz@gmail.com>
2019-10-21 14:42:42 +0200

    Build with python 3.7 (may be not working with python 2.x) (#601)
    * Build with python 3.7 (may be not working with python 2.x)
    
    * Add Python version
    
    * Add Python version

--- sources/plugins/EnOcean/profiles/util.py
+++ sources/plugins/EnOcean/profiles/util.py
@@ -32,21 +32,21 @@ __nb_warnings__ = 0
 __nb_errors__ = 0
 
 def info(s):
-   print __FILE__() + "(" + str(__LINE__()) + "):" + " information: " + s
+   print(__FILE__() + "(" + str(__LINE__()) + "):" + " information: " + s)
 
 def warning(s):
-   print __FILE__() + "(" + str(__LINE__()) + "):" + " warning: " + s
+   print(__FILE__() + "(" + str(__LINE__()) + "):" + " warning: " + s)
    global __nb_warnings__
    __nb_warnings__ += 1
 
 def error(s):
-   print __FILE__() + "(" + str(__LINE__()) + "):" + " error: " + s
+   print(__FILE__() + "(" + str(__LINE__()) + "):" + " error: " + s)
    global __nb_errors__
    __nb_errors__ += 1
 
 def finish():
    resultText = "Conversion failed" if __nb_errors__ != 0 else "Successfully converted"
-   print "   --- " + resultText + " (" +  str(__nb_errors__) + ") error(s), (" + str(__nb_warnings__) + ") warning(s) ---"
+   print("   --- " + resultText + " (" +  str(__nb_errors__) + ") error(s), (" + str(__nb_warnings__) + ") warning(s) ---")
    sys.exit(0 if __nb_errors__ == 0 else 1)
 
 
