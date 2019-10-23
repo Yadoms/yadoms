b767ff44c6c57728ba4ba3d04fe9ea0f0e36ac41
Sébastien Gallou <sebastien.gallou@gmail.com>
2019-10-23 08:17:25 +0200

    Refactor CHttpMethods class

--- sources/plugins/OledEspEasy/OledEspEasyController.cpp
+++ sources/plugins/OledEspEasy/OledEspEasyController.cpp
@@ -5,72 +5,65 @@
 #include "shared/StringExtension.h"
 
 COledEspEasyController::COledEspEasyController(const COledEspEasyConfiguration& configuration)
-	:m_configuration(configuration)
-{
-	
-}
-
-COledEspEasyController::~COledEspEasyController()
+   : m_configuration(configuration)
 {
 }
 
 //nested enum with specific strings
 DECLARE_ENUM_IMPLEMENTATION_NESTED(COledEspEasyController::ECommands, ECommands,
-	((Oled)("oled"))
-	((OledCommand)("oledcmd"))
+                                   ((Oled)("oled"))
+                                   ((OledCommand)("oledcmd"))
 );
-
 
 
 void COledEspEasyController::switch_on()
 {
-	sendCommand(ECommands::kOledCommand, "on");
+   sendCommand(ECommands::kOledCommand, "on");
 }
 
 void COledEspEasyController::switch_off()
 {
-	sendCommand(ECommands::kOledCommand, "off");
+   sendCommand(ECommands::kOledCommand, "off");
 }
 
 void COledEspEasyController::clear_screen()
 {
-	sendCommand(ECommands::kOledCommand, "clear");
+   sendCommand(ECommands::kOledCommand, "clear");
 }
 
 
 void COledEspEasyController::update_line(int iLine, int iCol, const std::string& text)
 {
-	std::string fixedText = processText(text);
-	sendCommand(ECommands::kOled, boost::lexical_cast<std::string>(iLine), boost::lexical_cast<std::string>(iCol), fixedText);
+   const auto fixedText = processText(text);
+   sendCommand(ECommands::kOled, std::to_string(iLine), std::to_string(iCol), fixedText);
 }
 
-void COledEspEasyController::sendCommand(const ECommands& command, const std::vector<std::string> & args) const
+void COledEspEasyController::sendCommand(const ECommands& command, const std::vector<std::string>& args) const
 {
-	try
-	{
-		std::ostringstream cmd;
-		cmd << "http://" << m_configuration.getIPAddress();
-		cmd << "/control?cmd=" << command.toString();
-		for (auto&& arg : args)
-		{
-			cmd << "," << arg;
-		}
-		shared::CHttpMethods::sendGetRequest(cmd.str());
-	}
-	catch (std::exception &ex)
-	{
-		YADOMS_LOG(error) << "Fail to send command to ESPEasy : " << ex.what();
-	}
-	catch (...)
-	{
-		YADOMS_LOG(error) << "Fail to send command to ESPEasy : unknown error";
-	}
+   try
+   {
+      std::ostringstream cmd;
+      cmd << "http://" << m_configuration.getIPAddress();
+      cmd << "/control?cmd=" << command.toString();
+      for (auto&& arg : args)
+      {
+         cmd << "," << arg;
+      }
+      shared::CHttpMethods::sendGetRequest(cmd.str());
+   }
+   catch (std::exception& ex)
+   {
+      YADOMS_LOG(error) << "Fail to send command to ESPEasy : " << ex.what();
+   }
+   catch (...)
+   {
+      YADOMS_LOG(error) << "Fail to send command to ESPEasy : unknown error";
+   }
 }
 
 std::string COledEspEasyController::processText(const std::string& text)
 {
-	std::string t = boost::replace_all_copy(text, " ", "%20");
+   std::string t = boost::replace_all_copy(text, " ", "%20");
 
-	return t;
+   return t;
 }
-
--- sources/shared/shared/http/SecureSession.cpp
+++ sources/shared/shared/http/SecureSession.cpp
@@ -6,32 +6,40 @@
 
 namespace shared
 {
-	CSecureSession::CSecureSession(std::string url) : 
-		m_url(url)
-	{
-		Poco::URI uri(url);
-		Poco::Crypto::OpenSSLInitializer::initialize();
-		const Poco::Net::Context::Ptr context(new Poco::Net::Context(Poco::Net::Context::CLIENT_USE, "", "", "", Poco::Net::Context::VERIFY_NONE, 9, false, "ALL:!ADH:!LOW:!EXP:!MD5:@STRENGTH"));
-		m_session = boost::make_shared<Poco::Net::HTTPSClientSession>(uri.getHost(), uri.getPort(), context);
-	}
+   CSecureSession::CSecureSession(const std::string& url)
+      : m_url(url)
+   {
+      const Poco::URI uri(url);
+      Poco::Crypto::OpenSSLInitializer::initialize();
+      const Poco::Net::Context::Ptr context(new Poco::Net::Context(Poco::Net::Context::CLIENT_USE,
+                                                                   "",
+                                                                   "",
+                                                                   "",
+                                                                   Poco::Net::Context::VERIFY_NONE,
+                                                                   9,
+                                                                   false,
+                                                                   "ALL:!ADH:!LOW:!EXP:!MD5:@STRENGTH"));
 
-	void CSecureSession::setTimeout(const boost::posix_time::time_duration& timeout)
-	{
-		m_session->setTimeout(Poco::Timespan(timeout.seconds(), 0));
-	}
+      m_session = boost::make_shared<Poco::Net::HTTPSClientSession>(uri.getHost(), uri.getPort(), context);
+   }
 
-	std::ostream& CSecureSession::sendRequest(Poco::Net::HTTPRequest& request)
-	{
-		return m_session->sendRequest(request);
-	}
+   void CSecureSession::setTimeout(const boost::posix_time::time_duration& timeout)
+   {
+      m_session->setTimeout(Poco::Timespan(static_cast<long>(timeout.seconds()), 0));
+   }
 
-	std::istream& CSecureSession::receiveResponse(Poco::Net::HTTPResponse& response)
-	{
-		return m_session->receiveResponse(response);
-	}
+   std::ostream& CSecureSession::sendRequest(Poco::Net::HTTPRequest& request)
+   {
+      return m_session->sendRequest(request);
+   }
 
-	const std::string& CSecureSession::getUrl() const
-	{
-		return m_url;
-	}
+   std::istream& CSecureSession::receiveResponse(Poco::Net::HTTPResponse& response)
+   {
+      return m_session->receiveResponse(response);
+   }
+
+   const std::string& CSecureSession::getUrl() const
+   {
+      return m_url;
+   }
 } // namespace shared
