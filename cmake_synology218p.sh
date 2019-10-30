58797b2c31d31144572b322d807c3ed03030fb14
jano42 <decoretjm@gmail.com>
2019-10-30 16:16:03 +0100

    Add Synology 218+ support

---
+++ cmake_synology218p.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#move to project directory
+BASEDIR=$(dirname $0);
+set -e
+cd $BASEDIR/projects
+
+#treat script parameter (if there is one, then directly use it for cases)
+if [ -z "$1" ]
+then
+	echo "Which choice would you like?"
+	echo " -> Generate a linux makefile for Cross Compilation (c)"
+	echo " -> Generate a linux makefile for Cross Compilation RELEASE (r)"
+	read choice
+else
+	choice=$1
+fi
+
+getCMakeExecutable () {
+   local cmakePath=$( grep -oP '^[\t ]*set\(ALTERNATIVE_CMAKE_ROOT[[:space:]]\"\K.*(?=")' ../sources/CMakeListsUserConfig.txt )
+   if [ -z "$cmakePath" ]
+   then
+      echo cmake
+   else
+      echo $cmakePath/bin/cmake
+   fi
+}
+cmake_executable=$(getCMakeExecutable)
+echo "Use cmake executable $cmake_executable"
+$cmake_executable --version
+
+#run cmake depending on user choice (or script parameter)
+case "$choice" in
+
+    c)
+	# cmake for cross compilation
+	$cmake_executable  -DCMAKE_YADOMS_PLATFORM=Synology218p -DCC_RPI_GCC=x86_64-pc-linux-gnu-gcc -DCC_RPI_GXX=x86_64-pc-linux-gnu-g++  -DCMAKE_TOOLCHAIN_FILE=../sources/cmake/Synology218p.cmake ../sources
+	;;
+
+    r)
+	# cmake for cross compilation RELEASE
+	cmake -DCMAKE_BUILD_TYPE="Release" -DCMAKE_YADOMS_PLATFORM=Synology218p -DCC_RPI_GCC=x86_64-pc-linux-gnu-gcc -DCC_RPI_GXX=x86_64-pc-linux-gnu-g++  -DCMAKE_TOOLCHAIN_FILE=../sources/cmake/Synology218p.cmake ../sources
+	;;
+
+    *)
+	# generate an error and return 1
+	echo "$choice is not a valid choice"
+	exit 1
+	;;
+esac
+
---
+++ sources/cmake/synology218p.cmake
@@ -0,0 +1,34 @@
+# this one is important
+SET(CMAKE_SYSTEM_NAME Linux)
+#this one not so much
+SET(CMAKE_SYSTEM_VERSION 1)
+SET(CMAKE_CROSSCOMPILING True)
+set(CMAKE_COMPILER_IS_RASPBERRY_CROSS_COMPILER ON)
+
+# User specific configuration
+include(CMakeListsUserConfig.txt OPTIONAL)
+
+# cross compiler tools
+set(CC_RPI_GCC ${CC_RPI_ROOT}/bin/x86_64-pc-linux-gnu-gcc)
+set(CC_RPI_GXX ${CC_RPI_ROOT}/bin/x86_64-pc-linux-gnu-g++)
+set(CC_RPI_LIBS ${CC_RPI_ROOT}/x86_64-pc-linux-gnu/sys-root)
+
+# specify the cross compiler
+SET(CMAKE_C_COMPILER   ${CC_RPI_GCC})
+SET(CMAKE_CXX_COMPILER ${CC_RPI_GXX})
+
+message(STATUS "Cross building for Synology 218+")
+message(STATUS "CC_RPI_ROOT : ${CC_RPI_ROOT}")
+message(STATUS "CC_RPI_GCC : ${CC_RPI_GCC}")
+message(STATUS "CC_RPI_GXX : ${CC_RPI_GXX}")
+message(STATUS "CC_RPI_LIBS : ${CC_RPI_LIBS}")
+
+# search for programs in the build host directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# for libraries and headers in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+#define the systemname (for good package name)
+set(CMAKE_PACKAGE_PLATFORM_NAME "Synology 218+")
