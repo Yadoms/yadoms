c9ed399781da49642e7a4b1af72971d7755dec8d
sgallou <sebastien.gallou@gmail.com>
2019-12-11 09:57:27 +0100

    Fix HTML user tests report

--- tests/user/suite-html.py
+++ tests/user/suite-html.py
@@ -2,37 +2,19 @@ import sys
 sys.path.append('.')
 import unittest
 import HtmlTestRunner
-
-try:
-    from StringIO import StringIO
-except ImportError:
-    from io import StringIO
-
 import tools
+import shutil
  
- #TODO gros ménage à faire
-# class Test_Suite(unittest.TestCase):
-
-#    def test_main(self):
-#       tools.deleteContext()      
-#       loader = unittest.TestLoader()         
-#       suite = loader.discover('.')
-
-      # with open("report/index.html", 'w') as outFile:
-      #    runner = HtmlTestRunner.HTMLTestRunner(
-      #       stream=outFile,
-      #       title='Yadoms user tests report',
-      #       description='Tests covering Yadoms usage from the web client'
-      #    )
-      #    runner.run(suite)
-
 
 if __name__ == "__main__":
    tools.deleteContext()         
    loader = unittest.TestLoader()         
    suite = loader.discover('.')
-   # runner = unittest.TextTestRunner()
-   # runner.run(suite)
-   # unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='report'), testLoader=loader)
-   runner = HtmlTestRunner.HTMLTestRunner(output='report')
+
+   shutil.rmtree('report')
+   runner = HtmlTestRunner.HTMLTestRunner(output = 'report',
+                                          combine_reports = True,
+                                          report_name = 'index',
+                                          add_timestamp = False,
+                                          report_title ='Yadoms user tests report')
    runner.run(suite)
