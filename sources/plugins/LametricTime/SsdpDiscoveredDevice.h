fca20f92db132916c5a443c3b07c47926d19acfa
oussama dahmaz <oussama.dahmaz@gmail.com>
2019-12-16 16:16:40 +0100

    Add Upnp protocol

---
+++ sources/plugins/LametricTime/SsdpDiscoveredDevice.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include <string>
+
+class CSsdpDiscoveredDevice
+{
+public:
+	CSsdpDiscoveredDevice() = default;
+	explicit CSsdpDiscoveredDevice(std::string& deviceDescription);
+	virtual ~CSsdpDiscoveredDevice() = default;
+	
+	std::string findTag(const std::string& tagName) const;
+	std::string getIp() const;
+private:
+	std::string m_deviceDescription;
+};
