79eafe2bfc2cbd391e778d0812c9d2ec6df8d0c7
oussama dahmaz <oussama.dahmaz@gmail.com>
2019-12-18 17:16:33 +0100

    First Import

---
+++ sources/plugins/StreamDeck/Configuration.cpp
@@ -0,0 +1,52 @@
+#include "stdafx.h"
+#include "Configuration.h"
+#include <shared/Log.h>
+
+CConfiguration::~CConfiguration()
+{
+}
+
+void CConfiguration::initializeWith(const shared::CDataContainer& data)
+{
+   m_configuration.initializeWith(data);
+}
+
+/*
+    You will find here an example how to retrieve enum variables from the configuration
+*/
+
+EEnumType CConfiguration::getEnumParameter() const
+{
+   // Enum type, declare keys labels
+   static const shared::CDataContainer::EnumValuesNames EEnumTypeNames = boost::assign::map_list_of
+      ("EnumValue1", kEnumValue1)
+      ("EnumValue2", kEnumValue2)
+      ("EnumValue3", kEnumValue3);
+
+   return m_configuration.getEnumValue<EEnumType>("EnumParameter", EEnumTypeNames);
+}
+
+void CConfiguration::trace() const
+{
+   try
+   {
+      // Get simple parameters
+      YADOMS_LOG(information) << "StreamDeck configuration, parameter 'StringParameter' is "
+         << (m_configuration.get<std::string>("StringParameter").empty() ? "empty" : m_configuration.get<std::string>("StringParameter"))
+        ;
+      YADOMS_LOG(information) << "StreamDeck configuration, parameter 'BoolParameter' is " << m_configuration.get<bool>("BoolParameter");
+      YADOMS_LOG(information) << "StreamDeck configuration, parameter 'DecimalParameter' is " << m_configuration.get<double>("DecimalParameter");
+      YADOMS_LOG(information) << "StreamDeck configuration, parameter 'IntParameter' is " << m_configuration.get<int>("IntParameter");
+
+      // Enum
+      YADOMS_LOG(information) << "StreamDeck configuration, parameter 'EnumParameter' is " << getEnumParameter();
+   }
+   catch (const shared::exception::CInvalidParameter& e)
+   {
+      YADOMS_LOG(error) << "Parameter not found : " << e.what();
+   }
+   catch (const shared::exception::COutOfRange& e)
+   {
+      YADOMS_LOG(error) << "Parameter value out of range : " << e.what();
+   }
+}
---
+++ sources/plugins/StreamDeck/StreamDeck.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <plugin_cpp_api/IPlugin.h>
+#include "Configuration.h"
+
+// Shortcut to yPluginApi namespace
+namespace yApi = shared::plugin::yPluginApi;
+
+//--------------------------------------------------------------
+/// \brief	This class is an empty plugin example
+/// \note   This plugin do nothing, you just have to :
+///         - rename all classes of this plugin
+///         - populate all code/functions
+//--------------------------------------------------------------
+class CStreamDeck : public plugin_cpp_api::IPlugin
+{
+public:
+   //--------------------------------------------------------------
+   /// \brief	Constructor
+   //--------------------------------------------------------------
+   CStreamDeck();
+
+   //--------------------------------------------------------------
+   /// \brief	Destructor
+   //--------------------------------------------------------------
+   virtual ~CStreamDeck();
+
+   // IPlugin implementation
+   void doWork(boost::shared_ptr<yApi::IYPluginApi> api) override;
+   // [END] IPlugin implementation
+
+private:
+   //--------------------------------------------------------------
+   /// \brief	The plugin configuration
+   //--------------------------------------------------------------
+   CConfiguration m_configuration;
+};
