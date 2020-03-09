fdc42090de04c64d7b77355feccfcabac9c4c12a
jano42 <decoretjm@gmail.com>
2020-03-09 15:20:43 +0100

    Fix rfplayer plugin

--- sources/plugins/RfPlayer/ManuallyDeviceFactory.cpp
+++ sources/plugins/RfPlayer/ManuallyDeviceFactory.cpp
@@ -19,18 +19,18 @@ void CManuallyDeviceFactory::createDeviceManually(boost::shared_ptr<shared::plug
       YADOMS_LOG(information) << "Receive event kEventManuallyDeviceCreation : \n"
          << "\tDevice name : " << request.getDeviceName();
 
-      request.getConfiguration().printToLog(YADOMS_LOG(information));
+      request.getConfiguration()->printToLog(YADOMS_LOG(information));
    }
 
    if (request.getDeviceType() == "blyss")
    {
       auto keyword = boost::make_shared<specificHistorizers::outgoing::CBlyssKeyword>("command");
-      shared::CDataContainer details;
-      details.set("groupCode", request.getConfiguration().get<std::string>("groupCode"));
-      details.set("unitCode", request.getConfiguration().get<int>("unitCode"));
-      details.set("frequency", "433");
-      details.set("protocol", "blyss");
-      details.set("burst", request.getConfiguration().getWithDefault<std::string>("burst", ""));
+      shared::CDataContainerSharedPtr details = new_CDataContainerSharedPtr();
+      details->set("groupCode", request.getConfiguration()->get<std::string>("groupCode"));
+      details->set("unitCode", request.getConfiguration()->get<int>("unitCode"));
+      details->set("frequency", "433");
+      details->set("protocol", "blyss");
+      details->set("burst", request.getConfiguration()->getWithDefault<std::string>("burst", ""));
       if (api->getDeviceModel(request.getDeviceName()).empty())
          api->updateDeviceModel(request.getDeviceName(), "BLYSS");
       api->updateDeviceDetails(request.getDeviceName(), details);
@@ -41,11 +41,11 @@ void CManuallyDeviceFactory::createDeviceManually(boost::shared_ptr<shared::plug
    if (request.getDeviceType() == "x10")
    {
       auto keyword = boost::make_shared<specificHistorizers::outgoing::CX10Keyword>("command");
-      shared::CDataContainer details;
-      details.set("id", request.getConfiguration().get<int>("id"));
-      details.set("frequency", "433");
-      details.set("protocol", "x10");
-      details.set("burst", request.getConfiguration().getWithDefault<std::string>("burst", ""));
+      shared::CDataContainerSharedPtr details = new_CDataContainerSharedPtr();
+      details->set("id", request.getConfiguration()->get<int>("id"));
+      details->set("frequency", "433");
+      details->set("protocol", "x10");
+      details->set("burst", request.getConfiguration()->getWithDefault<std::string>("burst", ""));
       if (api->getDeviceModel(request.getDeviceName()).empty())
          api->updateDeviceModel(request.getDeviceName(), "X10");
       api->updateDeviceDetails(request.getDeviceName(), details);
@@ -56,11 +56,11 @@ void CManuallyDeviceFactory::createDeviceManually(boost::shared_ptr<shared::plug
    if (request.getDeviceType() == "domia")
    {
       auto keyword = boost::make_shared<specificHistorizers::outgoing::CDomiaKeyword>("command");
-      shared::CDataContainer details;
-      details.set("id", request.getConfiguration().get<int>("id"));
-      details.set("frequency", "433");
-      details.set("protocol", "domia");
-      details.set("burst", request.getConfiguration().getWithDefault<std::string>("burst", ""));
+      shared::CDataContainerSharedPtr details = new_CDataContainerSharedPtr();
+      details->set("id", request.getConfiguration()->get<int>("id"));
+      details->set("frequency", "433");
+      details->set("protocol", "domia");
+      details->set("burst", request.getConfiguration()->getWithDefault<std::string>("burst", ""));
       if (api->getDeviceModel(request.getDeviceName()).empty())
          api->updateDeviceModel(request.getDeviceName(), "DOMIA");
       api->updateDeviceDetails(request.getDeviceName(), details);
@@ -71,11 +71,11 @@ void CManuallyDeviceFactory::createDeviceManually(boost::shared_ptr<shared::plug
    if (request.getDeviceType() == "chacon")
    {
       auto keyword = boost::make_shared<specificHistorizers::outgoing::CChaconKeyword>("command");
-      shared::CDataContainer details;
-      details.set("id", request.getConfiguration().get<int>("id"));
-      details.set("frequency", "433");
-      details.set("protocol", "chacon");
-      details.set("burst", request.getConfiguration().getWithDefault<std::string>("burst", ""));
+      shared::CDataContainerSharedPtr details = new_CDataContainerSharedPtr();
+      details->set("id", request.getConfiguration()->get<int>("id"));
+      details->set("frequency", "433");
+      details->set("protocol", "chacon");
+      details->set("burst", request.getConfiguration()->getWithDefault<std::string>("burst", ""));
       if (api->getDeviceModel(request.getDeviceName()).empty())
          api->updateDeviceModel(request.getDeviceName(), "CHACON");
       api->updateDeviceDetails(request.getDeviceName(), details);
@@ -86,11 +86,11 @@ void CManuallyDeviceFactory::createDeviceManually(boost::shared_ptr<shared::plug
    if (request.getDeviceType() == "kd101")
    {
       auto keyword = boost::make_shared<specificHistorizers::outgoing::CKd101Keyword>("command");
-      shared::CDataContainer details;
-      details.set("id", request.getConfiguration().get<int>("id"));
-      details.set("frequency", "433");
-      details.set("protocol", "kd101");
-      details.set("burst", request.getConfiguration().getWithDefault<std::string>("burst", ""));
+      shared::CDataContainerSharedPtr details = new_CDataContainerSharedPtr();
+      details->set("id", request.getConfiguration()->get<int>("id"));
+      details->set("frequency", "433");
+      details->set("protocol", "kd101");
+      details->set("burst", request.getConfiguration()->getWithDefault<std::string>("burst", ""));
       if (api->getDeviceModel(request.getDeviceName()).empty())
          api->updateDeviceModel(request.getDeviceName(), "KD101");
       api->updateDeviceDetails(request.getDeviceName(), details);
@@ -101,10 +101,10 @@ void CManuallyDeviceFactory::createDeviceManually(boost::shared_ptr<shared::plug
    if (request.getDeviceType() == "parrot")
    {
       auto keyword = boost::make_shared<specificHistorizers::outgoing::CParrotKeyword>("command");
-      shared::CDataContainer details;
-      details.set("id", request.getConfiguration().get<int>("id"));
-      details.set("protocol", "parrot");
-      details.set("burst", request.getConfiguration().getWithDefault<std::string>("burst", ""));
+      shared::CDataContainerSharedPtr details = new_CDataContainerSharedPtr();
+      details->set("id", request.getConfiguration()->get<int>("id"));
+      details->set("protocol", "parrot");
+      details->set("burst", request.getConfiguration()->getWithDefault<std::string>("burst", ""));
       if (api->getDeviceModel(request.getDeviceName()).empty())
          api->updateDeviceModel(request.getDeviceName(), "PARROT");
       api->updateDeviceDetails(request.getDeviceName(), details);
@@ -115,12 +115,12 @@ void CManuallyDeviceFactory::createDeviceManually(boost::shared_ptr<shared::plug
    if (request.getDeviceType() == "visonic")
    {
       auto keyword = boost::make_shared<specificHistorizers::outgoing::CVisonicKeyword>("command");
-      shared::CDataContainer details;
-      details.set("id", request.getConfiguration().get<int>("id"));
-      details.set("frequency", request.getConfiguration().get<int>("frequency"));
-      details.set("protocol", "visonic");
-      details.set("burst", request.getConfiguration().getWithDefault<std::string>("burst", ""));
-      details.set("qualifier", request.getConfiguration().getWithDefault<std::string>("qualifier", ""));
+      shared::CDataContainerSharedPtr details = new_CDataContainerSharedPtr();
+      details->set("id", request.getConfiguration()->get<int>("id"));
+      details->set("frequency", request.getConfiguration()->get<int>("frequency"));
+      details->set("protocol", "visonic");
+      details->set("burst", request.getConfiguration()->getWithDefault<std::string>("burst", ""));
+      details->set("qualifier", request.getConfiguration()->getWithDefault<std::string>("qualifier", ""));
       if (api->getDeviceModel(request.getDeviceName()).empty())
          api->updateDeviceModel(request.getDeviceName(), "VISONIC");
       api->updateDeviceDetails(request.getDeviceName(), details);
@@ -131,16 +131,16 @@ void CManuallyDeviceFactory::createDeviceManually(boost::shared_ptr<shared::plug
    if (request.getDeviceType() == "x2dAlarm433" || request.getDeviceType() == "x2dAlarm866" || request.getDeviceType() == "x2dShutter" || request.getDeviceType() == "x2dElec" || request.getDeviceType() == "x2dGas")
    {
       auto keyword = boost::make_shared<specificHistorizers::outgoing::CX2DKeyword>("command");
-      shared::CDataContainer details;
-      details.set("id", request.getConfiguration().get<int>("id"));
+      shared::CDataContainerSharedPtr details = new_CDataContainerSharedPtr();
+      details->set("id", request.getConfiguration()->get<int>("id"));
 
       if (request.getDeviceType() == "x2dAlarm433")
-         details.set("frequency", 433);
+         details->set("frequency", 433);
       else
-         details.set("frequency", 868);
+         details->set("frequency", 868);
 
-      details.set("protocol", "x2d");
-      details.set("burst", request.getConfiguration().getWithDefault<std::string>("burst", ""));
+      details->set("protocol", "x2d");
+      details->set("burst", request.getConfiguration()->getWithDefault<std::string>("burst", ""));
       if (api->getDeviceModel(request.getDeviceName()).empty())
          api->updateDeviceModel(request.getDeviceName(), "X2D");
       api->updateDeviceDetails(request.getDeviceName(), details);
@@ -150,22 +150,22 @@ void CManuallyDeviceFactory::createDeviceManually(boost::shared_ptr<shared::plug
 
    if (request.getDeviceType() == "rts")
    {
-      request.getConfiguration().printToLog(YADOMS_LOG(information));
+      request.getConfiguration()->printToLog(YADOMS_LOG(information));
       auto keyword = boost::make_shared<specificHistorizers::outgoing::CRtsKeyword>("command");
-      shared::CDataContainer details;
-      if (request.getConfiguration().get<std::string>("identifier.activeSection") == "id32")
+      shared::CDataContainerSharedPtr details = new_CDataContainerSharedPtr();
+      if (request.getConfiguration()->get<std::string>("identifier.activeSection") == "id32")
       {
-         details.set("identifier", request.getConfiguration().get<int>("identifier.content.id32.content.id"));
+         details->set("identifier", request.getConfiguration()->get<int>("identifier.content.id32.content.id"));
       }
-      else if (request.getConfiguration().get<std::string>("identifier.activeSection") == "idx10form")
+      else if (request.getConfiguration()->get<std::string>("identifier.activeSection") == "idx10form")
       {
-         details.set("groupCode", request.getConfiguration().get<std::string>("identifier.content.idx10form.content.groupCode"));
-         details.set("unitCode", request.getConfiguration().get<int>("identifier.content.idx10form.content.unitCode"));
+         details->set("groupCode", request.getConfiguration()->get<std::string>("identifier.content.idx10form.content.groupCode"));
+         details->set("unitCode", request.getConfiguration()->get<int>("identifier.content.idx10form.content.unitCode"));
       }
-      details.set("frequency", 433);
-      details.set("protocol", "rts");
-      details.set("burst", request.getConfiguration().getWithDefault<std::string>("burst", ""));
-      details.set("qualifier", request.getConfiguration().getWithDefault<std::string>("qualifier", ""));
+      details->set("frequency", 433);
+      details->set("protocol", "rts");
+      details->set("burst", request.getConfiguration()->getWithDefault<std::string>("burst", ""));
+      details->set("qualifier", request.getConfiguration()->getWithDefault<std::string>("qualifier", ""));
       if (api->getDeviceModel(request.getDeviceName()).empty())
          api->updateDeviceModel(request.getDeviceName(), "RTS");
       api->updateDeviceDetails(request.getDeviceName(), details);
