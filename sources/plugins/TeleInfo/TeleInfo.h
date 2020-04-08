c86f6d6c1502675bdc0ccc1779ccc8b276b29ebb
jano42 <decoretjm@gmail.com>
2020-04-08 16:17:08 +0200

    Fix teleinfo plugin

--- sources/plugins/TeleInfo/TeleInfo.h
+++ sources/plugins/TeleInfo/TeleInfo.h
@@ -44,7 +44,7 @@ protected:
    /// \param [in] newConfigurationData The new configuration
    //--------------------------------------------------------------
    void onUpdateConfiguration(boost::shared_ptr<yApi::IYPluginApi> api,
-                              const shared::CDataContainer& newConfigurationData);
+                              const shared::CDataContainerSharedPtr& newConfigurationData);
 
    //--------------------------------------------------------------
    /// \brief	                     Destroy the serial connection
