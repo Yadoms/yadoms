9f81fa07a3ae17d6c261caabf261ae38c4cd0b0a
jano42 <decoretjm@gmail.com>
2020-04-08 15:55:12 +0200

    Fix Somfy situo

--- sources/plugins/SomfySituo/SomfySituoConfiguration.cpp
+++ sources/plugins/SomfySituo/SomfySituoConfiguration.cpp
@@ -2,16 +2,21 @@
 #include "SomfySituoConfiguration.h"
 #include <shared/Log.h>
 
+CSomfySituoConfiguration::CSomfySituoConfiguration()
+   :m_data(new_CDataContainerSharedPtr())
+{
+}
+
 CSomfySituoConfiguration::~CSomfySituoConfiguration()
 {
 }
 
-void CSomfySituoConfiguration::initializeWith(const shared::CDataContainer& data)
+void CSomfySituoConfiguration::initializeWith(const shared::CDataContainerSharedPtr& data)
 {
-   m_data.initializeWith(data);
+   m_data->initializeWith(data);
 }
 
 std::string CSomfySituoConfiguration::getSerialPort() const
 {
-   return m_data.get<std::string>("SerialPort");
+   return m_data->get<std::string>("SerialPort");
 }
