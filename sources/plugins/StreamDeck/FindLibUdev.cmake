8447d369b4b42c21a2f24f879babe088c0f26891
Oussama DAHMAZ <oussama.dahmaz@gmail.com>
2020-04-15 16:16:25 +0200

    add FindLibUdev for linux platforms

---
+++ sources/plugins/StreamDeck/FindLibUdev.cmake
@@ -0,0 +1,33 @@
+find_package(PkgConfig QUIET)
+if (PKG_CONFIG_FOUND)
+  pkg_check_modules(LIBUDEV libudev)
+endif ()
+
+find_path(LIBUDEV_INCLUDE_DIR
+  NAMES libudev.h
+  PATHS ${LIBUDEV_INCLUDE_DIRS} ${LIBUDEV_INCLUDEDIR}
+  PATH_SUFFIXES include
+)
+
+find_library(LIBUDEV_LIBRARY
+  NAMES udev
+  PATHS ${LIBUDEV_LIBRARY_DIRS} ${LIBUDEV_LIBDIR}
+  PATH_SUFFIXES lib
+)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(LIBUDEV
+  DEFAULT_MSG
+  LIBUDEV_LIBRARY
+  LIBUDEV_INCLUDE_DIR
+)
+
+if (LIBUDEV_FOUND)
+  list(APPEND LIBUDEV_INCLUDE_DIRS ${LIBUDEV_INCLUDE_DIR})
+  list(APPEND LIBUDEV_LIBRARIES ${LIBUDEV_LIBRARY})
+endif ()
+
+mark_as_advanced(
+  LIBUDEV_INCLUDE_DIR
+  LIBUDEV_LIBRARY
+)
