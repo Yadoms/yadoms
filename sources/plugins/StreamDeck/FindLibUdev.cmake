d1954d38aa02a6c22ebaae1dbd07bced87f70d84
Oussama DAHMAZ <oussama.dahmaz@gmail.com>
2020-04-16 16:10:45 +0200

    update finlibudev.cmake and streamDeck cmake

--- sources/plugins/StreamDeck/FindLibUdev.cmake
+++ sources/plugins/StreamDeck/FindLibUdev.cmake
@@ -1,33 +1,53 @@
-find_package(PkgConfig QUIET)
-if (PKG_CONFIG_FOUND)
-  pkg_check_modules(LIBUDEV libudev)
-endif ()
+# razor-de: Configure libudev environment
+#
+# UDEV_FOUND - system has a libudev
+# UDEV_INCLUDE_DIR - where to find header files
+# UDEV_LIBRARIES - the libraries to link against udev
+# UDEV_STABLE - it's true when is the version greater or equals to 143 - version when the libudev was stabilized in its API
+#
+# copyright (c) 2011 Petr Vanek <petr@scribus.info>
+# Redistribution and use is allowed according to the terms of the BSD license.
+#
 
-find_path(LIBUDEV_INCLUDE_DIR
-  NAMES libudev.h
-  PATHS ${LIBUDEV_INCLUDE_DIRS} ${LIBUDEV_INCLUDEDIR}
-  PATH_SUFFIXES include
+FIND_PATH(
+    UDEV_INCLUDE_DIR
+    libudev.h
+    /usr/include
+    /usr/local/include
+    ${UDEV_PATH_INCLUDES}
 )
 
-find_library(LIBUDEV_LIBRARY
-  NAMES udev
-  PATHS ${LIBUDEV_LIBRARY_DIRS} ${LIBUDEV_LIBDIR}
-  PATH_SUFFIXES lib
+FIND_LIBRARY(
+    UDEV_LIBRARIES
+    NAMES udev libudev
+    PATHS
+        /usr/lib${LIB_SUFFIX}
+        /usr/local/lib${LIB_SUFFIX}
+        ${UDEV_PATH_LIB}
 )
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(LIBUDEV
-  DEFAULT_MSG
-  LIBUDEV_LIBRARY
-  LIBUDEV_INCLUDE_DIR
-)
+IF (UDEV_LIBRARIES AND UDEV_INCLUDE_DIR)
+    SET(UDEV_FOUND "YES")
+    execute_process(COMMAND pkg-config --atleast-version=143 libudev RESULT_VARIABLE UDEV_STABLE)
+    # retvale is 0 of the condition is "true" so we need to negate the value...
+    if (UDEV_STABLE)
+	set(UDEV_STABLE 0)
+    else (UDEV_STABLE)
+	set(UDEV_STABLE 1)
+    endif (UDEV_STABLE)
+    message(STATUS "libudev stable: ${UDEV_STABLE}")
+ENDIF (UDEV_LIBRARIES AND UDEV_INCLUDE_DIR)
 
-if (LIBUDEV_FOUND)
-  list(APPEND LIBUDEV_INCLUDE_DIRS ${LIBUDEV_INCLUDE_DIR})
-  list(APPEND LIBUDEV_LIBRARIES ${LIBUDEV_LIBRARY})
-endif ()
-
-mark_as_advanced(
-  LIBUDEV_INCLUDE_DIR
-  LIBUDEV_LIBRARY
-)
+IF (UDEV_FOUND)
+    MESSAGE(STATUS "Found UDev: ${UDEV_LIBRARIES}")
+    MESSAGE(STATUS "   include: ${UDEV_INCLUDE_DIR}")
+ELSE (UDEV_FOUND)
+    MESSAGE(STATUS "UDev not found.")
+    MESSAGE(STATUS "UDev: You can specify includes: -DUDEV_PATH_INCLUDES=/opt/udev/include")
+    MESSAGE(STATUS "      currently found includes: ${UDEV_INCLUDE_DIR}")
+    MESSAGE(STATUS "UDev: You can specify libs: -DUDEV_PATH_LIB=/opt/udev/lib")
+    MESSAGE(STATUS "      currently found libs: ${UDEV_LIBRARIES}")
+    IF (UDev_FIND_REQUIRED)
+        MESSAGE(FATAL_ERROR "Could not find UDev library")
+    ENDIF (UDev_FIND_REQUIRED)
+ENDIF (UDEV_FOUND)
