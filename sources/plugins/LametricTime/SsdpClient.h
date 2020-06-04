e7e25748ffbbdcc0233870b519545bc937a8bdac
Oussama DAHMAZ <oussama.dahmaz@gmail.com>
2020-06-04 23:22:40 +0200

    #652 Detect all connected devices using upnp

--- sources/plugins/LametricTime/SsdpClient.h
+++ sources/plugins/LametricTime/SsdpClient.h
@@ -8,10 +8,11 @@ public:
 
 	virtual ~CSsdpClient() = default;
 
-	CSsdpClient(boost::asio::io_service& ioService, const std::string& searchTarget);
+	CSsdpClient(boost::asio::io_service& ioService, const std::string& searchTarget, const std::chrono::seconds& timeout);
 
 	std::string getDescriptionUrl() const { return m_descriptionUrl; }
-
+	
+	std::vector<std::string> getDescriptionUrls() const { return m_descriptionUrls; }
 private:
 	const static boost::asio::ip::address MulticastAddress;
 	const static int MulticastPort;
@@ -22,18 +23,24 @@ private:
 	void startAsynchronousSend();
 	static std::string getMulticastSearchRequest(const std::string& searchTarget);
 
+	static void removeDuplicates(std::vector<std::string>& v);
+	static void removeEmptyStrings(std::vector<std::string>& strings);
+	void closeSocketAndTimer();
+	
 	boost::asio::ip::udp::endpoint m_endpoint;
 	boost::asio::ip::udp::socket m_socket;
 	boost::asio::steady_timer m_timer;
-
 	std::string m_descriptionUrl;
 	std::string m_ipAddress;
 	std::string m_deviceName;
 	std::string m_serialNumber;
 	std::string m_multicastSearchRequest;
 	std::string m_xmlTagValue;
-
+	std::chrono::seconds m_timeout;
+	
 	enum { kMaxlength = 1024 };
 
 	char m_data[kMaxlength]{};
+
+	std::vector<std::string> m_descriptionUrls;
 };
--- sources/plugins/LametricTime/SsdpDiscoverService.h
+++ sources/plugins/LametricTime/SsdpDiscoverService.h
@@ -2,16 +2,20 @@
 #include <string>
 #include "SsdpDiscoveredDevice.h"
 
+static std::chrono::duration<long long> SsdpClientTimeout(std::chrono::seconds(45));
+
 class CSsdpDiscoverService
 {
 public:
-	
+
 	explicit CSsdpDiscoverService(std::string& searchTarget);
 
 	virtual ~CSsdpDiscoverService() = default;
-	
-	static bool discover(std::string searchTarget, const boost::function<bool(CSsdpDiscoveredDevice& discoveredDevice)>& discoveredDeviceHandler);
+
+	static bool discover(const boost::function<bool(CSsdpDiscoveredDevice& discoveredDevice)>& discoveredDeviceHandler,
+	                     const std::chrono::duration<long long>& timeout = SsdpClientTimeout,
+	                     const std::string& searchTarget = "ssdp:all");
 
 private:
-	static std::string getDeviceDescription(std::string& descriptionUrl);
+	static std::vector<std::string> getDevicesDescription(std::vector<std::string>& descriptionUrl);
 };
