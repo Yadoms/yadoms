56b5a487980e77876b6c8f04774eecc53a341af2
Oussama DAHMAZ <oussama.dahmaz@gmail.com>
2020-06-07 10:25:33 +0200

    #673 system Profiler skeleton

---
+++ sources/server/hardware/usb/systemProfiler/SystemProfilerCall.cpp
@@ -0,0 +1,89 @@
+#include "stdafx.h"
+#include "SystemProfilerCall.h"
+#include <shared/Log.h>
+#include <shared/process/Process.h>
+#include <shared/process/NativeExecutableCommandLine.h>
+
+namespace hardware
+{
+namespace usb
+{
+class CSystemProfilerProcessLogger final : public shared::process::IExternalProcessLogger
+{
+public:
+   explicit CSystemProfilerProcessLogger() = default;
+   virtual ~CSystemProfilerProcessLogger() = default;
+
+   // IExternalProcessLogger Implementation
+   void init() override {}
+   void information(const std::string &line) override { m_stdoutContent.push_back(line); }
+   void error(const std::string &line) override { m_stderrContent += line + "\n"; }
+   void purgeLogFile() override {}
+   // [END] IExternalProcessLogger Implementation
+
+   const std::string &stderrContent() const { return m_stderrContent; }
+   const std::vector<std::string> &stdoutContent() const { return m_stdoutContent; }
+
+private:
+   std::string m_stderrContent;
+   std::vector<std::string> m_stdoutContent;
+};
+
+class CSystemProfilerProcessObserver final : public shared::process::IProcessObserver
+{
+public:
+   explicit CSystemProfilerProcessObserver() = default;
+   virtual ~CSystemProfilerProcessObserver() = default;
+
+   template <typename _Rep, typename _Period>
+   bool waitFinish(const std::chrono::duration<_Rep, _Period> &timeout)
+   {
+      std::unique_lock<std::mutex> lock(m_mutex);
+      return m_condition.wait_for(lock,
+                                  timeout) == std::cv_status::no_timeout;
+   }
+
+protected:
+   // IProcessObserver Implementation
+   void onStart() override
+   {
+   }
+   void onFinish(int returnCode,
+                 const std::string &error) override
+   {
+      std::unique_lock<std::mutex> lock(m_mutex);
+      m_condition.notify_one();
+   }
+   // [END] IProcessObserver Implementation
+
+private:
+   std::mutex m_mutex;
+   std::condition_variable m_condition;
+};
+
+CSystemProfilerCall::CSystemProfilerCall(const std::vector<std::string> &parameters)
+    : m_parameters(parameters)
+{
+}
+
+std::vector<std::string> CSystemProfilerCall::execute(bool ignoreError)
+{
+   const auto commandLine = boost::make_shared<shared::process::CNativeExecutableCommandLine>("SystemProfiler",
+                                                                                              ".",
+                                                                                              m_parameters);
+   const auto processOut = boost::make_shared<CSystemProfilerProcessLogger>();
+   const auto processObserver = boost::make_shared<CSystemProfilerProcessObserver>();
+   const shared::process::CProcess process(commandLine,
+                                           processObserver,
+                                           processOut);
+
+   if (!processObserver->waitFinish(std::chrono::seconds(20)))
+      throw std::runtime_error("Unable to list USB devices : timeout executing SystemProfiler");
+
+   if (!processOut->stderrContent().empty() && !ignoreError)
+      throw std::runtime_error("Unable to list USB devices : " + processOut->stderrContent());
+
+   return processOut->stdoutContent();
+}
+} // namespace usb
+} // namespace hardware
---
+++ sources/server/hardware/usb/systemProfiler/SystemProfilerDevicesLister.cpp
@@ -0,0 +1,53 @@
+#include "stdafx.h"
+#include "SystemProfilerDevicesLister.h"
+#include "SystemProfilerDevice.h"
+#include "SystemProfilerCall.h"
+#include <shared/Log.h>
+#include <regex>
+
+namespace hardware
+{
+namespace usb
+{
+std::vector<boost::shared_ptr<IDevice>> CSystemProfilerDevicesLister::listUsbDevices()
+{
+   try
+   {
+      CSystemProfilerCall SystemProfilerCall;
+      const auto lines = SystemProfilerCall.execute();
+
+      std::vector<boost::shared_ptr<IDevice>> devicesList;
+      for (const auto &line : lines)
+      {
+         try
+         {
+            std::smatch matches;
+            if (!std::regex_search(line,
+                                   matches,
+                                   std::regex(std::string("^Bus.*: ID ([[:xdigit:]]{4}):([[:xdigit:]]{4}) (.*)$"))) ||
+                matches.size() != 4)
+               continue;
+
+            const auto vid = std::stoi(std::string(matches[1].first, matches[1].second), nullptr, 16);
+            const auto pid = std::stoi(std::string(matches[2].first, matches[2].second), nullptr, 16);
+            const auto name = matches[3];
+
+            devicesList.emplace_back(boost::make_shared<CSystemProfilerDevice>(vid,
+                                                                      pid,
+                                                                      name));
+         }
+         catch (const std::exception &e)
+         {
+            YADOMS_LOG(warning) << "Unable to access device, " << e.what();
+         }
+      }
+      return devicesList;
+   }
+   catch (const std::exception &e)
+   {
+      YADOMS_LOG(warning) << "Unable to list USB devices, " << e.what();
+      return std::vector<boost::shared_ptr<IDevice>>();
+   }
+}
+} // namespace usb
+} // namespace hardware
