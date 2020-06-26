6b3874a7429df3a78c772c103fc4a5a5f92adef8
Oussama DAHMAZ <oussama.dahmaz@gmail.com>
2020-06-26 17:19:57 +0200

    Revert "add http status code enum"
    This reverts commit acbbde03284f0d3964cccb633a47fe1388d29cc3.

--- sources/shared/shared/http/curlppHelpers.h
+++ sources/shared/shared/http/curlppHelpers.h
@@ -1,46 +1,36 @@
 #pragma once
 #include <curlpp/Easy.hpp>
 
+
 namespace shared
 {
-	namespace http
-	{
-		//--------------------------------------------------------------
-		/// \brief	Helpers for cCURLpp use
-		//--------------------------------------------------------------
-		class CCurlppHelpers
-		{
-		public:
+   namespace http
+   {
+      //--------------------------------------------------------------
+      /// \brief	Helpers for cCURLpp use
+      //--------------------------------------------------------------
+      class CCurlppHelpers
+      {
+      public:
+         CCurlppHelpers() = delete;
+         virtual ~CCurlppHelpers() = default;
 
-			CCurlppHelpers() = delete;
-			virtual ~CCurlppHelpers() = default;
+         static void setProxy(curlpp::Easy& request,
+                              const std::string& url,
+                              const std::string& proxyHost,
+                              int proxyPort,
+                              const std::string& proxyUsername,
+                              const std::string& proxyPassword,
+                              const std::string& proxyBypassRegex);
 
-			static void setProxy(curlpp::Easy& request,
-			                     const std::string& url,
-			                     const std::string& proxyHost,
-			                     int proxyPort,
-			                     const std::string& proxyUsername,
-			                     const std::string& proxyPassword,
-			                     const std::string& proxyBypassRegex);
+         static void setHeaders(curlpp::Easy& request,
+                                const std::map<std::basic_string<char>, std::basic_string<char>>& headerParameters);
 
-			static void setHeaders(curlpp::Easy& request,
-			                       const std::map<std::basic_string<char>, std::basic_string<char>>& headerParameters);
+         static std::string stringifyParameters(const std::map<std::string, std::string>& parameters);
 
-			static std::string stringifyParameters(const std::map<std::string, std::string>& parameters);
+         static void checkResult(const curlpp::Easy& request);
 
-			static void checkResult(const curlpp::Easy& request);
-
-			static std::map<std::string, std::string> formatResponseHeaders(const std::string& headersBuffer);
-
-			enum EHttpCodeStatus
-			{
-				kHttpOk = 200,
-				kHttpCreated= 201,
-				kHttpBadRequest=400,
-				kHttpUnauthorized = 401,
-				kHttpForbidden= 403,
-				kHttpNotFound = 404,
-			};
-		};
-	}
+         static std::map<std::string, std::string> formatResponseHeaders(const std::string& headersBuffer);
+      };
+   }
 } // namespace shared::http
