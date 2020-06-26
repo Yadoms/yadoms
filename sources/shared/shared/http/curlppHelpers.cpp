acbbde03284f0d3964cccb633a47fe1388d29cc3
Oussama DAHMAZ <oussama.dahmaz@gmail.com>
2020-06-26 17:00:37 +0200

    add http status code enum

--- sources/shared/shared/http/curlppHelpers.cpp
+++ sources/shared/shared/http/curlppHelpers.cpp
@@ -11,90 +11,91 @@
 
 namespace shared
 {
-   namespace http
-   {
-      void CCurlppHelpers::setProxy(curlpp::Easy& request,
-                                    const std::string& url,
-                                    const std::string& proxyHost,
-                                    int proxyPort,
-                                    const std::string& proxyUsername,
-                                    const std::string& proxyPassword,
-                                    const std::string& proxyBypassRegex)
-      {
-         if (!proxyBypassRegex.empty() && std::regex_search(url, std::regex(proxyBypassRegex)))
-            return;
+	namespace http
+	{
+		void CCurlppHelpers::setProxy(curlpp::Easy& request,
+		                              const std::string& url,
+		                              const std::string& proxyHost,
+		                              int proxyPort,
+		                              const std::string& proxyUsername,
+		                              const std::string& proxyPassword,
+		                              const std::string& proxyBypassRegex)
+		{
+			if (!proxyBypassRegex.empty() && std::regex_search(url, std::regex(proxyBypassRegex)))
+				return;
 
-         request.setOpt(new curlpp::options::Proxy(proxyHost));
-         if (proxyPort != CProxy::kUseProxyDefaultPort)
-            request.setOpt(new curlpp::options::ProxyPort(proxyPort));
-         if (!proxyUsername.empty() && !proxyPassword.empty())
-            request.setOpt(new curlpp::options::ProxyUserPwd(proxyUsername + ":" + proxyPassword));
-      }
+			request.setOpt(new curlpp::options::Proxy(proxyHost));
+			if (proxyPort != CProxy::kUseProxyDefaultPort)
+				request.setOpt(new curlpp::options::ProxyPort(proxyPort));
+			if (!proxyUsername.empty() && !proxyPassword.empty())
+				request.setOpt(new curlpp::options::ProxyUserPwd(proxyUsername + ":" + proxyPassword));
+		}
 
-      void CCurlppHelpers::setHeaders(curlpp::Easy& request,
-                                      const std::map<std::basic_string<char>, std::basic_string<char>>&
-                                      headerParameters)
-      {
-         if (headerParameters.empty())
-            return;
+		void CCurlppHelpers::setHeaders(curlpp::Easy& request,
+		                                const std::map<std::basic_string<char>, std::basic_string<char>>&
+		                                headerParameters)
+		{
+			if (headerParameters.empty())
+				return;
 
-         std::list<std::string> headers;
-         for (const auto& headerParametersIterator : headerParameters)
-            headers.push_back(headerParametersIterator.first + ": " + headerParametersIterator.second);
+			std::list<std::string> headers;
+			for (const auto& headerParametersIterator : headerParameters)
+				headers.push_back(headerParametersIterator.first + ": " + headerParametersIterator.second);
 
-         request.setOpt(new curlpp::options::HttpHeader(headers));
-      }
+			request.setOpt(new curlpp::options::HttpHeader(headers));
+		}
 
-      std::string CCurlppHelpers::stringifyParameters(const std::map<std::string, std::string>& parameters)
-      {
-         if (parameters.empty())
-            return std::string();
+		std::string CCurlppHelpers::stringifyParameters(const std::map<std::string, std::string>& parameters)
+		{
+			if (parameters.empty())
+				return std::string();
 
-         std::string urlSuffix;
-         for (const auto& parameter : parameters)
-         {
-            urlSuffix += urlSuffix.empty() ? "?" : "&";
-            urlSuffix += curlpp::escape(parameter.first) + "=" + curlpp::escape(parameter.second);
-         }
-         return urlSuffix;
-      }
+			std::string urlSuffix;
+			for (const auto& parameter : parameters)
+			{
+				urlSuffix += urlSuffix.empty() ? "?" : "&";
+				urlSuffix += curlpp::escape(parameter.first) + "=" + curlpp::escape(parameter.second);
+			}
+			return urlSuffix;
+		}
 
-      void CCurlppHelpers::checkResult(const curlpp::Easy& request)
-      {
-         if (curlpp::infos::ResponseCode::get(request) != 200 && curlpp::infos::ResponseCode::get(request) != 201)
-         {
-            const auto message = (boost::format("Invalid HTTP result : %1%") % curlpp::infos::ResponseCode::get(request)
-            ).str();
-            YADOMS_LOG(warning) << message;
-            throw exception::CHttpException(message);
-         }
-      }
+		void CCurlppHelpers::checkResult(const curlpp::Easy& request)
+		{
+			if (curlpp::infos::ResponseCode::get(request) != kHttpOk && curlpp::infos::
+				ResponseCode::get(request) != kHttpCreated)
+			{
+				const auto message = (boost::format("Invalid HTTP result : %1%") % curlpp::infos::ResponseCode::get(request)
+				).str();
+				YADOMS_LOG(warning) << message;
+				throw exception::CHttpException(message);
+			}
+		}
 
-      std::map<std::string, std::string> CCurlppHelpers::formatResponseHeaders(const std::string& headersBuffer)
-      {
-         std::vector<std::string> headerKeyValues;
-         split(headerKeyValues, headersBuffer, boost::is_any_of("\n"), boost::algorithm::token_compress_on);
+		std::map<std::string, std::string> CCurlppHelpers::formatResponseHeaders(const std::string& headersBuffer)
+		{
+			std::vector<std::string> headerKeyValues;
+			split(headerKeyValues, headersBuffer, boost::is_any_of("\n"), boost::algorithm::token_compress_on);
 
-         std::map<std::string, std::string> responseHeaders;
-         for (const auto& headerKeyValue : headerKeyValues)
-         {
-            const auto separatorIterator = headerKeyValue.find(':');
-            if (separatorIterator == std::string::npos)
-               continue;
+			std::map<std::string, std::string> responseHeaders;
+			for (const auto& headerKeyValue : headerKeyValues)
+			{
+				const auto separatorIterator = headerKeyValue.find(':');
+				if (separatorIterator == std::string::npos)
+					continue;
 
-            // Http headers are not case-sensitive
-            // Make all lower to facilitate search and comparison
+				// Http headers are not case-sensitive
+				// Make all lower to facilitate search and comparison
 
-            auto key = headerKeyValue.substr(0, separatorIterator);
-            boost::to_lower(key);
+				auto key = headerKeyValue.substr(0, separatorIterator);
+				boost::to_lower(key);
 
-            auto value = headerKeyValue.substr(separatorIterator + 1, std::string::npos);
-            boost::to_lower(value);
+				auto value = headerKeyValue.substr(separatorIterator + 1, std::string::npos);
+				boost::to_lower(value);
 
-            responseHeaders[key] = value;
-         }
+				responseHeaders[key] = value;
+			}
 
-         return responseHeaders;
-      }
-   }
+			return responseHeaders;
+		}
+	}
 } // namespace shared::http
--- sources/shared/shared/http/curlppHelpers.h
+++ sources/shared/shared/http/curlppHelpers.h
@@ -1,36 +1,46 @@
 #pragma once
 #include <curlpp/Easy.hpp>
 
-
 namespace shared
 {
-   namespace http
-   {
-      //--------------------------------------------------------------
-      /// \brief	Helpers for cCURLpp use
-      //--------------------------------------------------------------
-      class CCurlppHelpers
-      {
-      public:
-         CCurlppHelpers() = delete;
-         virtual ~CCurlppHelpers() = default;
+	namespace http
+	{
+		//--------------------------------------------------------------
+		/// \brief	Helpers for cCURLpp use
+		//--------------------------------------------------------------
+		class CCurlppHelpers
+		{
+		public:
 
-         static void setProxy(curlpp::Easy& request,
-                              const std::string& url,
-                              const std::string& proxyHost,
-                              int proxyPort,
-                              const std::string& proxyUsername,
-                              const std::string& proxyPassword,
-                              const std::string& proxyBypassRegex);
+			CCurlppHelpers() = delete;
+			virtual ~CCurlppHelpers() = default;
 
-         static void setHeaders(curlpp::Easy& request,
-                                const std::map<std::basic_string<char>, std::basic_string<char>>& headerParameters);
+			static void setProxy(curlpp::Easy& request,
+			                     const std::string& url,
+			                     const std::string& proxyHost,
+			                     int proxyPort,
+			                     const std::string& proxyUsername,
+			                     const std::string& proxyPassword,
+			                     const std::string& proxyBypassRegex);
 
-         static std::string stringifyParameters(const std::map<std::string, std::string>& parameters);
+			static void setHeaders(curlpp::Easy& request,
+			                       const std::map<std::basic_string<char>, std::basic_string<char>>& headerParameters);
 
-         static void checkResult(const curlpp::Easy& request);
+			static std::string stringifyParameters(const std::map<std::string, std::string>& parameters);
 
-         static std::map<std::string, std::string> formatResponseHeaders(const std::string& headersBuffer);
-      };
-   }
+			static void checkResult(const curlpp::Easy& request);
+
+			static std::map<std::string, std::string> formatResponseHeaders(const std::string& headersBuffer);
+
+			enum EHttpCodeStatus
+			{
+				kHttpOk = 200,
+				kHttpCreated= 201,
+				kHttpBadRequest=400,
+				kHttpUnauthorized = 401,
+				kHttpForbidden= 403,
+				kHttpNotFound = 404,
+			};
+		};
+	}
 } // namespace shared::http
