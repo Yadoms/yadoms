5f19d67a483afc1349503fa8d23f0cef3d9e9e96
Oussama DAHMAZ <oussama.dahmaz@gmail.com>
2020-07-10 17:01:06 +0200

    philips hue first import

---
+++ sources/plugins/PhilipsHue/Configuration.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <shared/DataContainer.h>
+#include <shared/plugin/yPluginApi/YPluginConfiguration.hpp>
+
+// Shortcut to yPluginApi namespace
+namespace yApi = shared::plugin::yPluginApi;
+
+
+//--------------------------------------------------------------
+/// \brief	An example of an enum type of parameter
+//--------------------------------------------------------------
+enum EEnumType
+{
+   kEnumValue1 = 7,
+   kEnumValue2 = 12,
+   kEnumValue3
+};
+
+//--------------------------------------------------------------
+/// \brief	Configuration of the empty plugin
+//--------------------------------------------------------------
+class Configuration
+{
+public:
+   //--------------------------------------------------------------
+   /// \brief	    Destructor
+   //--------------------------------------------------------------
+   virtual ~Configuration();
+
+   //--------------------------------------------------------------
+   /// \brief		   Load configuration data
+   /// \param [in] data The data container
+   //--------------------------------------------------------------
+   void initializeWith(const boost::shared_ptr<shared::CDataContainer> & data);
+
+   //--------------------------------------------------------------
+   /// \brief	    Just for test, not needed for real plugin
+   //--------------------------------------------------------------
+   void trace() const;
+
+   //--------------------------------------------------------------
+   //--------------------------------------------------------------
+   // You can add your own accessors for better code readability
+   //--------------------------------------------------------------
+   //--------------------------------------------------------------
+
+   //--------------------------------------------------------------
+   /// \brief	    Enum parameter
+   //--------------------------------------------------------------
+   EEnumType getEnumParameter() const;
+
+private:
+   //--------------------------------------------------------------
+   /// \brief	    Configuration getter
+   //--------------------------------------------------------------
+   yApi::YPluginConfiguration m_configuration;
+};
+
---
+++ sources/plugins/PhilipsHue/PhilipsHue.cpp
@@ -0,0 +1,127 @@
+#include "stdafx.h"
+#include "PhilipsHue.h"
+#include <plugin_cpp_api/ImplementationHelper.h>
+#include <shared/Log.h>
+
+/* ----------------------------------
+
+Insert here all include files
+
+   ---------------------------------- */
+
+
+// Use this macro to define all necessary to make your plugin a Yadoms valid plugin.
+// Note that you have to provide some extra files, like package.json, and icon.png
+IMPLEMENT_PLUGIN(CPhilipsHue)
+
+
+CPhilipsHue::CPhilipsHue()
+{
+}
+
+CPhilipsHue::~CPhilipsHue()
+{
+}
+
+// Event IDs
+enum
+{
+   // Example of adding a custom event
+   kCustomEvent = yApi::IYPluginApi::kPluginFirstEventId, // Always start from yApi::IYPluginApi::kPluginFirstEventId
+
+   /* ----------------------------------
+
+   Insert here all your events
+
+   ---------------------------------- */
+};
+
+void CPhilipsHue::doWork(boost::shared_ptr<yApi::IYPluginApi> api)
+{
+   // Informs Yadoms about the plugin actual state
+   api->setPluginState(yApi::historization::EPluginState::kCustom, "connecting");
+
+   YADOMS_LOG(information) << "CPhilipsHue is starting...";
+
+   // Load configuration values (provided by database)
+   m_configuration.initializeWith(api->getConfiguration());
+
+   /* -----------------------------
+   
+      Create & declare here all your devices and keywords.
+      Create timers.
+
+      ----------------------------- */
+
+   api->setPluginState(yApi::historization::EPluginState::kRunning);
+
+   // the main loop
+   while (true)
+   {
+      // Wait for an event
+      switch (api->getEventHandler().waitForEvents())
+      {
+      case yApi::IYPluginApi::kEventStopRequested:
+         {
+            // Yadoms request the plugin to stop. Note that plugin must be stopped in 10 seconds max, otherwise it will be killed.
+            YADOMS_LOG(information) << "Stop requested";
+            api->setPluginState(yApi::historization::EPluginState::kStopped);
+            return;
+         }
+
+      case yApi::IYPluginApi::kEventUpdateConfiguration:
+         {
+            // Configuration was updated
+            api->setPluginState(yApi::historization::EPluginState::kCustom, "updateConfiguration");
+            const auto newConfiguration = api->getEventHandler().getEventData<boost::shared_ptr<shared::CDataContainer>>();
+            YADOMS_LOG(information) << "Update configuration...";
+
+            // Take into account the new configuration
+            // - Restart the plugin if necessary,
+            // - Update some resources,
+            // - etc...
+            m_configuration.initializeWith(newConfiguration);
+
+            // Trace the configuration
+            m_configuration.trace();
+
+            api->setPluginState(yApi::historization::EPluginState::kRunning);
+
+            break;
+         }
+
+      case yApi::IYPluginApi::kEventDeviceCommand:
+         {
+            // A command was received from Yadoms
+            const auto command = api->getEventHandler().getEventData<boost::shared_ptr<const yApi::IDeviceCommand>>();
+            YADOMS_LOG(information) << "Command received from Yadoms : " << yApi::IDeviceCommand::toString(command);
+
+            /*
+
+            Process the command here (to drive a keyword for example)
+
+            */
+
+            break;
+         }
+
+      case kCustomEvent:
+         {
+            /*
+
+            Process your custom event
+
+            */
+
+            break;
+         }
+
+      default:
+         {
+            YADOMS_LOG(error) << "Unknown or unsupported message id " << api->getEventHandler().getEventId();
+            break;
+         }
+      }
+   }
+}
+
