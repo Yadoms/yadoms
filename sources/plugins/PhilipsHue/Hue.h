a828459057beb62a74c6e68e548c33a503e6165d
Oussama DAHMAZ <oussama.dahmaz@gmail.com>
2020-07-23 12:34:39 +0200

    add hue light struct

--- sources/plugins/PhilipsHue/Hue.h
+++ sources/plugins/PhilipsHue/Hue.h
@@ -8,3 +8,84 @@ struct HueInformations
    std::string modelName;
    std::string modelNumber;
 };
+
+struct HueLightInformations
+{
+   struct STATE
+   {
+      bool on;
+      int bri;
+      int hue;
+      int sat;
+      std::string effect;
+      struct XY
+      {
+         int x;
+         int y;
+      };
+      XY xy;
+
+      int ct;
+      std::string alert;
+      std::string colormode;
+      std::string mode;
+      bool reachable;
+   };
+   STATE state;
+
+   struct SWUPDATE
+   {
+      std::string state;
+      std::string lastinstall;
+   };
+   SWUPDATE swupdate;
+
+   std::string type;
+   std::string name;
+   std::string modelid;
+   std::string manufacturername;
+   std::string productname;
+
+   struct CAPABILITIES
+   {
+      bool certified;
+      struct CONTROL
+      {
+         int mindimlevel;
+         int maxlumen;
+         std::string colorgamuttype;
+         struct COLORGAMUT
+         {
+            
+         };
+         COLORGAMUT colorgamut;
+
+         struct CT
+         {
+            int min;
+            int max;
+         };
+         CT ct;
+      };
+      CONTROL control;
+      struct STREAMING
+      {
+         bool renderer;
+         bool proxy;
+      };
+      STREAMING streaming;
+
+   };
+   CAPABILITIES capabilities;
+
+   struct CONFIG
+   {
+      std::string archetype;
+      std::string function;
+      std::string direction;
+   };
+   CONFIG config;
+
+   std::string uniqueid;
+   std::string swversion;
+};
