9bc2af7b05083f723fbdad4077ac1e947a7adec7
Oussama DAHMAZ <oussama.dahmaz@gmail.com>
2020-08-28 11:06:15 +0200

    #674 first import of default icons management

---
+++ sources/plugins/StreamDeck/DefaultIconSelector.cpp
@@ -0,0 +1,10 @@
+﻿#include "DefaultIconSelector.h"
+
+EIconSelectionMode CDefaultIconSelector::getIconSelectionMode(
+   boost::shared_ptr<shared::plugin::yPluginApi::IExtraQuery>& extraQuery)
+{
+   return extraQuery->getData()->data()->get<std::string>(
+             "iconSelectionMode.activeSectionText") == "Custom"
+             ? kCustom
+             : kDefault;
+}
