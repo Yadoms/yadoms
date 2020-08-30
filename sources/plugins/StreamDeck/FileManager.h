955243377950063bde3540a3aab7d997bfd03d29
Oussama DAHMAZ <oussama.dahmaz@gmail.com>
2020-08-30 20:47:48 +0200

    get data img from file

--- sources/plugins/StreamDeck/FileManager.h
+++ sources/plugins/StreamDeck/FileManager.h
@@ -3,5 +3,14 @@
 class CFileManager
 {
 public:
-   
+   CFileManager(std::string& path);
+   virtual ~CFileManager() = default;
+
+   void read();
+   void close();
+   std::string getData() const;
+private:
+   std::string m_path;
+   std::ifstream m_ifs;
+   std::string m_fileDataAsString;
 };
