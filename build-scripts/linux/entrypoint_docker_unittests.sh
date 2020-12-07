79167ce87413ba4c3bbf209d22c36351de50b436
jano42 <decoretjm@gmail.com>
2020-12-07 11:22:20 +0100

    Try UnitTest under Linux

---
+++ build-scripts/linux/entrypoint_docker_unittests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+cd /work/tests/unit
+
+echo "Copy build config file"
+cp $YADOMS_DEPS_PATH/CMakeListsUserConfig.txt sources/
+
+echo "Display config content"
+cat sources/CMakeListsUserConfig.txt
+
+echo "Create makefile"
+sh cmake_linux.sh m
+
+echo "Build Yadoms"
+cd projects
+make all_unity
+cd -
+
+cd builds
+./yadomsTests
