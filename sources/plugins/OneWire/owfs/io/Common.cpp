bb87e2f903884c283fbb3fd43d90b51e2ba4f432
Sébastien Gallou <sebastien.gallou@gmail.com>
2021-02-18 13:03:34 +0100

    Add log

--- sources/plugins/OneWire/owfs/io/Common.cpp
+++ sources/plugins/OneWire/owfs/io/Common.cpp
@@ -2,6 +2,7 @@
 #include "Common.h"
 #include <OneWireException.hpp>
 #include <fstream>
+#include <shared/Log.h>
 
 namespace owfs
 {
@@ -9,12 +10,14 @@ namespace owfs
    {
       std::string CCommon::read(const boost::filesystem::path& valuePath)
       {
+         YADOMS_LOG(debug) << "Read " << valuePath << "...";
          std::ifstream file(valuePath.string());
          if (!file.good())
             throw COneWireException("1-wire read : Error opening " + valuePath.string());
 
          std::string sLine;
          getline(file, sLine);
+         YADOMS_LOG(debug) << "  ==> " << sLine;
          return sLine;
       }
 
