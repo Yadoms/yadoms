ef6d85e078d2046bdfbdd2e11cb75a66fe3e4df9
Sébastien Gallou <sebastien.gallou@gmail.com>
2021-04-21 23:57:02 +0200

    Fix unit tests build under Linux

--- tests/unit/sources/shared/plugin/yPluginApi/historization/TestDateTime.cpp
+++ tests/unit/sources/shared/plugin/yPluginApi/historization/TestDateTime.cpp
@@ -13,17 +13,17 @@ BOOST_AUTO_TEST_SUITE(TestDateTime)
    //--------------------------------------------------------------
    BOOST_AUTO_TEST_CASE(FormatValue)
    {
-      shared::plugin::yPluginApi::historization::CDateTime iso("name");
-      iso.set(boost::posix_time::from_time_t(1596632400));
-      BOOST_CHECK_EQUAL(iso.formatValue(), "20200805T130000");
-      iso.setFromUnixTime(1596632400);
-      BOOST_CHECK_EQUAL(iso.formatValue(), "20200805T130000");
+      shared::plugin::yPluginApi::historization::CDateTime isoDate("name");
+      isoDate.set(boost::posix_time::from_time_t(1596632400));
+      BOOST_CHECK_EQUAL(isoDate.formatValue(), "20200805T130000");
+      isoDate.setFromUnixTime(1596632400);
+      BOOST_CHECK_EQUAL(isoDate.formatValue(), "20200805T130000");
 
-      shared::plugin::yPluginApi::historization::CDateTime unix("name");
-      unix.set(boost::posix_time::time_from_string("2020-08-05 13:00:00"));
-      BOOST_CHECK_EQUAL(unix.formatValue(), "20200805T130000");
-      unix.setFromIso("2020-08-05 13:00:00");
-      BOOST_CHECK_EQUAL(unix.formatValue(), "20200805T130000");
+      shared::plugin::yPluginApi::historization::CDateTime unixDate("name");
+      unixDate.set(boost::posix_time::time_from_string("2020-08-05 13:00:00"));
+      BOOST_CHECK_EQUAL(unixDate.formatValue(), "20200805T130000");
+      unixDate.setFromIso("2020-08-05 13:00:00");
+      BOOST_CHECK_EQUAL(unixDate.formatValue(), "20200805T130000");
    }
 
 BOOST_AUTO_TEST_SUITE_END()
