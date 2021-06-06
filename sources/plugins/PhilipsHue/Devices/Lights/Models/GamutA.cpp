853d21b4aecd7459bcb83dc6a2ab277afa54dc3c
Oussama DAHMAZ <oussama.dahmaz@gmail.com>
2021-06-06 12:29:26 +0200

    #713 add Gamut and none Gamut Light types

---
+++ sources/plugins/PhilipsHue/Devices/Lights/Models/GamutA.cpp
@@ -0,0 +1,96 @@
+﻿#include "GamutA.h"
+#include "../../Utils/ColorConverter.h"
+#include "shared/http/HttpRestHelpers.h"
+#include "shared/Log.h"
+
+const std::string CGamutA::LightState("STATE");
+const std::string CGamutA::RgbColor("RgbColor");
+
+CGamutA::CGamutA(boost::shared_ptr<CUrlManager>& urlManager, std::pair<int, CHueLightInformations>& lightInformations)
+   : m_urlManager(urlManager),
+   m_switch(boost::make_shared<yApi::historization::CSwitch>(LightState)),
+   m_rgb(boost::make_shared<yApi::historization::CColorRGB>(
+      RgbColor, shared::plugin::yPluginApi::EKeywordAccessMode::kGetSet)),
+   m_historizables({ m_switch, m_rgb }),
+   m_lightInformations(lightInformations)
+{
+}
+
+void CGamutA::lightOn()
+{
+   const auto urlPatternPath = m_urlManager->getUrlPatternPath(CUrlManager::kLightState, m_lightInformations.first);
+   const auto lightUrl = m_urlManager->getPatternUrl(urlPatternPath);
+
+   shared::CDataContainer body;
+   body.set("on", true);
+   setLightState(lightUrl, body);
+}
+
+
+void CGamutA::lightOff()
+{
+   const auto urlPatternPath = m_urlManager->getUrlPatternPath(CUrlManager::kLightState, m_lightInformations.first);
+   const auto lightUrl = m_urlManager->getPatternUrl(urlPatternPath);
+
+   shared::CDataContainer body;
+   body.set("on", false);
+   setLightState(lightUrl, body);
+}
+
+void CGamutA::setLightColorUsingXy(const std::string& hexRgb)
+{
+   const auto urlPatternPath = m_urlManager->getUrlPatternPath(CUrlManager::kLightState, m_lightInformations.first);
+   const auto lightUrl = m_urlManager->getPatternUrl(urlPatternPath);
+
+   auto rgb = CColorConverter::hexToRgb(hexRgb);
+   const auto xy = CColorConverter::rgbToXy(rgb);
+
+   shared::CDataContainer body;
+   body.set("on", true);
+   body.set("xy.0", xy.getX());
+   body.set("xy.1", xy.getY());
+
+   setLightState(lightUrl, body);
+}
+
+std::vector<boost::shared_ptr<const shared::plugin::yPluginApi::historization::IHistorizable>> CGamutA::
+getHistorizables()
+{
+   return m_historizables;
+}
+
+void CGamutA::setLightState(const std::string& lightUrl, shared::CDataContainer& body)
+{
+   try
+   {
+      const auto response = shared::http::CHttpRestHelpers::sendJsonPutRequest(lightUrl, body.serialize());
+   }
+   catch (std::exception& e)
+   {
+      const auto message = (boost::format("Fail to send Get http request or interpret answer \"%1%\" : %2%") % lightUrl
+         %
+         e.what()).str();
+      YADOMS_LOG(error) << "Fail to send Get http request or interpret answer " << lightUrl << " : " << e.what();
+      throw;
+   }
+}
+
+std::string CGamutA::getName()
+{
+   return m_lightInformations.second.getName();
+}
+
+std::string CGamutA::getType()
+{
+   return m_lightInformations.second.getType();
+}
+
+std::string CGamutA::getModelId()
+{
+   return m_lightInformations.second.getModelId();
+}
+
+int CGamutA::getDeviceId()
+{
+   return m_lightInformations.first;
+}
---
+++ sources/plugins/PhilipsHue/Devices/Lights/Models/GamutB.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include "../../../UrlManager.h"
+#include "../../Interfaces/ILight.h"
+#include <plugin_cpp_api/IPlugin.h>
+
+class CGamutB : public ILight
+{
+public:
+   CGamutB(boost::shared_ptr<CUrlManager>& urlManager, std::pair<int, CHueLightInformations>& lightInformations);
+   virtual ~CGamutB() = default;
+
+   void lightOn() override;
+
+   void lightOff() override;
+
+   void setLightColorUsingXy(const std::string& hexRgb) override;
+
+   std::vector<boost::shared_ptr<const shared::plugin::yPluginApi::historization::IHistorizable>> getHistorizables()
+      override;
+
+   std::string getName() override;
+
+   std::string getType() override;
+
+   std::string getModelId() override;
+
+   int getDeviceId() override;
+
+private:
+   static void setLightState(const std::string& lightUrl, shared::CDataContainer& body);
+   boost::shared_ptr<CUrlManager> m_urlManager;
+   static const std::string LightState;
+   static const std::string RgbColor;
+   boost::shared_ptr<yApi::historization::CSwitch> m_switch;
+   boost::shared_ptr<yApi::historization::CColorRGB> m_rgb;
+   std::vector<boost::shared_ptr<const shared::plugin::yPluginApi::historization::IHistorizable>> m_historizables;
+   std::pair<int, CHueLightInformations> m_lightInformations;
+};
