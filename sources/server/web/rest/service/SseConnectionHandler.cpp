e502de7fa27ad3a6c9f9a12a012a018c74e369a4
Sébastien Gallou <sebastien.gallou@gmail.com>
2023-10-06 19:27:38 +0200

    Send acquisitions in SSE mode

--- sources/server/web/rest/service/SseConnectionHandler.cpp
+++ sources/server/web/rest/service/SseConnectionHandler.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.h"
 #include "SseConnectionHandler.h"
 
+#include "notification/acquisition/Notification.hpp"
+
 using namespace web::oatppServer;
 
 
@@ -19,7 +21,11 @@ CSseConnectionHandler::~CSseConnectionHandler()
 std::string CSseConnectionHandler::handle() const
 {
    if (m_streamingEventHandler->waitForEvents() == m_streamingOnNewEventId)
-      return "event: hello !\n"
-         "data: <some data>\n\n";
+   {
+      const auto newAcquisition = m_streamingEventHandler->getEventData<boost::shared_ptr<notification::acquisition::CNotification>>()->
+                                                           getAcquisition();
+      return "event: new acquisition received\n"
+         "data: keyword=" + std::to_string(newAcquisition->KeywordId()) + ", value=" + newAcquisition->Value() + "\n\n";
+   }
    return {};
 }
