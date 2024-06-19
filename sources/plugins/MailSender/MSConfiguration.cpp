133077dd917c0320739289848216360e46b3465d
Sébastien Gallou <sebastien.gallou@gmail.com>
2024-06-20 00:14:06 +0200

    [Mailsender plugin] Manage new radioSection format

--- sources/plugins/MailSender/MSConfiguration.cpp
+++ sources/plugins/MailSender/MSConfiguration.cpp
@@ -19,44 +19,46 @@ void CMSConfiguration::initializeWith(const boost::shared_ptr<shared::CDataConta
       m_senderMail = m_data.get<std::string>("Sender");
       m_mailSubject = m_data.getWithDefault<std::string>("mailSubject", "");
 
-      if (m_data.exists("account.content.gmail.radio") && m_data.get<bool>("account.content.gmail.radio"))
+      switch (getSelectedProvider(m_data.getChild("account")))
       {
+      case kGmail:
          m_serverName = "smtp.gmail.com";
          m_serverPort = 587;
          m_kSecurityMode = ESecurityMode::kTLS;
          m_bRequireAuthentication = true;
          m_userAccount = m_senderMail;
          m_password = shared::encryption::CXor::decryptBase64(m_data.get<std::string>("account.content.gmail.content.password"));
-      }
-      else if (m_data.exists("account.content.outlook.radio") && m_data.get<bool>("account.content.outlook.radio"))
-      {
+         break;
+
+      case kOutlook:
          m_serverName = "smtp-mail.outlook.com";
          m_serverPort = 587;
          m_kSecurityMode = ESecurityMode::kTLS;
          m_bRequireAuthentication = true;
          m_userAccount = m_senderMail;
          m_password = shared::encryption::CXor::decryptBase64(m_data.get<std::string>("account.content.outlook.content.password"));
-      }
-      else if (m_data.exists("account.content.ovh.radio") && m_data.get<bool>("account.content.ovh.radio"))
-      {
+         break;
+
+      case kOvh:
          m_serverName = "ns0.ovh.net";
          m_serverPort = 587;
          m_kSecurityMode = ESecurityMode::kNone;
          m_bRequireAuthentication = true;
          m_userAccount = m_senderMail;
          m_password = shared::encryption::CXor::decryptBase64(m_data.get<std::string>("account.content.ovh.content.password"));
-      }
-      else if (m_data.exists("account.content.yahoo.radio") && m_data.get<bool>("account.content.yahoo.radio"))
-      {
+         break;
+
+      case kYahoo:
          m_serverName = "smtp.mail.yahoo.com";
          m_serverPort = 587;
          m_kSecurityMode = ESecurityMode::kTLS;
          m_bRequireAuthentication = true;
          m_userAccount = m_senderMail;
          m_password = shared::encryption::CXor::decryptBase64(m_data.get<std::string>("account.content.yahoo.content.password"));
-      }
-      else
-      {
+         break;
+
+      case kOther:
+      default:  // NOLINT(clang-diagnostic-covered-switch-default)
          m_serverName = m_data.get<std::string>("account.content.other.content.SMTPServer");
          m_serverPort = m_data.get<Poco::UInt16>("account.content.other.content.SMTPPort");
          m_kSecurityMode = m_data.get<ESecurityMode>("account.content.other.content.Security");
@@ -64,6 +66,7 @@ void CMSConfiguration::initializeWith(const boost::shared_ptr<shared::CDataConta
          m_userAccount = m_data.get<std::string>("account.content.other.content.authentication.content.user");
          m_password = shared::encryption::CXor::decryptBase64(
             m_data.get<std::string>("account.content.other.content.authentication.content.password"));
+         break;
       }
    }
    catch (shared::exception::CInvalidParameter& ip)
@@ -114,4 +117,33 @@ bool CMSConfiguration::getAuthenticationRequired() const
 std::string CMSConfiguration::getMailSubject() const
 {
    return m_mailSubject;
+}
+
+CMSConfiguration::Provider CMSConfiguration::getSelectedProvider(const boost::shared_ptr<shared::CDataContainer>& accountNode)
+{
+   // New radio section format
+   if (accountNode->exists("activeSection"))
+   {
+      const std::string activeSection = accountNode->get<std::string>("activeSection");
+      if (activeSection == "gmail")
+         return kGmail;
+      if (activeSection == "outlook")
+         return kOutlook;
+      if (activeSection == "ovh")
+         return kOvh;
+      if (activeSection == "yahoo")
+         return kYahoo;
+      return kOther;
+   }
+
+   // Obsolete method (Old radio section format)
+   if (accountNode->exists("content.gmail.radio") && accountNode->get<bool>("content.gmail.radio"))
+      return kGmail;
+   if (accountNode->exists("content.outlook.radio") && accountNode->get<bool>("content.outlook.radio"))
+      return kOutlook;
+   if (accountNode->exists("content.ovh.radio") && accountNode->get<bool>("content.ovh.radio"))
+      return kOvh;
+   if (accountNode->exists("content.yahoo.radio") && accountNode->get<bool>("content.yahoo.radio"))
+      return kYahoo;
+   return kOther;
 }
