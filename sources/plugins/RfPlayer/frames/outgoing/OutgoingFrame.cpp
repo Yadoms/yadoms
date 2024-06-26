a9acbf57384af77215026cefbdd823dfbcf0a2e5
Sébastien Gallou <sebastien.gallou@gmail.com>
2024-06-26 22:14:20 +0200

    [RfPlayer plugin] Manage new radioSection format

--- sources/plugins/RfPlayer/frames/outgoing/OutgoingFrame.cpp
+++ sources/plugins/RfPlayer/frames/outgoing/OutgoingFrame.cpp
@@ -2,19 +2,21 @@
 #include "OutgoingFrame.h"
 #include "AsciiCommand.h"
 
-namespace frames {
-namespace outgoing {
-   
-      COutgoingFrame::COutgoingFrame(const std::string & protocol)
-         :m_protocol(protocol)
+namespace frames
+{
+   namespace outgoing
+   {
+      COutgoingFrame::COutgoingFrame(const std::string& protocol)
+         : m_protocol(protocol)
       {
       }
 
       COutgoingFrame::~COutgoingFrame()
       {
       }
-      
-      const std::string COutgoingFrame::generateAsciiCommand(boost::shared_ptr<shared::plugin::yPluginApi::IYPluginApi> api, boost::shared_ptr<const shared::plugin::yPluginApi::IDeviceCommand> command)
+
+      const std::string COutgoingFrame::generateAsciiCommand(boost::shared_ptr<shared::plugin::yPluginApi::IYPluginApi> api,
+                                                             boost::shared_ptr<const shared::plugin::yPluginApi::IDeviceCommand> command)
       {
          boost::shared_ptr<shared::CDataContainer> details = api->getDeviceDetails(command->getDevice());
          boost::shared_ptr<shared::CDataContainer> config = api->getDeviceConfiguration(command->getDevice());
@@ -32,14 +34,16 @@ namespace outgoing {
             //a simple ID or x10 form is supported is provided
             if (details->containsValue("identifier"))
             {
-               if (details->exists("identifier.content.id32.radio") && details->get<bool>("identifier.content.id32.radio"))
+               if ((details->exists("identifier.activeSection") && details->get<std::string>("identifier.activeSection") == "id32")
+                  || (details->exists("identifier.content.id32.radio") && details->get<bool>("identifier.content.id32.radio")))
                {
                   std::string id = details->get<std::string>("identifier.content.id32.content.id");
                   return CAsciiCommand::generateCommandById(m_protocol, cmd, id);
-               } 
+               }
                else
                {
-                  std::string addr = (boost::format("%1%%2%") % config->get<std::string>("identifier.content.idx10form.content.groupCode") % config->get<int>("identifier.content.idx10form.content.unitCode")).str();
+                  std::string addr = (boost::format("%1%%2%") % config->get<std::string>("identifier.content.idx10form.content.groupCode") % config->
+                     get<int>("identifier.content.idx10form.content.unitCode")).str();
                   return CAsciiCommand::generateCommandByAddress(m_protocol, cmd, addr);
                }
             }
@@ -50,6 +54,5 @@ namespace outgoing {
             }
          }
       }
-      
-} //namespace incoming
+   } //namespace incoming
 } //namespace frames
