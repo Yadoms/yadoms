c499498977b1a9d01f2dab9baf940346a4e1170d
Sébastien Gallou <sebastien.gallou@gmail.com>
2025-08-27 19:02:27 +0200

    Fix startup

--- sources/server/logging/LogConfiguration.cpp
+++ sources/server/logging/LogConfiguration.cpp
@@ -4,15 +4,16 @@
 
 namespace logging
 {
-   //declare static PImpl configurator
-   boost::shared_ptr<CLogConfigurationImpl> CLogConfiguration::m_configurator(boost::make_shared<CLogConfigurationImpl>());
 
-   void CLogConfiguration::configure(const std::string& logLevel,
-                                     const boost::filesystem::path& logPath)
-   {
-      m_configurator->configure(logLevel,
-                                logPath);
-   }
+	void CLogConfiguration::configure(const std::string& logLevel,
+		                              const boost::filesystem::path& logPath)
+	{
+		if (!m_configurator)
+			m_configurator = boost::make_shared<CLogConfigurationImpl>();
+
+		m_configurator->configure(logLevel,
+			                      logPath);
+	}
 } // namespace logging
 
 
