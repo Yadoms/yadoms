c036b08b1e5901ef3c45d644423a0e944cf7dfda
Sébastien Gallou <sebastien.gallou@gmail.com>
2026-01-19 19:37:55 +0100

    WIP

--- cmake_windows.cmd
+++ cmake_windows.cmd
@@ -9,82 +9,28 @@ setlocal & pushd .
 @echo Yadoms build for Windows
 @echo ========================
 @echo.
-@echo To enable Windows XP compatiblity, please enter the platform toolset
-@echo.
-@echo v120_xp : platform toolset "Visual Studio 2013 - WindowsXP"
-@echo v140_xp : platform toolset "Visual Studio 2015 - WindowsXP"
-@echo v141_xp : platform toolset "Visual Studio 2017 - WindowsXP"
 @echo v142 : platform toolset "Visual Studio 2019"
 @echo v143 : platform toolset "Visual Studio 2022"
-@echo Leave empty to disable Windows XP compatibility
+@echo Leave empty to use default toolset
 @echo.
 @echo.
 
-set xp_compatibility=%1%
+cmake --version
 
-call:getCMakeExecutable cmake_executable
-"%cmake_executable%" --version
-
-::Move to destination folder
-cd /D %~dp0/projects
-
-if "%xp_compatibility%" == "" (
-   @echo Using default generator WITHOUT WindowsXP support
-	"%cmake_executable%" %~dp0/sources -A Win32
+if "%1%" == "" (
+   @echo Generating using default generator
+	cmake -S sources -B projects -DYADOMS_BINARY_DIR=bin -A Win32
    goto:eof
 )
-if "%xp_compatibility%" == "v120_xp" (
-   @echo Generating using "Visual Studio 2013" with WindowsXP support
-	"%cmake_executable%" %~dp0/sources -T %xp_compatibility% -A Win32
-   goto:eof
-)
-if "%xp_compatibility%" == "v140_xp" (
-   @echo Generating using "Visual Studio 2015" with WindowsXP support
-	"%cmake_executable%" %~dp0/sources -T %xp_compatibility% -A Win32
-   goto:eof
-)
-if "%xp_compatibility%" == "v141_xp" (
-   @echo Generating using "Visual Studio 2017" with WindowsXP support
-	"%cmake_executable%" %~dp0/sources -T %xp_compatibility% -A Win32
-   goto:eof
-)
-if "%xp_compatibility%" == "v142" (
+if "%1%" == "v142" (
    @echo Generating using "Visual Studio 2019"
-	"%cmake_executable%" %~dp0/sources -A Win32
+	cmake -S sources -B projects -G "Visual Studio 17 2019" -DYADOMS_BINARY_DIR=bin -A Win32
    goto:eof
 )
-if "%xp_compatibility%" == "v143" (
+if "%1%" == "v143" (
    @echo Generating using "Visual Studio 2022"
-	"%cmake_executable%" %~dp0/sources -A Win32
+	cmake -S sources -B projects -G "Visual Studio 17 2022" -DYADOMS_BINARY_DIR=bin -A Win32
    goto:eof
 )
 echo Error: invalid toolset selected
-goto:eof
-
-:: Function to find cmake executable
-:getCMakeExecutable
-@setlocal enableextensions enabledelayedexpansion
-set file=sources/CMakeListsUserConfig.txt
-set key=ALTERNATIVE_CMAKE_ROOT
-set value=
-for /f "usebackq delims=" %%a in ("!file!") do (
-   set ln=%%a
-   if not "x!ln:~0,1!"=="x#" (
-      for /f tokens^=2^ delims^=^(^)^  %%b in ("!ln!") do (
-         set current_key=%%b
-         if "x!key!"=="x!current_key!" (
-            for /f tokens^=2^ delims^=^" %%c in ("!ln!") do (
-               set value=%%c
-            )
-         )
-      )
-   )
-)
-if [!value!] == [] (
-   echo Use cmake from PATH
-   endlocal&set "%~1=cmake"
-) else (
-   echo Use cmake from "%value%"
-   endlocal&set "%~1=%value%/bin/cmake"
-)
 goto:eof
